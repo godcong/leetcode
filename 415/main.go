b6e0cd03977f3c3401749c0e723f34e42433c871
godcong <jumbycc@163.com>
2020-08-03 15:53:13 +0800

    add 415

--- 415/main.go
+++ 415/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 //var numList = map[rune]uint8{
@@ -17,8 +16,9 @@ import (
 //	'8': 8,
 //	'9': 9,
 //}
-
-var numList = "01234567890123456789"
+func getNum(b byte) int {
+	return int(b - 48)
+}
 
 func getSumByte(num1 string, num2 string) ([]byte, []byte) {
 	if len(num1) > len(num2) {
@@ -31,9 +31,9 @@ func sumString(num []byte, index int, add int) (ret []byte) {
 	if index < 0 {
 		return append([]byte{'1'}, num...)
 	}
-	indexByte := strings.IndexByte(numList, num[index])
-	num[index] = numList[indexByte+add]
-	if (indexByte + add) > 9 {
+	add = getNum(num[index]) + add
+	num[index] = byte(add%10 + 48)
+	if (add) > 9 {
 		num = sumString(num, index-1, 1)
 	}
 	return num
@@ -43,19 +43,19 @@ func addStrings(num1 string, num2 string) string {
 	n1, n2 := getSumByte(num1, num2)
 	loop := len(n1) - 1
 	for i := 1; loop >= 0; loop-- {
-		indexByte := strings.IndexByte(numList, n1[loop])
-		n2 = sumString(n2, len(n2)-i, indexByte)
+		n2 = sumString(n2, len(n2)-i, getNum(n1[loop]))
 		i++
 	}
 	return string(n2)
 }
 
 func main() {
-	fmt.Println("test string 1+1", addStrings("1", "1"))
-	fmt.Println("test string 4+4", addStrings("4", "4"))
-	fmt.Println("test string 9+1", addStrings("9", "1"))
-	fmt.Println("test string 99+1", addStrings("99", "1"))
-	fmt.Println("test string 999+999", addStrings("999", "999"))
-	fmt.Println("test string 123+456", addStrings("123", "456"))
-	fmt.Println("test string 9133+0", addStrings("9133", "0"))
+	fmt.Println("test string 0+0=", addStrings("0", "0"))
+	fmt.Println("test string 1+1=", addStrings("1", "1"))
+	fmt.Println("test string 4+4=", addStrings("4", "4"))
+	fmt.Println("test string 9+1=", addStrings("9", "1"))
+	fmt.Println("test string 99+1=", addStrings("99", "1"))
+	fmt.Println("test string 999+999=", addStrings("999", "999"))
+	fmt.Println("test string 123+456=", addStrings("123", "456"))
+	fmt.Println("test string 9133+0=", addStrings("9133", "0"))
 }
