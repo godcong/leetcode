747bf0004a8808adeaca8c69e5fc17b6dd3832e8
godcong <jumbycc@163.com>
2020-08-03 15:34:39 +0800

    add 415

--- 415/main.go
+++ 415/main.go
@@ -20,11 +20,11 @@ import (
 
 var numList = "01234567890123456789"
 
-func getMin(num1 string, num2 string) (string, string) {
+func getSumByte(num1 string, num2 string) ([]byte, []byte) {
 	if len(num1) > len(num2) {
-		return num2, num1
+		return []byte(num2), []byte(num1)
 	}
-	return num1, num2
+	return []byte(num1), []byte(num2)
 }
 
 func sumString(num []byte, index int, add int) (ret []byte) {
@@ -40,13 +40,12 @@ func sumString(num []byte, index int, add int) (ret []byte) {
 }
 
 func addStrings(num1 string, num2 string) string {
-	num1, num2 = getMin(num1, num2)
-	loop := len(num1) - 1
-	n2 := []byte(num2)
-	for i := len(num2) - 1; loop >= 0; loop-- {
-		indexByte := strings.IndexByte(numList, num1[loop])
-		n2 = sumString(n2, i, indexByte)
-		i--
+	n1, n2 := getSumByte(num1, num2)
+	loop := len(n1) - 1
+	for i := 1; loop >= 0; loop-- {
+		indexByte := strings.IndexByte(numList, n1[loop])
+		n2 = sumString(n2, len(n2)-i, indexByte)
+		i++
 	}
 	return string(n2)
 }
@@ -56,6 +55,7 @@ func main() {
 	fmt.Println("test string 4+4", addStrings("4", "4"))
 	fmt.Println("test string 9+1", addStrings("9", "1"))
 	fmt.Println("test string 99+1", addStrings("99", "1"))
-	fmt.Println("test string 99+1", addStrings("999", "999"))
-	fmt.Println("test string 99+1", addStrings("123", "456"))
+	fmt.Println("test string 999+999", addStrings("999", "999"))
+	fmt.Println("test string 123+456", addStrings("123", "456"))
+	fmt.Println("test string 9133+0", addStrings("9133", "0"))
 }
