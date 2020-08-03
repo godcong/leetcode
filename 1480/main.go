d8baa47a8ee61c62bffea662a88a4a0e92d12535
godcong <jumbycc@163.com>
2020-08-03 16:30:08 +0800

    add 1480

---
+++ 1480/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func runningSum(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] = nums[i] + nums[i-1])
+	}
+	return nums
+}
+
+func main() {
+	fmt.Println("test sum", runningSum([]int{1, 2, 3, 4}))
+	fmt.Println("test sum", runningSum([]int{1, 1, 1, 1, 1}))
+	fmt.Println("test sum", runningSum([]int{3, 1, 2, 10, 1}))
+}
