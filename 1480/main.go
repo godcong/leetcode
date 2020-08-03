6ece6542d327906f2e9c27e16b64add20cb934ce
godcong <jumbycc@163.com>
2020-08-03 16:33:48 +0800

    add 1480

--- 1480/main.go
+++ 1480/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func runningSum(nums []int) []int {
-	for i := 1; i < len(nums); i++ {
+	size := len(nums)
+	for i := 1; i < size; i++ {
 		nums[i] = nums[i] + nums[i-1]
 	}
 	return nums
