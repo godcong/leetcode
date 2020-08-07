8edf412148fbb42c809eb2a7ed65c69e04d892b0
godcong <jumbycc@163.com>
2020-08-07 19:12:44 +0800

    add 100

--- 100/main.go
+++ 100/main.go
@@ -17,10 +17,10 @@ type TreeNode struct {
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if (p == nil) && (q == nil) {
-		return true
-	}
 	if (p == nil) || (q == nil) {
+		if (p == nil) && (q == nil) {
+			return true
+		}
 		return false
 	}
 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) && p.Val == q.Val
