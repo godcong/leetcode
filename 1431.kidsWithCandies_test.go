7d7238ab9e26d696c90c72e219b6ac8df8be629a
godcong <jumbycc@163.com>
2020-08-08 11:56:12 +0800

    cleanup code

---
+++ 1431.kidsWithCandies_test.go
@@ -0,0 +1,48 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_kidsWithCandies(t *testing.T) {
+	type args struct {
+		candies      []int
+		extraCandies int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []bool
+	}{
+		{
+			name: "",
+			args: args{
+				candies:      []int{2, 3, 5, 1, 3},
+				extraCandies: 3,
+			},
+			want: []bool{true, true, true, false, true},
+		},
+		{
+			name: "",
+			args: args{
+				[]int{4, 2, 1, 1, 2}, 1,
+			},
+			want: []bool{true, false, false, false, false},
+		},
+		{
+			name: "",
+			args: args{
+				[]int{12, 1, 12}, 10,
+			},
+			want: []bool{true, false, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kidsWithCandies(tt.args.candies, tt.args.extraCandies); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("kidsWithCandies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
