f25083dd066f8bac7a6f684b800108d40d9f9167
godcong <jumbycc@163.com>
2020-08-08 15:14:08 +0800

    add 191

--- 191.hammingWeight.go
+++ 191.hammingWeight.go
@@ -35,10 +35,8 @@ package leetcode
 func hammingWeight(num uint32) int {
 	count := 0
 	for num > 0 {
-		if num&1 == 1 {
-			count++
-		}
-		num >>= 1
+		count += 1
+		num &= num - 1
 	}
 	return count
 }
