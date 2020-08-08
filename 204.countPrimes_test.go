5696b8f4a6d56dbcc199c5d2f9c31a7fb603ab1e
godcong <jumbycc@163.com>
2020-08-09 00:49:58 +0800

    add 204

---
+++ 204.countPrimes_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "testing"
+
+func Test_countPrimes(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 10,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPrimes(tt.args.n); got != tt.want {
+				t.Errorf("countPrimes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
