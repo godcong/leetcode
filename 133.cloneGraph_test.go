985e696ace61d3141dd90bac7cebe0f22c9a1e0a
godcong <jumbycc@163.com>
2020-08-12 00:55:48 +0800

    add 133

---
+++ 133.cloneGraph_test.go
@@ -0,0 +1,68 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_cloneGraph(t *testing.T) {
+	type args struct {
+		node *Node
+	}
+
+	node1 := &Node{
+		Val:       1,
+		Neighbors: nil,
+	}
+	node2 := &Node{
+		Val:       2,
+		Neighbors: nil,
+	}
+	node3 := &Node{
+		Val:       3,
+		Neighbors: nil,
+	}
+	node4 := &Node{
+		Val:       4,
+		Neighbors: nil,
+	}
+	node1.Neighbors = append(node1.Neighbors, node2, node4)
+	node2.Neighbors = append(node2.Neighbors, node1, node3)
+	node3.Neighbors = append(node3.Neighbors, node2, node4)
+	node4.Neighbors = append(node4.Neighbors, node1, node3)
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		{
+			name: "",
+			args: args{
+				node: node1,
+			},
+			want: node1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cloneGraph(tt.args.node); !deepEqualCloneGraph(t, got, tt.want, 10) {
+				t.Errorf("cloneGraph() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func deepEqualCloneGraph(t *testing.T, got, want *Node, count int) bool {
+	if count <= 0 {
+		return true
+	}
+	if got.Val != want.Val {
+		t.Errorf("cloneGraph() = %v, want %v", got.Val, want.Val)
+		return false
+	}
+	for i := range got.Neighbors {
+		if !deepEqualCloneGraph(t, got.Neighbors[i], want.Neighbors[i], count-1) {
+			return false
+		}
+	}
+	return true
+}
--- def.go
+++ def.go
@@ -1,7 +1,14 @@
 package leetcode
 
+//Definition for a TreeNode.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+//Definition for a Node.
+type Node struct {
+	Val       int
+	Neighbors []*Node
 }
