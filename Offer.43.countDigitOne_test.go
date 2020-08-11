284f5e261883ff22a3be308b4aaa5f470b203e0b
godcong <jumbycc@163.com>
2020-08-12 00:00:23 +0800

    add offer 43

---
+++ Offer.43.countDigitOne_test.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import "testing"
+
+func Test_countDigitOne(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 12,
+			},
+			want: 5,
+		},
+		{
+			name: "",
+			args: args{
+				n: 112,
+			},
+			want: 38,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countDigitOne(tt.args.n); got != tt.want {
+				t.Errorf("countDigitOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
