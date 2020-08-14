1fb93cbbd53e4a051c8bf81220c3893ad3e9970e
godcong <jumbycc@163.com>
2020-08-14 16:49:48 +0800

    add 13

--- 13.romanToInt.go
+++ 13.romanToInt.go
@@ -57,6 +57,26 @@ func romanToInt(s string) int {
 	return ret
 }
 
+//func romanToIntNumTable(b byte) int {
+//	switch b {
+//	case 'I':
+//		return 1
+//	case 'V':
+//		return 5
+//	case 'X':
+//		return 10
+//	case 'L':
+//		return 50
+//	case 'C':
+//		return 100
+//	case 'D':
+//		return 500
+//	case 'M':
+//		return 1000
+//	}
+//	return 0
+//}
+
 var romanToIntNumTable = []int{
 	'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
 }
