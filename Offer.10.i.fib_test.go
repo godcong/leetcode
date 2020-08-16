1270b02ba4ce68b89871e144abb295b233032f78
godcong <jumbycc@163.com>
2020-08-16 14:18:39 +0800

    add Offer 10.i

---
+++ Offer.10.i.fib_test.go
@@ -0,0 +1,62 @@
+package leetcode
+
+import "testing"
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fib1(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 5,
+			},
+			want: 5,
+		},
+		{
+			name: "",
+			args: args{
+				n: 2,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
