7d28f9f546a1c70ef3cbd71a9d9405b55dec80f1
godcong <jumbycc@163.com>
2020-08-19 14:04:01 +0800

    add 0005

---
+++ 0005.longestPalindrome_test.go
@@ -0,0 +1,50 @@
+package leetcode
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				s: "",
+			},
+			want: "",
+		},
+		{
+			name: "",
+			args: args{
+				s: "a",
+			},
+			want: "a",
+		},
+		{
+			name: "",
+			args: args{
+				s: "babad",
+			},
+			want: "bab",
+		},
+		{
+			name: "",
+			args: args{
+				s: "cbbd",
+			},
+			want: "bb",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
