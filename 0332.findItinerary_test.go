086981a58508668919a21d0ee09cda877d5bbd76
godcong <jumbycc@163.com>
2020-08-27 11:00:10 +0800

    add 0332

---
+++ 0332.findItinerary_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findItinerary(t *testing.T) {
+	type args struct {
+		tickets [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "",
+			args: args{
+				tickets: [][]string{
+					{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"},
+				},
+			},
+			want: []string{
+				"JFK", "MUC", "LHR", "SFO", "SJC",
+			},
+		},
+		{
+			name: "",
+			args: args{
+				tickets: [][]string{
+					{"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"},
+				},
+			},
+			want: []string{
+				"JFK", "ATL", "JFK", "SFO", "ATL", "SFO",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findItinerary(tt.args.tickets); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findItinerary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
