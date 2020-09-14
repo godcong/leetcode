da18bbc0a74aa5069724bfa804c6236249553b24
godcong <jumbycc@163.com>
2020-09-14 11:20:28 +0800

    update

--- 0094.inorderTraversal.go
+++ 0094.inorderTraversal.go
@@ -25,5 +25,17 @@ package leetcode
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
-	return nil
+	var res []int
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return res
 }
