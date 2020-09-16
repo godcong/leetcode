804dd5bdb12d1c55daa247b4b4dfdc46ff52f8da
godcong <jumbycc@163.com>
2020-09-16 10:21:17 +0800

    update

---
+++ code_test.go
@@ -0,0 +1,56 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_invertTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 1},
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{
+						Val:   7,
+						Left:  &TreeNode{Val: 6},
+						Right: &TreeNode{Val: 9},
+					},
+				},
+			},
+			want: &TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val:   7,
+					Left:  &TreeNode{Val: 9},
+					Right: &TreeNode{Val: 6},
+				},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 3},
+					Right: &TreeNode{Val: 1},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := invertTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("invertTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
