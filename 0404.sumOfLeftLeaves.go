fcfd5b0fa646fb7facf6d3015d949d9f41c6f0f3
godcong <jumbycc@163.com>
2020-09-19 14:42:55 +0800

    update

--- 0404.sumOfLeftLeaves.go
+++ 0404.sumOfLeftLeaves.go
@@ -15,13 +15,11 @@ package leetcode
 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
 */
 func sumOfLeftLeaves(root *TreeNode) int {
-	var ret int
 	if root == nil {
 		return 0
 	}
 	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
-		ret += root.Left.Val
+		return root.Left.Val + sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
 	}
-
-	return ret + sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
 }
