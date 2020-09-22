4b0afe865f2a89132548d07f6263b76cfc85edf1
godcong <jumbycc@163.com>
2020-09-22 16:24:32 +0800

    update

--- common.go
+++ common.go
@@ -1,11 +1,5 @@
 package leetcode
 
-import (
-	"fmt"
-	"strconv"
-	"testing"
-)
-
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -42,55 +36,4 @@ type Node struct {
 type ListNode struct {
 	Val  int
 	Next *ListNode
-}
-
-func treeNodeDeepPrint(root *TreeNode) {
-	fmt.Println(getTreeNodeIntArray(root))
-}
-
-func getTreeNodeIntArray(root *TreeNode) []string {
-	var ret []string
-	ret = append(ret, strconv.Itoa(root.Val))
-	if root.Left != nil {
-		ret = append(ret, getTreeNodeIntArray(root.Left)...)
-	} else {
-		ret = append(ret, "null")
-	}
-	if root.Right != nil {
-		ret = append(ret, getTreeNodeIntArray(root.Right)...)
-	} else {
-		//ret = append(ret, "null")
-	}
-	return ret
-}
-
-func treeNodeDeepEqual(t *testing.T, root *TreeNode, want *TreeNode) bool {
-	if root == nil || want == nil {
-		return root == want
-	}
-
-	if !treeNodeDeepEqual(t, root.Left, want.Left) {
-		t.Errorf("recoverTree(Left) = %v, want %v", root.Left, want.Left)
-		return false
-	}
-	if !treeNodeDeepEqual(t, root.Right, want.Right) {
-		t.Errorf("recoverTree(Right) = %v, want %v", root.Right, want.Right)
-		return false
-	}
-	if root.Val != want.Val {
-		t.Errorf("recoverTree() = %v, want %v", root.Val, want.Val)
-	}
-	return true
-}
-
-func printTreeNodeToArray(nodes []*TreeNode) {
-	for i := range nodes {
-		if nodes[i] == nil {
-			fmt.Print("null")
-		} else {
-			fmt.Print(nodes[i].Val)
-		}
-		fmt.Print(" ")
-	}
-	fmt.Println()
 }
--- common_test.go
+++ common_test.go
@@ -1,6 +1,7 @@
 package leetcode
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -280,4 +281,55 @@ func throughErrorPanic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func treeNodeDeepPrint(root *TreeNode) {
+	fmt.Println(getTreeNodeIntArray(root))
+}
+
+func getTreeNodeIntArray(root *TreeNode) []string {
+	var ret []string
+	ret = append(ret, strconv.Itoa(root.Val))
+	if root.Left != nil {
+		ret = append(ret, getTreeNodeIntArray(root.Left)...)
+	} else {
+		ret = append(ret, "null")
+	}
+	if root.Right != nil {
+		ret = append(ret, getTreeNodeIntArray(root.Right)...)
+	} else {
+		//ret = append(ret, "null")
+	}
+	return ret
+}
+
+func treeNodeDeepEqual(t *testing.T, root *TreeNode, want *TreeNode) bool {
+	if root == nil || want == nil {
+		return root == want
+	}
+
+	if !treeNodeDeepEqual(t, root.Left, want.Left) {
+		t.Errorf("recoverTree(Left) = %v, want %v", root.Left, want.Left)
+		return false
+	}
+	if !treeNodeDeepEqual(t, root.Right, want.Right) {
+		t.Errorf("recoverTree(Right) = %v, want %v", root.Right, want.Right)
+		return false
+	}
+	if root.Val != want.Val {
+		t.Errorf("recoverTree() = %v, want %v", root.Val, want.Val)
+	}
+	return true
+}
+
+func printTreeNodeToArray(nodes []*TreeNode) {
+	for i := range nodes {
+		if nodes[i] == nil {
+			fmt.Print("null")
+		} else {
+			fmt.Print(nodes[i].Val)
+		}
+		fmt.Print(" ")
+	}
+	fmt.Println()
 }
