13abcc7b1de58a6627d8a7d479914a9600a968c7
godcong <jumbycc@163.com>
2020-09-25 10:58:09 +0800

    update

--- 0106.go
+++ 0106.go
@@ -19,3 +19,14 @@ package leetcode
     /  \
    15   7
 */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	return nil
+}
