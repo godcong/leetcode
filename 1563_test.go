e5e5d1ce88498c95981ed59f0952df46f71eea26
godcong <jumbycc@163.com>
2020-10-03 22:35:03 +0800

    update

--- 1563_test.go
+++ 1563_test.go
@@ -14,7 +14,29 @@ func Test_stoneGameV(t *testing.T) {
 		// TODO: Add test cases.
 		{
 			name: "",
-			args: args{},
+			args: args{
+				stoneValue: []int{
+					6, 2, 3, 4, 5, 5,
+				},
+			},
+			want: 18,
+		},
+		{
+			name: "",
+			args: args{
+				stoneValue: []int{
+					7, 7, 7, 7, 7, 7, 7,
+				},
+			},
+			want: 28,
+		},
+		{
+			name: "",
+			args: args{
+				stoneValue: []int{
+					4,
+				},
+			},
 			want: 0,
 		},
 	}
