0867fb1ca0f39db59926b5f5c802f3bf6476603b
godcong <jumbycc@163.com>
2020-10-07 22:15:45 +0800

    update

---
+++ 0075_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortColors(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: []int{
+					2, 0, 2, 1, 1, 0,
+				},
+			},
+			want: []int{
+				0, 0, 1, 1, 2, 2,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sortColors(tt.args.nums)
+			if reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("climbStairs() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
