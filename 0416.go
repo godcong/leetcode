56906a4dc44b6bbb2969c6994c0d491cbebb2cce
godcong <jumbycc@163.com>
2020-10-11 22:51:16 +0800

    update

--- 0416.go
+++ 0416.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 /*
 416. 分割等和子集
 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
@@ -26,5 +28,44 @@ package leetcode
 解释: 数组不能分割成两个元素和相等的子集.
 */
 func canPartition(nums []int) bool {
-	return false
+	flag := false
+	var canPartitionDFS func(sum int, nums []int, current, start int)
+	canPartitionDFS = func(sum int, nums []int, current, start int) {
+		if current > sum {
+			return
+		}
+		if current == sum {
+			flag = true
+			return
+		}
+		if !flag {
+			for i := start; i < len(nums); i++ {
+				if current+nums[i] <= sum {
+					current += nums[i]
+					canPartitionDFS(sum, nums, current, i+1)
+					current -= nums[i]
+				}
+			}
+		}
+	}
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	if sum%2 != 0 {
+		return flag
+	}
+
+	sort.Ints(nums)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > sum/2 {
+			return false
+		}
+		if nums[i] < sum/2 {
+			break
+		}
+	}
+	canPartitionDFS(sum/2, nums, 0, 0)
+	return flag
 }
