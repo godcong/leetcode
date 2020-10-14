14dd2a41f3393d1a6fb24efbaaef26e1e1c6088a
godcong <jumbycc@163.com>
2020-10-14 10:41:13 +0800

    update

--- 1002.go
+++ 1002.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math"
+
 /*
 1002. 查找常用字符
 给定仅有小写字母组成的字符串数组 A，返回列表中的每个字符串中都显示的全部字符（包括重复字符）组成的列表。例如，如果一个字符在每个字符串中出现 3 次，但不是 4 次，则需要在最终答案中包含该字符 3 次。
@@ -25,5 +27,26 @@ package leetcode
 A[i][j] 是小写字母
 */
 func commonChars(A []string) []string {
-
+	var ret []string
+	minFreq := [26]int{}
+	for i := range minFreq {
+		minFreq[i] = math.MaxInt64
+	}
+	for _, word := range A {
+		freq := [26]int{}
+		for _, b := range word {
+			freq[b-'a']++
+		}
+		for i, f := range freq[:] {
+			if f < minFreq[i] {
+				minFreq[i] = f
+			}
+		}
+	}
+	for i := byte(0); i < 26; i++ {
+		for j := 0; j < minFreq[i]; j++ {
+			ret = append(ret, string('a'+i))
+		}
+	}
+	return ret
 }
