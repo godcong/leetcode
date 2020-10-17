7205976648d765dfac1694daff2e79f0ca5284f5
godcong <jumbycc@163.com>
2020-10-17 22:51:29 +0800

    update

--- 0052.totalNQueens_test.go
+++ 0052.totalNQueens_test.go
@@ -12,6 +12,13 @@ func Test_totalNQueens(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				n: 4,
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
