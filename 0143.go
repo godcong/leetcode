89ff8ebc3ed80b79f6415136326a91208b413073
godcong <jumbycc@163.com>
2020-10-20 21:18:09 +0800

    update

--- 0143.go
+++ 0143.go
@@ -22,5 +22,24 @@ package leetcode
  * }
  */
 func reorderList(head *ListNode) {
+	var nodes []*ListNode
+	for node := head; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
 
+	if nodes == nil {
+		return
+	}
+
+	i, j := 0, len(nodes)-1
+	for i < j {
+		nodes[i].Next = nodes[j]
+		i++
+		if i == j {
+			break
+		}
+		nodes[j].Next = nodes[i]
+		j--
+	}
+	nodes[i].Next = nil
 }
