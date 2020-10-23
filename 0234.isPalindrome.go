8ec0daadbec4d13af5e5b51e195fe24123c26001
godcong <jumbycc@163.com>
2020-10-23 11:08:47 +0800

    update

--- 0234.isPalindrome.go
+++ 0234.isPalindrome.go
@@ -23,5 +23,21 @@ package leetcode
  * }
  */
 func isPalindrome(head *ListNode) bool {
+	var vals []int
+	for ; head != nil; head = head.Next {
+		vals = append(vals, head.Val)
+	}
+
+	if vals == nil {
+		return true
+	}
+
+	n := len(vals)
+	for i, v := range vals[:n/2] {
+		if v != vals[n-1-i] {
+			return false
+		}
+	}
+
 	return true
 }
