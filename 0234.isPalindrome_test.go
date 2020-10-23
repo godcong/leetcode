c6aa189ed3014b31c0643004a449c8e7f9aa8458
godcong <jumbycc@163.com>
2020-10-23 11:35:16 +0800

    update

---
+++ 0234.isPalindrome_test.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				head: strToListNode("1->2", -1),
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				head: strToListNode("1->2->2->1", -1),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.head); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
