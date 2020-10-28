9c3c5d15f0c8c0bd344b8130ab6a318824947ad8
godcong <jumbycc@163.com>
2020-10-28 23:23:04 +0800

    update

--- 1207_test.go
+++ 1207_test.go
@@ -21,6 +21,24 @@ func Test_uniqueOccurrences(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				arr: []int{
+					1, 2,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				arr: []int{
+					-3, 0, 1, -3, 1, 1, 1, -3, 10, 0,
+				},
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
