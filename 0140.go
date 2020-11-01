3c4282a31db4eea18870fc623409d0d88e2c8a8f
godcong <jumbycc@163.com>
2020-11-01 22:55:22 +0800

    update

--- 0140.go
+++ 0140.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "strings"
+
 /*
 140. 单词拆分 II
 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
@@ -39,5 +41,37 @@ wordDict = ["cats", "dog", "sand", "and", "cat"]
 []
 */
 func wordBreak(s string, wordDict []string) []string {
-	return nil
+	var sentences []string
+	wordSet := map[string]struct{}{}
+	for _, w := range wordDict {
+		wordSet[w] = struct{}{}
+	}
+
+	n := len(s)
+	dp := make([][][]string, n)
+	var backtrack func(index int) [][]string
+	backtrack = func(index int) [][]string {
+		if dp[index] != nil {
+			return dp[index]
+		}
+		var wordsList [][]string
+		for i := index + 1; i < n; i++ {
+			word := s[index:i]
+			if _, has := wordSet[word]; has {
+				for _, nextWords := range backtrack(i) {
+					wordsList = append(wordsList, append([]string{word}, nextWords...))
+				}
+			}
+		}
+		word := s[index:]
+		if _, has := wordSet[word]; has {
+			wordsList = append(wordsList, []string{word})
+		}
+		dp[index] = wordsList
+		return wordsList
+	}
+	for _, words := range backtrack(0) {
+		sentences = append(sentences, strings.Join(words, " "))
+	}
+	return sentences
 }
