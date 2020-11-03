973834185becf2457a81eaeb7f3c2932b04b10b7
godcong <jumbycc@163.com>
2020-11-03 11:56:04 +0800

    update

--- 0941_test.go
+++ 0941_test.go
@@ -12,6 +12,27 @@ func Test_validMountainArray(t *testing.T) {
 		want bool
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[2,1]"),
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[3,5,5]"),
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[0,3,2,1]"),
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
