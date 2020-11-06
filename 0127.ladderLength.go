34365dec403f0eb3cb89bf03d325919550df0c88
godcong <jumbycc@163.com>
2020-11-06 11:08:48 +0800

    update

--- 0127.ladderLength.go
+++ 0127.ladderLength.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "math"
-
 /*
 127. 单词接龙
 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
@@ -38,58 +36,59 @@ wordList = ["hot","dot","dog","lot","log"]
 解释: endWord "cog" 不在字典中，所以无法进行转换。
 */
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	wordId := map[string]int{}
-	var graph [][]int
-	addWord := func(word string) int {
-		id, has := wordId[word]
-		if !has {
-			id = len(wordId)
-			wordId[word] = id
-			graph = append(graph, []int{})
-		}
-		return id
-	}
-	addEdge := func(word string) int {
-		id1 := addWord(word)
-		s := []byte(word)
-		for i, b := range s {
-			s[i] = '*'
-			id2 := addWord(string(s))
-			graph[id1] = append(graph[id1], id2)
-			graph[id2] = append(graph[id2], id1)
-			s[i] = b
-		}
-		return id1
-	}
-
-	for _, word := range wordList {
-		addEdge(word)
-	}
-	beginId := addEdge(beginWord)
-	endId, has := wordId[endWord]
-	if !has {
+	if ladderLengthIndexOf(endWord, wordList) == -1 {
 		return 0
 	}
+	step := 0
+	startQueue := []string{beginWord}
+	endQueue := []string{endWord}
+	used := make([]bool, len(wordList))
 
-	const inf int = math.MaxInt64
-	dist := make([]int, len(wordId))
-	for i := range dist {
-		dist[i] = inf
-	}
-	dist[beginId] = 0
-	queue := []int{beginId}
-	for len(queue) > 0 {
-		v := queue[0]
-		queue = queue[1:]
-		if v == endId {
-			return dist[endId]/2 + 1
-		}
-		for _, w := range graph[v] {
-			if dist[w] == inf {
-				dist[w] = dist[v] + 1
-				queue = append(queue, w)
+	for len(startQueue) > 0 {
+		step++
+		l := len(startQueue)
+		for i := 0; i < l; i++ {
+			for k := 0; k < len(endQueue); k++ {
+				if ladderLengthHasOneDiff(startQueue[i], endQueue[k]) {
+					return step + 1
+				}
 			}
+			for j, w := range wordList {
+				if !used[j] && ladderLengthHasOneDiff(startQueue[i], w) {
+					startQueue = append(startQueue, w)
+					used[j] = true
+				}
+			}
+		}
+		startQueue = startQueue[l:]
+		if len(startQueue) > len(endQueue) {
+			startQueue, endQueue = endQueue, startQueue
 		}
 	}
 	return 0
+}
+
+func ladderLengthIndexOf(str string, bank []string) int {
+	for i, s := range bank {
+		if str == s {
+			return i
+		}
+	}
+	return -1
+}
+
+func ladderLengthHasOneDiff(x, y string) bool {
+	count := 0
+	for i := 0; i < len(x); i++ {
+		if x[i] != y[i] {
+			count++
+		}
+		if count > 1 {
+			return false
+		}
+	}
+	if count == 1 {
+		return true
+	}
+	return false
 }
