3d608ac5b8c60d8c60036faeb90f2925ab032fe4
godcong <jumbycc@163.com>
2020-11-07 13:07:50 +0800

    update

--- 0327.countRangeSum.go
+++ 0327.countRangeSum.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "sort"
-
 /*
 327. 区间和的个数
 给定一个整数数组 nums，返回区间和在 [lower, upper] 之间的个数，包含 lower 和 upper。
@@ -17,17 +15,44 @@ import "sort"
 解释: 3个区间分别是: [0,0], [2,2], [0,2]，它们表示的和分别为: -2, -1, 2。
 */
 func countRangeSum(nums []int, lower int, upper int) int {
-	sort.Ints(nums)
-	li := 0
-	for ; li < len(nums)-1 && nums[li] < lower; li++ {
+	var mergeCount func([]int) int
+	mergeCount = func(arr []int) int {
+		n := len(arr)
+		if n <= 1 {
+			return 0
+		}
+
+		n1 := append([]int(nil), arr[:n/2]...)
+		n2 := append([]int(nil), arr[n/2:]...)
+		cnt := mergeCount(n1) + mergeCount(n2)
+
+		l, r := 0, 0
+		for _, v := range n1 {
+			for l < len(n2) && n2[l]-v < lower {
+				l++
+			}
+			for r < len(n2) && n2[r]-v <= upper {
+				r++
+			}
+			cnt += r - l
+		}
+
+		p1, p2 := 0, 0
+		for i := range arr {
+			if p1 < len(n1) && (p2 == len(n2) || n1[p1] <= n2[p2]) {
+				arr[i] = n1[p1]
+				p1++
+			} else {
+				arr[i] = n2[p2]
+				p2++
+			}
+		}
+		return cnt
 	}
 
-	uj := len(nums) - 1
-	for ; uj > 0 && nums[uj] > upper; uj-- {
+	prefixSum := make([]int, len(nums)+1)
+	for i, v := range nums {
+		prefixSum[i+1] = prefixSum[i] + v
 	}
-
-	if uj > li {
-		return uj - li
-	}
-	return 0
+	return mergeCount(prefixSum)
 }
