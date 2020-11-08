bffb6840bc181f9e61e780cc0bf4a0bd1b56a79c
godcong <jumbycc@163.com>
2020-11-08 21:59:22 +0800

    update

--- 0122.maxProfit.go
+++ 0122.maxProfit.go
@@ -36,19 +36,12 @@ package leetcode
 0 <= prices[i] <= 10 ^ 4
 */
 func maxProfit(prices []int) int {
-	n := len(prices)
-	dp := make([][2]int, n)
-	dp[0][1] = -prices[0]
-	for i := 1; i < n; i++ {
-		dp[i][0] = maxProfitMax(dp[i-1][0], dp[i-1][1]+prices[i])
-		dp[i][1] = maxProfitMax(dp[i-1][1], dp[i-1][0]-prices[i])
+	maxPro := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxPro += prices[i] - prices[i-1]
+		}
 	}
-	return dp[n-1][0]
-}
 
-func maxProfitMax(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return maxPro
 }
