6e259e5118749a38f88a2e3ff9de2016d61e145c
godcong <jumbycc@163.com>
2020-11-10 11:29:19 +0800

    update

--- 0031.go
+++ 0031.go
@@ -13,3 +13,24 @@ package leetcode
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 */
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	nextPermutationReverse(nums[i+1:])
+}
+
+func nextPermutationReverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
