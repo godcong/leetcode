d5e537b633dc823a19e892bbe548c1dcfedb5f20
godcong <jumbycc@163.com>
2020-11-10 11:53:39 +0800

    update

---
+++ 0031_test.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import "testing"
+
+func Test_nextPermutation(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
