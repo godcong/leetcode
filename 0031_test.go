a11fd990ef4131e6acf40129f722a274930f643b
godcong <jumbycc@163.com>
2020-11-10 11:55:50 +0800

    update

--- 0031_test.go
+++ 0031_test.go
@@ -1,19 +1,34 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_nextPermutation(t *testing.T) {
 	type args struct {
-		nums []int
+		nums  []int
+		after []int
 	}
 	tests := []struct {
 		name string
 		args args
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums:  strToIntArray("1,2,3"),
+				after: strToIntArray("1,3,2"),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			nextPermutation(tt.args.nums)
+			if !reflect.DeepEqual(tt.args.nums, tt.args.after) {
+				t.Errorf("want %v get %v", tt.args.after, tt.args.nums)
+			}
 		})
 	}
 }
