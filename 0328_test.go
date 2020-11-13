7d79ab80522db029da5758d35a1c563d45348ec5
godcong <jumbycc@163.com>
2020-11-13 14:42:58 +0800

    update

--- 0328_test.go
+++ 0328_test.go
@@ -22,6 +22,13 @@ func Test_oddEvenList(t *testing.T) {
 			},
 			want: strToListNode("1->3->5->2->4->NULL", -1),
 		},
+		{
+			name: "",
+			args: args{
+				head: strToListNode("2->1->3->5->6->4->7->NULL", -1),
+			},
+			want: strToListNode("2->3->6->7->1->5->4->NULL", -1),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
