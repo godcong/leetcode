714301ec3caee98f81d51c1058806abeb98d2694
godcong <jumbycc@163.com>
2020-11-15 16:54:23 +0800

    update

---
+++ 0402_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "testing"
+
+func Test_removeKdigits(t *testing.T) {
+	type args struct {
+		num string
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				num: "1432219",
+				k:   3,
+			},
+			want: "1219",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeKdigits(tt.args.num, tt.args.k); got != tt.want {
+				t.Errorf("removeKdigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
