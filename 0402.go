28982342e89952513ad185b175f4e6f6e51f5cbe
godcong <jumbycc@163.com>
2020-11-15 16:53:57 +0800

    update

--- 0402.go
+++ 0402.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "strings"
+
 /*
 402. 移掉K位数字
 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
@@ -25,5 +27,19 @@ num 不会包含任何前导零。
 解释: 从原数字移除所有的数字，剩余为空就是0。
 */
 func removeKdigits(num string, k int) string {
-	return ""
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		ans = "0"
+	}
+	return ans
 }
