465af2410fe439c6d0955a79204c7d518ab8655e
godcong <jumbycc@163.com>
2020-11-17 12:25:19 +0800

    update

---
+++ 1030.allCellsDistOrder_test.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_allCellsDistOrder(t *testing.T) {
+	type args struct {
+		R  int
+		C  int
+		r0 int
+		c0 int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				R:  1,
+				C:  2,
+				r0: 0,
+				c0: 0,
+			},
+			want: strToIntArrArray("[[0,0],[0,1]]"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := allCellsDistOrder(tt.args.R, tt.args.C, tt.args.r0, tt.args.c0); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("allCellsDistOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
