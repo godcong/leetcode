a03c1d838eb7ddca94bf5c6d3efc8e465f310ff3
godcong <jumbycc@163.com>
2020-11-17 12:25:45 +0800

    update

--- 1030.allCellsDistOrder_test.go
+++ 1030.allCellsDistOrder_test.go
@@ -28,6 +28,16 @@ func Test_allCellsDistOrder(t *testing.T) {
 			},
 			want: strToIntArrArray("[[0,0],[0,1]]"),
 		},
+		{
+			name: "",
+			args: args{
+				R:  2,
+				C:  2,
+				r0: 0,
+				c0: 1,
+			},
+			want: strToIntArrArray("[[0,1],[0,0],[1,1],[1,0]]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
