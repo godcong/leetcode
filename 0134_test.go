5afe5bbc11f109880293aa0dc7b43ad8a5c82243
godcong <jumbycc@163.com>
2020-11-18 19:28:41 +0800

    update

--- 0134_test.go
+++ 0134_test.go
@@ -21,6 +21,14 @@ func Test_canCompleteCircuit(t *testing.T) {
 			},
 			want: 3,
 		},
+		{
+			name: "",
+			args: args{
+				gas:  strToIntArray("[2,3,4]"),
+				cost: strToIntArray("[3,4,3]"),
+			},
+			want: -1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
