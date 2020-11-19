01193bf8735a0ead1554805082f68febf937f023
godcong <jumbycc@163.com>
2020-11-19 11:31:04 +0800

    update

--- 0283.moveZeroes_test.go
+++ 0283.moveZeroes_test.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_moveZeroes(t *testing.T) {
 	type args struct {
@@ -9,11 +12,23 @@ func Test_moveZeroes(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[0,1,0,3,12]"),
+			},
+			want: strToIntArray("[1,3,12,0,0]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+			if !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("moveZeroes() = %v, want %v", tt.args.nums, tt.want)
+			}
 		})
 	}
 }
