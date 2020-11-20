6b508d075e8f1a177336c582d41b82d15c6878a7
godcong <jumbycc@163.com>
2020-11-20 15:45:44 +0800

    update

--- 0147.go
+++ 0147.go
@@ -34,5 +34,24 @@ package leetcode
  * }
  */
 func insertionSortList(head *ListNode) *ListNode {
-	return nil
+	if head == nil {
+		return nil
+	}
+	dummyHead := &ListNode{Next: head}
+	lastSorted, curr := head, head.Next
+	for curr != nil {
+		if lastSorted.Val <= curr.Val {
+			lastSorted = lastSorted.Next
+		} else {
+			prev := dummyHead
+			for prev.Next.Val <= curr.Val {
+				prev = prev.Next
+			}
+			lastSorted.Next = curr.Next
+			curr.Next = prev.Next
+			prev.Next = curr
+		}
+		curr = lastSorted.Next
+	}
+	return dummyHead.Next
 }
