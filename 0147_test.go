40505b9ab57a720184dd39f71db033ade21c3e65
godcong <jumbycc@163.com>
2020-11-20 15:46:40 +0800

    update

---
+++ 0147_test.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_insertionSortList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				head: strToListNode("4->2->1->3", -1),
+			},
+			want: strToListNode("1->2->3->4", -1),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := insertionSortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("insertionSortList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
