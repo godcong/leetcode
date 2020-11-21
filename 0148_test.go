258cc64690ebe98f193d5fb326f786bf91ea6ed8
godcong <jumbycc@163.com>
2020-11-21 19:18:45 +0800

    update

--- 0148_test.go
+++ 0148_test.go
@@ -22,6 +22,13 @@ func Test_sortList(t *testing.T) {
 			},
 			want: strToListNode("[1,2,3,4]", -1),
 		},
+		{
+			name: "",
+			args: args{
+				head: strToListNode("[-1,5,3,4,0]", -1),
+			},
+			want: strToListNode("[-1,0,3,4,5]", -1),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
