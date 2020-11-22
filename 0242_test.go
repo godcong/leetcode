2c7b12ce83b995e0bfac027c2f8dd04a76ed1547
godcong <jumbycc@163.com>
2020-11-22 22:56:56 +0800

    update

--- 0242_test.go
+++ 0242_test.go
@@ -21,6 +21,14 @@ func Test_isAnagram(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				s: "rat",
+				t: "car",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
