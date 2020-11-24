3d740080872e54aad246e5c2402a6b21bc6c1197
godcong <jumbycc@163.com>
2020-11-24 11:44:01 +0800

    update

--- 0222.go
+++ 0222.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 /*
 222. 完全二叉树的节点个数
 给出一个完全二叉树，求出该树的节点个数。
@@ -28,5 +30,27 @@ package leetcode
  * }
  */
 func countNodes(root *TreeNode) int {
-	return 0
+	if root == nil {
+		return 0
+	}
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
 }
--- 0222_test.go
+++ 0222_test.go
@@ -12,6 +12,13 @@ func Test_countNodes(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				root: strToTreeNode("[1,2,3,4,5,6]"),
+			},
+			want: 6,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
