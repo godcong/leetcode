9fac5a7235a08cd2685f694a37be17b21470ce0a
godcong <jumbycc@163.com>
2020-11-26 11:33:55 +0800

    update

---
+++ 0164_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "testing"
+
+func Test_maximumGap(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumGap(tt.args.nums); got != tt.want {
+				t.Errorf("maximumGap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
