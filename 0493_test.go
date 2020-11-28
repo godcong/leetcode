5b9d881f6db8cfe1770215cdf16af59406d1d4da
godcong <jumbycc@163.com>
2020-11-28 21:59:26 +0800

    update

---
+++ 0493_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "testing"
+
+func Test_reversePairs(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reversePairs(tt.args.nums); got != tt.want {
+				t.Errorf("reversePairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
