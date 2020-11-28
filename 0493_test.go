7fbb2b40100a1d281232be10f7c9653a2a306b32
godcong <jumbycc@163.com>
2020-11-28 22:02:26 +0800

    update

--- 0493_test.go
+++ 0493_test.go
@@ -12,6 +12,13 @@ func Test_reversePairs(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,3,2,3,1]"),
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
