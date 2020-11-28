5ffb7a20c9c46a326cb5e805180e64df9e2b58e3
godcong <jumbycc@163.com>
2020-11-28 22:01:58 +0800

    update

--- 0493.go
+++ 0493.go
@@ -20,5 +20,38 @@ package leetcode
 输入数组中的所有数字都在32位整数的表示范围内。
 */
 func reversePairs(nums []int) int {
-	return 0
+	var reversePairsMergeSort func(nums []int, l, r int) int
+	reversePairsMergeSort = func(nums []int, l, r int) int {
+		if l >= r {
+			return 0
+		}
+
+		m := (l + r) >> 1
+
+		count := reversePairsMergeSort(nums, l, m) + reversePairsMergeSort(nums, m+1, r)
+		cache := make([]int, r-l+1)
+
+		i, k := l, 0
+
+		for j, idx := m+1, l; j <= r; j++ {
+			for ; i <= m && nums[i] < nums[j]; i++ {
+				cache[k] = nums[i]
+				k++
+			}
+
+			cache[k] = nums[j]
+			k++
+
+			for idx <= m && (nums[idx]+1)>>1 <= nums[j] {
+				idx++
+			}
+
+			count += m - idx + 1
+		}
+
+		copy(nums[l+k:], nums[i:m+1])
+		copy(nums[l:], cache[:k])
+		return count
+	}
+	return reversePairsMergeSort(nums, 0, len(nums)-1)
 }
