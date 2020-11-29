e1d586b48c8b78b791c2a7222dba16ea9953df3c
godcong <jumbycc@163.com>
2020-11-29 15:28:00 +0800

    update

---
+++ 0976_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_largestPerimeter(t *testing.T) {
+	type args struct {
+		A []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[2,1,2]"),
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestPerimeter(tt.args.A); got != tt.want {
+				t.Errorf("largestPerimeter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
