0ab0df14eadad595205666cb0d288816ac4afbf0
godcong <jumbycc@163.com>
2020-11-30 13:40:09 +0800

    update

--- 0767.go
+++ 0767.go
@@ -19,5 +19,38 @@ package leetcode
 S 只包含小写字母并且长度在[1, 500]区间内。
 */
 func reorganizeString(S string) string {
-	return ""
+	n := len(S)
+	if n <= 1 {
+		return S
+	}
+
+	cnt := [26]int{}
+	maxCnt := 0
+	for _, ch := range S {
+		ch -= 'a'
+		cnt[ch]++
+		if cnt[ch] > maxCnt {
+			maxCnt = cnt[ch]
+		}
+	}
+	if maxCnt > (n+1)/2 {
+		return ""
+	}
+
+	ans := make([]byte, n)
+	evenIdx, oddIdx, halfLen := 0, 1, n/2
+	for i, c := range cnt[:] {
+		ch := byte('a' + i)
+		for c > 0 && c <= halfLen && oddIdx < n {
+			ans[oddIdx] = ch
+			c--
+			oddIdx += 2
+		}
+		for c > 0 {
+			ans[evenIdx] = ch
+			c--
+			evenIdx += 2
+		}
+	}
+	return string(ans)
 }
