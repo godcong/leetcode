fd05ae5617daa61249765777a3cf8ee268c82c1b
godcong <jumbycc@163.com>
2020-12-01 12:49:05 +0800

    update

--- 0034_test.go
+++ 0034_test.go
@@ -32,6 +32,14 @@ func Test_searchRange(t *testing.T) {
 			},
 			want: strToIntArray("[-1,-1]"),
 		},
+		{
+			name: "",
+			args: args{
+				nums:   strToIntArray("[]"),
+				target: 0,
+			},
+			want: strToIntArray("[-1,-1]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
