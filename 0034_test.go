377d3f431a25c03c813cdf9979f61df0756704dd
godcong <jumbycc@163.com>
2020-12-01 12:47:08 +0800

    update

---
+++ 0034_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_searchRange(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums:   strToIntArray("[5,7,7,8,8,10]"),
+				target: 8,
+			},
+			want: strToIntArray("[3,4]"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
