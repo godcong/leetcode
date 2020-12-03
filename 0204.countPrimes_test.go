bac09b7ccfc6a235ae5790bde32c1bd6c4938fd3
godcong <jumbycc@163.com>
2020-12-03 22:42:48 +0800

    update

--- 0204.countPrimes_test.go
+++ 0204.countPrimes_test.go
@@ -18,6 +18,13 @@ func Test_countPrimes(t *testing.T) {
 			},
 			want: 4,
 		},
+		{
+			name: "",
+			args: args{
+				n: 0,
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
