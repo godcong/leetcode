03751f1aec615d1b966fb6ee31b524df29b48b92
godcong <jumbycc@163.com>
2020-12-04 11:44:18 +0800

    update

--- 0659.go
+++ 0659.go
@@ -34,5 +34,27 @@ package leetcode
 输出: False
 */
 func isPossible(nums []int) bool {
-	return false
+	left := map[int]int{}
+	for _, v := range nums {
+		left[v]++
+	}
+	endCnt := map[int]int{}
+	for _, v := range nums {
+		if left[v] == 0 {
+			continue
+		}
+		if endCnt[v-1] > 0 {
+			left[v]--
+			endCnt[v-1]--
+			endCnt[v]++
+		} else if left[v+1] > 0 && left[v+2] > 0 {
+			left[v]--
+			left[v+1]--
+			left[v+2]--
+			endCnt[v+2]++
+		} else {
+			return false
+		}
+	}
+	return true
 }
