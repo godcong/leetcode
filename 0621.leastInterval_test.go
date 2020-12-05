d987467c74e0ba8c4779c91b0825d563c5f500b0
godcong <jumbycc@163.com>
2020-12-05 21:35:20 +0800

    update

---
+++ 0621.leastInterval_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "testing"
+
+func Test_leastInterval(t *testing.T) {
+	type args struct {
+		tasks []byte
+		n     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := leastInterval(tt.args.tasks, tt.args.n); got != tt.want {
+				t.Errorf("leastInterval() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
