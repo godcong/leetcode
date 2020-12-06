0fae29f730333bb40f4139a1d9f73aacb9a3e5d8
godcong <jumbycc@163.com>
2020-12-06 21:16:50 +0800

    update

--- 0118.go
+++ 0118.go
@@ -21,5 +21,14 @@ package leetcode
 ]
 */
 func generate(numRows int) [][]int {
-	return nil
+	ans := make([][]int, numRows)
+	for i := range ans {
+		ans[i] = make([]int, i+1)
+		ans[i][0] = 1
+		ans[i][i] = 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j] + ans[i-1][j-1]
+		}
+	}
+	return ans
 }
