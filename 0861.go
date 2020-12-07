56ca8b5d45a5f6601e74180b45f410351ea2abbb
godcong <jumbycc@163.com>
2020-12-07 13:24:05 +0800

    update

--- 0861.go
+++ 0861.go
@@ -28,5 +28,19 @@ package leetcode
 A[i][j] 是 0 或 1
 */
 func matrixScore(A [][]int) int {
-	return 0
+	m, n := len(A), len(A[0])
+	ans := 1 << (n - 1) * m
+	for j := 1; j < n; j++ {
+		ones := 0
+		for _, row := range A {
+			if row[j] == row[0] {
+				ones++
+			}
+		}
+		if ones < m-ones {
+			ones = m - ones
+		}
+		ans += 1 << (n - 1 - j) * ones
+	}
+	return ans
 }
