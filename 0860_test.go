a29f999fc8bb3360cbd9edb57bdf8c4f80e51b03
godcong is not god <jumbycc@163.com>
2020-12-10 16:28:09 +0800

    update

---
+++ 0860_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_lemonadeChange(t *testing.T) {
+	type args struct {
+		bills []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				bills: strToIntArray("[5,5,5,10,20]"),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lemonadeChange(tt.args.bills); got != tt.want {
+				t.Errorf("lemonadeChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
