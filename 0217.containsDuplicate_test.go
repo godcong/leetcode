40ec5992e63299168213f64a378c7914c0202988
godcong is not god <jumbycc@163.com>
2020-12-13 07:53:09 +0800

    update

--- 0217.containsDuplicate_test.go
+++ 0217.containsDuplicate_test.go
@@ -26,6 +26,13 @@ func Test_containsDuplicate(t *testing.T) {
 			},
 			want: false,
 		},
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,1,1,3,3,4,3,2,4,2]"),
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
