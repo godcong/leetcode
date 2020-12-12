baf0088b35b6ee71a3336f79513384bd810ea3a3
godcong is not god <jumbycc@163.com>
2020-12-12 21:22:46 +0800

    update

---
+++ 0376_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_wiggleMaxLength(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,7,4,9,2,5]"),
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := wiggleMaxLength(tt.args.nums); got != tt.want {
+				t.Errorf("wiggleMaxLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
