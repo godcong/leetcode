af607b34db4b3343ff88b1242695549a0e6d8f25
godcong <jumbycc@163.com>
2020-12-14 17:13:00 +0800

    update

--- 0049.groupAnagrams_test.go
+++ 0049.groupAnagrams_test.go
@@ -29,6 +29,7 @@ func Test_groupAnagrams(t *testing.T) {
 			got := groupAnagrams(tt.args.strs)
 			for i := range tt.want {
 				sort.Strings(tt.want[i])
+				sort.Strings(got[i])
 				if !reflect.DeepEqual(got[i], tt.want[i]) {
 					t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
 				}
