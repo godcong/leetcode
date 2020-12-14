d0037e6b7ffd97d7e677a931fa0d3700162811e9
godcong <jumbycc@163.com>
2020-12-14 16:50:23 +0800

    update

--- 0049.groupAnagrams.go
+++ 0049.groupAnagrams.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 /*
 49. 字母异位词分组
 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
@@ -19,5 +21,16 @@ package leetcode
 不考虑答案输出的顺序。
 */
 func groupAnagrams(strs []string) [][]string {
-	return nil
+	mp := map[string][]string{}
+	for _, str := range strs {
+		s := []byte(str)
+		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		sortedStr := string(s)
+		mp[sortedStr] = append(mp[sortedStr], str)
+	}
+	ans := make([][]string, 0, len(mp))
+	for _, v := range mp {
+		ans = append(ans, v)
+	}
+	return ans
 }
