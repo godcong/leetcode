a42162ff6863c115201bd7400546d574033f924f
godcong <jumbycc@163.com>
2020-12-15 18:33:32 +0800

    update

---
+++ 0738_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_monotoneIncreasingDigits(t *testing.T) {
+	type args struct {
+		N int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				N: 10,
+			},
+			want: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := monotoneIncreasingDigits(tt.args.N); got != tt.want {
+				t.Errorf("monotoneIncreasingDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
