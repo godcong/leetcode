98607a88a175e255192eb80a93f6d427e19a4a60
godcong <jumbycc@163.com>
2020-12-15 18:34:03 +0800

    update

--- 0738_test.go
+++ 0738_test.go
@@ -26,6 +26,13 @@ func Test_monotoneIncreasingDigits(t *testing.T) {
 			},
 			want: 1234,
 		},
+		{
+			name: "",
+			args: args{
+				N: 332,
+			},
+			want: 299,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
