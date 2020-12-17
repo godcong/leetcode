ab6cc0825df9f5fc916adf929c601b38c556e964
godcong <jumbycc@163.com>
2020-12-17 10:12:30 +0800

    update

--- 0714.go
+++ 0714.go
@@ -25,4 +25,21 @@ package leetcode
 0 < prices.length <= 50000.
 0 < prices[i] < 50000.
 0 <= fee < 50000.
- */
+*/
+func maxProfit0714(prices []int, fee int) int {
+	n := len(prices)
+	dp := make([][2]int, n)
+	dp[0][1] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]-fee)
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
