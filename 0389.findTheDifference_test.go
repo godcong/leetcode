d52ab756e8d492cc5aab635f77be9824549790aa
godcong is not god <jumbycc@163.com>
2020-12-18 16:41:18 +0800

    update

---
+++ 0389.findTheDifference_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "testing"
+
+func Test_findTheDifference(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want byte
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s: "abcd",
+				t: "abcde",
+			},
+			want: 'e',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTheDifference(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("findTheDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
