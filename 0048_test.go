99c244c51eb1828c8349911fdbcaa85240be95cd
godcong is not god <jumbycc@163.com>
2020-12-19 12:43:25 +0800

    update

--- 0048_test.go
+++ 0048_test.go
@@ -22,6 +22,13 @@ func Test_rotate(t *testing.T) {
 			},
 			want: strToIntArrArray("[\n  [7,4,1],\n  [8,5,2],\n  [9,6,3]\n]"),
 		},
+		{
+			name: "",
+			args: args{
+				matrix: strToIntArrArray("[\n  [ 5, 1, 9,11],\n  [ 2, 4, 8,10],\n  [13, 3, 6, 7],\n  [15,14,12,16]\n] "),
+			},
+			want: strToIntArrArray("[\n  [15,13, 2, 5],\n  [14, 3, 4, 1],\n  [12, 6, 8, 9],\n  [16, 7,10,11]\n]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
