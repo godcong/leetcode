5e18502411eebd45bdc9e4b7a957d296ae1a5ae1
godcong is not god <jumbycc@163.com>
2020-12-19 12:42:41 +0800

    update

--- 0048_test.go
+++ 0048_test.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_rotate(t *testing.T) {
 	type args struct {
@@ -9,6 +12,7 @@ func Test_rotate(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want [][]int
 	}{
 		// TODO: Add test cases.
 		{
@@ -16,10 +20,15 @@ func Test_rotate(t *testing.T) {
 			args: args{
 				matrix: strToIntArrArray("[\n  [1,2,3],\n  [4,5,6],\n  [7,8,9]\n]"),
 			},
+			want: strToIntArrArray("[\n  [7,4,1],\n  [8,5,2],\n  [9,6,3]\n]"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rotate(tt.args.matrix)
+			if !reflect.DeepEqual(tt.args.matrix, tt.want) {
+				t.Errorf("rotate() = %v, want %v", tt.args.matrix, tt.want)
+			}
 		})
 	}
 }
