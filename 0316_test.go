2734a7e6a2d0e84c9e49935c0b3ed932cb683f47
godcong is not god <jumbycc@163.com>
2020-12-20 14:58:10 +0800

    update

---
+++ 0316_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_removeDuplicateLetters(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s: "bcabc",
+			},
+			want: "abc",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicateLetters(tt.args.s); got != tt.want {
+				t.Errorf("removeDuplicateLetters() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
