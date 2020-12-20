97e59d0aa0acebfa7264a15f409b26c3bf40e147
godcong is not god <jumbycc@163.com>
2020-12-20 14:57:32 +0800

    update

--- 0316.go
+++ 0316.go
@@ -24,5 +24,27 @@ package leetcode
 s 由小写英文字母组成
 */
 func removeDuplicateLetters(s string) string {
-	return s
+	left := [26]int{}
+	for _, ch := range s {
+		left[ch-'a']++
+	}
+	stack := []byte{}
+	inStack := [26]bool{}
+	for i := range s {
+		ch := s[i]
+		if !inStack[ch-'a'] {
+			for len(stack) > 0 && ch < stack[len(stack)-1] {
+				last := stack[len(stack)-1] - 'a'
+				if left[last] == 0 {
+					break
+				}
+				stack = stack[:len(stack)-1]
+				inStack[last] = false
+			}
+			stack = append(stack, ch)
+			inStack[ch-'a'] = true
+		}
+		left[ch-'a']--
+	}
+	return string(stack)
 }
