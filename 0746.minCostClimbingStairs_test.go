96a8fd21244a7507daf2cc7f2479e1ff1c7690ab
godcong is not god <jumbycc@163.com>
2020-12-21 19:02:53 +0800

    update

--- 0746.minCostClimbingStairs_test.go
+++ 0746.minCostClimbingStairs_test.go
@@ -19,6 +19,13 @@ func Test_minCostClimbingStairs(t *testing.T) {
 			},
 			want: 15,
 		},
+		{
+			name: "",
+			args: args{
+				cost: strToIntArray(" [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]"),
+			},
+			want: 6,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
