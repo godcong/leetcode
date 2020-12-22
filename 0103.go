98084747b071086fcae143f55f4becfaebb119f1
godcong is not god <jumbycc@163.com>
2020-12-22 20:28:49 +0800

    update

--- 0103.go
+++ 0103.go
@@ -29,5 +29,30 @@ package leetcode
  * }
  */
 func zigzagLevelOrder(root *TreeNode) [][]int {
-	return nil
+	var ans [][]int
+	if root == nil {
+		return ans
+	}
+	queue := []*TreeNode{root}
+	for level := 0; len(queue) > 0; level++ {
+		vals := []int{}
+		q := queue
+		queue = nil
+		for _, node := range q {
+			vals = append(vals, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		if level%2 == 1 {
+			for i, n := 0, len(vals); i < n/2; i++ {
+				vals[i], vals[n-1-i] = vals[n-1-i], vals[i]
+			}
+		}
+		ans = append(ans, vals)
+	}
+	return ans
 }
