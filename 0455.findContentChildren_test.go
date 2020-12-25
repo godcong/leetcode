09df9c592a43bd4a914b67e54f363c4cf6b4ed50
godcong <jumbycc@163.com>
2020-12-25 16:33:41 +0800

    update

---
+++ 0455.findContentChildren_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "testing"
+
+func Test_findContentChildren(t *testing.T) {
+	type args struct {
+		g []int
+		s []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				g: strToIntArray("[1,2,3]"),
+				s: strToIntArray("[1,1]"),
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findContentChildren(tt.args.g, tt.args.s); got != tt.want {
+				t.Errorf("findContentChildren() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
