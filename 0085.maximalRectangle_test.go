db3f5f6d5beca804255e86170ffc103fdded5be5
godcong is not god <jumbycc@163.com>
2020-12-26 21:52:02 +0800

    update

---
+++ 0085.maximalRectangle_test.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import "testing"
+
+func Test_maximalRectangle(t *testing.T) {
+	type args struct {
+		matrix [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				matrix: strToByteArrayArray("[[\"1\",\"0\",\"1\",\"0\",\"0\"],[\"1\",\"0\",\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\",\"1\",\"1\"],[\"1\",\"0\",\"0\",\"1\",\"0\"]]"),
+			},
+			want: 6,
+		},
+		{
+			name: "",
+			args: args{
+				matrix: strToByteArrayArray("[]"),
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximalRectangle(tt.args.matrix); got != tt.want {
+				t.Errorf("maximalRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
