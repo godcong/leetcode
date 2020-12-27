412d9aaf3d95f90c2785d4b4a1eb29e3638654f6
godcong is not god <jumbycc@163.com>
2020-12-27 23:00:08 +0800

    update

--- 0205.go
+++ 0205.go
@@ -24,5 +24,15 @@ package leetcode
 你可以假设 s 和 t 具有相同的长度。
  */
 func isIsomorphic(s string, t string) bool {
-	return false
+	s2t := map[byte]byte{}
+	t2s := map[byte]byte{}
+	for i := range s {
+		x, y := s[i], t[i]
+		if s2t[x] > 0 && s2t[x] != y || t2s[y] > 0 && t2s[y] != x {
+			return false
+		}
+		s2t[x] = y
+		t2s[y] = x
+	}
+	return true
 }
