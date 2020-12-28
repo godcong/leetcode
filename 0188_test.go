fad2d6884ffb6ccc8bb670045517624f701376ab
godcong is not god <jumbycc@163.com>
2020-12-28 19:45:07 +0800

    update

--- 0188_test.go
+++ 0188_test.go
@@ -13,6 +13,14 @@ func Test_maxProfit0188(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				k:      2,
+				prices: strToIntArray("[2,4,1]"),
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
