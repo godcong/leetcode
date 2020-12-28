106f9e83b8c7953e33c4ea8ebd675fce9b173971
godcong is not god <jumbycc@163.com>
2020-12-28 19:44:16 +0800

    update

--- 0188.go
+++ 0188.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math"
+
 /*
 188. 买卖股票的最佳时机 IV
 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
@@ -30,5 +32,47 @@ package leetcode
 0 <= prices[i] <= 1000
 */
 func maxProfit0188(k int, prices []int) int {
-	return 0
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+
+	k = maxProfit0188Min(k, n/2)
+	buy := make([][]int, n)
+	sell := make([][]int, n)
+	for i := range buy {
+		buy[i] = make([]int, k+1)
+		sell[i] = make([]int, k+1)
+	}
+	buy[0][0] = -prices[0]
+	for i := 1; i <= k; i++ {
+		buy[0][i] = math.MinInt64 / 2
+		sell[0][i] = math.MinInt64 / 2
+	}
+
+	for i := 1; i < n; i++ {
+		buy[i][0] = maxProfit0188Max(buy[i-1][0], sell[i-1][0]-prices[i])
+		for j := 1; j <= k; j++ {
+			buy[i][j] = maxProfit0188Max(buy[i-1][j], sell[i-1][j]-prices[i])
+			sell[i][j] = maxProfit0188Max(sell[i-1][j], buy[i-1][j-1]+prices[i])
+		}
+	}
+	return maxProfit0188Max(sell[n-1]...)
+}
+
+func maxProfit0188Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxProfit0188Max(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v > res {
+			res = v
+		}
+	}
+	return res
 }
