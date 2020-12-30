20697772d53ddab62efc6b1c2460b9e44bdfb94c
godcong <jumbycc@163.com>
2020-12-30 14:07:59 +0800

    update

--- 1046.go
+++ 1046.go
@@ -1,5 +1,10 @@
 package leetcode
 
+import (
+	"container/heap"
+	"sort"
+)
+
 /*
 1046. 最后一块石头的重量
 有一堆石头，每块石头的重量都是正整数。
@@ -29,5 +34,25 @@ package leetcode
 1 <= stones[i] <= 1000
 */
 func lastStoneWeight(stones []int) int {
+	q := &hp{stones}
+	heap.Init(q)
+	for q.Len() > 1 {
+		x, y := q.pop(), q.pop()
+		if x > y {
+			q.push(x - y)
+		}
+	}
+	if q.Len() > 0 {
+		return q.IntSlice[0]
+	}
 	return 0
+
 }
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool  { return h.IntSlice[i] > h.IntSlice[j] }
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{}   { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
+func (h *hp) push(v int)         { heap.Push(h, v) }
+func (h *hp) pop() int           { return heap.Pop(h).(int) }
