3cb5db41213131d455beff13235f55eb528ccd39
godcong <jumbycc@163.com>
2020-12-31 11:36:28 +0800

    update

---
+++ 0435_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "testing"
+
+func Test_eraseOverlapIntervals(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				intervals: strToIntArrArray("[ [1,2], [2,3], [3,4], [1,3] ]"),
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := eraseOverlapIntervals(tt.args.intervals); got != tt.want {
+				t.Errorf("eraseOverlapIntervals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
