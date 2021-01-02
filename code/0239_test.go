8331781f34cd77f277b31028f0ddc2066471e4eb
godcong is not god <jumbycc@163.com>
2021-01-02 22:51:37 +0800

    update

--- code/0239_test.go
+++ code/0239_test.go
@@ -32,6 +32,14 @@ func Test_maxSlidingWindow(t *testing.T) {
 			},
 			want: strToIntArray("[1]"),
 		},
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,-1]"),
+				k:    1,
+			},
+			want: strToIntArray("[1,-1]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
