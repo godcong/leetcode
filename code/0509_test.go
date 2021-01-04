14bf5635806ceac8be589be67d11f87c74f7950c
godcong is not god <jumbycc@163.com>
2021-01-04 22:39:16 +0800

    update

---
+++ code/0509_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				n: 2,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
