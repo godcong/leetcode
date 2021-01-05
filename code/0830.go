d91c94b52f19be6290c27731b5e8967752606e64
godcong <jumbycc@163.com>
2021-01-05 16:00:29 +0800

    update

--- code/0830.go
+++ code/0830.go
@@ -40,5 +40,17 @@ package code
 s 仅含小写英文字母
 */
 func largeGroupPositions(s string) [][]int {
-	return nil
+	var ans [][]int
+	cnt := 1
+	for i := range s {
+		if i == len(s)-1 || s[i] != s[i+1] {
+			if cnt >= 3 {
+				ans = append(ans, []int{i - cnt + 1, i})
+			}
+			cnt = 1
+		} else {
+			cnt++
+		}
+	}
+	return ans
 }
