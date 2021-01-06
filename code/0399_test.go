5790010983ed2e06753dd9e973d19e1fec0dfe09
godcong <jumbycc@163.com>
2021-01-06 11:36:09 +0800

    update

--- code/0399_test.go
+++ code/0399_test.go
@@ -30,6 +30,19 @@ func Test_calcEquation(t *testing.T) {
 				6.00000, 0.50000, -1.00000, 1.00000, -1.00000,
 			},
 		},
+		{
+			name: "",
+			args: args{
+				equations: strToStringArrayArray("[[\"a\",\"b\"],[\"b\",\"c\"],[\"bc\",\"cd\"]]"),
+				values: []float64{
+					1.5, 2.5, 5.0,
+				},
+				queries: strToStringArrayArray("[[\"a\",\"c\"],[\"c\",\"b\"],[\"bc\",\"cd\"],[\"cd\",\"bc\"]]"),
+			},
+			want: []float64{
+				3.75000, 0.40000, 5.00000, 0.20000,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
