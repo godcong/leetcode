0cef1d93c953a280921090a5c0947980a7b4295a
godcong <jumbycc@163.com>
2021-01-06 11:35:13 +0800

    update

---
+++ code/0399_test.go
@@ -0,0 +1,41 @@
+package code
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_calcEquation(t *testing.T) {
+	type args struct {
+		equations [][]string
+		values    []float64
+		queries   [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []float64
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				equations: strToStringArrayArray("[[\"a\",\"b\"],[\"b\",\"c\"]]"),
+				values: []float64{
+					2.0, 3.0,
+				},
+				queries: strToStringArrayArray("[[\"a\",\"c\"],[\"b\",\"a\"],[\"a\",\"e\"],[\"a\",\"a\"],[\"x\",\"x\"]]"),
+			},
+			want: []float64{
+				6.00000, 0.50000, -1.00000, 1.00000, -1.00000,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calcEquation(tt.args.equations, tt.args.values, tt.args.queries); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("calcEquation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
