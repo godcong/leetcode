e202b63772743a78fa4bedbbd97e12e15ad79423
godcong <jumbycc@163.com>
2021-01-08 17:50:23 +0800

    update

--- code/0189.rotate_test.go
+++ code/0189.rotate_test.go
@@ -10,8 +10,17 @@ func Test_rotate0189(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,2,3,4,5,6,7]"),
+				k:    3,
+			},
+			want: strToIntArray("[5,6,7,1,2,3,4]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
