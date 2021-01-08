e54b73c586cc58768fb223e1bc55c5c48e383e4e
godcong <jumbycc@163.com>
2021-01-08 17:48:25 +0800

    update

---
+++ code/0189.rotate_test.go
@@ -0,0 +1,20 @@
+package code
+
+import "testing"
+
+func Test_rotate0189(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
