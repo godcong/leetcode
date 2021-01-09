9b7e1b8221b8e7d4470c31f38a5861986026f0d7
godcong <jumbycc@163.com>
2021-01-09 18:25:56 +0800

    update

---
+++ code/0123.maxProfit0123_test.go
@@ -0,0 +1,31 @@
+package code
+
+import "testing"
+
+func Test_maxProfit0123(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				prices: strToIntArray("[3,3,5,0,0,3,1,4]"),
+			},
+			want: 6,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit0123(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit0123() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
