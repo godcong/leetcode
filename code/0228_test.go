e1e101e0c682e65c2b38fc101d6f802c2c3d516b
godcong <jumbycc@163.com>
2021-01-10 19:45:44 +0800

    update

---
+++ code/0228_test.go
@@ -0,0 +1,33 @@
+package code
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_summaryRanges(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: nil,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := summaryRanges(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("summaryRanges() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
