3219d94dd65709d9d1c232b940274eaf640e9d3d
godcong <jumbycc@163.com>
2021-01-12 15:29:41 +0800

    update

--- code/1203_test.go
+++ code/1203_test.go
@@ -28,6 +28,16 @@ func Test_sortItems(t *testing.T) {
 			},
 			want: strToIntArray("[6,3,4,1,5,2,0,7]"),
 		},
+		{
+			name: "",
+			args: args{
+				n:           8,
+				m:           2,
+				group:       strToIntArray("[-1,-1,1,0,0,1,0,-1]"),
+				beforeItems: strToIntArrArray("[[],[6],[5],[6],[3],[],[4],[]]"),
+			},
+			want: strToIntArray("[]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
