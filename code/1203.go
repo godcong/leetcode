84c8e7d38743eabea82e7de46fbce7f676978439
godcong <jumbycc@163.com>
2021-01-12 15:36:52 +0800

    update

--- code/1203.go
+++ code/1203.go
@@ -38,5 +38,71 @@ i != beforeItems[i][j]
 beforeItems[i] 不含重复元素
 */
 func sortItems(n int, m int, group []int, beforeItems [][]int) []int {
-	return nil
+	var ans []int
+	groupItems := make([][]int, m+n) // groupItems[i] 表示第 i 个组负责的项目列表
+	for i := range group {
+		if group[i] == -1 {
+			group[i] = m + i // 给不属于任何组的项目分配一个组
+		}
+		groupItems[group[i]] = append(groupItems[group[i]], i)
+	}
+
+	groupGraph := make([][]int, m+n) // 组间依赖关系
+	groupDegree := make([]int, m+n)
+	itemGraph := make([][]int, n) // 组内依赖关系
+	itemDegree := make([]int, n)
+	for cur, items := range beforeItems {
+		curGroupID := group[cur]
+		for _, pre := range items {
+			preGroupID := group[pre]
+			if preGroupID != curGroupID { // 不同组项目，确定组间依赖关系
+				groupGraph[preGroupID] = append(groupGraph[preGroupID], curGroupID)
+				groupDegree[curGroupID]++
+			} else { // 同组项目，确定组内依赖关系
+				itemGraph[pre] = append(itemGraph[pre], cur)
+				itemDegree[cur]++
+			}
+		}
+	}
+
+	// 组间拓扑序
+	items := make([]int, m+n)
+	for i := range items {
+		items[i] = i
+	}
+	groupOrders := sortItemsTopSort(groupGraph, groupDegree, items)
+	if len(groupOrders) < len(items) {
+		return nil
+	}
+
+	// 按照组间的拓扑序，依次求得各个组的组内拓扑序，构成答案
+	for _, groupID := range groupOrders {
+		items := groupItems[groupID]
+		orders := sortItemsTopSort(itemGraph, itemDegree, items)
+		if len(orders) < len(items) {
+			return nil
+		}
+		ans = append(ans, orders...)
+	}
+	return ans
+}
+func sortItemsTopSort(graph [][]int, deg, items []int) (orders []int) {
+	q := []int{}
+	for _, i := range items {
+		if deg[i] == 0 {
+			q = append(q, i)
+		}
+	}
+	for len(q) > 0 {
+		from := q[0]
+		q = q[1:]
+		orders = append(orders, from)
+		for _, to := range graph[from] {
+			deg[to]--
+			if deg[to] == 0 {
+				q = append(q, to)
+			}
+		}
+	}
+	return
 }
