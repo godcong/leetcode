2c731614daefbc7151fdb74a0849b9b451a7417b
godcong <jumbycc@163.com>
2021-01-13 13:29:30 +0800

    update

--- code/0684.go
+++ code/0684.go
@@ -34,5 +34,29 @@ package code
 我们已经重新检查了问题描述及测试用例，明确图是无向 图。对于有向图详见冗余连接II。对于造成任何不便，我们深感歉意。
 */
 func findRedundantConnection(edges [][]int) []int {
+	parent := make([]int, len(edges)+1)
+	for i := range parent {
+		parent[i] = i
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+	union := func(from, to int) bool {
+		x, y := find(from), find(to)
+		if x == y {
+			return false
+		}
+		parent[x] = y
+		return true
+	}
+	for _, e := range edges {
+		if !union(e[0], e[1]) {
+			return e
+		}
+	}
 	return nil
 }
