bfe0dfc73064ebdaef3250d9131c18efefb7ccaf
godcong <jumbycc@163.com>
2021-01-14 23:06:08 +0800

    update

---
+++ code/1018_test.go
@@ -0,0 +1,35 @@
+package code
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_prefixesDivBy5(t *testing.T) {
+	type args struct {
+		A []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[0,1,1]"),
+			},
+			want: []bool{
+				true, false, false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := prefixesDivBy5(tt.args.A); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("prefixesDivBy5() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
