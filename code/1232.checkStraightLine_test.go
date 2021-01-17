7af8cfb064c62df865567a1b42083b9d77dd8895
godcong <jumbycc@163.com>
2021-01-17 22:17:42 +0800

    update

--- code/1232.checkStraightLine_test.go
+++ code/1232.checkStraightLine_test.go
@@ -19,6 +19,13 @@ func Test_checkStraightLine(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				coordinates: strToIntArrArray("[[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]"),
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
