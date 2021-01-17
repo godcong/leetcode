7e0d74f290928f4799b36050a7911b83f7df5ea0
godcong <jumbycc@163.com>
2021-01-17 22:17:24 +0800

    update

---
+++ code/1232.checkStraightLine_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_checkStraightLine(t *testing.T) {
+	type args struct {
+		coordinates [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				coordinates: strToIntArrArray("[[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]"),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkStraightLine(tt.args.coordinates); got != tt.want {
+				t.Errorf("checkStraightLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
