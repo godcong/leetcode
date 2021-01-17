f1695a853b28c3f5c41c956b0d432a083214640f
godcong <jumbycc@163.com>
2021-01-17 22:16:46 +0800

    update

--- code/1232.go
+++ code/1232.go
@@ -30,5 +30,17 @@ coordinates[i].length == 2
 coordinates 中不含重复的点
 */
 func checkStraightLine(coordinates [][]int) bool {
-	return false
+	deltaX, deltaY := coordinates[0][0], coordinates[0][1]
+	for _, p := range coordinates {
+		p[0] -= deltaX
+		p[1] -= deltaY
+	}
+	A, B := coordinates[1][1], -coordinates[1][0]
+	for _, p := range coordinates[2:] {
+		x, y := p[0], p[1]
+		if A*x+B*y != 0 {
+			return false
+		}
+	}
+	return true
 }
