9d2e51c30308b13c868d9351c79dadaf5898b610
godcong <jumbycc@163.com>
2021-01-18 20:35:00 +0800

    update

--- code/0721.accountsMerge_test.go
+++ code/0721.accountsMerge_test.go
@@ -20,12 +20,13 @@ func Test_accountsMerge(t *testing.T) {
 			args: args{
 				accounts: strToStringArrayArray("[[\"John\", \"johnsmith@mail.com\", \"john00@mail.com\"], [\"John\", \"johnnybravo@mail.com\"], [\"John\", \"johnsmith@mail.com\", \"john_newyork@mail.com\"], [\"Mary\", \"mary@mail.com\"]]"),
 			},
-			want: strToStringArrayArray("[[\"John\", \"john00@mail.com\", \"john_newyork@mail.com\", \"johnsmith@mail.com\"],  [\"John\", \"johnnybravo@mail.com\"], [\"Mary\", \"mary@mail.com\"]]"),
+			want: strToStringArrayArray("[[\"John\", \"johnnybravo@mail.com\"], [\"Mary\", \"mary@mail.com\"], [\"John\", \"john00@mail.com\", \"john_newyork@mail.com\", \"johnsmith@mail.com\"]]"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := accountsMerge(tt.args.accounts); !reflect.DeepEqual(got, tt.want) {
+			got := accountsMerge(tt.args.accounts)
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("accountsMerge() = %v, want %v", got, tt.want)
 			}
 		})
