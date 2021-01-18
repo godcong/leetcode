ea472ce916943911d1c2b43be0ce3af6bcdb794f
godcong <jumbycc@163.com>
2021-01-18 20:14:58 +0800

    update

--- code/0721.go
+++ code/0721.go
@@ -1,5 +1,7 @@
 package code
 
+import "sort"
+
 /*
 721. 账户合并
 给定一个列表 accounts，每个元素 accounts[i] 是一个字符串列表，其中第一个元素 accounts[i][0] 是 名称 (name)，其余元素是 emails 表示该账户的邮箱地址。
@@ -30,5 +32,51 @@ accounts[i]的长度将在[1，10]的范围内。
 accounts[i][j]的长度将在[1，30]的范围内。
 */
 func accountsMerge(accounts [][]string) [][]string {
-	return nil
+	var ans [][]string
+	emailToIndex := map[string]int{}
+	emailToName := map[string]string{}
+	for _, account := range accounts {
+		name := account[0]
+		for _, email := range account[1:] {
+			if _, has := emailToIndex[email]; !has {
+				emailToIndex[email] = len(emailToIndex)
+				emailToName[email] = name
+			}
+		}
+	}
+
+	parent := make([]int, len(emailToIndex))
+	for i := range parent {
+		parent[i] = i
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+	union := func(from, to int) {
+		parent[find(from)] = find(to)
+	}
+
+	for _, account := range accounts {
+		firstIndex := emailToIndex[account[1]]
+		for _, email := range account[2:] {
+			union(emailToIndex[email], firstIndex)
+		}
+	}
+
+	indexToEmails := map[int][]string{}
+	for email, index := range emailToIndex {
+		index = find(index)
+		indexToEmails[index] = append(indexToEmails[index], email)
+	}
+
+	for _, emails := range indexToEmails {
+		sort.Strings(emails)
+		account := append([]string{emailToName[emails[0]]}, emails...)
+		ans = append(ans, account)
+	}
+	return ans
 }
