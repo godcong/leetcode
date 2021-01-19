66ac762a83d8cfe9af8cd4253369faaecbd3affc
godcong <jumbycc@163.com>
2021-01-19 23:09:57 +0800

    update

---
+++ code/1584_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_minCostConnectPoints(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				points: strToIntArrArray("[[3,12],[-2,5],[-4,1]]"),
+			},
+			want: 18,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCostConnectPoints(tt.args.points); got != tt.want {
+				t.Errorf("minCostConnectPoints() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
