8e201ec1f71bdc093a57ba6717206301b682bd49
godcong <jumbycc@163.com>
2021-01-19 23:10:59 +0800

    update

--- code/1584_test.go
+++ code/1584_test.go
@@ -33,6 +33,13 @@ func Test_minCostConnectPoints(t *testing.T) {
 			},
 			want: 4000000,
 		},
+		{
+			name: "",
+			args: args{
+				points: strToIntArrArray("[[0,0]]"),
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
