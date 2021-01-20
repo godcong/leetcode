42e0c56e94c444480b95f5d4a7158559346d2eab
godcong <jumbycc@163.com>
2021-01-20 22:48:38 +0800

    update

--- code/0628.go
+++ code/0628.go
@@ -1,8 +1,6 @@
 package code
 
-import (
-	"math"
-)
+import "math"
 
 /*
 628. 三个数的最大乘积
@@ -30,26 +28,24 @@ func maximumProduct(nums []int) int {
 			min1 = v
 		} else if v < min2 && v >= min1 {
 			min2 = v
-		}
-		if v > max1 { //是否比最大的数大
+		} else if v > max1 {
 			max3 = max2
 			max2 = max1
 			max1 = v
-		} else {          //比最大的数小
-			if v > max2 { //是否有第二大的数大
+		} else {
+			if v > max2 {
 				max3 = max2
 				max2 = v
 			} else {
-				if v > max3 { //是否有第三大的数大
+				if v > max3 {
 					max3 = v
 				}
 			}
 		}
-
 	}
 	if min1*min2*max1 >= max1*max2*max3 {
 		return min1 * min2 * max1
+	} else {
+		return max1 * max2 * max3
 	}
-	return max1 * max2 * max3
-
 }
