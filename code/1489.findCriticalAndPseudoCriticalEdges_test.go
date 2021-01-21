aa045134071d4b5aae279c2270cd5528fc9b7a70
godcong <jumbycc@163.com>
2021-01-21 18:46:33 +0800

    update

---
+++ code/1489.findCriticalAndPseudoCriticalEdges_test.go
@@ -0,0 +1,35 @@
+package code
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findCriticalAndPseudoCriticalEdges(t *testing.T) {
+	type args struct {
+		n     int
+		edges [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				n:     5,
+				edges: strToIntArrArray("[[0,1,1],[1,2,1],[2,3,2],[0,3,2],[0,4,3],[3,4,3],[1,4,6]]"),
+			},
+			want: strToIntArrArray("[[0,1],[2,3,4,5]]"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findCriticalAndPseudoCriticalEdges(tt.args.n, tt.args.edges); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findCriticalAndPseudoCriticalEdges() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
