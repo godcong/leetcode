287f9200260c0ca6e4bbb8abb36269768c466d1d
godcong <jumbycc@163.com>
2021-01-22 17:46:49 +0800

    update

--- code/0989.addToArrayForm_test.go
+++ code/0989.addToArrayForm_test.go
@@ -16,6 +16,14 @@ func Test_addToArrayForm(t *testing.T) {
 		wantAns []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[1,2,0,0]"),
+				K: 34,
+			},
+			wantAns: strToIntArray("[1,2,3,4]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
