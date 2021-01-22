495df7f507f95de93923e0ac1afcbd8a5f8dc210
godcong <jumbycc@163.com>
2021-01-22 17:28:37 +0800

    update

--- code/0989.go
+++ code/0989.go
@@ -37,6 +37,25 @@ package code
 0 <= K <= 10000
 如果 A.length > 1，那么 A[0] != 0
 */
-func addToArrayForm(A []int, K int) []int {
-	return nil
+func addToArrayForm(A []int, K int) (ans []int) {
+	for i := len(A) - 1; i >= 0; i-- {
+		sum := A[i] + K%10
+		K /= 10
+		if sum >= 10 {
+			K++
+			sum -= 10
+		}
+		ans = append(ans, sum)
+	}
+	for ; K > 0; K /= 10 {
+		ans = append(ans, K%10)
+	}
+	addToArrayFormReverse(ans)
+	return
+}
+
+func addToArrayFormReverse(A []int) {
+	for i, n := 0, len(A); i < n/2; i++ {
+		A[i], A[n-1-i] = A[n-1-i], A[i]
+	}
 }
