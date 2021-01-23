03c22981821545842e1bda5d1b08dd8379b8e021
godcong <jumbycc@163.com>
2021-01-23 14:42:06 +0800

    update

--- code/1319_test.go
+++ code/1319_test.go
@@ -21,6 +21,14 @@ func Test_makeConnected(t *testing.T) {
 			},
 			want: 1,
 		},
+		{
+			name: "",
+			args: args{
+				n:           6,
+				connections: strToIntArrArray("[[0,1],[0,2],[0,3],[1,2],[1,3]]"),
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
