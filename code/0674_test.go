7ee3186ae39e48ed7ae2012d9a4d4ba470922f17
godcong <jumbycc@163.com>
2021-01-24 19:10:15 +0800

    update

--- code/0674_test.go
+++ code/0674_test.go
@@ -19,6 +19,13 @@ func Test_findLengthOfLCIS(t *testing.T) {
 			},
 			want: 3,
 		},
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[2,2,2,2,2]"),
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
