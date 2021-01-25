d2b7ef926cf472270770a494509bc6f07bfb779d
godcong <jumbycc@163.com>
2021-01-25 17:48:03 +0800

    update

--- code/0959.regionsBySlashes_test.go
+++ code/0959.regionsBySlashes_test.go
@@ -52,6 +52,16 @@ func Test_regionsBySlashes(t *testing.T) {
 			},
 			want: 5,
 		},
+		{
+			name: "",
+			args: args{
+				grid: []string{
+					"//",
+					"/ ",
+				},
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
