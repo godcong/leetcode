14144e2cee20162eb82a82f0efa25f4f41be37f6
godcong <jumbycc@163.com>
2021-01-26 18:01:22 +0800

    update

--- code/1128.go
+++ code/1128.go
@@ -24,5 +24,15 @@ package code
 1 <= dominoes[i][j] <= 9
 */
 func numEquivDominoPairs(dominoes [][]int) int {
-	return 0
+	var ans int
+	cnt := [100]int{}
+	for _, d := range dominoes {
+		if d[0] > d[1] {
+			d[0], d[1] = d[1], d[0]
+		}
+		v := d[0]*10 + d[1]
+		ans += cnt[v]
+		cnt[v]++
+	}
+	return ans
 }
