ae7dc9f27e08a131c3ca54910eff65071612c9ad
godcong <jumbycc@163.com>
2021-01-28 17:21:22 +0800

    update

--- code/0724.pivotIndex_test.go
+++ code/0724.pivotIndex_test.go
@@ -12,6 +12,13 @@ func Test_pivotIndex(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1, 7, 3, 6, 5, 6]"),
+			},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
