5e657c8da2d53a0526937b95d2f2447e725dd075
godcong <jumbycc@163.com>
2021-01-28 17:20:50 +0800

    update

--- code/0724.go
+++ code/0724.go
@@ -45,5 +45,16 @@ nums 的长度范围为 [0, 10000]。
 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
 */
 func pivotIndex(nums []int) int {
-	return 0
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	sum := 0
+	for i, v := range nums {
+		if 2*sum+v == total {
+			return i
+		}
+		sum += v
+	}
+	return -1
 }
