85567f3fb76915f9e2e2de61772d75a0a291bc30
godcong <jumbycc@163.com>
2021-01-29 22:10:56 +0800

    update

---
+++ code/1631_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_minimumEffortPath(t *testing.T) {
+	type args struct {
+		heights [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				heights: strToIntArrArray("[[1,2,2],[3,8,2],[5,3,5]]"),
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumEffortPath(tt.args.heights); got != tt.want {
+				t.Errorf("minimumEffortPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
