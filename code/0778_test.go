6387ee935524e67c0199b6034dc497c54ab03ea2
godcong <jumbycc@163.com>
2021-01-30 23:29:34 +0800

    update

--- code/0778_test.go
+++ code/0778_test.go
@@ -19,6 +19,13 @@ func Test_swimInWater(t *testing.T) {
 			},
 			want: 3,
 		},
+		{
+			name: "",
+			args: args{
+				grid: strToIntArrArray("[[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]"),
+			},
+			want: 16,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
