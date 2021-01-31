e569114586c8afdb6cf2a551ff91152cb78a3447
godcong <jumbycc@163.com>
2021-01-31 23:25:00 +0800

    update

---
+++ code/0839.numSimilarGroups_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_numSimilarGroups(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				strs: strToStringArray("[\"tars\",\"rats\",\"arts\",\"star\"]"),
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSimilarGroups(tt.args.strs); got != tt.want {
+				t.Errorf("numSimilarGroups() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
