586998205e2b5e18517e75b55a92bc2c21e66ceb
godcong <jumbycc@163.com>
2021-02-04 11:56:13 +0800

    update

--- code/0643.go
+++ code/0643.go
@@ -19,5 +19,17 @@ package code
 所给数据范围 [-10,000，10,000]。
 */
 func findMaxAverage(nums []int, k int) float64 {
-	return 0
+	sum := 0
+	for _, v := range nums[:k] {
+		sum += v
+	}
+	maxSum := sum
+	for i := k; i < len(nums); i++ {
+		sum = sum - nums[i-k] + nums[i]
+		if maxSum < sum {
+			maxSum = sum
+		}
+
+	}
+	return float64(maxSum) / float64(k)
 }
