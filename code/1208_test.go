fd45ccfd610c853564fda4bb9ca84a5897feff27
godcong <jumbycc@163.com>
2021-02-05 19:50:21 +0800

    update

---
+++ code/1208_test.go
@@ -0,0 +1,34 @@
+package code
+
+import "testing"
+
+func Test_equalSubstring(t *testing.T) {
+	type args struct {
+		s       string
+		t       string
+		maxCost int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s:       "abcd",
+				t:       "bcdf",
+				maxCost: 3,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := equalSubstring(tt.args.s, tt.args.t, tt.args.maxCost); got != tt.want {
+				t.Errorf("equalSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
