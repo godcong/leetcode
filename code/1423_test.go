5ab56aa3ae44bdbc758f4dd6064835835b9e26de
godcong <jumbycc@163.com>
2021-02-06 22:36:57 +0800

    update

---
+++ code/1423_test.go
@@ -0,0 +1,32 @@
+package code
+
+import "testing"
+
+func Test_maxScore(t *testing.T) {
+	type args struct {
+		cardPoints []int
+		k          int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				cardPoints: strToIntArray("[1,2,3,4,5,6,1]"),
+				k:          3,
+			},
+			want: 12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScore(tt.args.cardPoints, tt.args.k); got != tt.want {
+				t.Errorf("maxScore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
