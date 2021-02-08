3a5ef40966d3f3488e1cff0b4b6a4854673e8d24
godcong <jumbycc@163.com>
2021-02-08 19:25:24 +0800

    update

---
+++ code/0978_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_maxTurbulenceSize(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				arr: strToIntArray("[9,4,2,10,7,8,8,1,9]"),
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxTurbulenceSize(tt.args.arr); got != tt.want {
+				t.Errorf("maxTurbulenceSize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
