9588e1526c8524bd469d210844a203e40a970a26
godcong <jumbycc@163.com>
2021-02-09 20:27:40 +0800

    update

---
+++ code/0992_test.go
@@ -0,0 +1,32 @@
+package code
+
+import "testing"
+
+func Test_subarraysWithKDistinct(t *testing.T) {
+	type args struct {
+		A []int
+		K int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[1,2,1,2,3]"),
+				K: 2,
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subarraysWithKDistinct(tt.args.A, tt.args.K); got != tt.want {
+				t.Errorf("subarraysWithKDistinct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
