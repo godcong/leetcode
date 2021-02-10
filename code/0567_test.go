2d32c1e721157edf0ef7cf01e7048df6ccb4636a
godcong <jumbycc@163.com>
2021-02-10 12:11:28 +0800

    update

--- code/0567_test.go
+++ code/0567_test.go
@@ -21,6 +21,14 @@ func Test_checkInclusion(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				s1: "ab",
+				s2: "eidboaoo",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
