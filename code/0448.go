d279c958b0abf5c4e5cd26e6939d3992b03c6198
godcong is not god <jumbycc@163.com>
2021-02-13 18:44:31 +0800

    update

--- code/0448.go
+++ code/0448.go
@@ -17,5 +17,16 @@ package code
 [5,6]
 */
 func findDisappearedNumbers(nums []int) []int {
-	return nil
+	var ans []int
+	n := len(nums)
+	for _, v := range nums {
+		v = (v - 1) % n
+		nums[v] += n
+	}
+	for i, v := range nums {
+		if v <= n {
+			ans = append(ans, i+1)
+		}
+	}
+	return ans
 }
