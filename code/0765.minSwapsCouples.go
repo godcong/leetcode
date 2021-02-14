4c94b27f72f676ad0dc99e1fdd6019909c9e1e8d
godcong is not god <jumbycc@163.com>
2021-02-14 12:41:13 +0800

    update

--- code/0765.minSwapsCouples.go
+++ code/0765.minSwapsCouples.go
@@ -23,5 +23,35 @@ N 对情侣坐在连续排列的 2N 个座位上，想要牵到对方的手。 �
 len(row) 是偶数且数值在 [4, 60]范围内。
 */
 func minSwapsCouples(row []int) int {
-	return 0
+	var ans int
+	n := len(row)
+	graph := make([][]int, n/2)
+	for i := 0; i < n; i += 2 {
+		l, r := row[i]/2, row[i+1]/2
+		if l != r {
+			graph[l] = append(graph[l], r)
+			graph[r] = append(graph[r], l)
+		}
+	}
+	vis := make([]bool, n/2)
+	for i, vs := range vis {
+		if !vs {
+			vis[i] = true
+			cnt := 0
+			q := []int{i}
+			for len(q) > 0 {
+				cnt++
+				v := q[0]
+				q = q[1:]
+				for _, w := range graph[v] {
+					if !vis[w] {
+						vis[w] = true
+						q = append(q, w)
+					}
+				}
+			}
+			ans += cnt - 1
+		}
+	}
+	return ans
 }
