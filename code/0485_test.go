14f6dbdcceea5a708cdfc537240bb981700219e9
godcong is not god <jumbycc@163.com>
2021-02-15 18:33:31 +0800

    update

--- code/0485_test.go
+++ code/0485_test.go
@@ -12,6 +12,13 @@ func Test_findMaxConsecutiveOnes(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,1,0,1,1,1]"),
+			},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
