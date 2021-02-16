fd66ce1a677cd26cfe1b259c23b9b17d8fa4d0aa
godcong is not god <jumbycc@163.com>
2021-02-16 15:21:45 +0800

    update

---
+++ code/0561_test.go
@@ -0,0 +1,30 @@
+package code
+
+import "testing"
+
+func Test_arrayPairSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArray("[1,4,3,2]"),
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := arrayPairSum(tt.args.nums); got != tt.want {
+				t.Errorf("arrayPairSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
