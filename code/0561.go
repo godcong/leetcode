a19e889e9fba3763a01ae57306744d4fdeb487d1
godcong is not god <jumbycc@163.com>
2021-02-16 15:21:17 +0800

    update

--- code/0561.go
+++ code/0561.go
@@ -1,5 +1,7 @@
 package code
 
+import "sort"
+
 /*
 561. 数组拆分 I
 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
@@ -31,5 +33,10 @@ nums.length == 2 * n
 -104 <= nums[i] <= 104
 */
 func arrayPairSum(nums []int) int {
-	return 0
+	ans := 0
+	sort.Ints(nums)
+	for i := 0; i < len(nums); i += 2 {
+		ans += nums[i]
+	}
+	return ans
 }
