803985f681b20ffb3e4f841267b7643aaeb5cf77
godcong <jumbycc@163.com>
2021-02-18 23:27:22 +0800

    update

---
+++ code/0995_test.go
@@ -0,0 +1,32 @@
+package code
+
+import "testing"
+
+func Test_minKBitFlips(t *testing.T) {
+	type args struct {
+		A []int
+		K int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[0,1,0]"),
+				K: 1,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minKBitFlips(tt.args.A, tt.args.K); got != tt.want {
+				t.Errorf("minKBitFlips() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
