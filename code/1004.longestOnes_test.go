b7c9dcfe9a0fbf63506bc730ab598c09c1177228
godcong <jumbycc@163.com>
2021-02-19 22:57:05 +0800

    update

---
+++ code/1004.longestOnes_test.go
@@ -0,0 +1,32 @@
+package code
+
+import "testing"
+
+func Test_longestOnes(t *testing.T) {
+	type args struct {
+		A []int
+		K int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[1,1,1,0,0,0,1,1,1,1,0]"),
+				K: 2,
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestOnes(tt.args.A, tt.args.K); got != tt.want {
+				t.Errorf("longestOnes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
