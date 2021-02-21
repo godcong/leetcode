38d1f46306b71bdf8727400c39116a2a52bca81f
godcong <jumbycc@163.com>
2021-02-21 23:22:37 +0800

    update

--- code/1438_test.go
+++ code/1438_test.go
@@ -13,6 +13,14 @@ func Test_longestSubarray(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums:  strToIntArray("[8,2,4,7]"),
+				limit: 4,
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
