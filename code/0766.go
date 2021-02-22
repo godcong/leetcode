f117ef75aed67c89b3b4f81f7add5d5fe80999ea
godcong <jumbycc@163.com>
2021-02-22 23:28:31 +0800

    update

--- code/0766.go
+++ code/0766.go
@@ -42,10 +42,6 @@ n == matrix[i].length
 func isToeplitzMatrix(matrix [][]int) bool {
 	col := len(matrix)
 	coluM := len(matrix[0])
-	if col == 1 || coluM == 1 {
-		return true
-	}
-
 	for i, tmp := 0, 1; tmp < col; i, tmp = i+1, tmp+1 {
 		for index := 0; index < coluM-1; index++ {
 			if matrix[i][:coluM-1][index] != matrix[tmp][1:][index] {
