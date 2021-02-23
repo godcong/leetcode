ccd98be0d1a41867b87bef1a42209f28e1bb3ef4
godcong <jumbycc@163.com>
2021-02-23 23:09:00 +0800

    update

--- code/1052.go
+++ code/1052.go
@@ -27,5 +27,23 @@ package code
 0 <= grumpy[i] <= 1
 */
 func maxSatisfied(customers []int, grumpy []int, X int) int {
-	return 0
+	total := 0
+	n := len(customers)
+	for i := 0; i < n; i++ {
+		if grumpy[i] == 0 {
+			total += customers[i]
+		}
+	}
+	increase := 0
+	for i := 0; i < X; i++ {
+		increase += customers[i] * grumpy[i]
+	}
+	maxIncrease := increase
+	for i := X; i < n; i++ {
+		increase = increase - customers[i-X]*grumpy[i-X] + customers[i]*grumpy[i]
+		if maxIncrease > increase {
+			maxIncrease = increase
+		}
+	}
+	return total + maxIncrease
 }
