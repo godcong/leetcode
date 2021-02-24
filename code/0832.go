b5d6de9b4bd9dc202edf60833a72c7c441bfe1de
godcong <jumbycc@163.com>
2021-02-24 20:52:26 +0800

    update

--- code/0832.go
+++ code/0832.go
@@ -30,5 +30,19 @@ package code
 0 <= A[i][j] <= 1
 */
 func flipAndInvertImage(A [][]int) [][]int {
-	return nil
+	for _, row := range A {
+		left, right := 0, len(row)-1
+		for left < right {
+			if row[left] == row[right] {
+				row[left] ^= 1
+				row[right] ^= 1
+			}
+			left++
+			right--
+		}
+		if left == right {
+			row[left] ^= 1
+		}
+	}
+	return A
 }
