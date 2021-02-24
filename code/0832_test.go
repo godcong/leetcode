70969446525455767c737a6cdee261393a6c823f
godcong <jumbycc@163.com>
2021-02-24 20:53:11 +0800

    update

--- code/0832_test.go
+++ code/0832_test.go
@@ -15,6 +15,13 @@ func Test_flipAndInvertImage(t *testing.T) {
 		want [][]int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				A: strToIntArrArray("[[1,1,0],[1,0,1],[0,0,0]]"),
+			},
+			want: strToIntArrArray("[[1,0,0],[0,1,0],[1,1,1]]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
