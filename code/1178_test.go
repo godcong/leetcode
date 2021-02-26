2ef5bba5c120a17a7f8f773b82e95285ec7206fc
godcong <jumbycc@163.com>
2021-02-26 12:38:43 +0800

    update

---
+++ code/1178_test.go
@@ -0,0 +1,27 @@
+package code
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findNumOfValidWords(t *testing.T) {
+	type args struct {
+		words   []string
+		puzzles []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNumOfValidWords(tt.args.words, tt.args.puzzles); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findNumOfValidWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
