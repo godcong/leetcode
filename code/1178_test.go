5566ea414ce8a90272455e70758853ae8ec101ca
godcong <jumbycc@163.com>
2021-02-26 12:39:11 +0800

    update

--- code/1178_test.go
+++ code/1178_test.go
@@ -16,6 +16,14 @@ func Test_findNumOfValidWords(t *testing.T) {
 		want []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				words:   strToStringArray("[\"aaaa\",\"asas\",\"able\",\"ability\",\"actt\",\"actor\",\"access\"]"),
+				puzzles: strToStringArray("[\"aboveyz\",\"abrodyz\",\"abslute\",\"absoryz\",\"actresz\",\"gaswxyz\"]"),
+			},
+			want: strToIntArray("[1,1,3,2,4,0]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
