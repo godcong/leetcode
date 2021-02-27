ea82d029d92c111d554e51469b7e69da81af97f7
godcong <jumbycc@163.com>
2021-02-27 22:26:09 +0800

    update

--- code/0395.go
+++ code/0395.go
@@ -1,5 +1,7 @@
 package code
 
+import "strings"
+
 /*
 395. 至少有 K 个重复字符的最长子串
 给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k 。返回这一子串的长度。
@@ -25,5 +27,31 @@ s 仅由小写英文字母组成
 1 <= k <= 105
 */
 func longestSubstring(s string, k int) int {
-	return 0
+	var ans int
+	if s == "" {
+		return 0
+	}
+
+	cnt := [26]int{}
+	for _, ch := range s {
+		cnt[ch-'a']++
+	}
+
+	var split byte
+	for i, c := range cnt[:] {
+		if 0 < c && c < k {
+			split = 'a' + byte(i)
+			break
+		}
+	}
+	if split == 0 {
+		return len(s)
+	}
+
+	for _, subStr := range strings.Split(s, string(split)) {
+		if ret := longestSubstring(subStr, k); ret > ans {
+			ans = ret
+		}
+	}
+	return ans
 }
