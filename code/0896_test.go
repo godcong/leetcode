126e191c63924a606c64262fd16d7a08d0ae9706
godcong <jumbycc@163.com>
2021-02-28 18:11:35 +0800

    update

--- code/0896_test.go
+++ code/0896_test.go
@@ -26,6 +26,13 @@ func Test_isMonotonic(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				A: strToIntArray("[1,3,2]"),
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
