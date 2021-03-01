24b27c576cd493840c2f9308076bae58dffd1eca
godcong <jumbycc@163.com>
2021-03-01 22:49:22 +0800

    update

--- code/0303/0303.SumRange_test.go
+++ code/0303/0303.SumRange_test.go
@@ -1,31 +1,49 @@
 package _303
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
-func TestNumArray_SumRange(t *testing.T) {
-	type fields struct {
-		sum []int
-	}
+func TestConstructor(t *testing.T) {
 	type args struct {
-		i int
-		j int
+		nums [][]int
+		fns  []string
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   int
+		name string
+		args args
+		want NumArray
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: strToIntArrArray("[[-2, 0, 3, -5, 2, -1], [0, 2], [2, 5], [0, 5]]"),
+				fns:  strToStringArray("[\"NumArray\", \"sumRange\", \"sumRange\", \"sumRange\"]"),
+			},
+			want: NumArray{
+				sum: strToIntArray("[null, 1, -1, -3]"),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			n := &NumArray{
-				sum: tt.fields.sum,
+			var numArray NumArray
+			for i, ints := range tt.args.nums[1:] {
+				switch {
+				case strings.Compare(tt.args.fns[i], "NumArray") > 0:
+					numArray = Constructor(ints)
+					t.Log(numArray)
+				case strings.Compare(tt.args.fns[i], "sumRange") > 0:
+					got := numArray.SumRange(ints[0], ints[1])
+					if !reflect.DeepEqual(got, tt.want) {
+						t.Errorf("Constructor() = %v, want %v", got, tt.want)
+					}
+				}
 			}
-			if got := n.SumRange(tt.args.i, tt.args.j); got != tt.want {
-				t.Errorf("SumRange() = %v, want %v", got, tt.want)
-			}
+
 		})
 	}
 }
