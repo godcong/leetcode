1132d86d239c4d3a731a7705dbe431bba1acd078
godcong <jumbycc@163.com>
2021-03-04 23:29:01 +0800

    update

--- code/0354.go
+++ code/0354.go
@@ -1,5 +1,7 @@
 package code
 
+import "sort"
+
 /*
 354. 俄罗斯套娃信封问题
 
@@ -34,5 +36,36 @@ package code
 
 */
 func maxEnvelopes(envelopes [][]int) int {
+	n := len(envelopes)
+	if n == 0 {
+		return 0
+	}
 
+	sort.Slice(envelopes, func(i, j int) bool {
+		a, b := envelopes[i], envelopes[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+
+	f := make([]int, n)
+	for i := range f {
+		f[i] = 1
+	}
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if envelopes[j][1] < envelopes[i][1] {
+				f[i] = maxEnvelopesMax(f[i], f[j]+1)
+			}
+		}
+	}
+	return maxEnvelopesMax(f...)
+}
+
+func maxEnvelopesMax(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v > res {
+			res = v
+		}
+	}
+	return res
 }
