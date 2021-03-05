df80996c8765659f74529e4d057e9364df6d6e2a
godcong <jumbycc@163.com>
2021-03-05 23:56:25 +0800

    update

--- code/0232/0232.MyQueue_test.go
+++ code/0232/0232.MyQueue_test.go
@@ -1,6 +1,7 @@
 package _0232
 
 import (
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -30,28 +31,37 @@ func TestConstructor(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var my MyQueue
+			var got []string
 			for i, s := range tt.args.step {
 				switch s {
 				case "MyQueue":
 					my = Constructor()
+					got = append(got, "null")
 				case "push":
 					my.Push(tt.args.nums[i][0])
+					got = append(got, "null")
 				case "peek":
 					p := my.Peek()
 					if strconv.Itoa(p) != tt.want[i] {
 						t.Errorf("Peek() = %v, want %v", p, tt.want[i])
 					}
+					got = append(got, strconv.Itoa(p))
 				case "pop":
 					p := my.Pop()
 					if strconv.Itoa(p) != tt.want[i] {
 						t.Errorf("Pop() = %v, want %v", p, tt.want[i])
 					}
+					got = append(got, strconv.Itoa(p))
 				case "empty":
 					e := my.Empty()
 					if strconv.FormatBool(e) != tt.want[i] {
 						t.Errorf("Empty() = %v, want %v", e, tt.want[i])
 					}
+					got = append(got, strconv.FormatBool(e))
 				}
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
 			}
 		})
 	}
