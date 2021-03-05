a20ef3e79074fec0a528c6b8f97a19b4835442fc
godcong <jumbycc@163.com>
2021-03-05 23:44:10 +0800

    update

--- code/0232/0232.go
+++ code/0232/0232.go
@@ -1,4 +1,4 @@
-package code
+package _232
 
 /*
 232. 用栈实现队列
@@ -52,31 +52,47 @@ myQueue.empty(); // return false
     假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
 */
 type MyQueue struct {
+	inStack, outStack []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() MyQueue {
-
+	return MyQueue{}
 }
 
 /** Push element x to the back of queue. */
 func (this *MyQueue) Push(x int) {
-
+	this.inStack = append(this.inStack, x)
 }
 
 /** Removes the element from in front of queue and returns that element. */
 func (this *MyQueue) Pop() int {
-
+	if len(this.outStack) == 0 {
+		this.in2out()
+	}
+	x := this.outStack[len(this.outStack)-1]
+	this.outStack = this.outStack[:len(this.outStack)-1]
+	return x
 }
 
 /** Get the front element. */
 func (this *MyQueue) Peek() int {
-
+	if len(this.outStack) == 0 {
+		this.in2out()
+	}
+	return this.outStack[len(this.outStack)-1]
 }
 
 /** Returns whether the queue is empty. */
 func (this *MyQueue) Empty() bool {
+	return len(this.inStack) == 0 && len(this.outStack) == 0
+}
 
+func (this *MyQueue) in2out() {
+	for len(this.inStack) > 0 {
+		this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
+		this.inStack = this.inStack[:len(this.inStack)-1]
+	}
 }
 
 /**
