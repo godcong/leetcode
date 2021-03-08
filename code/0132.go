309504f52572caafa7d825746163e84872bf9fec
godcong <jumbycc@163.com>
2021-03-08 22:41:54 +0800

    update

--- code/0132.go
+++ code/0132.go
@@ -1,5 +1,7 @@
 package code
 
+import "math"
+
 /*
 132. 分割回文串 II
 
@@ -33,5 +35,31 @@ package code
     s 仅由小写英文字母组成
 */
 func minCut(s string) int {
-	return 0
+	n := len(s)
+	g := make([][]bool, n)
+	for i := range g {
+		g[i] = make([]bool, n)
+		for j := range g[i] {
+			g[i][j] = true
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			g[i][j] = s[i] == s[j] && g[i+1][j-1]
+		}
+	}
+
+	f := make([]int, n)
+	for i := range f {
+		if g[0][i] {
+			continue
+		}
+		f[i] = math.MaxInt64
+		for j := 0; j < i; j++ {
+			if g[j+1][i] && f[j]+1 < f[i] {
+				f[i] = f[j] + 1
+			}
+		}
+	}
+	return f[n-1]
 }
