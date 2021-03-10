4ccab71f15d5cc420ab808c140593c2c6c5c7574
godcong <jumbycc@163.com>
2021-03-10 19:10:21 +0800

    update

--- code/0224.calculate.go
+++ code/0224.calculate.go
@@ -27,5 +27,33 @@ s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
 s 表示一个有效的表达式
 */
 func calculate(s string) int {
-	return 0
+	var ans int
+	ops := []int{1}
+	sign := 1
+	n := len(s)
+	for i := 0; i < n; {
+		switch s[i] {
+		case ' ':
+			i++
+		case '+':
+			sign = ops[len(ops)-1]
+			i++
+		case '-':
+			sign = -ops[len(ops)-1]
+			i++
+		case '(':
+			ops = append(ops, sign)
+			i++
+		case ')':
+			ops = ops[:len(ops)-1]
+			i++
+		default:
+			num := 0
+			for ; i < n && '0' <= s[i] && s[i] <= '9'; i++ {
+				num = num*10 + int(s[i]-'0')
+			}
+			ans += sign * num
+		}
+	}
+	return ans
 }
