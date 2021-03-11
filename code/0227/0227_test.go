5b23901ec84d3b49d4fdadbed0aa497b58ac91d0
godcong <jumbycc@163.com>
2021-03-11 19:04:49 +0800

    update

---
+++ code/0227/0227_test.go
@@ -0,0 +1,23 @@
+package _0227
+
+import "testing"
+
+func Test_calculate(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculate(tt.args.s); got != tt.want {
+				t.Errorf("calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
