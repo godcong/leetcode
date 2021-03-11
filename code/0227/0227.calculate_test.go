51e011dc4997f5a0fc86ab6e83c3ae1460c32f3f
godcong <jumbycc@163.com>
2021-03-11 19:05:13 +0800

    update

--- code/0227/0227.calculate_test.go
+++ code/0227/0227.calculate_test.go
@@ -12,6 +12,13 @@ func Test_calculate(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s: "3+2*2",
+			},
+			want: 7,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
