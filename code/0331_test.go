17d44700025e46c06bcdbfa0fcbd37d8aab38258
godcong <jumbycc@163.com>
2021-03-12 22:24:48 +0800

    update

--- code/0331_test.go
+++ code/0331_test.go
@@ -26,6 +26,13 @@ func Test_isValidSerialization(t *testing.T) {
 			},
 			want: false,
 		},
+		{
+			name: "",
+			args: args{
+				preorder: "9,#,#,1",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
