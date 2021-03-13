3594deef9e3a3bf93a9418a30843e4c7aa90336d
godcong <jumbycc@163.com>
2021-03-13 22:08:22 +0800

    update

--- code/0705.go
+++ code/0705.go
@@ -38,3 +38,37 @@ myHashSet.contains(2); // 返回 False ，（已移除）
 
 进阶：你可以不使用内建的哈希集合库解决此问题吗？
 */
+type MyHashSet struct {
+
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() MyHashSet {
+
+}
+
+
+func (this *MyHashSet) Add(key int)  {
+
+}
+
+
+func (this *MyHashSet) Remove(key int)  {
+
+}
+
+
+/** Returns true if this set contains the specified element */
+func (this *MyHashSet) Contains(key int) bool {
+
+}
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
