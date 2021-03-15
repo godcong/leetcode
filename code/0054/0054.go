3ef541de9df5b829ad719ab906aa1b26279c7ff9
godcong <jumbycc@163.com>
2021-03-15 20:37:18 +0800

    update

--- code/0054/0054.go
+++ code/0054/0054.go
@@ -27,5 +27,32 @@ n == matrix[i].length
 通过次数128,564提交次数282,799
 */
 func spiralOrder(matrix [][]int) []int {
-	return nil
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	rows, columns := len(matrix), len(matrix[0])
+	visited := make([][]bool, rows)
+	for i := 0; i < rows; i++ {
+		visited[i] = make([]bool, columns)
+	}
+
+	var (
+		total          = rows * columns
+		order          = make([]int, total)
+		row, column    = 0, 0
+		directions     = [][]int{[]int{0, 1}, []int{1, 0}, []int{0, -1}, []int{-1, 0}}
+		directionIndex = 0
+	)
+
+	for i := 0; i < total; i++ {
+		order[i] = matrix[row][column]
+		visited[row][column] = true
+		nextRow, nextColumn := row+directions[directionIndex][0], column+directions[directionIndex][1]
+		if nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn] {
+			directionIndex = (directionIndex + 1) % 4
+		}
+		row += directions[directionIndex][0]
+		column += directions[directionIndex][1]
+	}
+	return order
 }
