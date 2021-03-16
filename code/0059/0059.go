d2f11073b4e1fd52047833ae9ce2c81640aa788f
godcong <jumbycc@163.com>
2021-03-16 20:13:11 +0800

    update

--- code/0059/0059.go
+++ code/0059/0059.go
@@ -21,6 +21,25 @@ package _0059
 
 1 <= n <= 20
 */
+type pair struct{ x, y int }
+
+var dirs = []pair{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} // 右下左上
+
 func generateMatrix(n int) [][]int {
-	return nil
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+	row, col, dirIdx := 0, 0, 0
+	for i := 1; i <= n*n; i++ {
+		matrix[row][col] = i
+		dir := dirs[dirIdx]
+		if r, c := row+dir.x, col+dir.y; r < 0 || r >= n || c < 0 || c >= n || matrix[r][c] > 0 {
+			dirIdx = (dirIdx + 1) % 4 // 顺时针旋转至下一个方向
+			dir = dirs[dirIdx]
+		}
+		row += dir.x
+		col += dir.y
+	}
+	return matrix
 }
