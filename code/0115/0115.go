790e7d2369acc228f38a4c431df420faf9261f2e
godcong <jumbycc@163.com>
2021-03-17 20:37:27 +0800

    update

--- code/0115/0115.go
+++ code/0115/0115.go
@@ -47,7 +47,8 @@ babgbag
 0 <= s.length, t.length <= 1000
 s 和 t 由英文字母组成
 */
-func numDistinct(s string, t string) int {
+func numDist
+inct(s string, t string) int {
 	m, n := len(s), len(t)
 	if m < n {
 		return 0
