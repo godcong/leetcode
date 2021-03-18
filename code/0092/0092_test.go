3e74c426279cb02a7910f8db2770653eeb18368e
godcong <jumbycc@163.com>
2021-03-18 20:31:51 +0800

    update

---
+++ code/0092/0092_test.go
@@ -0,0 +1,28 @@
+package _0092
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseBetween(t *testing.T) {
+	type args struct {
+		head  *ListNode
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBetween(tt.args.head, tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseBetween() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
