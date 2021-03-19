744cc7b463596546d7d4b6c98bcbb282a5c177d7
godcong <jumbycc@163.com>
2021-03-19 22:56:51 +0800

    update

--- code/1603/1603.go
+++ code/1603/1603.go
@@ -32,3 +32,19 @@ parkingSystem.addCar(1); // 返回 false ，因为没有空的大车位，唯一
 carType 取值为 1， 2 或 3
 最多会调用 addCar 函数 1000 次
 */
+type ParkingSystem struct {
+}
+
+func Constructor(big int, medium int, small int) ParkingSystem {
+
+}
+
+func (this *ParkingSystem) AddCar(carType int) bool {
+
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * obj := Constructor(big, medium, small);
+ * param_1 := obj.AddCar(carType);
+ */
