7a0395c4b3f311643576d31f70c5e3d9674d9487
godcong <jumbycc@163.com>
2021-03-20 21:05:28 +0800

    update

---
+++ code/0150/0150._test.go
@@ -0,0 +1,34 @@
+package _0150
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_evalRPN(t *testing.T) {
+	type args struct {
+		tokens []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				tokens: StrToStringArray("[\"2\",\"1\",\"+\",\"3\",\"*\"]"),
+			},
+			want: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := evalRPN(tt.args.tokens); got != tt.want {
+				t.Errorf("evalRPN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
