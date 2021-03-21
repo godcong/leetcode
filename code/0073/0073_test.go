642d3a0c2c6b5875fcfec01890dce0f523be54f1
godcong <jumbycc@163.com>
2021-03-21 13:54:25 +0800

    update

---
+++ code/0073/0073_test.go
@@ -0,0 +1,19 @@
+package _0073
+
+import "testing"
+
+func Test_setZeroes(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
