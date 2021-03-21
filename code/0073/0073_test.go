c5d3470089df17611b5bca1d30ba9273ea4f40b8
godcong <jumbycc@163.com>
2021-03-21 13:56:31 +0800

    update

--- code/0073/0073_test.go
+++ code/0073/0073_test.go
@@ -22,6 +22,13 @@ func Test_setZeroes(t *testing.T) {
 			},
 			want: StrToIntArrArray("[[1,0,1],[0,0,0],[1,0,1]]"),
 		},
+		{
+			name: "",
+			args: args{
+				matrix: StrToIntArrArray("[[0,1,2,0],[3,4,5,2],[1,3,1,5]]"),
+			},
+			want: StrToIntArrArray("[[0,0,0,0],[0,4,5,0],[0,3,1,0]]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
