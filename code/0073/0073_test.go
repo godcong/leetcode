544cef881e8fcb792d2f6dd7e3e4902dadde98a2
godcong <jumbycc@163.com>
2021-03-21 13:56:07 +0800

    update

--- code/0073/0073_test.go
+++ code/0073/0073_test.go
@@ -1,6 +1,9 @@
 package _0073
 
-import "testing"
+import (
+	. "github.com/godcong/leetcode/common"
+	"testing"
+)
 
 func Test_setZeroes(t *testing.T) {
 	type args struct {
@@ -9,8 +12,16 @@ func Test_setZeroes(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want [][]int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				matrix: StrToIntArrArray("[[1,1,1],[1,0,1],[1,1,1]]"),
+			},
+			want: StrToIntArrArray("[[1,0,1],[0,0,0],[1,0,1]]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
