e810d447c886df2bd2d33fc2b1e437c8d0afce31
godcong <jumbycc@163.com>
2021-03-23 20:35:38 +0800

    update

--- code/0341/0341.go
+++ code/0341/0341.go
@@ -1,5 +1,9 @@
 package _0341
 
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
 /*
 341. 扁平化嵌套列表迭代器
 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
@@ -19,3 +23,58 @@ package _0341
 输出: [1,4,6]
 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
 */
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
+
+type NestedIterator struct {
+	vals []int
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	var vals []int
+	var dfs func([]*NestedInteger)
+	dfs = func(nestedList []*NestedInteger) {
+		for _, nest := range nestedList {
+			if nest.IsInteger() {
+				vals = append(vals, nest.GetInteger())
+			} else {
+				dfs(nest.GetList())
+			}
+		}
+	}
+	dfs(nestedList)
+	return &NestedIterator{vals}
+}
+
+func (this *NestedIterator) Next() int {
+	val := this.vals[0]
+	this.vals = this.vals[1:]
+	return val
+}
+
+func (this *NestedIterator) HasNext() bool {
+	return len(this.vals) > 0
+}
