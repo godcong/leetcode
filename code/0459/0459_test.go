dfae5e748c20653bbaa47189440f0b66d464b32d
godcong <jumbycc@163.com>
2021-03-24 22:52:54 +0800

    update

--- code/0459/0459_test.go
+++ code/0459/0459_test.go
@@ -1,6 +1,10 @@
 package _0459
 
-import "testing"
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
 
 func Test_find132pattern(t *testing.T) {
 	type args struct {
@@ -12,6 +16,13 @@ func Test_find132pattern(t *testing.T) {
 		want bool
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: StrToIntArray("[1,2,3,4]"),
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
