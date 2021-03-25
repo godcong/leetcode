f8f1e20443b5d532ec994c972b0059aae4219551
godcong <jumbycc@163.com>
2021-03-25 23:07:53 +0800

    update

--- code/0082/0082_test.go
+++ code/0082/0082_test.go
@@ -17,6 +17,13 @@ func Test_deleteDuplicates(t *testing.T) {
 		want *ListNode
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				head: StrToListNode("[1,2,3,3,4,4,5]", -1),
+			},
+			want: StrToListNode("[1,2,5]", -1),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
