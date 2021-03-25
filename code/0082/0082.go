6281df1444a3ba7c280d0f78b8059b975d82b813
godcong <jumbycc@163.com>
2021-03-25 23:07:03 +0800

    update

--- code/0082/0082.go
+++ code/0082/0082.go
@@ -41,5 +41,23 @@ import (
  * }
  */
 func deleteDuplicates(head *ListNode) *ListNode {
-	return nil
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{0, head}
+
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
 }
