d3eda3c139f0e028499c147c1b2e35c0179944b2
godcong <jumbycc@163.com>
2021-03-27 16:07:36 +0800

    update

--- code/0061/0061.go
+++ code/0061/0061.go
@@ -36,5 +36,25 @@ import (
  * }
  */
 func rotateRight(head *ListNode, k int) *ListNode {
-	return nil
+	if k == 0 || head == nil || head.Next == nil {
+		return head
+	}
+	n := 1
+	iter := head
+	for iter.Next != nil {
+		iter = iter.Next
+		n++
+	}
+	add := n - k%n
+	if add == n {
+		return head
+	}
+	iter.Next = head
+	for add > 0 {
+		iter = iter.Next
+		add--
+	}
+	ret := iter.Next
+	iter.Next = nil
+	return ret
 }
