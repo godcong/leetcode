0c6fbb95c8d99597d529683b64c8765ce17ecec8
godcong <jumbycc@163.com>
2021-03-29 20:09:02 +0800

    update

---
+++ code/0190/0190_test.go
@@ -0,0 +1,30 @@
+package _0190
+
+import "testing"
+
+func Test_reverseBits(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				num: 0b00000010100101000001111010011100,
+			},
+			want: 0b00111001011110000010100101000000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBits(tt.args.num); got != tt.want {
+				t.Errorf("reverseBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
