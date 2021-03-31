815f681986f8f7364bb0bee815bb100f15e15615
godcong <jumbycc@163.com>
2021-03-31 19:58:54 +0800

    update

---
+++ code/0090/0090_test.go
@@ -0,0 +1,34 @@
+package _0090
+
+import (
+	. "github.com/godcong/leetcode/common"
+	"reflect"
+	"testing"
+)
+
+func Test_subsetsWithDup(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: StrToIntArray("[1,2,2]"),
+			},
+			want: StrToIntArrArray("[[],[1],[1,2],[1,2,2],[2],[2,2]]"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subsetsWithDup(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subsetsWithDup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
