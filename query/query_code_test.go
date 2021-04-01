46072b0866ad345ab7d442f5d095dabdbdc99822
godcong <jumbycc@163.com>
2021-04-01 19:56:39 +0800

    update

---
+++ query/query_code_test.go
@@ -0,0 +1,19 @@
+package query
+
+import "testing"
+
+func TestRequest(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Request()
+		})
+	}
+}
