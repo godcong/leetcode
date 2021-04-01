6ed64e39753f6bc0a1c689856236fb08e09fc884
godcong <jumbycc@163.com>
2021-04-01 19:33:51 +0800

    update

---
+++ code/1006/1006_test.go
@@ -0,0 +1,30 @@
+package _1006
+
+import "testing"
+
+func Test_clumsy(t *testing.T) {
+	type args struct {
+		N int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				N: 4,
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := clumsy(tt.args.N); got != tt.want {
+				t.Errorf("clumsy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
