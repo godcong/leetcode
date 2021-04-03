735bba50973a7d3bf67521a5d7560ec4955fcb39
godcong <jumbycc@163.com>
2021-04-03 20:48:35 +0800

    update

--- code/1143/11433.go
+++ code/1143/11433.go
@@ -34,5 +34,23 @@ package _1143
 text1 和 text2 仅由小写英文字符组成。
 */
 func longestCommonSubsequence(text1 string, text2 string) int {
-	return 0
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i, c1 := range text1 {
+		for j, c2 := range text2 {
+			if c1 == c2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = dp[i][j+1]
+				if dp[i][j+1] < dp[i+1][j] {
+					dp[i+1][j+1] = dp[i+1][j]
+				}
+
+			}
+		}
+	}
+	return dp[m][n]
 }
