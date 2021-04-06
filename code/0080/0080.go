2a534d4b4ebd878f0a4e8f05eb74c57d91050545
godcong <jumbycc@163.com>
2021-04-06 23:33:29 +0800

    update

--- code/0080/0080.go
+++ code/0080/0080.go
@@ -45,18 +45,12 @@ for (int i = 0; i < len; i++) {
 nums 已按升序排列
 */
 func removeDuplicates(nums []int) int {
-	n := len(nums)
-	if n <= 2 {
-		return n
-	}
-	slow, fast := 2, 2
-	for fast < n {
-		if nums[slow-2] != nums[fast] {
-			nums[slow] = nums[fast]
-			slow++
+	var index int
+	for i := range nums {
+		if index < 2 || nums[i] != nums[index-2] {
+			nums[index] = nums[i]
+			index++
 		}
-		fast++
 	}
-	return slow
-
+	return index
 }
