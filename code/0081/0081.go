7faec2890cfeb82eeb241289409f4f0cfcdf10af
godcong <jumbycc@163.com>
2021-04-07 21:23:44 +0800

    update

--- code/0081/0081.go
+++ code/0081/0081.go
@@ -34,5 +34,35 @@ package _0081
 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
 */
 func search(nums []int, target int) bool {
+	n := len(nums)
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		return nums[0] == target
+	}
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[l] == nums[mid] && nums[mid] == nums[r] {
+			l++
+			r--
+		} else if nums[l] <= nums[mid] {
+			if nums[l] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[n-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
 	return false
 }
