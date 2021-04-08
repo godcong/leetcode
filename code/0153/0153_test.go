ef3586888ed9ef8fc4a43ced81545cd567fe5ec4
godcong <jumbycc@163.com>
2021-04-08 20:13:31 +0800

    update

---
+++ code/0153/0153_test.go
@@ -0,0 +1,33 @@
+package _0153
+
+import (
+	. "github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_findMin(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: StrToIntArray("[3,4,5,1,2]"),
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin(tt.args.nums); got != tt.want {
+				t.Errorf("findMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
