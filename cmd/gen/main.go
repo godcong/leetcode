9c1b5a9b26a888f9779b35ac5ed5c7d35cf50471
godcong <jumbycc@163.com>
2021-04-09 10:13:05 +0800

    update

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -2,11 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	wd,_ := os.Getwd()
-	fmt.Println(wd)
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	name := ""
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
 
+	if name == "" {
+		return
+	}
+
+	codePath := filepath.Join(wd, "code", name)
+
+	_, err = os.Stat(codePath)
+	if err == nil {
+		fmt.Println(codePath, " is already exist")
+		os.Exit(0)
+	}
+
+	_ = os.MkdirAll(codePath, 0755)
+
+	err = ioutil.WriteFile(filepath.Join(codePath, name+".go"), packageHeader(name), 0755)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func packageHeader(name string) []byte {
+	return []byte(fmt.Sprintf("package _%v", name))
 }
