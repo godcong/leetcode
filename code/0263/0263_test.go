1b4ca1b28941b31d5c34f1f84129182f0ab978d2
godcong <jumbycc@163.com>
2021-04-10 10:17:11 +0800

    update

--- code/0263/0263_test.go
+++ code/0263/0263_test.go
@@ -19,6 +19,13 @@ func Test_isUgly(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				n: 8,
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
