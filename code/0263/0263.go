95314335b228dbb7a41d0b7ad80c9f1a27f9bee3
godcong <jumbycc@163.com>
2021-04-10 10:16:38 +0800

    update

--- code/0263/0263.go
+++ code/0263/0263.go
@@ -34,3 +34,16 @@ package _0263
 
 -231 <= n <= 231 - 1
 */
+var factors = []int{2, 3, 5}
+
+func isUgly(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	for _, f := range factors {
+		for n%f == 0 {
+			n /= f
+		}
+	}
+	return n == 1
+}
