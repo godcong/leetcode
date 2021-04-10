939fdca059ecf034ef7f2dde7b78ea6078416129
godcong <jumbycc@163.com>
2021-04-10 10:16:57 +0800

    update

---
+++ code/0263/0263_test.go
@@ -0,0 +1,30 @@
+package _0263
+
+import "testing"
+
+func Test_isUgly(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				n: 6,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isUgly(tt.args.n); got != tt.want {
+				t.Errorf("isUgly() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
