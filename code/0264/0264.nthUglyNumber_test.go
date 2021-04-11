bb0c58c759928c4e61176e49314006fc54585bc1
godcong <jumbycc@163.com>
2021-04-11 14:08:42 +0800

    update

--- code/0264/0264.nthUglyNumber_test.go
+++ code/0264/0264.nthUglyNumber_test.go
@@ -19,6 +19,13 @@ func Test_nthUglyNumber(t *testing.T) {
 			},
 			want: 12,
 		},
+		{
+			name: "",
+			args: args{
+				n: 1,
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
