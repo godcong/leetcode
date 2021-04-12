3931f7d411811c2e97a25454a1d314e024732efa
godcong <jumbycc@163.com>
2021-04-12 13:44:40 +0800

    update

---
+++ code/0179/0179_test.go
@@ -0,0 +1,40 @@
+package _0179
+
+import (
+	. "github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_largestNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: StrToIntArray(" [10,2]"),
+			},
+			want: "210",
+		},
+		{
+			name: "",
+			args: args{
+				nums: StrToIntArray("[3,30,34,5,9]"),
+			},
+			want: "9534330",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestNumber(tt.args.nums); got != tt.want {
+				t.Errorf("largestNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
