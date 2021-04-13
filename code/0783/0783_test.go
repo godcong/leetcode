98c20756aa550c99b5d02d16e27001ae81bb1757
godcong <jumbycc@163.com>
2021-04-13 13:48:16 +0800

    update

---
+++ code/0783/0783_test.go
@@ -0,0 +1,26 @@
+package _0783
+
+import (
+	. "github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_minDiffInBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minDiffInBST(tt.args.root); got != tt.want {
+				t.Errorf("minDiffInBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
