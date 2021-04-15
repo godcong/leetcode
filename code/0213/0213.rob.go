e06ab2ac6d0c67040798827afbaeb0b376821588
godcong <jumbycc@163.com>
2021-04-15 20:31:04 +0800

    update

--- code/0213/0213.rob.go
+++ code/0213/0213.rob.go
@@ -30,7 +30,7 @@ package _0213
 1 <= nums.length <= 100
 0 <= nums[i] <= 1000
 */
-func _rob(nums []int) int {
+func robTheft(nums []int) int {
 	first, second := nums[0], max(nums[0], nums[1])
 	for _, v := range nums[2:] {
 		first, second = second, max(first+v, second)
@@ -46,7 +46,7 @@ func rob(nums []int) int {
 	if n == 2 {
 		return max(nums[0], nums[1])
 	}
-	return max(_rob(nums[:n-1]), _rob(nums[1:]))
+	return max(robTheft(nums[:n-1]), robTheft(nums[1:]))
 }
 
 func max(a, b int) int {
