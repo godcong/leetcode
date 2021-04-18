8ab33d091e44486ce4d62cedc3e00ecb8587502d
godcong <jumbycc@163.com>
2021-04-18 14:33:25 +0800

    update

---
+++ code/0026/0026_test.go
@@ -0,0 +1,25 @@
+package _0026
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := removeDuplicates(tt.args.nums); got != tt.want {
+					t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
