b29bc9882ecd71444c33c97c8e7d7e74147acf08
godcong <jumbycc@163.com>
2021-04-20 21:41:51 +0800

    update

---
+++ code/0028/0028_test.go
@@ -0,0 +1,34 @@
+package _0028
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				haystack: "hello",
+				needle:   "ll",
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+					t.Errorf("strStr() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
