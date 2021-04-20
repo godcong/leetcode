57108edf29cca308c9eedc514eeca756a0752436
godcong <jumbycc@163.com>
2021-04-20 21:40:39 +0800

    update

--- code/0028/0028.go
+++ code/0028/0028.go
@@ -35,3 +35,16 @@ package _0028
 0 <= haystack.length, needle.length <= 5 * 104
 haystack 和 needle 仅由小写英文字符组成
 */
+func strStr(haystack, needle string) int {
+	n, m := len(haystack), len(needle)
+outer:
+	for i := 0; i+m <= n; i++ {
+		for j := range needle {
+			if haystack[i+j] != needle[j] {
+				continue outer
+			}
+		}
+		return i
+	}
+	return -1
+}
