1351ea384c81ed592f05fd29672b213c9da9d961
godcong <jumbycc@163.com>
2021-04-21 10:31:57 +0800

    update

--- code/0091/0091.go
+++ code/0091/0091.go
@@ -50,5 +50,16 @@ package _0091
 s 只包含数字，并且可能包含前导零。
 */
 func numDecodings(s string) int {
-	return 0
+	n := len(s)
+	f := make([]int, n+1)
+	f[0] = 1
+	for i := 1; i <= n; i++ {
+		if s[i-1] != '0' {
+			f[i] += f[i-1]
+		}
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0') <= 26) {
+			f[i] += f[i-2]
+		}
+	}
+	return f[n]
 }
