8425ac71074aa9ca30c60ab3854481a005d03949
godcong <jumbycc@163.com>
2021-04-21 10:32:15 +0800

    update

---
+++ code/0091/0091_test.go
@@ -0,0 +1,32 @@
+package _0091
+
+import "testing"
+
+func Test_numDecodings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s: "12",
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := numDecodings(tt.args.s); got != tt.want {
+					t.Errorf("numDecodings() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
