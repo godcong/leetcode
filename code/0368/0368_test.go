0468d70eeeefc0337b363847090fd2b9df16e53f
godcong <jumbycc@163.com>
2021-04-23 14:34:34 +0800

    update

---
+++ code/0368/0368_test.go
@@ -0,0 +1,28 @@
+package _0368
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_largestDivisibleSubset(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := largestDivisibleSubset(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("largestDivisibleSubset() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
