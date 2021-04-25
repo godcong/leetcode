00b0e1dbc690fe85c19753704568c4e3499fb07d
godcong <jumbycc@163.com>
2021-04-25 21:05:40 +0800

    update

--- code/0897/0897_test.go
+++ code/0897/0897_test.go
@@ -24,6 +24,13 @@ func Test_increasingBST(t *testing.T) {
 			},
 			want: StrToTreeNode("[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]"),
 		},
+		{
+			name: "",
+			args: args{
+				root: StrToTreeNode("[5,1,7]"),
+			},
+			want: StrToTreeNode("[1,null,5,null,7]"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(
