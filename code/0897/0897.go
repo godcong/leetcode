dc617371fee2a348c5fb7c3981eef79df1b7c4c3
godcong <jumbycc@163.com>
2021-04-25 21:04:11 +0800

    update

--- code/0897/0897.go
+++ code/0897/0897.go
@@ -1,5 +1,9 @@
 package _0897
 
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
 /*
 897. 递增顺序搜索树
 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
@@ -23,3 +27,31 @@ package _0897
 树中节点数的取值范围是 [1, 100]
 0 <= Node.val <= 1000
 */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func increasingBST(root *TreeNode) *TreeNode {
+	vals := []int{}
+	var inorder func(*TreeNode)
+	inorder = func(node *TreeNode) {
+		if node != nil {
+			inorder(node.Left)
+			vals = append(vals, node.Val)
+			inorder(node.Right)
+		}
+	}
+	inorder(root)
+
+	dummyNode := &TreeNode{}
+	curNode := dummyNode
+	for _, val := range vals {
+		curNode.Right = &TreeNode{Val: val}
+		curNode = curNode.Right
+	}
+	return dummyNode.Right
+}
