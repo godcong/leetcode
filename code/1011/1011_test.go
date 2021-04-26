e17831046be8d66f94b39d729df9cc15abeef6a6
godcong <jumbycc@163.com>
2021-04-26 19:26:35 +0800

    update

---
+++ code/1011/1011_test.go
@@ -0,0 +1,35 @@
+package _1011
+
+import (
+	"github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_shipWithinDays(t *testing.T) {
+	type args struct {
+		weights []int
+		D       int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				weights: common.StrToIntArray("[1,2,3,4,5,6,7,8,9,10]"),
+				D:       5,
+			},
+			want: 15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shipWithinDays(tt.args.weights, tt.args.D); got != tt.want {
+				t.Errorf("shipWithinDays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
