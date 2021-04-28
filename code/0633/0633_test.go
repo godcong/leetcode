05beb853a621fc16c39efb7baaeacf2881ccfa16
godcong <jumbycc@163.com>
2021-04-28 11:21:37 +0800

    update

---
+++ code/0633/0633_test.go
@@ -0,0 +1,30 @@
+package _0633
+
+import "testing"
+
+func Test_judgeSquareSum(t *testing.T) {
+	type args struct {
+		c int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				c: 5,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := judgeSquareSum(tt.args.c); got != tt.want {
+				t.Errorf("judgeSquareSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
