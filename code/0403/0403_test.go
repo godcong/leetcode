6b5563f2eeb7a2ae3b14b3539de8db166cfe1c62
godcong <jumbycc@163.com>
2021-04-29 11:38:09 +0800

    update

---
+++ code/0403/0403_test.go
@@ -0,0 +1,33 @@
+package _0403
+
+import (
+	"github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_canCross(t *testing.T) {
+	type args struct {
+		stones []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				stones: common.StrToIntArray("[0,1,3,5,6,8,12,17]"),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canCross(tt.args.stones); got != tt.want {
+				t.Errorf("canCross() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
