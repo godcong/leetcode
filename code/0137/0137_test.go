187c80f58f7a70671f7efc834d10bb961714540a
godcong <jumbycc@163.com>
2021-04-30 19:26:08 +0800

    update

---
+++ code/0137/0137_test.go
@@ -0,0 +1,35 @@
+package _0137
+
+import (
+	"github.com/godcong/leetcode/common"
+	"testing"
+)
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: common.StrToIntArray("[2,2,3,2]"),
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := singleNumber(tt.args.nums); got != tt.want {
+					t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
