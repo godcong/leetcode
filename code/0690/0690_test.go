b53e903903115e38c61c45c4090285b4f1cf642f
godcong <jumbycc@163.com>
2021-05-01 13:58:18 +0800

    update

---
+++ code/0690/0690_test.go
@@ -0,0 +1,32 @@
+package _0690
+
+import "testing"
+
+func Test_getImportance(t *testing.T) {
+	type args struct {
+		employees []*Employee
+		id        int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				employees: nil,
+				id:        0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getImportance(tt.args.employees, tt.args.id); got != tt.want {
+				t.Errorf("getImportance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
