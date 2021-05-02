eab9675526d6c2e0911f3df05940b6c352d2d0d5
godcong <jumbycc@163.com>
2021-05-02 14:08:25 +0800

    update

--- code/0554/0554.go
+++ code/0554/0554.go
@@ -30,5 +30,19 @@ n == wall.length
 1 <= wall[i][j] <= 231 - 1
 */
 func leastBricks(wall [][]int) int {
-	return 0
+	cnt := map[int]int{}
+	for _, widths := range wall {
+		sum := 0
+		for _, width := range widths[:len(widths)-1] {
+			sum += width
+			cnt[sum]++
+		}
+	}
+	maxCnt := 0
+	for _, c := range cnt {
+		if c > maxCnt {
+			maxCnt = c
+		}
+	}
+	return len(wall) - maxCnt
 }
