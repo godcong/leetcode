a2266a438d95fe102212d9979bd827c0ae887a25
godcong <jumbycc@163.com>
2021-05-03 14:12:09 +0800

    update

---
+++ code/0007/0007_test.go
@@ -0,0 +1,30 @@
+package _0007
+
+import "testing"
+
+func Test_reverse(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				x: 123,
+			},
+			want: 321,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverse(tt.args.x); got != tt.want {
+				t.Errorf("reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
