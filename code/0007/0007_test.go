97edebf699a56d417a016dff9c858585e8c5dd10
godcong <jumbycc@163.com>
2021-05-03 14:12:43 +0800

    update

--- code/0007/0007_test.go
+++ code/0007/0007_test.go
@@ -26,6 +26,13 @@ func Test_reverse(t *testing.T) {
 			},
 			want: -321,
 		},
+		{
+			name: "",
+			args: args{
+				x: 120,
+			},
+			want: 21,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
