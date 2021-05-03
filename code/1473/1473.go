adec47117623ac95873c4a2910aef9f24f4e0327
godcong <jumbycc@163.com>
2021-05-04 01:11:49 +0800

    update

--- code/1473/1473.go
+++ code/1473/1473.go
@@ -1,5 +1,7 @@
 package _1473
 
+import "math"
+
 /*
 1473. 粉刷房子 III
 在一个小城市里，有 m 个房子排成一排，你需要给每个房子涂上 n 种颜色之一（颜色编号为 1 到 n ）。有的房子去年夏天已经涂过颜色了，所以这些房子不需要被重新涂色。
@@ -49,6 +51,80 @@ n == cost[i].length
 0 <= houses[i] <= n
 1 <= cost[i][j] <= 10^4
 */
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func minCost(houses []int, cost [][]int, m int, n int, target int) int {
-	return 0
+	dp0 := make([][]int, m+1)
+	dp1 := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp0[i] = make([]int, n+1)
+		dp1[i] = make([]int, n+1)
+	}
+	maxVal := math.MaxInt32
+	resetMat(dp0, maxVal)
+	for i := 0; i <= n; i++ {
+		dp0[0][i] = 0
+	}
+	for k := 1; k <= target; k++ {
+		resetMat(dp1, maxVal)
+		for i := k; i <= m; i++ {
+			minC1 := 1
+			minV1 := dp0[i-1][1]
+			minV2 := maxVal
+			for j := 2; j <= n; j++ {
+				if minV1 > dp0[i-1][j] {
+					minV2 = minV1
+					minV1 = dp0[i-1][j]
+					minC1 = j
+				} else if minV2 > dp0[i-1][j] {
+					minV2 = dp0[i-1][j]
+				}
+			}
+			cc := houses[i-1]
+			if cc != 0 {
+				if cc == minC1 {
+					dp1[i][cc] = min(dp1[i-1][cc], minV2)
+				} else {
+					dp1[i][cc] = min(dp1[i-1][cc], minV1)
+				}
+				continue
+			}
+			for j := 1; j <= n; j++ {
+				mj := minV1
+				if minC1 == j {
+					mj = minV2
+				}
+				dp1[i][j] = min(dp1[i-1][j], mj)
+				if dp1[i][j] < maxVal {
+					dp1[i][j] += cost[i-1][j-1]
+				}
+			}
+		}
+		dp0, dp1 = dp1, dp0
+	}
+	minV := maxVal
+	for j := 1; j <= n; j++ {
+		if minV > dp0[m][j] {
+			minV = dp0[m][j]
+		}
+	}
+	if minV == maxVal {
+		return -1
+	}
+	return minV
+}
+
+func resetMat(dp [][]int, v int) {
+	m := len(dp)
+	n := len(dp[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			dp[i][j] = v
+		}
+	}
 }
