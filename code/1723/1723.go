451c4f2c508c287290718917d28698e354ca6515
godcong <jumbycc@163.com>
2021-05-08 11:57:59 +0800

    update

--- code/1723/1723.go
+++ code/1723/1723.go
@@ -1,5 +1,7 @@
 package _1723
 
+import "sort"
+
 /*
 1723. 完成所有工作的最短时间
 给你一个整数数组 jobs ，其中 jobs[i] 是完成第 i 项工作要花费的时间。
@@ -31,5 +33,37 @@ package _1723
 1 <= jobs[i] <= 107
 */
 func minimumTimeRequired(jobs []int, k int) int {
-	return 0
+	n := len(jobs)
+	sort.Sort(sort.Reverse(sort.IntSlice(jobs)))
+	l, r := jobs[0], 0
+	for _, v := range jobs {
+		r += v
+	}
+	return l + sort.Search(
+		r-l, func(limit int) bool {
+			limit += l
+			workloads := make([]int, k)
+			var backtrack func(int) bool
+			backtrack = func(idx int) bool {
+				if idx == n {
+					return true
+				}
+				cur := jobs[idx]
+				for i := range workloads {
+					if workloads[i]+cur <= limit {
+						workloads[i] += cur
+						if backtrack(idx + 1) {
+							return true
+						}
+						workloads[i] -= cur
+					}
+					if workloads[i] == 0 || workloads[i]+cur == limit {
+						break
+					}
+				}
+				return false
+			}
+			return backtrack(0)
+		},
+	)
 }
