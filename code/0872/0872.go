cc100e43efef43bf3310aeb360d4e035b4f2dc08
godcong <jumbycc@163.com>
2021-05-10 12:11:40 +0800

    update

--- code/0872/0872.go
+++ code/0872/0872.go
@@ -58,5 +58,30 @@ import (
  * }
  */
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
-	return false
+	vals := []int{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Left == nil && node.Right == nil {
+			vals = append(vals, node.Val)
+			return
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root1)
+	vals1 := append([]int(nil), vals...)
+	vals = []int{}
+	dfs(root2)
+	if len(vals) != len(vals1) {
+		return false
+	}
+	for i, v := range vals1 {
+		if v != vals[i] {
+			return false
+		}
+	}
+	return true
 }
