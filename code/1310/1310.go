8ed3d8f85ec26b4eae1eb633cadb2ba6a4db9e21
godcong <jumbycc@163.com>
2021-05-12 15:25:30 +0800

    update

--- code/1310/1310.go
+++ code/1310/1310.go
@@ -40,5 +40,13 @@ queries[i].length == 2
 0 <= queries[i][0] <= queries[i][1] < arr.length
 */
 func xorQueries(arr []int, queries [][]int) []int {
-	return nil
+	xors := make([]int, len(arr)+1)
+	for i, v := range arr {
+		xors[i+1] = xors[i] ^ v
+	}
+	ans := make([]int, len(queries))
+	for i, q := range queries {
+		ans[i] = xors[q[0]] ^ xors[q[1]+1]
+	}
+	return ans
 }
