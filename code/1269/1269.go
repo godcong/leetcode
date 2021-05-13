d0980a93272ad232dd9712a51c3cc58e5779384a
godcong <jumbycc@163.com>
2021-05-13 15:36:18 +0800

    update

--- code/1269/1269.go
+++ code/1269/1269.go
@@ -40,5 +40,30 @@ package _1269
 1 <= arrLen <= 10^6
 */
 func numWays(steps int, arrLen int) int {
-	return 0
+	const mod = 1e9 + 7
+	maxColumn := min(arrLen-1, steps)
+	dp := make([][]int, steps+1)
+	for i := range dp {
+		dp[i] = make([]int, maxColumn+1)
+	}
+	dp[0][0] = 1
+	for i := 1; i <= steps; i++ {
+		for j := 0; j <= maxColumn; j++ {
+			dp[i][j] = dp[i-1][j]
+			if j-1 >= 0 {
+				dp[i][j] = (dp[i][j] + dp[i-1][j-1]) % mod
+			}
+			if j+1 <= maxColumn {
+				dp[i][j] = (dp[i][j] + dp[i-1][j+1]) % mod
+			}
+		}
+	}
+	return dp[steps][0]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
