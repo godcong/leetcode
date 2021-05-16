66ed7e71e6c9dc6b0f51e13b88d50b9970f30c2f
godcong <jumbycc@163.com>
2021-05-16 15:43:52 +0800

    update

--- code/0993/0993.go
+++ code/0993/0993.go
@@ -48,5 +48,37 @@ import (
  * }
  */
 func isCousins(root *TreeNode, x int, y int) bool {
-	return false
+	var xParent, yParent *TreeNode
+	var xDepth, yDepth int
+	var xFound, yFound bool
+
+	var dfs func(node, parent *TreeNode, depth int)
+	dfs = func(node, parent *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+
+		if node.Val == x {
+			xParent, xDepth, xFound = parent, depth, true
+		} else if node.Val == y {
+			yParent, yDepth, yFound = parent, depth, true
+		}
+
+		// 如果两个节点都找到了，就可以提前退出遍历
+		// 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+		if xFound && yFound {
+			return
+		}
+
+		dfs(node.Left, node, depth+1)
+
+		if xFound && yFound {
+			return
+		}
+
+		dfs(node.Right, node, depth+1)
+	}
+	dfs(root, nil, 0)
+
+	return xDepth == yDepth && xParent != yParent
 }
