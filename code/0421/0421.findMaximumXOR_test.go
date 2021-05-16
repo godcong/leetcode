2f1bd43663e4310a2609f5146b3310f283ac7557
godcong <jumbycc@163.com>
2021-05-16 13:44:30 +0800

    update

--- code/0421/0421.findMaximumXOR_test.go
+++ code/0421/0421.findMaximumXOR_test.go
@@ -23,6 +23,13 @@ func Test_findMaximumXOR(t *testing.T) {
 			},
 			want: 28,
 		},
+		{
+			name: "",
+			args: args{
+				nums: common.StrToIntArray("[0]"),
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
