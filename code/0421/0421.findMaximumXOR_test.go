f7b43ced37adcfd035283c15cfa51e2a81d223b7
godcong <jumbycc@163.com>
2021-05-16 13:44:10 +0800

    update

---
+++ code/0421/0421.findMaximumXOR_test.go
@@ -0,0 +1,34 @@
+package _0421
+
+import (
+	"testing"
+
+	"github.com/godcong/leetcode/common"
+)
+
+func Test_findMaximumXOR(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums: common.StrToIntArray("[3,10,5,25,2,8]"),
+			},
+			want: 28,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaximumXOR(tt.args.nums); got != tt.want {
+				t.Errorf("findMaximumXOR() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
