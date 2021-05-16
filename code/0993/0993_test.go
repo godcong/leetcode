d45d16ecf66890d568a4b7e990a1308497a5a20c
godcong <jumbycc@163.com>
2021-05-16 15:45:11 +0800

    update

---
+++ code/0993/0993_test.go
@@ -0,0 +1,38 @@
+package _0993
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isCousins(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		x    int
+		y    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				root: StrToTreeNode("[1,2,3,4]"),
+				x:    4,
+				y:    3,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isCousins(tt.args.root, tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("isCousins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
