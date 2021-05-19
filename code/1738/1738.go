02c977ed83b51433d7d0c031c05ead44f288be26
godcong <jumbycc@163.com>
2021-05-19 10:59:29 +0800

    update

--- code/1738/1738.go
+++ code/1738/1738.go
@@ -1,5 +1,9 @@
 package _1738
 
+import (
+	"sort"
+)
+
 /*
 1738. 找出第 K 大的异或坐标值
 给你一个二维矩阵 matrix 和一个整数 k ，矩阵大小为 m x n 由非负整数组成。
@@ -41,5 +45,25 @@ n == matrix[i].length
 1 <= k <= m * n
 */
 func kthLargestValue(matrix [][]int, k int) int {
-	return 0
+	m, n := len(matrix), len(matrix[0])
+	ar := make([]int, 0, m*n)
+
+	for j, c, r := 0, 0, matrix[0]; j < n; j++ {
+		v := r[j]
+		c ^= v
+		r[j] = c
+		ar = append(ar, c)
+	}
+
+	for i := 1; i < m; i++ {
+		for j, c, r, rp := 0, 0, matrix[i], matrix[i-1]; j < n; j++ {
+			v := r[j]
+			c ^= v
+			v = c ^ rp[j]
+			r[j] = v
+			ar = append(ar, v)
+		}
+	}
+	sort.Slice(ar, func(i, j int) bool { return ar[i] > ar[j] })
+	return ar[k-1]
 }
