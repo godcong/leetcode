26eaaa4c93321baeb8709143f1aa8a7b9d7fb676
godcong <jumbycc@163.com>
2021-05-20 11:15:49 +0800

    update

---
+++ code/0692/0692.topKFrequent_test.go
@@ -0,0 +1,27 @@
+package _0692
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_topKFrequent(t *testing.T) {
+	type args struct {
+		words []string
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := topKFrequent(tt.args.words, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("topKFrequent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
