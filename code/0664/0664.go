a2e1f14393e828eedb2f5fa072bb24f84a1c1dcf
godcong <jumbycc@163.com>
2021-05-24 13:36:49 +0800

    update

--- code/0664/0664.go
+++ code/0664/0664.go
@@ -1,5 +1,9 @@
 package _0664
 
+import (
+	"math"
+)
+
 /*
 664. 奇怪的打印机
 有台奇怪的打印机有以下两个特殊要求：
@@ -27,5 +31,30 @@ package _0664
 s 由小写英文字母组成
 */
 func strangePrinter(s string) int {
-	return 0
+	n := len(s)
+	f := make([][]int, n)
+	for i := range f {
+		f[i] = make([]int, n)
+	}
+	for i := n - 1; i >= 0; i-- {
+		f[i][i] = 1
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				f[i][j] = f[i][j-1]
+			} else {
+				f[i][j] = math.MaxInt64
+				for k := i; k < j; k++ {
+					f[i][j] = min(f[i][j], f[i][k]+f[k+1][j])
+				}
+			}
+		}
+	}
+	return f[0][n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
