d57be62486f344787e5649986de6a54c1c562959
godcong <jumbycc@163.com>
2021-05-25 11:33:45 +0800

    update

---
+++ code/1787/1787.minChanges_test.go
@@ -0,0 +1,26 @@
+package _1787
+
+import (
+	"testing"
+)
+
+func Test_minChanges(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minChanges(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("minChanges() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
