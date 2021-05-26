f28f6dedc3a5a06a436701edd1faa379141a1564
godcong <jumbycc@163.com>
2021-05-26 11:42:34 +0800

    update

---
+++ code/1190/1190_test.go
@@ -0,0 +1,25 @@
+package _1190
+
+import (
+	"testing"
+)
+
+func Test_reverseParentheses(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseParentheses(tt.args.s); got != tt.want {
+				t.Errorf("reverseParentheses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
