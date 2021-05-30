eec1745dabf0c3a9037578c609247a98ce0a9f88
godcong <jumbycc@163.com>
2021-05-30 11:36:40 +0800

    update

---
+++ code/0231/0231_test.go
@@ -0,0 +1,25 @@
+package _0231
+
+import (
+	"testing"
+)
+
+func Test_isPowerOfTwo(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfTwo(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
