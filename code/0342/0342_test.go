da5691f4face2957f6b92ecc5da3ac15e8894945
godcong <jumbycc@163.com>
2021-05-31 11:39:50 +0800

    update

---
+++ code/0342/0342_test.go
@@ -0,0 +1,25 @@
+package _0342
+
+import (
+	"testing"
+)
+
+func Test_isPowerOfFour(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfFour(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfFour() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
