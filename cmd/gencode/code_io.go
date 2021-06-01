eebb3061870f24f06816f435d581f7998be22720
godcong <jumbycc@163.com>
2021-06-01 21:39:55 +0800

    update

--- cmd/gencode/code_io.go
+++ cmd/gencode/code_io.go
@@ -1,20 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
-func genCodeDir(name string) {
+func getWorkPath(name string) string {
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
-	}
-
-	if name == "" {
-		return
 	}
 
 	codePath := filepath.Join(wd, "code", name)
@@ -24,19 +21,38 @@ func genCodeDir(name string) {
 		fmt.Println(codePath, " is already exist")
 		os.Exit(0)
 	}
+	return codePath
+}
+
+func genCodeDir(name string) {
+
+	if name == "" {
+		return
+	}
+
+	codePath := getWorkPath(name)
 
 	_ = os.MkdirAll(codePath, 0755)
 
-	err = ioutil.WriteFile(filepath.Join(codePath, name+".go"), packageHeader(name), 0755)
+	err := ioutil.WriteFile(filepath.Join(codePath, name+".go"), packageHeader(name), 0755)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func writeCodeJSON(code Code) {
+	name := code.Data.Question.Questionid
+	indent, err := json.MarshalIndent(code, "", " ")
+	if err != nil {
+		return
+	}
+	codePath := getWorkPath(name)
+	err = ioutil.WriteFile(filepath.Join(codePath, name+".json"), indent, 0755)
+	if err != nil {
+		panic(err)
+	}
 
 }
-
 
 func packageHeader(name string) []byte {
 	return []byte(fmt.Sprintf("package _%v", name))
