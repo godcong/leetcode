0ef46e1d8b372f8aef7627bcda5e0078cacce86f
godcong <jumbycc@163.com>
2021-06-02 11:37:05 +0800

    update

--- cmd/gen/code.go
+++ cmd/gen/code.go
@@ -1,58 +1,89 @@
 package main
 
 type Code struct {
-	Data struct {
-		Question struct {
-			Questionid            string        `json:"questionId"`
-			Questionfrontendid    string        `json:"questionFrontendId"`
-			Categorytitle         string        `json:"categoryTitle"`
-			Boundtopicid          int           `json:"boundTopicId"`
-			Title                 string        `json:"title"`
-			Titleslug             string        `json:"titleSlug"`
-			Content               string        `json:"content"`
-			Translatedtitle       string        `json:"translatedTitle"`
-			Translatedcontent     string        `json:"translatedContent"`
-			Ispaidonly            bool          `json:"isPaidOnly"`
-			Difficulty            string        `json:"difficulty"`
-			Likes                 int           `json:"likes"`
-			Dislikes              int           `json:"dislikes"`
-			Isliked               interface{}   `json:"isLiked"`
-			Similarquestions      string        `json:"similarQuestions"`
-			Contributors          []interface{} `json:"contributors"`
-			Langtovalidplayground string        `json:"langToValidPlayground"`
-			Topictags             []struct {
-				Name           string `json:"name"`
-				Slug           string `json:"slug"`
-				Translatedname string `json:"translatedName"`
-				Typename       string `json:"__typename"`
-			} `json:"topicTags"`
-			Companytagstats interface{} `json:"companyTagStats"`
-			Codesnippets    []struct {
-				Lang     string `json:"lang"`
-				Langslug string `json:"langSlug"`
-				Code     string `json:"code"`
-				Typename string `json:"__typename"`
-			} `json:"codeSnippets"`
-			Stats             string        `json:"stats"`
-			Hints             []string      `json:"hints"`
-			Solution          interface{}   `json:"solution"`
-			Status            interface{}   `json:"status"`
-			Sampletestcase    string        `json:"sampleTestCase"`
-			Metadata          string        `json:"metaData"`
-			Judgeravailable   bool          `json:"judgerAvailable"`
-			Judgetype         string        `json:"judgeType"`
-			Mysqlschemas      []interface{} `json:"mysqlSchemas"`
-			Enableruncode     bool          `json:"enableRunCode"`
-			Envinfo           string        `json:"envInfo"`
-			Book              interface{}   `json:"book"`
-			Issubscribed      bool          `json:"isSubscribed"`
-			Isdailyquestion   bool          `json:"isDailyQuestion"`
-			Dailyrecordstatus string        `json:"dailyRecordStatus"`
-			Editortype        string        `json:"editorType"`
-			Ugcquestionid     interface{}   `json:"ugcQuestionId"`
-			Style             string        `json:"style"`
-			Exampletestcases  string        `json:"exampleTestcases"`
-			Typename          string        `json:"__typename"`
-		} `json:"question"`
-	} `json:"data"`
+	Data Data `json:"data"`
 }
+
+type Data struct {
+	DailyQuestionRecords []DailyQuestionRecord `json:"dailyQuestionRecords,omitempty"`
+	Question             DataQuestion          `json:"question,omitempty"`
+}
+
+type DailyQuestionRecord struct {
+	Date       string                      `json:"date"`
+	Question   DailyQuestionRecordQuestion `json:"question"`
+	UserStatus string                      `json:"userStatus"`
+	Typename   string                      `json:"__typename"`
+}
+
+type DailyQuestionRecordQuestion struct {
+	QuestionID         string `json:"questionId"`
+	QuestionFrontendID string `json:"questionFrontendId"`
+	QuestionTitle      string `json:"questionTitle"`
+	QuestionTitleSlug  string `json:"questionTitleSlug"`
+	TranslatedTitle    string `json:"translatedTitle"`
+	Typename           string `json:"__typename"`
+}
+
+type DataQuestion struct {
+	QuestionID            string        `json:"questionId"`
+	QuestionFrontendID    string        `json:"questionFrontendId"`
+	CategoryTitle         string        `json:"categoryTitle"`
+	BoundTopicID          int64         `json:"boundTopicId"`
+	Title                 string        `json:"title"`
+	TitleSlug             string        `json:"titleSlug"`
+	Content               string        `json:"content"`
+	TranslatedTitle       string        `json:"translatedTitle"`
+	TranslatedContent     string        `json:"translatedContent"`
+	IsPaidOnly            bool          `json:"isPaidOnly"`
+	Difficulty            string        `json:"difficulty"`
+	Likes                 int64         `json:"likes"`
+	Dislikes              int64         `json:"dislikes"`
+	IsLiked               interface{}   `json:"isLiked"`
+	SimilarQuestions      string        `json:"similarQuestions"`
+	Contributors          []interface{} `json:"contributors"`
+	LangToValidPlayground string        `json:"langToValidPlayground"`
+	TopicTags             []TopicTag    `json:"topicTags"`
+	CompanyTagStats       interface{}   `json:"companyTagStats"`
+	CodeSnippets          []CodeSnippet `json:"codeSnippets"`
+	Stats                 string        `json:"stats"`
+	Hints                 []interface{} `json:"hints"`
+	Solution              interface{}   `json:"solution"`
+	Status                string        `json:"status"`
+	SampleTestCase        string        `json:"sampleTestCase"`
+	MetaData              string        `json:"metaData"`
+	JudgerAvailable       bool          `json:"judgerAvailable"`
+	JudgeType             string        `json:"judgeType"`
+	MysqlSchemas          []interface{} `json:"mysqlSchemas"`
+	EnableRunCode         bool          `json:"enableRunCode"`
+	EnvInfo               string        `json:"envInfo"`
+	Book                  interface{}   `json:"book"`
+	IsSubscribed          bool          `json:"isSubscribed"`
+	IsDailyQuestion       bool          `json:"isDailyQuestion"`
+	DailyRecordStatus     string        `json:"dailyRecordStatus"`
+	EditorType            string        `json:"editorType"`
+	UgcQuestionID         interface{}   `json:"ugcQuestionId"`
+	Style                 string        `json:"style"`
+	ExampleTestcases      string        `json:"exampleTestcases"`
+	Typename              string        `json:"__typename"`
+}
+
+type CodeSnippet struct {
+	Lang     string   `json:"lang"`
+	LangSlug string   `json:"langSlug"`
+	Code     string   `json:"code"`
+	Typename Typename `json:"__typename"`
+}
+
+type TopicTag struct {
+	Name           string `json:"name"`
+	Slug           string `json:"slug"`
+	TranslatedName string `json:"translatedName"`
+	Typename       string `json:"__typename"`
+}
+
+type Typename string
+
+const (
+	CodeSnippetNode Typename = "CodeSnippetNode"
+)
