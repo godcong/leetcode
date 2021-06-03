69f739d9fdc28b2547c1420afa4bb81e85ebccfc
godcong <jumbycc@163.com>
2021-06-03 14:30:02 +0800

    update

--- code/0525/0525.go
+++ code/0525/0525.go
@@ -1,5 +1,27 @@
 package _0525
 
 func findMaxLength(nums []int) int {
+	m := make(map[int]int, 0)
+	m[0] = -1
+	maxLen, count := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 1 {
+			count++
+		} else {
+			count--
+		}
+		if v, exist := m[count]; exist {
+			maxLen = max(maxLen, i-v)
+		} else {
+			m[count] = i
+		}
+	}
+	return maxLen
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
