3031a9da0c307585a2e4e2aa38021c859469aa2a
godcong <jumbycc@163.com>
2021-06-03 14:30:26 +0800

    update

---
+++ code/0525/0525.findMaxLength_test.go
@@ -0,0 +1,25 @@
+package _0525
+
+import (
+	"testing"
+)
+
+func Test_findMaxLength(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaxLength(tt.args.nums); got != tt.want {
+				t.Errorf("findMaxLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
