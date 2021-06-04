c6e15114a792511d61f86af4bc32a2600f3b5fed
godcong <jumbycc@163.com>
2021-06-04 11:56:48 +0800

    update

--- code/0160/0160.go
+++ code/0160/0160.go
@@ -12,5 +12,14 @@ import (
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	vis := map[*ListNode]bool{}
+	for tmp := headA; tmp != nil; tmp = tmp.Next {
+		vis[tmp] = true
+	}
+	for tmp := headB; tmp != nil; tmp = tmp.Next {
+		if vis[tmp] {
+			return tmp
+		}
+	}
 	return nil
 }
