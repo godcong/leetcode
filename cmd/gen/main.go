657cfecfe4629275f71383ca07d6702b1c4546f2
godcong <jumbycc@163.com>
2021-06-09 11:25:59 +0800

    update

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"github.com/godcong/leetcode/query"
@@ -20,4 +21,6 @@ func main() {
 	if err := query.WriteMarkdownTo(filepath.Join(path, "README.md"), code); err != nil {
 		return
 	}
+	fmt.Println("Code Generated:", code.Data.Question.QuestionFrontendID)
+
 }
