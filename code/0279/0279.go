893d2da4789857fff2bcada1f6e9f05e322a6e8d
godcong <jumbycc@163.com>
2021-06-11 14:38:33 +0800

    update

--- code/0279/0279.go
+++ code/0279/0279.go
@@ -1,5 +1,33 @@
 package _0279
 
+import (
+	"math"
+)
+
+func isPerfectSquare(x int) bool {
+	y := int(math.Sqrt(float64(x)))
+	return y*y == x
+}
+
+func checkAnswer4(x int) bool {
+	for x%4 == 0 {
+		x /= 4
+	}
+	return x%8 == 7
+}
+
 func numSquares(n int) int {
-	return 0
+	if isPerfectSquare(n) {
+		return 1
+	}
+	if checkAnswer4(n) {
+		return 4
+	}
+	for i := 1; i*i <= n; i++ {
+		j := n - i*i
+		if isPerfectSquare(j) {
+			return 2
+		}
+	}
+	return 3
 }
