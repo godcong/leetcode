9903e5b15782494ce112237fb53ac073b538d1f8
godcong <jumbycc@163.com>
2021-06-13 11:38:26 +0800

    update

---
+++ code/0278/0278.firstBadVersion_test.go
@@ -0,0 +1,25 @@
+package _0278
+
+import (
+	"testing"
+)
+
+func Test_firstBadVersion(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstBadVersion(tt.args.n); got != tt.want {
+				t.Errorf("firstBadVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
