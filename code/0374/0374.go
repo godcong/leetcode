90b8946d4ae6166d88731bddbf303ae01a123650
godcong <jumbycc@163.com>
2021-06-14 11:39:08 +0800

    update

---
+++ code/0374/0374.go
@@ -0,0 +1,14 @@
+package _0374
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+    
+}
