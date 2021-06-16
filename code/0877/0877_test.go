8c1c8b1e8de138d74e2869d9df8e9b314c58f898
godcong <jumbycc@163.com>
2021-06-16 11:28:00 +0800

    update

---
+++ code/0877/0877_test.go
@@ -0,0 +1,25 @@
+package _0877
+
+import (
+	"testing"
+)
+
+func Test_stoneGame(t *testing.T) {
+	type args struct {
+		piles []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := stoneGame(tt.args.piles); got != tt.want {
+				t.Errorf("stoneGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
