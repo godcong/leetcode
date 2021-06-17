8daf364fc26e71e37e97c3dd8968a59298dcf73c
godcong <jumbycc@163.com>
2021-06-17 11:50:14 +0800

    update

---
+++ code/0065/0065.isNumber_test.go
@@ -0,0 +1,25 @@
+package _0065
+
+import (
+	"testing"
+)
+
+func Test_isNumber(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isNumber(tt.args.s); got != tt.want {
+				t.Errorf("isNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
