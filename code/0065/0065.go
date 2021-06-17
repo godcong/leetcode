f0158c8919693e95dfd7d38294ec51ec30fd1d73
godcong <jumbycc@163.com>
2021-06-17 11:48:50 +0800

    update

--- code/0065/0065.go
+++ code/0065/0065.go
@@ -1,5 +1,88 @@
 package _0065
 
+const (
+	StateInitial = iota
+	StateIntSign
+	StateInteger
+	StatePoint
+	StatePointWithoutInt
+	StateFraction
+	StateExp
+	StateExpSign
+	StateExpNumber
+	StateEnd
+	StateMax
+)
+
+const (
+	CharNumber = iota
+	CharExp
+	CharPoint
+	CharSign
+	CharIllegal
+	CharMax
+)
+
+func toCharType(ch byte) int {
+	switch ch {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return CharNumber
+	case 'e', 'E':
+		return CharExp
+	case '.':
+		return CharPoint
+	case '+', '-':
+		return CharSign
+	default:
+		return CharIllegal
+	}
+}
+
 func isNumber(s string) bool {
-	return false
+	transfer := [StateMax][CharMax]int{
+		StateInitial: {
+			CharNumber: StateInteger,
+			CharPoint:  StatePointWithoutInt,
+			CharSign:   StateIntSign,
+		},
+		StateIntSign: {
+			CharNumber: StateInteger,
+			CharPoint:  StatePointWithoutInt,
+		},
+		StateInteger: {
+			CharNumber: StateInteger,
+			CharExp:    StateExp,
+			CharPoint:  StatePoint,
+		},
+		StatePoint: {
+			CharNumber: StateFraction,
+			CharExp:    StateExp,
+		},
+		StatePointWithoutInt: {
+			CharNumber: StateFraction,
+		},
+		StateFraction: {
+			CharNumber: StateFraction,
+			CharExp:    StateExp,
+		},
+		StateExp: {
+			CharNumber: StateExpNumber,
+			CharSign:   StateExpSign,
+		},
+		StateExpSign: {
+			CharNumber: StateExpNumber,
+		},
+		StateExpNumber: {
+			CharNumber: StateExpNumber,
+		},
+	}
+	state := StateInitial
+	for i := 0; i < len(s); i++ {
+		typ := toCharType(s[i])
+		state = transfer[state][typ]
+		if state == StateInitial {
+			return false
+		}
+	}
+	return state == StateInteger || state == StatePoint || state == StateFraction || state == StateExpNumber || state == StateEnd
 }
