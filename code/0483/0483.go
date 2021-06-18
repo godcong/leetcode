d5fc0e26ce0a6209169dccd638284f8b0a14c206
godcong <jumbycc@163.com>
2021-06-18 17:09:59 +0800

    update

--- code/0483/0483.go
+++ code/0483/0483.go
@@ -1,5 +1,24 @@
 package _0483
 
+import (
+	"math"
+	"math/bits"
+	"strconv"
+)
+
 func smallestGoodBase(n string) string {
-	return ""
+	nVal, _ := strconv.Atoi(n)
+	mMax := bits.Len(uint(nVal)) - 1
+	for m := mMax; m > 1; m-- {
+		k := int(math.Pow(float64(nVal), 1/float64(m)))
+		mul, sum := 1, 1
+		for i := 0; i < m; i++ {
+			mul *= k
+			sum += mul
+		}
+		if sum == nVal {
+			return strconv.Itoa(k)
+		}
+	}
+	return strconv.Itoa(nVal - 1)
 }
