ecf198c31306a4578b1cb44cab623cd93acee9eb
godcong <jumbycc@163.com>
2021-06-19 11:13:30 +0800

    update

--- code/1239/1239.maximum-length-of-a-concatenated-string-with-unique-characters.go
+++ code/1239/1239.maximum-length-of-a-concatenated-string-with-unique-characters.go
@@ -1,5 +1,43 @@
 package _1239
 
+import (
+	"math/bits"
+)
+
 func maxLength(arr []string) int {
-	return 0
+	var masks []int
+	var ans int
+outer:
+	for _, s := range arr {
+		mask := 0
+		for _, ch := range s {
+			ch -= 'a'
+			if mask>>ch&1 > 0 {
+				continue outer
+			}
+			mask |= 1 << ch
+		}
+		masks = append(masks, mask)
+	}
+
+	var backtrack func(int, int)
+	backtrack = func(pos, mask int) {
+		if pos == len(masks) {
+			ans = max(ans, bits.OnesCount(uint(mask)))
+			return
+		}
+		if mask&masks[pos] == 0 {
+			backtrack(pos+1, mask|masks[pos])
+		}
+		backtrack(pos+1, mask)
+	}
+	backtrack(0, 0)
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
