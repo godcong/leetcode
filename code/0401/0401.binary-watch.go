846524cd81433ff4b993c8e6a4c5e37a7bb5c6f1
godcong <jumbycc@163.com>
2021-06-21 11:41:01 +0800

    update

--- code/0401/0401.binary-watch.go
+++ code/0401/0401.binary-watch.go
@@ -1,5 +1,18 @@
 package _0401
 
+import (
+	"fmt"
+	"math/bits"
+)
+
 func readBinaryWatch(turnedOn int) []string {
-	return nil
+	var ans []string
+	for h := uint8(0); h < 12; h++ {
+		for m := uint8(0); m < 60; m++ {
+			if bits.OnesCount8(h)+bits.OnesCount8(m) == turnedOn {
+				ans = append(ans, fmt.Sprintf("%d:%02d", h, m))
+			}
+		}
+	}
+	return ans
 }
