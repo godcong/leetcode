af6de45c417b61034736aa4b21d4a4a782359702
godcong <jumbycc@163.com>
2021-06-23 10:51:46 +0800

    update

---
+++ code/剑指_Offer_15/剑指_Offer_15.er-jin-zhi-zhong-1de-ge-shu-lcof_test.go
@@ -0,0 +1,25 @@
+package _剑指_Offer_15
+
+import (
+	"testing"
+)
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
