6ffcd2bb53f939c3f0ca4709eaf8d15530a225fb
godcong <jumbycc@163.com>
2021-06-26 12:35:51 +0800

    update

---
+++ code/0773/0773.sliding-puzzle_test.go
@@ -0,0 +1,25 @@
+package _0773
+
+import (
+	"testing"
+)
+
+func Test_slidingPuzzle(t *testing.T) {
+	type args struct {
+		board [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := slidingPuzzle(tt.args.board); got != tt.want {
+				t.Errorf("slidingPuzzle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
