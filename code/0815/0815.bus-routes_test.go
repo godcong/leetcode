ded4ba9904cbb4b69e7a9606740c946cc8ef7a52
godcong <jumbycc@163.com>
2021-06-28 20:50:44 +0800

    update

---
+++ code/0815/0815.bus-routes_test.go
@@ -0,0 +1,27 @@
+package _0815
+
+import (
+	"testing"
+)
+
+func Test_numBusesToDestination(t *testing.T) {
+	type args struct {
+		routes [][]int
+		source int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numBusesToDestination(tt.args.routes, tt.args.source, tt.args.target); got != tt.want {
+				t.Errorf("numBusesToDestination() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
