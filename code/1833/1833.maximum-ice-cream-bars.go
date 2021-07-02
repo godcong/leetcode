9ee11972e1296eff6772049710a6644e65bcf215
godcong <jumbycc@163.com>
2021-07-02 18:02:35 +0800

    update

--- code/1833/1833.maximum-ice-cream-bars.go
+++ code/1833/1833.maximum-ice-cream-bars.go
@@ -1,5 +1,33 @@
 package _1833
 
+import (
+	"runtime/debug"
+)
+
+const max int = 1e5
+
 func maxIceCream(costs []int, coins int) int {
-	return 0
+	ans := 0
+	freq := [max + 1]int{}
+	for _, c := range costs {
+		freq[c]++
+	}
+	for i := 1; i <= max && coins >= i; i++ {
+		if freq[i] == 0 {
+			continue
+		}
+		c := min(freq[i], coins/i)
+		ans += c
+		coins -= i * c
+	}
+	return ans
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func init() { debug.SetGCPercent(-1) }
