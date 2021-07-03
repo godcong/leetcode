415548ad0301da6b4efcbf1c9fa1abfdf0256de0
godcong <jumbycc@163.com>
2021-07-03 16:33:17 +0800

    update

---
+++ code/0451/0451.sort-characters-by-frequency_test.go
@@ -0,0 +1,25 @@
+package _0451
+
+import (
+	"testing"
+)
+
+func Test_frequencySort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := frequencySort(tt.args.s); got != tt.want {
+				t.Errorf("frequencySort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
