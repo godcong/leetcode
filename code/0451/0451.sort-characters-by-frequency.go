efe9571dd6b1e95b45237cff71ec35d3840cf613
godcong <jumbycc@163.com>
2021-07-03 16:31:16 +0800

    update

--- code/0451/0451.sort-characters-by-frequency.go
+++ code/0451/0451.sort-characters-by-frequency.go
@@ -1,5 +1,29 @@
 package _0451
 
+import (
+	"bytes"
+	"sort"
+)
+
 func frequencySort(s string) string {
-	return ""
+	cnt := map[byte]int{}
+	for i := range s {
+		cnt[s[i]]++
+	}
+
+	type pair struct {
+		ch  byte
+		cnt int
+	}
+	pairs := make([]pair, 0, len(cnt))
+	for k, v := range cnt {
+		pairs = append(pairs, pair{k, v})
+	}
+	sort.Slice(pairs, func(i, j int) bool { return pairs[i].cnt > pairs[j].cnt })
+
+	ans := make([]byte, 0, len(s))
+	for _, p := range pairs {
+		ans = append(ans, bytes.Repeat([]byte{p.ch}, p.cnt)...)
+	}
+	return string(ans)
 }
