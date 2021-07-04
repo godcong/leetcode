1d047b4b24730913923f96417984ed159626f03c
godcong <jumbycc@163.com>
2021-07-04 16:34:38 +0800

    update

--- code/0645/0645.set-mismatch.go
+++ code/0645/0645.set-mismatch.go
@@ -1,5 +1,24 @@
 package _0645
 
+import (
+	"sort"
+)
+
 func findErrorNums(nums []int) []int {
-	return nil
+	ans := make([]int, 2)
+	sort.Ints(nums)
+	pre := 0
+	for _, v := range nums {
+		if v == pre {
+			ans[0] = v
+		} else if v-pre > 1 {
+			ans[1] = pre + 1
+		}
+		pre = v
+	}
+	n := len(nums)
+	if nums[n-1] != n {
+		ans[1] = n
+	}
+	return ans
 }
