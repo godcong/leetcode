cfb0bb349053b18f2c4a8b65a127583a28c07ff3
godcong <jumbycc@163.com>
2021-07-04 16:38:43 +0800

    update

---
+++ code/0645/0645.set-mismatch_test.go
@@ -0,0 +1,26 @@
+package _0645
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findErrorNums(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findErrorNums(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findErrorNums() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
