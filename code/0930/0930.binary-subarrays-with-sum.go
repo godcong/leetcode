1e28c8edd871f067f6528d40c54f8b4a7664ca56
godcong <jumbycc@163.com>
2021-07-08 21:00:45 +0800

    update

--- code/0930/0930.binary-subarrays-with-sum.go
+++ code/0930/0930.binary-subarrays-with-sum.go
@@ -1,5 +1,13 @@
 package _0930
 
 func numSubarraysWithSum(nums []int, goal int) int {
-	return 0
+	cnt := map[int]int{}
+	ans := 0
+	sum := 0
+	for _, num := range nums {
+		cnt[sum]++
+		sum += num
+		ans += cnt[sum-goal]
+	}
+	return ans
 }
