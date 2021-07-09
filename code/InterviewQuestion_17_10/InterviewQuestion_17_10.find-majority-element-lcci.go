a1bd41f07280d33bdbb5d339b25b6b5449aea314
godcong <jumbycc@163.com>
2021-07-09 21:26:01 +0800

    update

--- code/InterviewQuestion_17_10/InterviewQuestion_17_10.find-majority-element-lcci.go
+++ code/InterviewQuestion_17_10/InterviewQuestion_17_10.find-majority-element-lcci.go
@@ -1,5 +1,26 @@
 package _InterviewQuestion_17_10
 
 func majorityElement(nums []int) int {
-	return 0
+	candidate := -1
+	count := 0
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+	count = 0
+	for _, num := range nums {
+		if num == candidate {
+			count++
+		}
+	}
+	if count*2 > len(nums) {
+		return candidate
+	}
+	return -1
 }
