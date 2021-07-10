87af31244e6c7b99fa612f639190805c79afa433
godcong <jumbycc@163.com>
2021-07-10 20:47:10 +0800

    update

---
+++ code/0981/0981.time-based-key-value-store.go
@@ -0,0 +1,29 @@
+package _0981
+
+type TimeMap struct {
+
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() TimeMap {
+
+}
+
+
+func (this *TimeMap) Set(key string, value string, timestamp int)  {
+
+}
+
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+
+}
+
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
