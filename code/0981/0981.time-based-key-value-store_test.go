a3b7656ba450a47108d8f37fb5cf7228509ca0c9
godcong <jumbycc@163.com>
2021-07-10 20:48:24 +0800

    update

---
+++ code/0981/0981.time-based-key-value-store_test.go
@@ -0,0 +1,22 @@
+package _0981
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	tests := []struct {
+		name string
+		want TimeMap
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
