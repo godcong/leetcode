60124244b6662624107679028e8bf8447085cf1b
godcong <jumbycc@163.com>
2021-07-14 11:36:22 +0800

    update

--- code/1818/1818.minimum-absolute-sum-difference.go
+++ code/1818/1818.minimum-absolute-sum-difference.go
@@ -1,5 +1,45 @@
 package _1818
 
+import (
+	"runtime/debug"
+)
+
 func minAbsoluteSumDiff(nums1 []int, nums2 []int) int {
-	return 0
+	sum := 0
+	m := 0
+	for i, n2 := range nums2 {
+		d := abs(nums1[i] - n2)
+		sum += d
+		if m < d {
+			t := 100001
+			for _, n1 := range nums1 {
+				t = min(t, abs(n1-n2))
+			}
+			m = max(m, d-t)
+		}
+	}
+	return (sum - m) % (1e9 + 7)
 }
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func init() { debug.SetGCPercent(-1) }
