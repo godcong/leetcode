ed20ccd0ce82570117f55422498ed757a5c557a8
godcong <jumbycc@163.com>
2021-07-15 11:37:43 +0800

    update

--- code/1846/1846.maximum-element-after-decreasing-and-rearranging.go
+++ code/1846/1846.maximum-element-after-decreasing-and-rearranging.go
@@ -1,5 +1,22 @@
 package _1846
 
+import (
+	"sort"
+)
+
 func maximumElementAfterDecrementingAndRearranging(arr []int) int {
-	return 0
+	n := len(arr)
+	sort.Ints(arr)
+	arr[0] = 1
+	for i := 1; i < n; i++ {
+		arr[i] = min(arr[i], arr[i-1]+1)
+	}
+	return arr[n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
