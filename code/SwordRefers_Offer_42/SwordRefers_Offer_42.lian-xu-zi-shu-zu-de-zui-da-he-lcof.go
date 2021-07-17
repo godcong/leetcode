c76d03bebbc24d3fbc40aa58e2c873874bbb07fe
godcong <jumbycc@163.com>
2021-07-17 11:22:27 +0800

    update

--- code/SwordRefers_Offer_42/SwordRefers_Offer_42.lian-xu-zi-shu-zu-de-zui-da-he-lcof.go
+++ code/SwordRefers_Offer_42/SwordRefers_Offer_42.lian-xu-zi-shu-zu-de-zui-da-he-lcof.go
@@ -1,5 +1,14 @@
 package _SwordRefers_Offer_42
 
 func maxSubArray(nums []int) int {
-	return 0
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
 }
