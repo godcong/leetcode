26f2942f0dfe60fa8a888e4057e2ea0e97cc8c9d
godcong <jumbycc@163.com>
2021-07-17 11:22:33 +0800

    update

---
+++ code/SwordRefers_Offer_42/SwordRefers_Offer_42.lian-xu-zi-shu-zu-de-zui-da-he-lcof_test.go
@@ -0,0 +1,25 @@
+package _SwordRefers_Offer_42
+
+import (
+	"testing"
+)
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
