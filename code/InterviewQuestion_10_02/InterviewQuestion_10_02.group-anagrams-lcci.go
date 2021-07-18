3e6a444e3abca72349f082c39be6341cd3a8028f
godcong <jumbycc@163.com>
2021-07-18 11:27:50 +0800

    update

--- code/InterviewQuestion_10_02/InterviewQuestion_10_02.group-anagrams-lcci.go
+++ code/InterviewQuestion_10_02/InterviewQuestion_10_02.group-anagrams-lcci.go
@@ -1,5 +1,17 @@
 package _InterviewQuestion_10_02
 
 func groupAnagrams(strs []string) [][]string {
-	return nil
+	mp := map[[26]int][]string{}
+	for _, str := range strs {
+		cnt := [26]int{}
+		for _, b := range str {
+			cnt[b-'a']++
+		}
+		mp[cnt] = append(mp[cnt], str)
+	}
+	ans := make([][]string, 0, len(mp))
+	for _, v := range mp {
+		ans = append(ans, v)
+	}
+	return ans
 }
