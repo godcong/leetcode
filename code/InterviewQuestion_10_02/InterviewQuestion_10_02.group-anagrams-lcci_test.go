de91588b9da79586958ef538044f09cb4abc502b
godcong <jumbycc@163.com>
2021-07-18 11:27:57 +0800

    update

---
+++ code/InterviewQuestion_10_02/InterviewQuestion_10_02.group-anagrams-lcci_test.go
@@ -0,0 +1,26 @@
+package _InterviewQuestion_10_02
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_groupAnagrams(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := groupAnagrams(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
