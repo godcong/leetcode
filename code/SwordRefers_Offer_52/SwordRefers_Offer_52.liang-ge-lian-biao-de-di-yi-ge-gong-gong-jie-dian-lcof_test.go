128c558a779100e60bda524d9b090325a479dbce
godcong <jumbycc@163.com>
2021-07-21 15:48:47 +0800

    update

---
+++ code/SwordRefers_Offer_52/SwordRefers_Offer_52.liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof_test.go
@@ -0,0 +1,29 @@
+package _SwordRefers_Offer_52
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_getIntersectionNode(t *testing.T) {
+	type args struct {
+		headA *ListNode
+		headB *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getIntersectionNode(tt.args.headA, tt.args.headB); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getIntersectionNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
