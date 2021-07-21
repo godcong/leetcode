362f297656c3a3e4a8c33f7a450e9cdc32e4dfc3
godcong <jumbycc@163.com>
2021-07-21 15:47:34 +0800

    update

---
+++ code/SwordRefers_Offer_52/SwordRefers_Offer_52.liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.go
@@ -0,0 +1,16 @@
+package _SwordRefers_Offer_52
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	return nil
+}
