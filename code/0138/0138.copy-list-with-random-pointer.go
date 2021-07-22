0916a95d6a8e85f6b5956ae804b1fb0c4b87995c
godcong <jumbycc@163.com>
2021-07-22 16:12:45 +0800

    update

---
+++ code/0138/0138.copy-list-with-random-pointer.go
@@ -0,0 +1,18 @@
+package _0138
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+	return nil
+}
