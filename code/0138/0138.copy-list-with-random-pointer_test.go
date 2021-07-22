d25457dccf6fb99bda9f426c110c8d12ab9d7cea
godcong <jumbycc@163.com>
2021-07-22 16:17:23 +0800

    update

---
+++ code/0138/0138.copy-list-with-random-pointer_test.go
@@ -0,0 +1,28 @@
+package _0138
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_copyRandomList(t *testing.T) {
+	type args struct {
+		head *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := copyRandomList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("copyRandomList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
