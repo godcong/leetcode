df282cd233bf853901bbd0bd0d68a03b3c5492d9
godcong <jumbycc@163.com>
2021-07-23 16:20:33 +0800

    update

--- code/1893/1893.check-if-all-the-integers-in-a-range-are-covered.go
+++ code/1893/1893.check-if-all-the-integers-in-a-range-are-covered.go
@@ -1,5 +1,17 @@
 package _1893
 
 func isCovered(ranges [][]int, left int, right int) bool {
-	return false
+	diff := [52]int{} // 差分数组
+	for _, r := range ranges {
+		diff[r[0]]++
+		diff[r[1]+1]--
+	}
+	cnt := 0 // 前缀和
+	for i := 1; i <= 50; i++ {
+		cnt += diff[i]
+		if cnt <= 0 && left <= i && i <= right {
+			return false
+		}
+	}
+	return true
 }
