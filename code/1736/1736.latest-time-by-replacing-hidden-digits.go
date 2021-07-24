3e816b41e6c698716e1b2932cb95c626de526917
godcong <jumbycc@163.com>
2021-07-24 18:42:59 +0800

    update

--- code/1736/1736.latest-time-by-replacing-hidden-digits.go
+++ code/1736/1736.latest-time-by-replacing-hidden-digits.go
@@ -1,5 +1,26 @@
 package _1736
 
 func maximumTime(time string) string {
-	return ""
+	t := []byte(time)
+	if t[0] == '?' {
+		if '4' <= t[1] && t[1] <= '9' {
+			t[0] = '1'
+		} else {
+			t[0] = '2'
+		}
+	}
+	if t[1] == '?' {
+		if t[0] == '2' {
+			t[1] = '3'
+		} else {
+			t[1] = '9'
+		}
+	}
+	if t[3] == '?' {
+		t[3] = '5'
+	}
+	if t[4] == '?' {
+		t[4] = '9'
+	}
+	return string(t)
 }
