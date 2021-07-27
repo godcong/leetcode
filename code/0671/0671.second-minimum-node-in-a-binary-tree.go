05e35fb85e15619982ca95c2b344fc0e688a2526
godcong <jumbycc@163.com>
2021-07-27 18:50:09 +0800

    update

---
+++ code/0671/0671.second-minimum-node-in-a-binary-tree.go
@@ -0,0 +1,17 @@
+package _0671
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findSecondMinimumValue(root *TreeNode) int {
+	return 0
+}
