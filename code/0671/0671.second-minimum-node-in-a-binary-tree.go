6cba9b9b03d8f41f7864ecdf4961b9eee3bea71d
godcong <jumbycc@163.com>
2021-07-27 18:50:44 +0800

    update

--- code/0671/0671.second-minimum-node-in-a-binary-tree.go
+++ code/0671/0671.second-minimum-node-in-a-binary-tree.go
@@ -13,5 +13,20 @@ import (
  * }
  */
 func findSecondMinimumValue(root *TreeNode) int {
-	return 0
+	ans := -1
+	rootVal := root.Val
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil || ans != -1 && node.Val >= ans {
+			return
+		}
+		if node.Val > rootVal {
+			ans = node.Val
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return ans
+
 }
