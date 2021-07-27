5dcdf3a6cae2abb976ee78a10bab4515c1ca0585
godcong <jumbycc@163.com>
2021-07-27 18:51:00 +0800

    update

---
+++ code/0671/0671.second-minimum-node-in-a-binary-tree_test.go
@@ -0,0 +1,27 @@
+package _0671
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findSecondMinimumValue(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findSecondMinimumValue(tt.args.root); got != tt.want {
+				t.Errorf("findSecondMinimumValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
