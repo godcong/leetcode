bc42accb3d4ea5995e1407c69e20f7a7a501facf
godcong <jumbycc@163.com>
2021-07-28 22:03:00 +0800

    update

--- code/0863/0863.all-nodes-distance-k-in-binary-tree.go
+++ code/0863/0863.all-nodes-distance-k-in-binary-tree.go
@@ -13,5 +13,40 @@ import (
  * }
  */
 func distanceK(root *TreeNode, target *TreeNode, k int) []int {
-	return nil
+	var ans []int
+	parents := map[int]*TreeNode{}
+	var findParents func(*TreeNode)
+	findParents = func(node *TreeNode) {
+		if node.Left != nil {
+			parents[node.Left.Val] = node
+			findParents(node.Left)
+		}
+		if node.Right != nil {
+			parents[node.Right.Val] = node
+			findParents(node.Right)
+		}
+	}
+	findParents(root)
+
+	var findAns func(*TreeNode, *TreeNode, int)
+	findAns = func(node, from *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+		if depth == k { // 将所有深度为 k 的结点的值计入结果
+			ans = append(ans, node.Val)
+			return
+		}
+		if node.Left != from {
+			findAns(node.Left, node, depth+1)
+		}
+		if node.Right != from {
+			findAns(node.Right, node, depth+1)
+		}
+		if parents[node.Val] != from {
+			findAns(parents[node.Val], node, depth+1)
+		}
+	}
+	findAns(target, nil, 0)
+	return ans
 }
