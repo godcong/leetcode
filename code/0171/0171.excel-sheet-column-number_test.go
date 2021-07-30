5f8b62e12b0b74622dc079a0995f805aec5b0eae
godcong <jumbycc@163.com>
2021-07-30 20:33:02 +0800

    update

---
+++ code/0171/0171.excel-sheet-column-number_test.go
@@ -0,0 +1,25 @@
+package _0171
+
+import (
+	"testing"
+)
+
+func Test_titleToNumber(t *testing.T) {
+	type args struct {
+		columnTitle string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := titleToNumber(tt.args.columnTitle); got != tt.want {
+				t.Errorf("titleToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
