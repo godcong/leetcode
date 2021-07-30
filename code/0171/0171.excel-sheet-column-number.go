f93c11cdc3d4786e91a39a7379ed3351a5985535
godcong <jumbycc@163.com>
2021-07-30 20:32:54 +0800

    update

--- code/0171/0171.excel-sheet-column-number.go
+++ code/0171/0171.excel-sheet-column-number.go
@@ -1,5 +1,12 @@
 package _0171
 
 func titleToNumber(columnTitle string) int {
-	return 0
+	number := 0
+	for i, multiple := len(columnTitle)-1, 1; i >= 0; i-- {
+		k := columnTitle[i] - 'A' + 1
+		number += int(k) * multiple
+		multiple *= 26
+	}
+	return number
+
 }
