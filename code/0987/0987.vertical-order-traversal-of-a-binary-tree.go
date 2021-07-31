b594c988b16d81ac41db654364fc7d73423994f2
godcong <jumbycc@163.com>
2021-07-31 20:35:02 +0800

    update

--- code/0987/0987.vertical-order-traversal-of-a-binary-tree.go
+++ code/0987/0987.vertical-order-traversal-of-a-binary-tree.go
@@ -1,8 +1,13 @@
 package _0987
 
 import (
+	"math"
+	"sort"
+
 	. "github.com/godcong/leetcode/common"
 )
+
+type data struct{ col, row, val int }
 
 /**
  * Definition for a binary tree node.
@@ -13,5 +18,31 @@ import (
  * }
  */
 func verticalTraversal(root *TreeNode) [][]int {
-	return nil
+	var nodes []data
+	var ans [][]int
+	var dfs func(*TreeNode, int, int)
+	dfs = func(node *TreeNode, row, col int) {
+		if node == nil {
+			return
+		}
+		nodes = append(nodes, data{col, row, node.Val})
+		dfs(node.Left, row+1, col-1)
+		dfs(node.Right, row+1, col+1)
+	}
+	dfs(root, 0, 0)
+
+	sort.Slice(nodes, func(i, j int) bool {
+		a, b := nodes[i], nodes[j]
+		return a.col < b.col || a.col == b.col && (a.row < b.row || a.row == b.row && a.val < b.val)
+	})
+
+	lastCol := math.MinInt32
+	for _, node := range nodes {
+		if node.col != lastCol {
+			lastCol = node.col
+			ans = append(ans, nil)
+		}
+		ans[len(ans)-1] = append(ans[len(ans)-1], node.val)
+	}
+	return ans
 }
