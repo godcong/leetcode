f15c46727ec94e1c704416030f40c165a5e94655
godcong <jumbycc@163.com>
2021-08-01 20:38:18 +0800

    update

---
+++ code/1337/1337.the-k-weakest-rows-in-a-matrix_test.go
@@ -0,0 +1,27 @@
+package _1337
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_kWeakestRows(t *testing.T) {
+	type args struct {
+		mat [][]int
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kWeakestRows(tt.args.mat, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("kWeakestRows() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
