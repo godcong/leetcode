0825a10eb55d54873d5f4b917ba4cf23c6af8fa7
godcong <jumbycc@163.com>
2021-08-02 20:40:35 +0800

    update

--- code/0743/0743.network-delay-time.go
+++ code/0743/0743.network-delay-time.go
@@ -1,5 +1,62 @@
 package _0743
 
+import (
+	"math"
+)
+
 func networkDelayTime(times [][]int, n int, k int) int {
-	return 0
+	const inf = math.MaxInt64 / 2
+	g := make([][]int, n)
+	ans := 0
+	for i := range g {
+		g[i] = make([]int, n)
+		for j := range g[i] {
+			g[i][j] = inf
+		}
+	}
+	for _, t := range times {
+		x, y := t[0]-1, t[1]-1
+		g[x][y] = t[2]
+	}
+
+	dist := make([]int, n)
+	for i := range dist {
+		dist[i] = inf
+	}
+	dist[k-1] = 0
+	used := make([]bool, n)
+	for i := 0; i < n; i++ {
+		x := -1
+		for y, u := range used {
+			if !u && (x == -1 || dist[y] < dist[x]) {
+				x = y
+			}
+		}
+		used[x] = true
+		for y, time := range g[x] {
+			dist[y] = min(dist[y], dist[x]+time)
+		}
+	}
+
+	for _, d := range dist {
+		if d == inf {
+			return -1
+		}
+		ans = max(ans, d)
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
