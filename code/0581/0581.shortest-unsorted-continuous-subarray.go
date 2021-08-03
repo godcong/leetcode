2f92db7eff6dae81842d60f6a40edb23deb54bde
godcong <jumbycc@163.com>
2021-08-03 21:56:16 +0800

    update

--- code/0581/0581.shortest-unsorted-continuous-subarray.go
+++ code/0581/0581.shortest-unsorted-continuous-subarray.go
@@ -1,5 +1,21 @@
 package _0581
 
+import (
+	"sort"
+)
+
 func findUnsortedSubarray(nums []int) int {
-	return 0
+	if sort.IntsAreSorted(nums) {
+		return 0
+	}
+	numsSorted := append([]int(nil), nums...)
+	sort.Ints(numsSorted)
+	left, right := 0, len(nums)-1
+	for nums[left] == numsSorted[left] {
+		left++
+	}
+	for nums[right] == numsSorted[right] {
+		right--
+	}
+	return right - left + 1
 }
