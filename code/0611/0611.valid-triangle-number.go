454550223d54b8d7902aaa5752b694176a46bf57
godcong <jumbycc@163.com>
2021-08-04 21:28:57 +0800

    update

--- code/0611/0611.valid-triangle-number.go
+++ code/0611/0611.valid-triangle-number.go
@@ -1,5 +1,24 @@
 package _0611
 
+import (
+	"sort"
+)
+
 func triangleNumber(nums []int) int {
-	return 0
+	sort.Sort(sort.IntSlice(nums))
+	res := 0
+	for i := len(nums) - 1; i >= 2; i-- {
+		left := 0
+		right := i - 1
+		for left < right {
+			if nums[left]+nums[right] > nums[i] {
+				// left .... right
+				res += right - left
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return res
 }
