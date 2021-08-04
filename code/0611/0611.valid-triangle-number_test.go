77f447731c6d72f8afaba11d84d59b785aa48f36
godcong <jumbycc@163.com>
2021-08-04 21:29:06 +0800

    update

---
+++ code/0611/0611.valid-triangle-number_test.go
@@ -0,0 +1,25 @@
+package _0611
+
+import (
+	"testing"
+)
+
+func Test_triangleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := triangleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("triangleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
