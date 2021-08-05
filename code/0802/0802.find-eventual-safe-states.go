698b5ae2200e6a258eb0b35fd154a26f8a361e39
godcong <jumbycc@163.com>
2021-08-05 21:55:08 +0800

    update

--- code/0802/0802.find-eventual-safe-states.go
+++ code/0802/0802.find-eventual-safe-states.go
@@ -1,5 +1,27 @@
 package _0802
 
 func eventualSafeNodes(graph [][]int) []int {
-	return nil
+	n := len(graph)
+	var ans []int
+	color := make([]int, n)
+	var safe func(int) bool
+	safe = func(x int) bool {
+		if color[x] > 0 {
+			return color[x] == 2
+		}
+		color[x] = 1
+		for _, y := range graph[x] {
+			if !safe(y) {
+				return false
+			}
+		}
+		color[x] = 2
+		return true
+	}
+	for i := 0; i < n; i++ {
+		if safe(i) {
+			ans = append(ans, i)
+		}
+	}
+	return ans
 }
