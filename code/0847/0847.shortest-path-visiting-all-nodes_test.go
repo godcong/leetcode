bc81044dbdf921a430e90284ee4f8dc0bc9437bb
godcong <jumbycc@163.com>
2021-08-06 11:53:36 +0800

    update

---
+++ code/0847/0847.shortest-path-visiting-all-nodes_test.go
@@ -0,0 +1,25 @@
+package _0847
+
+import (
+	"testing"
+)
+
+func Test_shortestPathLength(t *testing.T) {
+	type args struct {
+		graph [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shortestPathLength(tt.args.graph); got != tt.want {
+				t.Errorf("shortestPathLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
