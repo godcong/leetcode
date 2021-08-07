28d767f6d58d68cc62cd3d9ee8648a34389476b5
godcong <jumbycc@163.com>
2021-08-07 11:55:44 +0800

    update

---
+++ code/0457/0457.circular-array-loop_test.go
@@ -0,0 +1,25 @@
+package _0457
+
+import (
+	"testing"
+)
+
+func Test_circularArrayLoop(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := circularArrayLoop(tt.args.nums); got != tt.want {
+				t.Errorf("circularArrayLoop() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
