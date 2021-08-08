3b54ae4cf7a228e375dc63cdb7693b26f03d38a6
godcong <jumbycc@163.com>
2021-08-08 11:57:26 +0800

    update

---
+++ code/1137/1137.n-th-tribonacci-number_test.go
@@ -0,0 +1,25 @@
+package _1137
+
+import (
+	"testing"
+)
+
+func Test_tribonacci(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tribonacci(tt.args.n); got != tt.want {
+				t.Errorf("tribonacci() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
