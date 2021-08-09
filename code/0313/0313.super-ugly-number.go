e5e2424362b1617323a73a0f6d58e53a3000e8f4
godcong <jumbycc@163.com>
2021-08-09 13:01:13 +0800

    update

--- code/0313/0313.super-ugly-number.go
+++ code/0313/0313.super-ugly-number.go
@@ -1,9 +1,30 @@
 package _0313
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
 )
 
 func nthSuperUglyNumber(n int, primes []int) int {
+	pIndex := make([]int, len(primes))
+	dp := make([]int, n)
+	dp[0] = 1
 
+	for i := 1; i < n; i++ {
+		minVal := math.MaxInt64
+		for j := range pIndex {
+			if dp[pIndex[j]]*primes[j] < minVal {
+				minVal = dp[pIndex[j]] * primes[j]
+			}
+		}
+
+		for j := range pIndex {
+			if dp[pIndex[j]]*primes[j] == minVal {
+				pIndex[j]++
+			}
+		}
+
+		dp[i] = minVal
+	}
+
+	return dp[n-1]
 }
