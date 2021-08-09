7069ece1e8a125e5baa81aa19e3624acaf1ba4cf
godcong <jumbycc@163.com>
2021-08-09 13:02:55 +0800

    update

---
+++ code/0001/0001.two-sum.go
@@ -0,0 +1,12 @@
+package _0001
+
+func twoSum(nums []int, target int) []int {
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
