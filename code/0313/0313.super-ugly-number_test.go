41ecea3e97ed80552087d4ab6952de8712e948a9
godcong <jumbycc@163.com>
2021-08-09 13:01:22 +0800

    update

---
+++ code/0313/0313.super-ugly-number_test.go
@@ -0,0 +1,26 @@
+package _0313
+
+import (
+	"testing"
+)
+
+func Test_nthSuperUglyNumber(t *testing.T) {
+	type args struct {
+		n      int
+		primes []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nthSuperUglyNumber(tt.args.n, tt.args.primes); got != tt.want {
+				t.Errorf("nthSuperUglyNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
