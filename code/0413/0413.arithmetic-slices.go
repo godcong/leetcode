d4f4ae452bb5cc9545fbe3e4a61b672315cc02f7
godcong <jumbycc@163.com>
2021-08-10 22:07:09 +0800

    update

--- code/0413/0413.arithmetic-slices.go
+++ code/0413/0413.arithmetic-slices.go
@@ -1,9 +1,20 @@
 package _0413
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func numberOfArithmeticSlices(nums []int) int {
+	n := len(nums)
+	ans := 0
+	if n == 1 {
+		return ans
+	}
 
+	d, t := nums[0]-nums[1], 0
+	for i := 2; i < n; i++ {
+		if nums[i-1]-nums[i] == d {
+			t++
+		} else {
+			d, t = nums[i-1]-nums[i], 0
+		}
+		ans += t
+	}
+	return ans
 }
