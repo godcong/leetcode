b18287d120a29560c962491961ffda710afc24e8
godcong <jumbycc@163.com>
2021-08-11 21:32:42 +0800

    update

--- code/0446/0446.arithmetic-slices-ii-subsequence.go
+++ code/0446/0446.arithmetic-slices-ii-subsequence.go
@@ -1,5 +1,16 @@
 package _0446
 
 func numberOfArithmeticSlices(nums []int) int {
-	return 0
+	f := make([]map[int]int, len(nums))
+	ans := 0
+	for i, x := range nums {
+		f[i] = map[int]int{}
+		for j, y := range nums[:i] {
+			d := x - y
+			cnt := f[j][d]
+			ans += cnt
+			f[i][d] += cnt + 1
+		}
+	}
+	return ans
 }
