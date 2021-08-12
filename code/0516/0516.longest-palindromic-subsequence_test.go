f0bc95bd6cbee3a2642aebc4283493e09272c4c7
godcong <jumbycc@163.com>
2021-08-12 11:02:54 +0800

    update

---
+++ code/0516/0516.longest-palindromic-subsequence_test.go
@@ -0,0 +1,25 @@
+package _0516
+
+import (
+	"testing"
+)
+
+func Test_longestPalindromeSubseq(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindromeSubseq(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindromeSubseq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
