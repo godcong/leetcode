b78db58073d95ba52431f706a428767a233ff2ac
godcong <jumbycc@163.com>
2021-08-12 11:02:39 +0800

    update

--- code/0516/0516.longest-palindromic-subsequence.go
+++ code/0516/0516.longest-palindromic-subsequence.go
@@ -1,9 +1,26 @@
 package _0516
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func longestPalindromeSubseq(s string) int {
-
+	n := len(s)
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		m := 0
+		for j := i - 1; j >= 0; j-- {
+			t := dp[j]
+			if s[i] == s[j] {
+				dp[j] = m + 2
+			}
+			if t > m {
+				m = t
+			}
+		}
+		dp[i] = 1
+	}
+	max := 0
+	for _, v := range dp {
+		if v > max {
+			max = v
+		}
+	}
+	return max
 }
