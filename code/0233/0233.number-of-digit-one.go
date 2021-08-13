f076772c3a6232bd026a30f9c37a51bbd4dd7715
godcong <jumbycc@163.com>
2021-08-13 11:08:28 +0800

    update

--- code/0233/0233.number-of-digit-one.go
+++ code/0233/0233.number-of-digit-one.go
@@ -1,9 +1,24 @@
 package _0233
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countDigitOne(n int) int {
+	ans := 0
+	for k, mulk := 0, 1; n >= mulk; k++ {
+		ans += (n/(mulk*10))*mulk + min(max(n%(mulk*10)-mulk+1, 0), mulk)
+		mulk *= 10
+	}
+	return ans
+}
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
