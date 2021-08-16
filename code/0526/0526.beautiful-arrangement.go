f4a652c940d19184c79a3fbe5a2ce70c4c80550e
godcong <jumbycc@163.com>
2021-08-16 11:24:50 +0800

    update

--- code/0526/0526.beautiful-arrangement.go
+++ code/0526/0526.beautiful-arrangement.go
@@ -1,9 +1,31 @@
 package _0526
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countArrangement(n int) int {
+	vis := make([]bool, n+1)
+	match := make([][]int, n+1)
+	ans := 0
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			if i%j == 0 || j%i == 0 {
+				match[i] = append(match[i], j)
+			}
+		}
+	}
 
+	var backtrack func(int)
+	backtrack = func(index int) {
+		if index > n {
+			ans++
+			return
+		}
+		for _, x := range match[index] {
+			if !vis[x] {
+				vis[x] = true
+				backtrack(index + 1)
+				vis[x] = false
+			}
+		}
+	}
+	backtrack(1)
+	return ans
 }
