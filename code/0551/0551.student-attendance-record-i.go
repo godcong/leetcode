a0c5d53d41b04533f1258ae1f9ae66215c2f8b24
godcong <jumbycc@163.com>
2021-08-17 22:04:49 +0800

    update

--- code/0551/0551.student-attendance-record-i.go
+++ code/0551/0551.student-attendance-record-i.go
@@ -1,9 +1,22 @@
 package _0551
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func checkRecord(s string) bool {
-
+	absents, lates := 0, 0
+	for _, ch := range s {
+		if ch == 'A' {
+			absents++
+			if absents >= 2 {
+				return false
+			}
+		}
+		if ch == 'L' {
+			lates++
+			if lates >= 3 {
+				return false
+			}
+		} else {
+			lates = 0
+		}
+	}
+	return true
 }
