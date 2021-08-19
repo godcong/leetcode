869b9eb9fc183143305e6d5aa6579c98907e0197
godcong <jumbycc@163.com>
2021-08-19 21:21:11 +0800

    update

---
+++ code/0345/0345.reverse-vowels-of-a-string_test.go
@@ -0,0 +1,25 @@
+package _0345
+
+import (
+	"testing"
+)
+
+func Test_reverseVowels(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseVowels(tt.args.s); got != tt.want {
+				t.Errorf("reverseVowels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
