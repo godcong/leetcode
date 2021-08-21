9c1c078136a3b01d84298419e26e46ef9684c6a5
godcong <jumbycc@163.com>
2021-08-21 11:55:39 +0800

    update

--- code/0443/0443.string-compression.go
+++ code/0443/0443.string-compression.go
@@ -1,5 +1,25 @@
 package _0443
 
 func compress(chars []byte) int {
-	return 0
+	write, left := 0, 0
+	for read, ch := range chars {
+		if read == len(chars)-1 || ch != chars[read+1] {
+			chars[write] = ch
+			write++
+			num := read - left + 1
+			if num > 1 {
+				anchor := write
+				for ; num > 0; num /= 10 {
+					chars[write] = '0' + byte(num%10)
+					write++
+				}
+				s := chars[anchor:write]
+				for i, n := 0, len(s); i < n/2; i++ {
+					s[i], s[n-1-i] = s[n-1-i], s[i]
+				}
+			}
+			left = read + 1
+		}
+	}
+	return write
 }
