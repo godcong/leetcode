e2207175dc48f0730fdf173c3ccd3f03fc880139
godcong <jumbycc@163.com>
2021-08-21 11:56:40 +0800

    update

--- code/0443/0443.string-compression.go
+++ code/0443/0443.string-compression.go
@@ -1,25 +1,30 @@
 package _0443
 
+import (
+	"strconv"
+)
+
 func compress(chars []byte) int {
-	write, left := 0, 0
-	for read, ch := range chars {
-		if read == len(chars)-1 || ch != chars[read+1] {
-			chars[write] = ch
-			write++
-			num := read - left + 1
+	p1, p2, num, c := 0, 1, 1, chars[0]
+	for p2 = 1; p2 <= len(chars); p2++ {
+		if p2 == len(chars) || chars[p2] != c {
+			chars[p1] = c
+			p1++
 			if num > 1 {
-				anchor := write
-				for ; num > 0; num /= 10 {
-					chars[write] = '0' + byte(num%10)
-					write++
-				}
-				s := chars[anchor:write]
-				for i, n := 0, len(s); i < n/2; i++ {
-					s[i], s[n-1-i] = s[n-1-i], s[i]
+				ns := []byte(strconv.Itoa(num))
+				for i := 0; i < len(ns); i++ {
+					chars[p1] = ns[i]
+					p1++
 				}
 			}
-			left = read + 1
+			num = 1
+			if p2 < len(chars) {
+				c = chars[p2]
+			}
+
+		} else {
+			num++
 		}
 	}
-	return write
+	return p1
 }
