5456bd4ecef59cd322e0eda36d73810d04102f89
godcong <jumbycc@163.com>
2021-08-22 12:02:49 +0800

    update

--- code/0789/0789.escape-the-ghosts.go
+++ code/0789/0789.escape-the-ghosts.go
@@ -1,5 +1,24 @@
 package _0789
 
 func escapeGhosts(ghosts [][]int, target []int) bool {
-	return false
+	source := []int{0, 0}
+	distance := calDis(source, target)
+	for _, item := range ghosts {
+		ghostDis := calDis(item, target)
+		if ghostDis <= distance {
+			return false
+		}
+	}
+	return true
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func calDis(source, target []int) int {
+	return abs(target[0]-source[0]) + abs(target[1]-source[1])
 }
