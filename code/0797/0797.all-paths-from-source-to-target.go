60352a27406307bb1e25b8fb9cd25dcf2c1b0be8
godcong <jumbycc@163.com>
2021-08-25 14:58:25 +0800

    update

--- code/0797/0797.all-paths-from-source-to-target.go
+++ code/0797/0797.all-paths-from-source-to-target.go
@@ -1,9 +1,20 @@
 package _0797
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func allPathsSourceTarget(graph [][]int) [][]int {
-
+	stk := []int{0}
+	var ans [][]int
+	var dfs func(int)
+	dfs = func(x int) {
+		if x == len(graph)-1 {
+			ans = append(ans, append([]int(nil), stk...))
+			return
+		}
+		for _, y := range graph[x] {
+			stk = append(stk, y)
+			dfs(y)
+			stk = stk[:len(stk)-1]
+		}
+	}
+	dfs(0)
+	return ans
 }
---
+++ code/0797/0797.all-paths-from-source-to-target_test.go
@@ -0,0 +1,26 @@
+package _0797
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_allPathsSourceTarget(t *testing.T) {
+	type args struct {
+		graph [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := allPathsSourceTarget(tt.args.graph); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("allPathsSourceTarget() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
