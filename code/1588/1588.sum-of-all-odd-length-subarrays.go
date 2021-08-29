aa5b3dd1955ad09f3969d749e1b041cd00c4aab1
godcong <jumbycc@163.com>
2021-08-29 21:06:59 +0800

    update

--- code/1588/1588.sum-of-all-odd-length-subarrays.go
+++ code/1588/1588.sum-of-all-odd-length-subarrays.go
@@ -1,9 +1,18 @@
 package _1588
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func sumOddLengthSubarrays(arr []int) int {
-
+	res, lens := 0, len(arr)
+	for i := 1; i < lens; i++ {
+		arr[i] += arr[i-1]
+	}
+	for i := 1; i <= lens; i += 2 {
+		a, b := 0, i
+		res += arr[b-1]
+		for b < lens {
+			res += arr[b] - arr[a]
+			a++
+			b++
+		}
+	}
+	return res
 }
