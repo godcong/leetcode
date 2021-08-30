6b85ac2fb2b112718c8220e6a3d6a5fb839b2813
godcong <jumbycc@163.com>
2021-08-30 21:13:09 +0800

    update

--- code/0528/0528.random-pick-with-weight.go
+++ code/0528/0528.random-pick-with-weight.go
@@ -1,23 +1,32 @@
 package _0528
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 type Solution struct {
-
+	pre   []int
+	total uint
+	rd    uint
 }
-
 
 func Constructor(w []int) Solution {
-
+	for i := 1; i < len(w); i++ {
+		w[i] += w[i-1]
+	}
+	return Solution{
+		pre:   w,
+		total: uint(w[len(w)-1]),
+		rd:    1,
+	}
 }
-
 
 func (this *Solution) PickIndex() int {
-
+	this.rd ^= this.rd << 13
+	this.rd ^= this.rd >> 17
+	this.rd ^= this.rd << 5
+	return sort.SearchInts(this.pre, int(this.rd%this.total)+1)
 }
-
 
 /**
  * Your Solution object will be instantiated and called as such:
