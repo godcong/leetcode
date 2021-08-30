9555cb301bd09e60999a655d43372c235573b262
godcong <jumbycc@163.com>
2021-08-30 21:07:52 +0800

    update

---
+++ code/0528/0528.random-pick-with-weight.go
@@ -0,0 +1,26 @@
+package _0528
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type Solution struct {
+
+}
+
+
+func Constructor(w []int) Solution {
+
+}
+
+
+func (this *Solution) PickIndex() int {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(w);
+ * param_1 := obj.PickIndex();
+ */
