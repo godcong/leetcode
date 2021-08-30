9088b628f36301269edba79828208ae4ab8c0fa9
godcong <jumbycc@163.com>
2021-08-30 21:13:19 +0800

    update

---
+++ code/0528/0528.random-pick-with-weight_test.go
@@ -0,0 +1,26 @@
+package _0528
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	type args struct {
+		w []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Solution
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(tt.args.w); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
