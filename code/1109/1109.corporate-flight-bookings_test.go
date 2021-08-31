c3b878d648e64eafa33a1f32dccbd5a185ee3a35
godcong <jumbycc@163.com>
2021-08-31 21:15:01 +0800

    update

---
+++ code/1109/1109.corporate-flight-bookings_test.go
@@ -0,0 +1,27 @@
+package _1109
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_corpFlightBookings(t *testing.T) {
+	type args struct {
+		bookings [][]int
+		n        int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := corpFlightBookings(tt.args.bookings, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("corpFlightBookings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
