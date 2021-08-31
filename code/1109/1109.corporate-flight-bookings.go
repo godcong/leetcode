828e0d919e554882b8113adff50d9150f7335d36
godcong <jumbycc@163.com>
2021-08-31 21:14:17 +0800

    update

--- code/1109/1109.corporate-flight-bookings.go
+++ code/1109/1109.corporate-flight-bookings.go
@@ -1,9 +1,15 @@
 package _1109
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func corpFlightBookings(bookings [][]int, n int) []int {
-
+	nums := make([]int, n)
+	for _, booking := range bookings {
+		nums[booking[0]-1] += booking[2]
+		if booking[1] < n {
+			nums[booking[1]] -= booking[2]
+		}
+	}
+	for i := 1; i < n; i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
 }
