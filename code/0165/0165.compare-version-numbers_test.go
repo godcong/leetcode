f63ba4fe66db295886088b2135c4209ec7f0e79a
godcong <jumbycc@163.com>
2021-09-01 21:17:15 +0800

    update

---
+++ code/0165/0165.compare-version-numbers_test.go
@@ -0,0 +1,26 @@
+package _0165
+
+import (
+	"testing"
+)
+
+func Test_compareVersion(t *testing.T) {
+	type args struct {
+		version1 string
+		version2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compareVersion(tt.args.version1, tt.args.version2); got != tt.want {
+				t.Errorf("compareVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
