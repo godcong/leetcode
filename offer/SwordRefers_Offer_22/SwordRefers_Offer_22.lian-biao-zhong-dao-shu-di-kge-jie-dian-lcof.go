7a6ba5160e3d52cdbd374007042e31f6e8996e88
godcong <jumbycc@163.com>
2021-09-02 19:52:57 +0800

    update

---
+++ offer/SwordRefers_Offer_22/SwordRefers_Offer_22.lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.go
@@ -0,0 +1,16 @@
+package _SwordRefers_Offer_22
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	return nil
+}
