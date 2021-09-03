b586eef4619091ffbe8591b5ca5fee1b9b3c3812
godcong <jumbycc@163.com>
2021-09-03 19:57:47 +0800

    update

---
+++ qustion/InterviewQuestion_17_14/InterviewQuestion_17_14.smallest-k-lcci_test.go
@@ -0,0 +1,27 @@
+package _InterviewQuestion_17_14
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_smallestK(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := smallestK(tt.args.arr, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("smallestK() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
