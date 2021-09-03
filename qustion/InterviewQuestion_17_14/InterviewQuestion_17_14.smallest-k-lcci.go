03a65edc28f4bb513dcb2e2a967bb65e3c0b3dfc
godcong <jumbycc@163.com>
2021-09-03 19:57:34 +0800

    update

--- qustion/InterviewQuestion_17_14/InterviewQuestion_17_14.smallest-k-lcci.go
+++ qustion/InterviewQuestion_17_14/InterviewQuestion_17_14.smallest-k-lcci.go
@@ -1,10 +1,33 @@
 package _InterviewQuestion_17_14
 
-import (
-	"sort"
-)
-
 func smallestK(arr []int, k int) []int {
-	sort.Ints(arr)
-	return arr[:k]
+	if len(arr) == 0 || k == 0 {
+		return nil
+	}
+	var partition = func(i, j int) int {
+		tmp := arr[i]
+		for i < j {
+			for ; i < j && arr[j] >= tmp; j-- {
+			}
+			arr[i] = arr[j]
+			for ; i < j && arr[i] <= tmp; i++ {
+			}
+			arr[j] = arr[i]
+		}
+		arr[i] = tmp
+		return i
+	}
+	i, j := 0, len(arr)-1
+	k--
+	idx := partition(i, j)
+	for idx != k {
+		if idx > k {
+			j = idx - 1
+			idx = partition(i, j)
+		} else {
+			i = idx + 1
+			idx = partition(i, j)
+		}
+	}
+	return arr[:k+1]
 }
