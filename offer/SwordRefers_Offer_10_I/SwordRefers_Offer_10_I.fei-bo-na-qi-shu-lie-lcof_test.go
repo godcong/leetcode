476b735e33fc6ff8d82777dbc28c77c1ea95a6cd
godcong <jumbycc@163.com>
2021-09-04 21:16:20 +0800

    update

---
+++ offer/SwordRefers_Offer_10_I/SwordRefers_Offer_10_I.fei-bo-na-qi-shu-lie-lcof_test.go
@@ -0,0 +1,25 @@
+package _SwordRefers_Offer_10_I
+
+import (
+	"testing"
+)
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
