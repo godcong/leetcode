de80f5075c2372ef07d440093b40191e01dc32c6
godcong <jumbycc@163.com>
2021-09-04 21:16:11 +0800

    update

--- offer/SwordRefers_Offer_10_I/SwordRefers_Offer_10_I.fei-bo-na-qi-shu-lie-lcof.go
+++ offer/SwordRefers_Offer_10_I/SwordRefers_Offer_10_I.fei-bo-na-qi-shu-lie-lcof.go
@@ -1,9 +1,16 @@
 package _SwordRefers_Offer_10_I
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+const mod int = 1e9 + 7
 
 func fib(n int) int {
-
+	if n < 2 {
+		return n
+	}
+	p, q, r := 0, 0, 1
+	for i := 2; i <= n; i++ {
+		p = q
+		q = r
+		r = (p + q) % mod
+	}
+	return r
 }
