799e7d56e3804c0de6343e9af1f2e1633611440b
godcong <jumbycc@163.com>
2021-09-05 21:21:18 +0800

    update

---
+++ code/0470/0470.implement-rand10-using-rand7_test.go
@@ -0,0 +1,21 @@
+package _0470
+
+import (
+	"testing"
+)
+
+func Test_rand10(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rand10(); got != tt.want {
+				t.Errorf("rand10() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
