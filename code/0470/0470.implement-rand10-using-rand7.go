f2195cd5bf2af2384e34b15b2b35abd1841a5b42
godcong <jumbycc@163.com>
2021-09-05 21:18:27 +0800

    update

--- code/0470/0470.implement-rand10-using-rand7.go
+++ code/0470/0470.implement-rand10-using-rand7.go
@@ -1,9 +1,12 @@
 package _0470
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func rand10() int {
-    
+	for {
+		row := rand7()
+		col := rand7()
+		idx := (row-1)*7 + col
+		if idx <= 40 {
+			return 1 + (idx-1)%10
+		}
+	}
 }
