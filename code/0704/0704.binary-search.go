17dc034aab6a3b62b577e668fe4be2851e175092
godcong <jumbycc@163.com>
2021-09-06 21:22:19 +0800

    update

--- code/0704/0704.binary-search.go
+++ code/0704/0704.binary-search.go
@@ -1,9 +1,17 @@
 package _0704
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func search(nums []int, target int) int {
-
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := (high-low)/2 + low
+		num := nums[mid]
+		if num == target {
+			return mid
+		} else if num > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
 }
