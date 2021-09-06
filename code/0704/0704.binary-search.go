3817e0efcc7a6cdfecf640affc66f06823238cdb
godcong <jumbycc@163.com>
2021-09-06 21:23:16 +0800

    update

--- code/0704/0704.binary-search.go
+++ code/0704/0704.binary-search.go
@@ -1,16 +1,15 @@
 package _0704
 
 func search(nums []int, target int) int {
-	low, high := 0, len(nums)-1
-	for low <= high {
-		mid := (high-low)/2 + low
-		num := nums[mid]
-		if num == target {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		if nums[mid] == target {
 			return mid
-		} else if num > target {
-			high = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
 		} else {
-			low = mid + 1
+			right = mid - 1
 		}
 	}
 	return -1
