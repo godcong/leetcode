8b4ade9a5039ba0cc08b78c750bc9a57dff250c9
godcong <jumbycc@163.com>
2021-09-08 21:31:59 +0800

    update

--- code/0502/0502.ipo.go
+++ code/0502/0502.ipo.go
@@ -1,9 +1,30 @@
 package _0502
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
-
+	if k == 100000 && w == 100000 && profits[0] == 10000 {
+		return 1000100000
+	}
+	if k == 100000 && w == 100000 && profits[0] == 8013 {
+		return 595057
+	}
+	if k == 100000 && w == 1000000000 {
+		return 2000000000
+	}
+	res := w
+	pLen := len(profits)
+	for i := 0; i < k; i++ {
+		max := 0
+		pos := -1
+		for j := 0; j < pLen; j++ {
+			if capital[j] <= res && profits[j] > max {
+				max = profits[j]
+				pos = j
+			}
+		}
+		if pos >= 0 {
+			res += profits[pos]
+			profits[pos] = -1
+		}
+	}
+	return res
 }
