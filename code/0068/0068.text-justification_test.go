850be24cf70b4db3c151785ae27c33eb92035f73
godcong <jumbycc@163.com>
2021-09-09 14:04:21 +0800

    update

---
+++ code/0068/0068.text-justification_test.go
@@ -0,0 +1,27 @@
+package _0068
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fullJustify(t *testing.T) {
+	type args struct {
+		words    []string
+		maxWidth int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fullJustify(tt.args.words, tt.args.maxWidth); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fullJustify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
