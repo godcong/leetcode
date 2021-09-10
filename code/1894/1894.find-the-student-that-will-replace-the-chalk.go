b7684907c5b40d8e8802d5580a6edc2b9e2c3cc1
godcong <jumbycc@163.com>
2021-09-10 11:31:56 +0800

    update

--- code/1894/1894.find-the-student-that-will-replace-the-chalk.go
+++ code/1894/1894.find-the-student-that-will-replace-the-chalk.go
@@ -1,9 +1,16 @@
 package _1894
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func chalkReplacer(chalk []int, k int) int {
-
+	total := 0
+	for _, v := range chalk {
+		total += v
+	}
+	k %= total
+	for i, c := range chalk {
+		if k < c {
+			return i
+		}
+		k -= c
+	}
+	return 0
 }
