20a4519257e096a35eeb7f5eb08da873ce66b826
godcong <jumbycc@163.com>
2021-09-12 15:15:55 +0800

    update

--- code/0678/0678.valid-parenthesis-string.go
+++ code/0678/0678.valid-parenthesis-string.go
@@ -1,9 +1,28 @@
 package _0678
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func checkValidString(s string) bool {
+	minCount, maxCount := 0, 0
+	for _, ch := range s {
+		if ch == '(' {
+			minCount++
+			maxCount++
+		} else if ch == ')' {
+			minCount = max(minCount-1, 0)
+			maxCount--
+			if maxCount < 0 {
+				return false
+			}
+		} else {
+			minCount = max(minCount-1, 0)
+			maxCount++
+		}
+	}
+	return minCount == 0
+}
 
+func max(i int, i2 int) int {
+	if i > i2 {
+		return i
+	}
+	return i2
 }
