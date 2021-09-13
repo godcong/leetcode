2282d33ad40265d9f67ce773abfc81c90133ad53
godcong <jumbycc@163.com>
2021-09-13 15:19:56 +0800

    update

--- code/0447/0447.number-of-boomerangs.go
+++ code/0447/0447.number-of-boomerangs.go
@@ -3,13 +3,12 @@ package _0447
 func numberOfBoomerangs(points [][]int) int {
 	ans := 0
 	for _, p := range points {
-		cnt := map[int]int{}
+		d := make(map[int]int, len(points))
 		for _, q := range points {
-			dis := (p[0]-q[0])*(p[0]-q[0]) + (p[1]-q[1])*(p[1]-q[1])
-			cnt[dis]++
+			d[(p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])]++
 		}
-		for _, m := range cnt {
-			ans += m * (m - 1)
+		for _, c := range d {
+			ans += c * (c - 1)
 		}
 	}
 	return ans
