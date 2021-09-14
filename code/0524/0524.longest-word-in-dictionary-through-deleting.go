12b61c7d4bb2b3361810db5f8c4f131c212e944a
godcong <jumbycc@163.com>
2021-09-14 20:53:25 +0800

    update

--- code/0524/0524.longest-word-in-dictionary-through-deleting.go
+++ code/0524/0524.longest-word-in-dictionary-through-deleting.go
@@ -1,9 +1,27 @@
 package _0524
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func findLongestWord(s string, dictionary []string) string {
+	sort.Slice(dictionary, func(i, j int) bool {
+		a, b := dictionary[i], dictionary[j]
+		return len(a) > len(b) || len(a) == len(b) && a < b
+	})
 
+	for _, t := range dictionary {
+		i := 0
+		for j := range s {
+			if s[j] == t[i] {
+				i++
+				if i == len(t) {
+					return t
+				}
+			}
+		}
+
+	}
+
+	return ""
 }
