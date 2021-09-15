7dd4bad67d54d202d829e4a1c5d1aab44f3966d4
godcong <jumbycc@163.com>
2021-09-15 20:58:38 +0800

    update

---
+++ code/0162/0162.find-peak-element_test.go
@@ -0,0 +1,25 @@
+package _0162
+
+import (
+	"testing"
+)
+
+func Test_findPeakElement(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findPeakElement(tt.args.nums); got != tt.want {
+				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
