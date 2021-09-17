50f36595408932be45629bb0e4d4a79059720a09
godcong <jumbycc@163.com>
2021-09-17 16:15:25 +0800

    update

--- code/0036/0036.valid-sudoku.go
+++ code/0036/0036.valid-sudoku.go
@@ -1,9 +1,21 @@
 package _0036
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func isValidSudoku(board [][]byte) bool {
-
+	var rows, columns [9][9]int
+	var boxes [3][3][9]int
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			boxes[i/3][j/3][index]++
+			if rows[i][index] > 1 || columns[j][index] > 1 || boxes[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+	return true
 }
