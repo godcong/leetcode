1a22844d24ae9e9364a467f2575d724af1e26700
godcong <jumbycc@163.com>
2021-09-18 16:18:43 +0800

    update

---
+++ code/0292/0292.nim-game_test.go
@@ -0,0 +1,25 @@
+package _0292
+
+import (
+	"testing"
+)
+
+func Test_canWinNim(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canWinNim(tt.args.n); got != tt.want {
+				t.Errorf("canWinNim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
