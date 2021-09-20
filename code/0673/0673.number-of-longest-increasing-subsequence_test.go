5cf99031e75f9b3dc3b1e1ba246508453cc0c7bb
godcong <jumbycc@163.com>
2021-09-20 16:22:15 +0800

    update

---
+++ code/0673/0673.number-of-longest-increasing-subsequence_test.go
@@ -0,0 +1,25 @@
+package _0673
+
+import (
+	"testing"
+)
+
+func Test_findNumberOfLIS(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNumberOfLIS(tt.args.nums); got != tt.want {
+				t.Errorf("findNumberOfLIS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
