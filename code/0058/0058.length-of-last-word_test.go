3e506ec4e39cbe5dc3cb6745f48ad670c29ca4c2
godcong <jumbycc@163.com>
2021-09-21 16:37:02 +0800

    update

---
+++ code/0058/0058.length-of-last-word_test.go
@@ -0,0 +1,25 @@
+package _0058
+
+import (
+	"testing"
+)
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
