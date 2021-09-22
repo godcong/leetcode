6d5086cca7dc0977f609c40c23fcc8e7c711d859
godcong <jumbycc@163.com>
2021-09-22 16:37:30 +0800

    add split-linked-list-in-parts

---
+++ code/0725/0725.split-linked-list-in-parts.go
@@ -0,0 +1,16 @@
+package _0725
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func splitListToParts(head *ListNode, k int) []*ListNode {
+
+}
