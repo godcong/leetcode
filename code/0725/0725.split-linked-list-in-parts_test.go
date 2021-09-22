fe75d6e73433471fd64fb919a45d971b3e4ed177
godcong <jumbycc@163.com>
2021-09-22 16:39:01 +0800

    update

---
+++ code/0725/0725.split-linked-list-in-parts_test.go
@@ -0,0 +1,29 @@
+package _0725
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_splitListToParts(t *testing.T) {
+	type args struct {
+		head *ListNode
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitListToParts(tt.args.head, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitListToParts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
