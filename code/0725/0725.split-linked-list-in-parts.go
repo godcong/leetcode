2e1b4a5624d9f32f2f054e444c810bab9df93541
godcong <jumbycc@163.com>
2021-09-22 16:38:38 +0800

    update

--- code/0725/0725.split-linked-list-in-parts.go
+++ code/0725/0725.split-linked-list-in-parts.go
@@ -12,5 +12,23 @@ import (
  * }
  */
 func splitListToParts(head *ListNode, k int) []*ListNode {
+	n := 0
+	for node := head; node != nil; node = node.Next {
+		n++
+	}
+	quotient, remainder := n/k, n%k
 
+	parts := make([]*ListNode, k)
+	for i, curr := 0, head; i < k && curr != nil; i++ {
+		parts[i] = curr
+		partSize := quotient
+		if i < remainder {
+			partSize++
+		}
+		for j := 1; j < partSize; j++ {
+			curr = curr.Next
+		}
+		curr, curr.Next = curr.Next, nil
+	}
+	return parts
 }
