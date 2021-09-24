6cc88b6b7939abeb024808d09ca00c6d4b3f915d
godcong <jumbycc@163.com>
2021-09-24 12:05:19 +0800

    add code(430) flatten-a-multilevel-doubly-linked-list

---
+++ code/0430/0430.flatten-a-multilevel-doubly-linked-list.go
@@ -0,0 +1,19 @@
+package _0430
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Prev *Node
+ *     Next *Node
+ *     Child *Node
+ * }
+ */
+
+func flatten(root *Node) *Node {
+    
+}
