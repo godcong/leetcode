5352f4b4867c1335ac9043ef9bd534d9d32b4391
godcong <jumbycc@163.com>
2021-09-24 12:07:57 +0800

    update

---
+++ code/0430/0430.flatten-a-multilevel-doubly-linked-list_test.go
@@ -0,0 +1,26 @@
+package _0430
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_flatten(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := flatten(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("flatten() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
