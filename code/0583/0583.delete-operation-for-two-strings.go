cd1565f13d42175718302d7d156212495adc862b
godcong <jumbycc@163.com>
2021-09-25 12:11:11 +0800

    update

--- code/0583/0583.delete-operation-for-two-strings.go
+++ code/0583/0583.delete-operation-for-two-strings.go
@@ -1,9 +1,26 @@
 package _0583
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func minDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([]int, n+1)
+	for i := 1; i <= m; i++ {
+		lt := 0
+		for j := 1; j <= n; j++ {
+			tmp := dp[j]
+			if word1[i-1] == word2[j-1] {
+				dp[j] = lt + 1
+			} else {
+				dp[j] = max(dp[j], dp[j-1])
+			}
+			lt = tmp
+		}
+	}
+	return m + n - 2*dp[n]
+}
 
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
 }
