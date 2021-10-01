0eb85d41fcd4a406509000d12ab3cfc344262208
godcong <jumbycc@163.com>
2021-10-01 15:13:29 +0800

    feat(code): update new code

--- code/1436/1436.destination-city.go
+++ code/1436/1436.destination-city.go
@@ -1,9 +1,14 @@
 package _1436
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func destCity(paths [][]string) string {
-
+	citiesA := map[string]bool{}
+	for _, path := range paths {
+		citiesA[path[0]] = true
+	}
+	for _, path := range paths {
+		if !citiesA[path[1]] {
+			return path[1]
+		}
+	}
+	return ""
 }
