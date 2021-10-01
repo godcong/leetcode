c930f064acd33a1569f7c35c7442d3d6b6978acc
godcong <jumbycc@163.com>
2021-10-01 15:13:53 +0800

    feat(code): update new code

---
+++ code/1436/1436.destination-city_test.go
@@ -0,0 +1,25 @@
+package _1436
+
+import (
+	"testing"
+)
+
+func Test_destCity(t *testing.T) {
+	type args struct {
+		paths [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := destCity(tt.args.paths); got != tt.want {
+				t.Errorf("destCity() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
