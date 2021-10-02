2fb48a5e079aefc2bbe644601c865d5b9c3ac6ae
godcong <jumbycc@163.com>
2021-10-02 15:18:38 +0800

    feat(code): update new code

--- code/0405/0405.convert-a-number-to-hexadecimal.go
+++ code/0405/0405.convert-a-number-to-hexadecimal.go
@@ -1,9 +1,25 @@
 package _0405
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 func toHex(num int) string {
-
+	if num == 0 {
+		return "0"
+	}
+	builder := &strings.Builder{}
+	for i := 7; i >= 0; i-- {
+		val := num >> (4 * i) & 0xf
+		if val > 0 || builder.Len() > 0 {
+			var digit byte
+			if val < 10 {
+				digit = '0' + byte(val)
+			} else {
+				digit = 'a' + byte(val-10)
+			}
+			builder.WriteByte(digit)
+		}
+	}
+	return builder.String()
 }
