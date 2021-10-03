fb778440f708a2a69c24068f34606d5a24343c04
godcong <jumbycc@163.com>
2021-10-03 15:20:33 +0800

    feat(code): update new code

--- code/0166/0166.fraction-to-recurring-decimal.go
+++ code/0166/0166.fraction-to-recurring-decimal.go
@@ -1,9 +1,45 @@
 package _0166
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strconv"
 )
 
 func fractionToDecimal(numerator int, denominator int) string {
+	if numerator%denominator == 0 {
+		return strconv.Itoa(numerator / denominator)
+	}
 
+	var s []byte
+	if numerator < 0 != (denominator < 0) {
+		s = append(s, '-')
+	}
+
+	numerator = abs(numerator)
+	denominator = abs(denominator)
+	integerPart := numerator / denominator
+	s = append(s, strconv.Itoa(integerPart)...)
+	s = append(s, '.')
+
+	indexMap := map[int]int{}
+	remainder := numerator % denominator
+	for remainder != 0 && indexMap[remainder] == 0 {
+		indexMap[remainder] = len(s)
+		remainder *= 10
+		s = append(s, '0'+byte(remainder/denominator))
+		remainder %= denominator
+	}
+	if remainder > 0 {
+		insertIndex := indexMap[remainder]
+		s = append(s[:insertIndex], append([]byte{'('}, s[insertIndex:]...)...)
+		s = append(s, ')')
+	}
+
+	return string(s)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
