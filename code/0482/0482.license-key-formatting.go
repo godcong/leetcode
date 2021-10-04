a020e7afd3001821a47c0918f80750179b3f39db
godcong <jumbycc@163.com>
2021-10-04 15:26:32 +0800

    feat(code): update new code

--- code/0482/0482.license-key-formatting.go
+++ code/0482/0482.license-key-formatting.go
@@ -1,9 +1,25 @@
 package _0482
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"unicode"
 )
 
 func licenseKeyFormatting(s string, k int) string {
-
+	var ans []byte
+	for i, cnt := len(s)-1, 0; i >= 0; i-- {
+		if s[i] != '-' {
+			ans = append(ans, byte(unicode.ToUpper(rune(s[i]))))
+			cnt++
+			if cnt%k == 0 {
+				ans = append(ans, '-')
+			}
+		}
+	}
+	if len(ans) > 0 && ans[len(ans)-1] == '-' {
+		ans = ans[:len(ans)-1]
+	}
+	for i, n := 0, len(ans); i < n/2; i++ {
+		ans[i], ans[n-1-i] = ans[n-1-i], ans[i]
+	}
+	return string(ans)
 }
