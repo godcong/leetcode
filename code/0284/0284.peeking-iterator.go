e066b36040a2ecbb2725878badc233da27158dc6
godcong <jumbycc@163.com>
2021-10-05 15:27:57 +0800

    feat(code): add new code(284) peeking-iterator

---
+++ code/0284/0284.peeking-iterator.go
@@ -0,0 +1,40 @@
+package _0284
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/*   Below is the interface for Iterator, which is already defined for you.
+ *
+ *   type Iterator struct {
+ *       
+ *   }
+ *
+ *   func (this *Iterator) hasNext() bool {
+ *		// Returns true if the iteration has more elements.
+ *   }
+ *
+ *   func (this *Iterator) next() int {
+ *		// Returns the next element in the iteration.
+ *   }
+ */
+
+type PeekingIterator struct {
+    
+}
+
+func Constructor(iter *Iterator) *PeekingIterator {
+    
+}
+
+func (this *PeekingIterator) hasNext() bool {
+    
+}
+
+func (this *PeekingIterator) next() int {
+    
+}
+
+func (this *PeekingIterator) peek() int {
+    
+}
