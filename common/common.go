1bb8d21fd00dbc8f90fb8118d213830f16efda07
godcong <jumbycc@163.com>
2021-10-05 15:31:25 +0800

    feat(code): update new code

--- common/common.go
+++ common/common.go
@@ -78,3 +78,17 @@ type Employee struct {
 	Importance   int
 	Subordinates []int
 }
+
+type Iterator struct {
+	current int
+	ele     []interface{}
+}
+
+func (this *Iterator) hasNext() bool {
+	// Returns true if the iteration has more elements.
+	return this.current < len(this.ele)
+}
+
+func (this *Iterator) next() int {
+	return this.current
+}
