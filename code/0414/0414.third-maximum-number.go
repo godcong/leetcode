6da1783df1784e61594953c2d5208d30015e63d8
godcong <jumbycc@163.com>
2021-10-06 15:37:00 +0800

    feat(code): update new code

--- code/0414/0414.third-maximum-number.go
+++ code/0414/0414.third-maximum-number.go
@@ -1,9 +1,18 @@
 package _0414
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func thirdMax(nums []int) int {
-
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	for i, diff := 1, 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			diff++
+			if diff == 3 {
+				return nums[i]
+			}
+		}
+	}
+	return nums[0]
 }
