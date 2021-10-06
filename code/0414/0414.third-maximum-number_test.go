6083dfe5117d4af5c2075bfbfba785f7cdb1d009
godcong <jumbycc@163.com>
2021-10-06 15:37:10 +0800

    feat(code): update new code

---
+++ code/0414/0414.third-maximum-number_test.go
@@ -0,0 +1,25 @@
+package _0414
+
+import (
+	"testing"
+)
+
+func Test_thirdMax(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := thirdMax(tt.args.nums); got != tt.want {
+				t.Errorf("thirdMax() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
