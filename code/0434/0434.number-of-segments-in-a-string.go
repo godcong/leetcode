53bc1dd3b26b20d25297d9ac6e2799c4d7f2566d
godcong <jumbycc@163.com>
2021-10-07 15:44:38 +0800

    feat(code): update new code

--- code/0434/0434.number-of-segments-in-a-string.go
+++ code/0434/0434.number-of-segments-in-a-string.go
@@ -1,9 +1,11 @@
 package _0434
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countSegments(s string) int {
-
+	var ans int
+	for i, ch := range s {
+		if (i == 0 || s[i-1] == ' ') && ch != ' ' {
+			ans++
+		}
+	}
+	return ans
 }
