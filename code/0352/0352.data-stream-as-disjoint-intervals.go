939c7221b35f22b9018833e3707088c95b233a99
godcong <jumbycc@163.com>
2021-10-09 11:33:31 +0800

    feat(code): add new code(352) data-stream-as-disjoint-intervals

---
+++ code/0352/0352.data-stream-as-disjoint-intervals.go
@@ -0,0 +1,32 @@
+package _0352
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type SummaryRanges struct {
+
+}
+
+
+func Constructor() SummaryRanges {
+
+}
+
+
+func (this *SummaryRanges) AddNum(val int)  {
+
+}
+
+
+func (this *SummaryRanges) GetIntervals() [][]int {
+
+}
+
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(val);
+ * param_2 := obj.GetIntervals();
+ */
