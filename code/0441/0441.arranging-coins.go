2c94ff1c684cc9c91dcf983f936c8204ffd14ca1
godcong <jumbycc@163.com>
2021-10-10 11:48:15 +0800

    feat(code): update new code

--- code/0441/0441.arranging-coins.go
+++ code/0441/0441.arranging-coins.go
@@ -1,9 +1,9 @@
 package _0441
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func arrangeCoins(n int) int {
-
+	return sort.Search(n, func(k int) bool { k++; return k*(k+1) > 2*n })
 }
