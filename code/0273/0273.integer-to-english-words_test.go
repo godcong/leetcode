5230c691db03ad9f0c3877e0a70b2d7ddd0f680c
godcong <jumbycc@163.com>
2021-10-11 11:54:45 +0800

    feat(code): update new code

---
+++ code/0273/0273.integer-to-english-words_test.go
@@ -0,0 +1,25 @@
+package _0273
+
+import (
+	"testing"
+)
+
+func Test_numberToWords(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberToWords(tt.args.num); got != tt.want {
+				t.Errorf("numberToWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
