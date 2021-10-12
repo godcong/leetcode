8ff7acf1e884c3ea78892f5b56aa4b9602a0cb0e
godcong <jumbycc@163.com>
2021-10-12 12:08:43 +0800

    feat(code): update new code

--- code/0029/0029.divide-two-integers.go
+++ code/0029/0029.divide-two-integers.go
@@ -1,9 +1,45 @@
 package _0029
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
 )
 
 func divide(dividend int, divisor int) int {
+	if dividend == 0 {
+		return 0
+	}
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
 
+	diffSign := false
+	if (dividend < 0) != (divisor < 0) {
+		diffSign = true
+	}
+
+	i, j, sum := 0, 0, 0
+	var temp int
+	for {
+		temp = 0
+		if diffSign {
+			temp = sum - divisor<<j
+		} else {
+			temp = sum + divisor<<j
+		}
+		if (dividend > 0 && temp > dividend) || (dividend < 0 && temp < dividend) {
+			if j == 0 {
+				break
+			}
+			j--
+			continue
+		}
+		sum = temp
+		i += 1 << j
+		j++
+	}
+
+	if diffSign {
+		return -i
+	}
+	return i
 }
