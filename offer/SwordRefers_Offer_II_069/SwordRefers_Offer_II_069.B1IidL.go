56fba3857c4b9bc540c7d59f5333b12bf48b655b
godcong <jumbycc@163.com>
2021-10-14 12:55:35 +0800

    feat(code): update new code

--- offer/SwordRefers_Offer_II_069/SwordRefers_Offer_II_069.B1IidL.go
+++ offer/SwordRefers_Offer_II_069/SwordRefers_Offer_II_069.B1IidL.go
@@ -1,9 +1,25 @@
 package _SwordRefers_Offer_II_069
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func peakIndexInMountainArray(arr []int) int {
+	left := 0
+	right := len(arr) - 1
 
+	for left < right {
+
+		mid := (left + right) / 2
+
+		if arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1] {
+			return mid
+		}
+
+		if arr[mid] > arr[mid-1] {
+			left = mid
+		}
+
+		if arr[mid] > arr[mid+1] {
+			right = mid
+		}
+	}
+
+	return 0
 }
