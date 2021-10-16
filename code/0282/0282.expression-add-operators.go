9b0449db9831e4b03c5e7748c4cbde4c437cbc76
godcong <jumbycc@163.com>
2021-10-16 13:11:15 +0800

    feat(code): update new code

--- code/0282/0282.expression-add-operators.go
+++ code/0282/0282.expression-add-operators.go
@@ -1,9 +1,35 @@
 package _0282
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func addOperators(num string, target int) []string {
-
+	n := len(num)
+	ans := make([]string, 0)
+	var backtrack func(expr []byte, i, res, mul int)
+	backtrack = func(expr []byte, i, res, mul int) {
+		if i == n {
+			if res == target {
+				ans = append(ans, string(expr))
+			}
+			return
+		}
+		signIndex := len(expr)
+		if i > 0 {
+			expr = append(expr, 0)
+		}
+		for j, val := i, 0; j < n && (j == i || num[i] != '0'); j++ {
+			val = val*10 + int(num[j]-'0')
+			expr = append(expr, num[j])
+			if i == 0 {
+				backtrack(expr, j+1, val, val)
+			} else {
+				expr[signIndex] = '+'
+				backtrack(expr, j+1, res+val, val)
+				expr[signIndex] = '-'
+				backtrack(expr, j+1, res-val, -val)
+				expr[signIndex] = '*'
+				backtrack(expr, j+1, res-mul+mul*val, mul*val)
+			}
+		}
+	}
+	backtrack(make([]byte, 0, n*2-1), 0, 0, 0)
+	return ans
 }
