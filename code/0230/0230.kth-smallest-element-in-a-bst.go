8cfb5ebe855a7e0cd390e5376e91f7153b036d22
godcong <jumbycc@163.com>
2021-10-17 13:12:23 +0800

    feat(code): add new code(230) kth-smallest-element-in-a-bst

---
+++ code/0230/0230.kth-smallest-element-in-a-bst.go
@@ -0,0 +1,17 @@
+package _0230
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+
+}
