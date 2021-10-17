9278cc58a78a509310308d0b4ba0e3593ce112b1
godcong <jumbycc@163.com>
2021-10-17 13:14:11 +0800

    feat(code): update new code

---
+++ code/0230/0230.kth-smallest-element-in-a-bst_test.go
@@ -0,0 +1,28 @@
+package _0230
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_kthSmallest(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kthSmallest(tt.args.root, tt.args.k); got != tt.want {
+				t.Errorf("kthSmallest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
