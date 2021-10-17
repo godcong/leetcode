c7c011c59b1b8a7dca7bd5b05219a72a5e8f34fc
godcong <jumbycc@163.com>
2021-10-17 13:13:41 +0800

    feat(code): update new code

--- code/0230/0230.kth-smallest-element-in-a-bst.go
+++ code/0230/0230.kth-smallest-element-in-a-bst.go
@@ -13,5 +13,17 @@ import (
  * }
  */
 func kthSmallest(root *TreeNode, k int) int {
-
+	stack := make([]*TreeNode, 0)
+	for {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		stack, root = stack[:len(stack)-1], stack[len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
 }
