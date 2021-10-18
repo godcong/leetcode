f5eae99977d1e59a05b2247fd0e415c6c3b3a2d2
godcong <jumbycc@163.com>
2021-10-18 13:17:05 +0800

    feat(code): update new code

--- code/0476/0476.number-complement.go
+++ code/0476/0476.number-complement.go
@@ -1,9 +1,13 @@
 package _0476
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findComplement(num int) int {
-
+	highBit := 0
+	for i := 1; i <= 30; i++ {
+		if num < 1<<i {
+			break
+		}
+		highBit = i
+	}
+	mask := 1<<(highBit+1) - 1
+	return num ^ mask
 }
