20ae453841adfe38bea1eae7ab68884f4bcc315d
godcong <jumbycc@163.com>
2021-10-19 14:14:38 +0800

    feat(code): update new code

--- code/0211/0211.design-add-and-search-words-data-structure.go
+++ code/0211/0211.design-add-and-search-words-data-structure.go
@@ -1,28 +1,45 @@
 package _0211
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"runtime/debug"
 )
 
 type WordDictionary struct {
-
+	items map[int][]string
 }
-
 
 func Constructor() WordDictionary {
-
+	return WordDictionary{make(map[int][]string)}
 }
 
-
-func (this *WordDictionary) AddWord(word string)  {
-
+func (this *WordDictionary) AddWord(word string) {
+	l := len(word)
+	if list, ok := this.items[l]; ok {
+		this.items[l] = append(list, word)
+	} else {
+		this.items[l] = []string{word}
+	}
 }
-
 
 func (this *WordDictionary) Search(word string) bool {
-
+	l := len(word)
+	if list, ok := this.items[l]; ok {
+		for _, w := range list {
+			i := 0
+			for i < l {
+				if w[i] != word[i] && word[i] != '.' {
+					break
+				}
+				i++
+			}
+			if i == l {
+				return true
+			}
+		}
+	}
+	return false
 }
-
+func init() { debug.SetGCPercent(-1) }
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
