89b411f0b356a4ee19a13d558be717848bf0db0a
godcong <jumbycc@163.com>
2021-10-20 14:25:22 +0800

    feat(code): update new code

--- code/0453/0453.minimum-moves-to-equal-array-elements.go
+++ code/0453/0453.minimum-moves-to-equal-array-elements.go
@@ -1,9 +1,13 @@
 package _0453
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func minMoves(nums []int) int {
-
+	var min int = nums[0]
+	var sum int = 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < min {
+			min = nums[i]
+		}
+		sum += nums[i]
+	}
+	return sum - len(nums)*min
 }
