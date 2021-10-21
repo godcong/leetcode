e060a4ad2919a5378e933288695737754b0618c2
godcong <jumbycc@163.com>
2021-10-21 14:35:02 +0800

    feat(code): update new code

---
+++ code/0066/0066.plus-one_test.go
@@ -0,0 +1,26 @@
+package _0066
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOne(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
