bf5ff77ffd82a94514534e559b7732a693794b3d
godcong <jumbycc@163.com>
2021-10-21 14:34:50 +0800

    feat(code): update new code

--- code/0066/0066.plus-one.go
+++ code/0066/0066.plus-one.go
@@ -1,9 +1,18 @@
 package _0066
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func plusOne(digits []int) []int {
+	n := len(digits)
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			for j := i + 1; j < n; j++ {
+				digits[j] = 0
+			}
+			return digits
+		}
+	}
 
+	digits = make([]int, n+1)
+	digits[0] = 1
+	return digits
 }
