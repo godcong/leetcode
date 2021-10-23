ac2bb517d54118bba6649e5df00100bfed6a6442
godcong <jumbycc@163.com>
2021-10-23 12:07:31 +0800

    feat(code): update new code

---
+++ code/0492/0492.construct-the-rectangle_test.go
@@ -0,0 +1,26 @@
+package _0492
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_constructRectangle(t *testing.T) {
+	type args struct {
+		area int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := constructRectangle(tt.args.area); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
