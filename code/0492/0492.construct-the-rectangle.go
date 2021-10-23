19d7d83b0f235ac6fe5f86ae1b84d020af5fc013
godcong <jumbycc@163.com>
2021-10-23 12:07:20 +0800

    feat(code): update new code

--- code/0492/0492.construct-the-rectangle.go
+++ code/0492/0492.construct-the-rectangle.go
@@ -1,9 +1,13 @@
 package _0492
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
 )
 
 func constructRectangle(area int) []int {
-
+	w := int(math.Sqrt(float64(area)))
+	for area%w > 0 {
+		w--
+	}
+	return []int{area / w, w}
 }
