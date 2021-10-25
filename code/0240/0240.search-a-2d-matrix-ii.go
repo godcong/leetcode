dea258a98c15f437073d3580155196d6eef7eac7
godcong <jumbycc@163.com>
2021-10-25 12:15:16 +0800

    feat(code): update new code

--- code/0240/0240.search-a-2d-matrix-ii.go
+++ code/0240/0240.search-a-2d-matrix-ii.go
@@ -1,9 +1,15 @@
 package _0240
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func searchMatrix(matrix [][]int, target int) bool {
-    
+	for _, row := range matrix {
+		i := sort.SearchInts(row, target)
+		if i < len(row) && row[i] == target {
+			return true
+		}
+	}
+	return false
 }
