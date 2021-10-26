63e65a55e2700d40c8c78e3b2506e6e923a64345
godcong <jumbycc@163.com>
2021-10-26 12:21:07 +0800

    feat(code): update new code

--- code/0496/0496.next-greater-element-i.go
+++ code/0496/0496.next-greater-element-i.go
@@ -1,9 +1,26 @@
 package _0496
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	hash := make(map[int]int, len(nums2))
+	var stack []int
+	for _, num := range nums2 {
+		for len(stack) > 0 && stack[len(stack)-1] <= num {
+			stackTop := stack[len(stack)-1]
+			hash[stackTop] = num
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, num)
+	}
 
+	res := make([]int, 0, len(nums1))
+	for _, num := range nums1 {
+		if value, ok := hash[num]; ok {
+			res = append(res, value)
+		} else {
+			res = append(res, -1)
+		}
+
+	}
+
+	return res
 }
