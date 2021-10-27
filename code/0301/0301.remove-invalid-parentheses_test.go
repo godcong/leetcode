85e19f1400c73ff2b75973b02db953d8c33f8309
godcong <jumbycc@163.com>
2021-10-27 16:06:29 +0800

    feat(code): update new code

---
+++ code/0301/0301.remove-invalid-parentheses_test.go
@@ -0,0 +1,26 @@
+package _0301
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_removeInvalidParentheses(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeInvalidParentheses(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeInvalidParentheses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
