22594d2f2f8e032d5ee55a4b4f47e1d43f8fccc6
godcong <jumbycc@163.com>
2021-10-28 18:34:19 +0800

    feat(code): update new code

--- code/0869/0869.reordered-power-of-2.go
+++ code/0869/0869.reordered-power-of-2.go
@@ -1,9 +1,23 @@
 package _0869
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+var powerOf2Digits = initPowerOf2Digits()
+
+func initPowerOf2Digits() map[[10]uint8]bool {
+	ret := make(map[[10]uint8]bool)
+	for n := 1; n <= 1e9; n <<= 1 {
+		ret[countDigits(n)] = true
+	}
+	return ret
+}
+
+func countDigits(n int) (cnt [10]uint8) {
+	for n > 0 {
+		cnt[n%10]++
+		n /= 10
+	}
+	return
+}
 
 func reorderedPowerOf2(n int) bool {
-
+	return powerOf2Digits[countDigits(n)]
 }
