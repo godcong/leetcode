127de905fe34e1085212cf72527a568df75dc60d
godcong <jumbycc@163.com>
2021-10-28 18:34:30 +0800

    feat(code): update new code

---
+++ code/0869/0869.reordered-power-of-2_test.go
@@ -0,0 +1,25 @@
+package _0869
+
+import (
+	"testing"
+)
+
+func Test_reorderedPowerOf2(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reorderedPowerOf2(tt.args.n); got != tt.want {
+				t.Errorf("reorderedPowerOf2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
