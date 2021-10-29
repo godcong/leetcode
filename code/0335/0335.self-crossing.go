66563a3555b731ff9114bbb10afd336fa70aaa73
godcong <jumbycc@163.com>
2021-10-29 18:48:51 +0800

    feat(code): update new code

--- code/0335/0335.self-crossing.go
+++ code/0335/0335.self-crossing.go
@@ -1,9 +1,16 @@
 package _0335
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func isSelfCrossing(distance []int) bool {
-
+	for i := 3; i < len(distance); i++ {
+		if distance[i] >= distance[i-2] && distance[i-1] <= distance[i-3] {
+			return true
+		}
+		if i == 4 && distance[3] == distance[1] && distance[4] >= distance[2]-distance[0] {
+			return true
+		}
+		if i >= 5 && distance[i-3]-distance[i-5] <= distance[i-1] && distance[i-1] <= distance[i-3] && distance[i] >= distance[i-2]-distance[i-4] && distance[i-2] > distance[i-4] {
+			return true
+		}
+	}
+	return false
 }
