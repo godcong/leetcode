019cec88ce8146d70b9b1b495d5c9c0c2e14db2e
godcong <jumbycc@163.com>
2021-10-30 19:13:14 +0800

    feat(code): update new code

--- code/0260/0260.single-number-iii.go
+++ code/0260/0260.single-number-iii.go
@@ -1,9 +1,21 @@
 package _0260
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func singleNumber(nums []int) []int {
-
+	var a = 0
+	for _, v := range nums {
+		a ^= v
+	}
+	var t = 1
+	for (t & a) == 0 {
+		t <<= 1
+	}
+	var res1, res2 int
+	for _, v := range nums {
+		if (v & t) > 0 {
+			res1 ^= v
+		} else {
+			res2 ^= v
+		}
+	}
+	return []int{res1, res2}
 }
