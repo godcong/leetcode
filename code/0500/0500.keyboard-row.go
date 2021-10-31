2970a26fbaa3bfbd8cee5554089b28aad085f7db
godcong <jumbycc@163.com>
2021-10-31 19:20:29 +0800

    feat(code): update new code

--- code/0500/0500.keyboard-row.go
+++ code/0500/0500.keyboard-row.go
@@ -1,9 +1,22 @@
 package _0500
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"unicode"
 )
 
-func findWords(words []string) []string {
+const rowIdx = "12210111011122000010020202"
 
+func findWords(words []string) []string {
+	var ans []string
+next:
+	for _, word := range words {
+		idx := rowIdx[unicode.ToLower(rune(word[0]))-'a']
+		for _, ch := range word[1:] {
+			if rowIdx[unicode.ToLower(ch)-'a'] != idx {
+				continue next
+			}
+		}
+		ans = append(ans, word)
+	}
+	return ans
 }
