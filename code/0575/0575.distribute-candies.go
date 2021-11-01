ec79f3d4d1827ff444c05c65287f9058f3535220
godcong <jumbycc@163.com>
2021-11-01 19:35:29 +0800

    feat(code): update new code

--- code/0575/0575.distribute-candies.go
+++ code/0575/0575.distribute-candies.go
@@ -1,9 +1,14 @@
 package _0575
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func distributeCandies(candyType []int) int {
+	max := len(candyType) / 2
+	set := make(map[int]bool, len(candyType)/2)
 
+	for _, t := range candyType {
+		set[t] = true
+		if len(set) >= max {
+			return max
+		}
+	}
+	return len(set)
 }
