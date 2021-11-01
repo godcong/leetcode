a01571d8402b5db1e501b3ea4caff2533fcb48ab
godcong <jumbycc@163.com>
2021-11-01 19:35:43 +0800

    feat(code): update new code

---
+++ code/0575/0575.distribute-candies_test.go
@@ -0,0 +1,25 @@
+package _0575
+
+import (
+	"testing"
+)
+
+func Test_distributeCandies(t *testing.T) {
+	type args struct {
+		candyType []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := distributeCandies(tt.args.candyType); got != tt.want {
+				t.Errorf("distributeCandies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
