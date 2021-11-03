f53b2f8e5567d20db3ee3251abd2abc7eaa7b048
godcong <jumbycc@163.com>
2021-11-03 18:50:21 +0700

    feat(code): update new code

---
+++ code/0407/0407.trapping-rain-water-ii_test.go
@@ -0,0 +1,25 @@
+package _0407
+
+import (
+	"testing"
+)
+
+func Test_trapRainWater(t *testing.T) {
+	type args struct {
+		heightMap [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trapRainWater(tt.args.heightMap); got != tt.want {
+				t.Errorf("trapRainWater() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
