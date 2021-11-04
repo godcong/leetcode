ac3335614b7d5aaeb120912fa699921713bcccc9
godcong <jumbycc@163.com>
2021-11-04 18:58:52 +0700

    feat(code): update new code

---
+++ code/0367/0367.valid-perfect-square_test.go
@@ -0,0 +1,25 @@
+package _0367
+
+import (
+	"testing"
+)
+
+func Test_isPerfectSquare(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPerfectSquare(tt.args.num); got != tt.want {
+				t.Errorf("isPerfectSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
