dbcbd488b99455f959df1b79cbee6970910bc925
godcong <jumbycc@163.com>
2021-11-05 19:02:12 +0700

    feat(code): update new code

--- code/1218/1218.longest-arithmetic-subsequence-of-given-difference.go
+++ code/1218/1218.longest-arithmetic-subsequence-of-given-difference.go
@@ -1,9 +1,13 @@
 package _1218
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func longestSubsequence(arr []int, difference int) int {
-
+	ans := 0
+	dp := make(map[int]int, len(arr))
+	for _, v := range arr {
+		dp[v] = dp[v-difference] + 1
+		if dp[v] > ans {
+			ans = dp[v]
+		}
+	}
+	return ans
 }
