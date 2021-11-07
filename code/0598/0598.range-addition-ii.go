a7e171e5d4bb133355c606f8fd9284d56fedfe3e
godcong <jumbycc@163.com>
2021-11-07 12:04:29 +0700

    feat(code): update new code

--- code/0598/0598.range-addition-ii.go
+++ code/0598/0598.range-addition-ii.go
@@ -1,9 +1,17 @@
 package _0598
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func maxCount(m int, n int, ops [][]int) int {
+	mina, minb := m, n
+	for _, op := range ops {
+		mina = min(mina, op[0])
+		minb = min(minb, op[1])
+	}
+	return mina * minb
+}
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
