aca15c6b6e26838df80c4deb3579ca740f6311c0
godcong <jumbycc@163.com>
2021-11-08 17:27:50 +0800

    feat(code): update new code

--- code/0299/0299.bulls-and-cows.go
+++ code/0299/0299.bulls-and-cows.go
@@ -1,9 +1,29 @@
 package _0299
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"fmt"
 )
 
 func getHint(secret string, guess string) string {
+	var bulls, cows int
+	s, g := make([]int, 10), make([]int, 10)
+	for i := 0; i < len(secret); i++ {
+		if secret[i] == guess[i] {
+			bulls++
+		} else {
+			s[secret[i]-'0']++
+			g[guess[i]-'0']++
+		}
+	}
+	for i := 0; i < 10; i++ {
+		cows += min(s[i], g[i])
+	}
+	return fmt.Sprintf("%dA%dB", bulls, cows)
+}
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
