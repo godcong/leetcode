65300d1cb06a90da81cd4a2f0c7a4c0560a058b8
godcong <jumbycc@163.com>
2021-11-10 18:01:34 +0800

    feat(code): update new code

--- code/0495/0495.teemo-attacking.go
+++ code/0495/0495.teemo-attacking.go
@@ -1,9 +1,20 @@
 package _0495
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findPoisonedDuration(timeSeries []int, duration int) int {
+	if len(timeSeries) == 0 {
+		return 0
+	}
+	var res int
+	for i := 1; i < len(timeSeries); i++ {
+		res += min(timeSeries[i]-timeSeries[i-1], duration)
+	}
+	res += duration
+	return res
+}
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
