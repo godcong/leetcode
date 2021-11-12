4678ac4556a73b9525d06ed46ce602d45a9d0e8f
godcong <jumbycc@163.com>
2021-11-12 17:25:26 +0800

    feat(code): update new code

--- code/0375/0375.guess-number-higher-or-lower-ii.go
+++ code/0375/0375.guess-number-higher-or-lower-ii.go
@@ -1,9 +1,27 @@
 package _0375
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func getMoneyAmount(n int) int {
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+	for i := n - 1; i >= 1; i-- {
+		for j := i + 1; j <= n; j++ {
+			f[i][j] = j + f[i][j-1]
+			for k := i; k < j; k++ {
+				cost := k + max(f[i][k-1], f[k+1][j])
+				if cost < f[i][j] {
+					f[i][j] = cost
+				}
+			}
+		}
+	}
+	return f[1][n]
+}
 
+func max(i int, i2 int) int {
+	if i > i2 {
+		return i
+	}
+	return i2
 }
