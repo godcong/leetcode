6a9253ef76dee06b2da0444d232d2b5cfdb5c0db
godcong <jumbycc@163.com>
2021-11-12 17:25:42 +0800

    feat(code): update new code

---
+++ code/0375/0375.guess-number-higher-or-lower-ii_test.go
@@ -0,0 +1,25 @@
+package _0375
+
+import (
+	"testing"
+)
+
+func Test_getMoneyAmount(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMoneyAmount(tt.args.n); got != tt.want {
+				t.Errorf("getMoneyAmount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
