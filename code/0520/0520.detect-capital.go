d80a5832b74c74a568487d09c0d2bacbbe19bb29
godcong <jumbycc@163.com>
2021-11-13 17:55:55 +0800

    feat(code): update new code

--- code/0520/0520.detect-capital.go
+++ code/0520/0520.detect-capital.go
@@ -1,9 +1,18 @@
 package _0520
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"unicode"
 )
 
 func detectCapitalUse(word string) bool {
+	if len(word) >= 2 && unicode.IsLower(rune(word[0])) && unicode.IsUpper(rune(word[1])) {
+		return false
+	}
 
+	for i := 2; i < len(word); i++ {
+		if unicode.IsLower(rune(word[i])) != unicode.IsLower(rune(word[1])) {
+			return false
+		}
+	}
+	return true
 }
