fbff700f954d319cd0592368064d996c4b82f270
godcong <jumbycc@163.com>
2021-11-14 18:10:35 +0800

    feat(code): update new code

--- code/0677/0677.map-sum-pairs.go
+++ code/0677/0677.map-sum-pairs.go
@@ -1,32 +1,34 @@
 package _0677
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 type MapSum struct {
-
+	m map[string]int
 }
-
 
 func Constructor() MapSum {
-
+	return MapSum{m: make(map[string]int)}
 }
 
-
-func (this *MapSum) Insert(key string, val int)  {
-
+func (this *MapSum) Insert(key string, val int) {
+	this.m[key] = val
 }
-
 
 func (this *MapSum) Sum(prefix string) int {
-
+	sum := 0
+	for s, v := range this.m {
+		if strings.HasPrefix(s, prefix) {
+			sum += v
+		}
+	}
+	return sum
 }
-
 
 /**
  * Your MapSum object will be instantiated and called as such:
  * obj := Constructor();
- * obj.Insert(key,val);
+ * obj.Insert(key,m);
  * param_2 := obj.Sum(prefix);
  */
