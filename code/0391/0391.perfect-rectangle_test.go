41f2fe0d26a9cb5a78e54672fab9bac40950a7ac
godcong <jumbycc@163.com>
2021-11-16 18:17:32 +0800

    feat(code): update new code

---
+++ code/0391/0391.perfect-rectangle_test.go
@@ -0,0 +1,25 @@
+package _0391
+
+import (
+	"testing"
+)
+
+func Test_isRectangleCover(t *testing.T) {
+	type args struct {
+		rectangles [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRectangleCover(tt.args.rectangles); got != tt.want {
+				t.Errorf("isRectangleCover() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
