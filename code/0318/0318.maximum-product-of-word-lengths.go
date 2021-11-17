0fa1dedb0c3ac8a4cc40e30dc805c86b8adf8543
godcong <jumbycc@163.com>
2021-11-17 18:20:17 +0800

    feat(code): update new code

--- code/0318/0318.maximum-product-of-word-lengths.go
+++ code/0318/0318.maximum-product-of-word-lengths.go
@@ -1,9 +1,20 @@
 package _0318
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func maxProduct(words []string) int {
+	ans := 0
+	masks := make([]int, len(words))
+	for i, word := range words {
+		for _, ch := range word {
+			masks[i] |= 1 << (ch - 'a')
+		}
+	}
 
+	for i, x := range masks {
+		for j, y := range masks[:i] {
+			if x&y == 0 && len(words[i])*len(words[j]) > ans {
+				ans = len(words[i]) * len(words[j])
+			}
+		}
+	}
+	return ans
 }
