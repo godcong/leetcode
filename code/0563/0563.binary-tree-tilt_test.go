d35e64062ace217760f77238b8dfa8b530905a40
godcong <jumbycc@163.com>
2021-11-18 18:28:41 +0800

    feat(code): update new code

---
+++ code/0563/0563.binary-tree-tilt_test.go
@@ -0,0 +1,27 @@
+package _0563
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findTilt(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTilt(tt.args.root); got != tt.want {
+				t.Errorf("findTilt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
