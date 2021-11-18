6cbeb8c5d6a8aa218787a2890e62d3495f7b8a7f
godcong <jumbycc@163.com>
2021-11-18 18:28:19 +0800

    feat(code): update new code

--- code/0563/0563.binary-tree-tilt.go
+++ code/0563/0563.binary-tree-tilt.go
@@ -1,6 +1,8 @@
 package _0563
 
 import (
+	"math"
+
 	. "github.com/godcong/leetcode/common"
 )
 
@@ -13,5 +15,21 @@ import (
  * }
  */
 func findTilt(root *TreeNode) int {
+	var ans int
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		sumLeft := dfs(node.Left)
+		sumRight := dfs(node.Right)
+		ans += abs(sumLeft - sumRight)
+		return sumLeft + sumRight + node.Val
+	}
+	dfs(root)
+	return ans
+}
 
+func abs(v int) int {
+	return int(math.Abs(float64(v)))
 }
