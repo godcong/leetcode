39ad8c28e8b31a7f2cbf44dce2c48e1c5339d259
godcong <jumbycc@163.com>
2021-11-19 14:29:40 +0800

    feat(code): update new code

---
+++ code/0397/0397.integer-replacement_test.go
@@ -0,0 +1,25 @@
+package _0397
+
+import (
+	"testing"
+)
+
+func Test_integerReplacement(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := integerReplacement(tt.args.n); got != tt.want {
+				t.Errorf("integerReplacement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
