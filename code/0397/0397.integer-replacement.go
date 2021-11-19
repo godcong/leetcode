41429a72ec594b3536aca9780b09e774fab9c147
godcong <jumbycc@163.com>
2021-11-19 14:29:28 +0800

    feat(code): update new code

--- code/0397/0397.integer-replacement.go
+++ code/0397/0397.integer-replacement.go
@@ -1,9 +1,18 @@
 package _0397
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func integerReplacement(n int) int {
+	if n == 1 {
+		return 0
+	}
+	if n%2 == 0 {
+		return 1 + integerReplacement(n/2)
+	}
+	return 2 + min(integerReplacement(n/2), integerReplacement(n/2+1))
+}
 
+func min(replacement int, replacement2 int) int {
+	if replacement < replacement2 {
+		return replacement
+	}
+	return replacement2
 }
