cc974500a21f9bb04cde123c722cc55cae3a6a65
godcong <jumbycc@163.com>
2021-11-20 14:32:17 +0800

    feat(code): update new code

---
+++ code/0594/0594.longest-harmonious-subsequence_test.go
@@ -0,0 +1,25 @@
+package _0594
+
+import (
+	"testing"
+)
+
+func Test_findLHS(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLHS(tt.args.nums); got != tt.want {
+				t.Errorf("findLHS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
