b045f053d9ea3dfae26ec38e3d76d5fa59d576d3
godcong <jumbycc@163.com>
2021-11-20 14:32:01 +0800

    feat(code): update new code

--- code/0594/0594.longest-harmonious-subsequence.go
+++ code/0594/0594.longest-harmonious-subsequence.go
@@ -1,9 +1,23 @@
 package _0594
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findLHS(nums []int) int {
+	m := make(map[int]int)
+	for _, num := range nums {
+		m[num] += 1
+	}
 
+	var res int
+	for key, value := range m {
+		if nextValue, ok := m[key+1]; ok {
+			res = max(res, value+nextValue)
+		}
+	}
+	return res
+}
+
+func max(res int, i int) int {
+	if res < i {
+		return i
+	}
+	return res
 }
