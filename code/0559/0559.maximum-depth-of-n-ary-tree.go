3f804e95562f7c80eaa3f45db973f4237300ad70
godcong <jumbycc@163.com>
2021-11-21 14:33:31 +0800

    feat(code): add new code(559) maximum-depth-of-n-ary-tree

---
+++ code/0559/0559.maximum-depth-of-n-ary-tree.go
@@ -0,0 +1,17 @@
+package _0559
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func maxDepth(root *Node) int {
+    
+}
