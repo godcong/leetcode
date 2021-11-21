6ba6dce266a27052317b2364dc6715cc694a0057
godcong <jumbycc@163.com>
2021-11-21 14:45:11 +0800

    feat(code): update new code

---
+++ code/0559/0559.maximum-depth-of-n-ary-tree_test.go
@@ -0,0 +1,27 @@
+package _0559
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
