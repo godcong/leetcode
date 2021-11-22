f84e74967cefb0d9a6cc8eb8b54d48020800c7f6
godcong <jumbycc@163.com>
2021-11-22 14:46:11 +0800

    feat(code): add new code(384) shuffle-an-array

---
+++ code/0384/0384.shuffle-an-array.go
@@ -0,0 +1,32 @@
+package _0384
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type Solution struct {
+
+}
+
+
+func Constructor(nums []int) Solution {
+
+}
+
+
+func (this *Solution) Reset() []int {
+
+}
+
+
+func (this *Solution) Shuffle() []int {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Reset();
+ * param_2 := obj.Shuffle();
+ */
