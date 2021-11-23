9447fa5db2dadc449e1b57f3c00670b0b4b04e23
godcong <jumbycc@163.com>
2021-11-23 14:50:22 +0800

    feat(code): update new code

--- code/0859/0859.buddy-strings.go
+++ code/0859/0859.buddy-strings.go
@@ -1,9 +1,32 @@
 package _0859
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func buddyStrings(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
 
+	if s == goal {
+		seen := [26]bool{}
+		for _, ch := range s {
+			if seen[ch-'a'] {
+				return true
+			}
+			seen[ch-'a'] = true
+		}
+		return false
+	}
+
+	first, second := -1, -1
+	for i := range s {
+		if s[i] != goal[i] {
+			if first == -1 {
+				first = i
+			} else if second == -1 {
+				second = i
+			} else {
+				return false
+			}
+		}
+	}
+	return second != -1 && s[first] == goal[second] && s[second] == goal[first]
 }
