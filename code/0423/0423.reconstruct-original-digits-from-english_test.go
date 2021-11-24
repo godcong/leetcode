2e4fbe98a93040fa5b63a0b60b66f69d4cf91f28
godcong <jumbycc@163.com>
2021-11-24 14:52:29 +0800

    feat(code): update new code

---
+++ code/0423/0423.reconstruct-original-digits-from-english_test.go
@@ -0,0 +1,25 @@
+package _0423
+
+import (
+	"testing"
+)
+
+func Test_originalDigits(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := originalDigits(tt.args.s); got != tt.want {
+				t.Errorf("originalDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
