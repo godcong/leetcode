f5d8492ca6a2c5144c0b66592d3675f54153ad25
godcong <jumbycc@163.com>
2021-11-25 14:55:06 +0800

    feat(code): update new code

---
+++ code/0458/0458.poor-pigs_test.go
@@ -0,0 +1,27 @@
+package _0458
+
+import (
+	"testing"
+)
+
+func Test_poorPigs(t *testing.T) {
+	type args struct {
+		buckets       int
+		minutesToDie  int
+		minutesToTest int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := poorPigs(tt.args.buckets, tt.args.minutesToDie, tt.args.minutesToTest); got != tt.want {
+				t.Errorf("poorPigs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
