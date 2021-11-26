61cf5f16bce07a6ff57b86f8cd2f44e4b82aecb9
godcong <jumbycc@163.com>
2021-11-26 14:56:38 +0800

    feat(code): add new code(700) search-in-a-binary-search-tree

---
+++ code/0700/0700.search-in-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package _0700
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+
+}
