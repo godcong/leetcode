4c6259c306d0c23cf0ab380b07542ecac294aa09
godcong <jumbycc@163.com>
2021-11-26 14:58:14 +0800

    feat(code): update new code

---
+++ code/0700/0700.search-in-a-binary-search-tree_test.go
@@ -0,0 +1,29 @@
+package _0700
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_searchBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchBST(tt.args.root, tt.args.val); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
