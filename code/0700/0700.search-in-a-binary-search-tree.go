189fb35e1f6808ea78fa38e18801f17e200625b5
godcong <jumbycc@163.com>
2021-11-26 14:58:03 +0800

    feat(code): update new code

--- code/0700/0700.search-in-a-binary-search-tree.go
+++ code/0700/0700.search-in-a-binary-search-tree.go
@@ -13,5 +13,14 @@ import (
  * }
  */
 func searchBST(root *TreeNode, val int) *TreeNode {
-
+	if root == nil {
+		return nil
+	}
+	if val == root.Val {
+		return root
+	}
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
 }
