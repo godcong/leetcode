e8d212d5c78d3d64416e485c6d32c3739ab97997
godcong <jumbycc@163.com>
2021-11-27 16:09:37 +0800

    feat(code): update new code

--- code/0519/0519.random-flip-matrix.go
+++ code/0519/0519.random-flip-matrix.go
@@ -1,28 +1,36 @@
 package _0519
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math/rand"
 )
 
 type Solution struct {
-
+	m, n, total int
+	mp          map[int]int
 }
-
 
 func Constructor(m int, n int) Solution {
-
+	return Solution{m, n, m * n, map[int]int{}}
 }
-
 
 func (this *Solution) Flip() []int {
-
+	x := rand.Intn(this.total)
+	this.total--
+	ans := []int{x / this.n, x % this.n}
+	if y, ok := this.mp[x]; ok {
+		ans = []int{y / this.n, y % this.n}
+	}
+	this.mp[x] = this.total
+	if y, ok := this.mp[this.total]; ok {
+		this.mp[x] = y
+	}
+	return ans
 }
 
-
-func (this *Solution) Reset()  {
-
+func (this *Solution) Reset() {
+	this.total = this.m * this.n
+	this.mp = map[int]int{}
 }
-
 
 /**
  * Your Solution object will be instantiated and called as such:
