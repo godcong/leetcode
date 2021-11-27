891ef0c5fcb885bffe13a857d994022c83c3bd67
godcong <jumbycc@163.com>
2021-11-27 16:09:45 +0800

    feat(code): update new code

---
+++ code/0519/0519.random-flip-matrix_test.go
@@ -0,0 +1,27 @@
+package _0519
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Solution
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(tt.args.m, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
