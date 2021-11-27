e2943266781d39f2c1f12f18cd8354c7547871fd
godcong <jumbycc@163.com>
2021-11-27 16:05:41 +0800

    feat(code): add new code(519) random-flip-matrix

---
+++ code/0519/0519.random-flip-matrix.go
@@ -0,0 +1,32 @@
+package _0519
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type Solution struct {
+
+}
+
+
+func Constructor(m int, n int) Solution {
+
+}
+
+
+func (this *Solution) Flip() []int {
+
+}
+
+
+func (this *Solution) Reset()  {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(m, n);
+ * param_1 := obj.Flip();
+ * obj.Reset();
+ */
