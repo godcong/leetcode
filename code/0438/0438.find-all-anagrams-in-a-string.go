e48f5adc22987f954908a2aa7c95bb7adec95731
godcong <jumbycc@163.com>
2021-11-28 16:19:14 +0800

    feat(code): update new code

--- code/0438/0438.find-all-anagrams-in-a-string.go
+++ code/0438/0438.find-all-anagrams-in-a-string.go
@@ -1,9 +1,28 @@
 package _0438
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findAnagrams(s string, p string) []int {
+	var ans []int
+	sLen, pLen := len(s), len(p)
+	if sLen < pLen {
+		return ans
+	}
+
+	var sCount, pCount [26]int
+	for i, ch := range p {
+		sCount[s[i]-'a']++
+		pCount[ch-'a']++
+	}
+	if sCount == pCount {
+		ans = append(ans, 0)
+	}
+
+	for i, ch := range s[:sLen-pLen] {
+		sCount[ch-'a']--
+		sCount[s[i+pLen]-'a']++
+		if sCount == pCount {
+			ans = append(ans, i+1)
+		}
+	}
+	return ans
 
 }
