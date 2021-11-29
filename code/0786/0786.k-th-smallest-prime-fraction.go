cbaba40bef4918bbac180f7942091874f28512f6
godcong <jumbycc@163.com>
2021-11-29 16:39:08 +0800

    feat(code): update new code

--- code/0786/0786.k-th-smallest-prime-fraction.go
+++ code/0786/0786.k-th-smallest-prime-fraction.go
@@ -1,9 +1,30 @@
 package _0786
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func kthSmallestPrimeFraction(arr []int, k int) []int {
+	n := len(arr)
+	left, right := 0., 1.
+	for {
+		mid := (left + right) / 2
+		i, count := -1, 0
+		x, y := 0, 1
 
+		for j := 1; j < n; j++ {
+			for float64(arr[i+1])/float64(arr[j]) < mid {
+				i++
+				if arr[i]*y > arr[j]*x {
+					x, y = arr[i], arr[j]
+				}
+			}
+			count += i + 1
+		}
+
+		if count == k {
+			return []int{x, y}
+		}
+		if count < k {
+			left = mid
+		} else {
+			right = mid
+		}
+	}
 }
