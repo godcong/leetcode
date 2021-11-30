b85054fccfbb1829db693ef10dd8e22900a6405d
godcong <jumbycc@163.com>
2021-11-30 16:34:32 +0800

    feat(code): update new code

--- code/0400/0400.nth-digit.go
+++ code/0400/0400.nth-digit.go
@@ -1,9 +1,27 @@
 package _0400
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
+	"sort"
 )
 
-func findNthDigit(n int) int {
+func totalDigits(n int) int {
+	digits := 0
+	for curLength, curCount := 1, 9; curLength <= n; curLength++ {
+		digits += curLength * curCount
+		curCount *= 10
+	}
+	return digits
+}
 
+func findNthDigit(n int) int {
+	d := 1 + sort.Search(8, func(length int) bool {
+		return totalDigits(length+1) >= n
+	})
+	prevDigits := totalDigits(d - 1)
+	index := n - prevDigits - 1
+	start := int(math.Pow10(d - 1))
+	num := start + index/d
+	digitIndex := index % d
+	return num / int(math.Pow10(d-digitIndex-1)) % 10
 }
