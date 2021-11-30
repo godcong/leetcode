2dc7f700a92b55207fde135d3accc500ac638bbb
godcong <jumbycc@163.com>
2021-11-30 16:34:42 +0800

    feat(code): update new code

---
+++ code/0400/0400.nth-digit_test.go
@@ -0,0 +1,25 @@
+package _0400
+
+import (
+	"testing"
+)
+
+func Test_findNthDigit(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNthDigit(tt.args.n); got != tt.want {
+				t.Errorf("findNthDigit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
