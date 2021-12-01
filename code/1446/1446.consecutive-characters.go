3349c2345126e571a9cc27b5646057301e2a614f
godcong <jumbycc@163.com>
2021-12-01 16:36:30 +0800

    feat(code): update new code

--- code/1446/1446.consecutive-characters.go
+++ code/1446/1446.consecutive-characters.go
@@ -1,9 +1,16 @@
 package _1446
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func maxPower(s string) int {
-
+	ans, cnt := 1, 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			cnt++
+			if cnt > ans {
+				ans = cnt
+			}
+		} else {
+			cnt = 1
+		}
+	}
+	return ans
 }
