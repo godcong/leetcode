3146901cbcb0f60053f370183592a38971543936
godcong <jumbycc@163.com>
2021-12-01 16:36:48 +0800

    feat(code): update new code

---
+++ code/1446/1446.consecutive-characters_test.go
@@ -0,0 +1,25 @@
+package _1446
+
+import (
+	"testing"
+)
+
+func Test_maxPower(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxPower(tt.args.s); got != tt.want {
+				t.Errorf("maxPower() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
