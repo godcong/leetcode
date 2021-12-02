7745399a64e20ecf0631e7044de15ebc7f80bdf6
godcong <jumbycc@163.com>
2021-12-02 16:50:50 +0800

    feat(code): update new code

--- code/0506/0506.relative-ranks.go
+++ code/0506/0506.relative-ranks.go
@@ -1,9 +1,36 @@
 package _0506
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
+	"strconv"
 )
 
-func findRelativeRanks(score []int) []string {
+var desc = [3]string{"Gold Medal", "Silver Medal", "Bronze Medal"}
 
+func findRelativeRanks(score []int) []string {
+	const (
+		SHIFT = 20
+		MASK  = (1 << SHIFT) - 1
+	)
+
+	for i, v := range score {
+		score[i] = (v << SHIFT) | i
+	}
+
+	sort.Ints(score)
+
+	var ret = make([]string, len(score))
+
+	var all = len(score) - 1
+	for r, v := range score {
+		ret[v&MASK] = getMedal(all - r)
+	}
+	return ret
+}
+
+func getMedal(rank int) string {
+	if rank >= 3 {
+		return strconv.Itoa(rank + 1)
+	}
+	return desc[rank]
 }
