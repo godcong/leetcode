b2ab7d0a9a3d6c0b38e153c306622e4c1e61bfa1
godcong <jumbycc@163.com>
2021-12-03 17:00:47 +0800

    feat(code): update new code

--- code/1005/1005.maximize-sum-of-array-after-k-negations.go
+++ code/1005/1005.maximize-sum-of-array-after-k-negations.go
@@ -1,9 +1,34 @@
 package _1005
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func largestSumAfterKNegations(nums []int, k int) int {
+	var ans int
+	freq := map[int]int{}
+	for _, num := range nums {
+		freq[num]++
+		ans += num
+	}
+	for i := -100; i < 0 && k != 0; i++ {
+		if freq[i] > 0 {
+			ops := min(k, freq[i])
+			ans -= i * ops * 2
+			freq[-i] += ops
+			k -= ops
+		}
+	}
+	if k > 0 && k%2 == 1 && freq[0] == 0 {
+		for i := 1; i <= 100; i++ {
+			if freq[i] > 0 {
+				ans -= i * 2
+				break
+			}
+		}
+	}
+	return ans
+}
 
+func min(k int, i int) int {
+	if k < i {
+		return k
+	}
+	return i
 }
