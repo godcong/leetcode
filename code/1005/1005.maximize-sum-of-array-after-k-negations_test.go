4e59179341e701bfaf949b477a60c3351393d249
godcong <jumbycc@163.com>
2021-12-03 17:01:55 +0800

    feat(code): update new code

---
+++ code/1005/1005.maximize-sum-of-array-after-k-negations_test.go
@@ -0,0 +1,26 @@
+package _1005
+
+import (
+	"testing"
+)
+
+func Test_largestSumAfterKNegations(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestSumAfterKNegations(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("largestSumAfterKNegations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
