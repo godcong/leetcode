1ee8a4d27f77b555e03c1f1fb31088276a81dd59
godcong <jumbycc@163.com>
2021-12-04 15:33:16 +0800

    feat(code): update new code

--- code/0383/0383.ransom-note.go
+++ code/0383/0383.ransom-note.go
@@ -1,9 +1,18 @@
 package _0383
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func canConstruct(ransomNote string, magazine string) bool {
-
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+	cnt := [26]int{}
+	for _, ch := range magazine {
+		cnt[ch-'a']++
+	}
+	for _, ch := range ransomNote {
+		cnt[ch-'a']--
+		if cnt[ch-'a'] < 0 {
+			return false
+		}
+	}
+	return true
 }
