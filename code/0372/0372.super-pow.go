c35f5f14d587c0070d22a0566f6a6a7739473871
godcong <jumbycc@163.com>
2021-12-05 15:50:25 +0800

    feat(code): update new code

--- code/0372/0372.super-pow.go
+++ code/0372/0372.super-pow.go
@@ -1,9 +1,23 @@
 package _0372
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+const mod = 1337
 
 func superPow(a int, b []int) int {
+	ans := 1
+	for i := len(b) - 1; i >= 0; i-- {
+		ans = ans * pow(a, b[i]) % mod
+		a = pow(a, 10)
+	}
+	return ans
+}
 
+func pow(x, n int) int {
+	res := 1
+	for ; n > 0; n /= 2 {
+		if n&1 > 0 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+	}
+	return res
 }
