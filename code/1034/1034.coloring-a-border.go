3a290f742907705543c29fb28b9d6bb41680d160
godcong <jumbycc@163.com>
2021-12-07 16:03:49 +0800

    feat(code): update new code

--- code/1034/1034.coloring-a-border.go
+++ code/1034/1034.coloring-a-border.go
@@ -1,9 +1,38 @@
 package _1034
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+var dirs = [4]struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
 func colorBorder(grid [][]int, row int, col int, color int) [][]int {
+	m, n := len(grid), len(grid[0])
+	type point struct{ x, y int }
+	var borders []point
+	originalColor := grid[row][col]
+	vis := make([][]bool, m)
+	for i := range vis {
+		vis[i] = make([]bool, n)
+	}
 
+	var dfs func(int, int)
+	dfs = func(x, y int) {
+		vis[x][y] = true
+		isBorder := false
+		for i := range dirs {
+			nx, ny := x+dirs[i].x, y+dirs[i].y
+			if !(0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == originalColor) {
+				isBorder = true
+			} else if !vis[nx][ny] {
+				vis[nx][ny] = true
+				dfs(nx, ny)
+			}
+		}
+		if isBorder {
+			borders = append(borders, point{x, y})
+		}
+	}
+	dfs(row, col)
+
+	for _, p := range borders {
+		grid[p.x][p.y] = color
+	}
+	return grid
 }
