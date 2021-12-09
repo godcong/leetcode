002bd7d43ec93de3083e31410c469f7e9957523d
godcong <jumbycc@163.com>
2021-12-09 16:10:52 +0800

    feat(code): update new code

---
+++ code/0794/0794.valid-tic-tac-toe-state_test.go
@@ -0,0 +1,25 @@
+package _0794
+
+import (
+	"testing"
+)
+
+func Test_validTicTacToe(t *testing.T) {
+	type args struct {
+		board []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validTicTacToe(tt.args.board); got != tt.want {
+				t.Errorf("validTicTacToe() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
