cbd5c4e0b4f038e7d7ba45f6ca7936e021a8070d
godcong <jumbycc@163.com>
2021-12-10 16:14:13 +0800

    feat(code): update new code

--- code/0748/0748.shortest-completing-word.go
+++ code/0748/0748.shortest-completing-word.go
@@ -1,9 +1,31 @@
 package _0748
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"unicode"
 )
 
 func shortestCompletingWord(licensePlate string, words []string) string {
-
+	cnt := [26]int{}
+	for _, ch := range licensePlate {
+		if unicode.IsLetter(ch) {
+			cnt[unicode.ToLower(ch)-'a']++
+		}
+	}
+	var ans string
+next:
+	for _, word := range words {
+		c := [26]int{}
+		for _, ch := range word {
+			c[ch-'a']++
+		}
+		for i := 0; i < 26; i++ {
+			if c[i] < cnt[i] {
+				continue next
+			}
+		}
+		if ans == "" || len(word) < len(ans) {
+			ans = word
+		}
+	}
+	return ans
 }
