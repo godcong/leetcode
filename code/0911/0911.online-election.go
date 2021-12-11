8731da7aa95d803e11906190676b52a264fede6f
godcong <jumbycc@163.com>
2021-12-11 12:53:32 +0800

    feat(code): update new code

--- code/0911/0911.online-election.go
+++ code/0911/0911.online-election.go
@@ -1,23 +1,31 @@
 package _0911
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 type TopVotedCandidate struct {
-
+	tops, times []int
 }
-
 
 func Constructor(persons []int, times []int) TopVotedCandidate {
+	tops := []int{}
+	top := -1
+	voteCounts := map[int]int{-1: -1}
+	for _, p := range persons {
+		voteCounts[p]++
+		if voteCounts[p] >= voteCounts[top] {
+			top = p
+		}
+		tops = append(tops, top)
+	}
+	return TopVotedCandidate{tops, times}
 
 }
-
 
 func (this *TopVotedCandidate) Q(t int) int {
-
+	return this.tops[sort.SearchInts(this.times, t+1)-1]
 }
-
 
 /**
  * Your TopVotedCandidate object will be instantiated and called as such:
