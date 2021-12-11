054487b4b43703a01e63bb4c7db8793e2d1945c2
godcong <jumbycc@163.com>
2021-12-11 12:48:35 +0800

    feat(code): add new code(911) online-election

---
+++ code/0911/0911.online-election.go
@@ -0,0 +1,26 @@
+package _0911
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type TopVotedCandidate struct {
+
+}
+
+
+func Constructor(persons []int, times []int) TopVotedCandidate {
+
+}
+
+
+func (this *TopVotedCandidate) Q(t int) int {
+
+}
+
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * obj := Constructor(persons, times);
+ * param_1 := obj.Q(t);
+ */
