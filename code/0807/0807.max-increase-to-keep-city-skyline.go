0309837257c846f6c57a957b9cddce4fbfcafbc1
godcong <jumbycc@163.com>
2021-12-13 12:57:06 +0800

    feat(code): update new code

--- code/0807/0807.max-increase-to-keep-city-skyline.go
+++ code/0807/0807.max-increase-to-keep-city-skyline.go
@@ -1,9 +1,34 @@
 package _0807
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func maxIncreaseKeepingSkyline(grid [][]int) int {
+	n := len(grid)
+	rowMax := make([]int, n)
+	colMax := make([]int, n)
+	ans := 0
+	for i, row := range grid {
+		for j, h := range row {
+			rowMax[i] = max(rowMax[i], h)
+			colMax[j] = max(colMax[j], h)
+		}
+	}
+	for i, row := range grid {
+		for j, h := range row {
+			ans += min(rowMax[i], colMax[j]) - h
+		}
+	}
+	return ans
+}
 
+func min(i int, i2 int) int {
+	if i < i2 {
+		return i
+	}
+	return i2
+}
+
+func max(i int, i2 int) int {
+	if i > i2 {
+		return i
+	}
+	return i2
 }
