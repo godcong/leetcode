49406d9a156456e6c13cd214c4405723328279d6
godcong <jumbycc@163.com>
2021-12-13 12:57:16 +0800

    feat(code): update new code

---
+++ code/0807/0807.max-increase-to-keep-city-skyline_test.go
@@ -0,0 +1,25 @@
+package _0807
+
+import (
+	"testing"
+)
+
+func Test_maxIncreaseKeepingSkyline(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxIncreaseKeepingSkyline(tt.args.grid); got != tt.want {
+				t.Errorf("maxIncreaseKeepingSkyline() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
