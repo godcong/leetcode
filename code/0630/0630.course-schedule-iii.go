ee4affe0d9670bdd78a387bb96948fefe8885940
godcong <jumbycc@163.com>
2021-12-14 13:01:38 +0800

    feat(code): update new code

--- code/0630/0630.course-schedule-iii.go
+++ code/0630/0630.course-schedule-iii.go
@@ -1,9 +1,45 @@
 package _0630
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"container/heap"
+	"sort"
 )
 
 func scheduleCourse(courses [][]int) int {
+	sort.Slice(courses, func(i, j int) bool {
+		return courses[i][1] < courses[j][1]
+	})
 
+	h := &interHeap{}
+	total := 0
+	for _, course := range courses {
+		if t := course[0]; total+t <= course[1] {
+			total += t
+			heap.Push(h, t)
+		} else if h.Len() > 0 && t < h.IntSlice[0] {
+			total += t - h.IntSlice[0]
+			h.IntSlice[0] = t
+			heap.Fix(h, 0)
+		}
+	}
+	return h.Len()
+}
+
+type interHeap struct {
+	sort.IntSlice
+}
+
+func (h interHeap) Less(i, j int) bool {
+	return h.IntSlice[i] > h.IntSlice[j]
+}
+
+func (h *interHeap) Push(x interface{}) {
+	h.IntSlice = append(h.IntSlice, x.(int))
+}
+
+func (h *interHeap) Pop() interface{} {
+	a := h.IntSlice
+	x := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return x
 }
