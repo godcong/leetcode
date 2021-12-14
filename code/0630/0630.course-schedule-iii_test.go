33c259107e2195bf06fee968fb8492c0a49a48cd
godcong <jumbycc@163.com>
2021-12-14 13:01:50 +0800

    feat(code): update new code

---
+++ code/0630/0630.course-schedule-iii_test.go
@@ -0,0 +1,25 @@
+package _0630
+
+import (
+	"testing"
+)
+
+func Test_scheduleCourse(t *testing.T) {
+	type args struct {
+		courses [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := scheduleCourse(tt.args.courses); got != tt.want {
+				t.Errorf("scheduleCourse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
