c85886ca941edf2f35744940c15cc49f2dfa138f
godcong <jumbycc@163.com>
2021-12-15 13:11:12 +0800

    feat(code): update new code

---
+++ code/0851/0851.loud-and-rich_test.go
@@ -0,0 +1,27 @@
+package _0851
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_loudAndRich(t *testing.T) {
+	type args struct {
+		richer [][]int
+		quiet  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := loudAndRich(tt.args.richer, tt.args.quiet); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loudAndRich() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
