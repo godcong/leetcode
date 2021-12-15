970875daabcb9e2859ea095b47625885bd56298d
godcong <jumbycc@163.com>
2021-12-15 13:11:04 +0800

    feat(code): update new code

--- code/0851/0851.loud-and-rich.go
+++ code/0851/0851.loud-and-rich.go
@@ -1,9 +1,31 @@
 package _0851
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func loudAndRich(richer [][]int, quiet []int) []int {
+	n := len(quiet)
+	g := make([][]int, n)
+	for _, r := range richer {
+		g[r[1]] = append(g[r[1]], r[0])
+	}
 
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	var dfs func(int)
+	dfs = func(x int) {
+		if ans[x] != -1 {
+			return
+		}
+		ans[x] = x
+		for _, y := range g[x] {
+			dfs(y)
+			if quiet[ans[y]] < quiet[ans[x]] {
+				ans[x] = ans[y]
+			}
+		}
+	}
+	for i := 0; i < n; i++ {
+		dfs(i)
+	}
+	return ans
 }
