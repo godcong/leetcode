e00dc3da62fa53fabebd7b090ca7c934850cfc2f
godcong <jumbycc@163.com>
2021-12-19 13:05:06 +0800

    feat(code): update new code

--- code/0997/0997.find-the-town-judge.go
+++ code/0997/0997.find-the-town-judge.go
@@ -1,9 +1,27 @@
 package _0997
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+type degrees struct {
+	in, out int
+}
 
 func findJudge(n int, trust [][]int) int {
+	d := make([]degrees, n)
+	var degreesMax int
+	for _, t := range trust {
+		d[t[1]-1].in++
+		d[t[0]-1].out++
+		degreesMax = updateDegreesMax(d, t[1]-1, degreesMax)
+	}
 
+	if d[degreesMax].in == n-1 && d[degreesMax].out == 0 {
+		return degreesMax
+	}
+	return -1
+}
+
+func updateDegreesMax(d []degrees, l, r int) int {
+	if d[l].in > d[r].in {
+		return l
+	}
+	return r
 }
