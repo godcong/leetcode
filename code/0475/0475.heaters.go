6d4ed6c7ee28d4090e50b1660d1d828e73bd4990
godcong <jumbycc@163.com>
2021-12-20 13:32:58 +0800

    feat(code): update new code

--- code/0475/0475.heaters.go
+++ code/0475/0475.heaters.go
@@ -1,9 +1,27 @@
 package _0475
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
+	"sort"
 )
 
 func findRadius(houses []int, heaters []int) int {
+	sort.Ints(heaters)
+	ans := 0
+	for _, house := range houses {
+		j := sort.SearchInts(heaters, house+1)
+		minDis := math.MaxInt32
+		if j < len(heaters) {
+			minDis = heaters[j] - house
+		}
+		i := j - 1
+		if i >= 0 && house-heaters[i] < minDis {
+			minDis = house - heaters[i]
+		}
+		if minDis > ans {
+			ans = minDis
+		}
+	}
 
+	return ans
 }
