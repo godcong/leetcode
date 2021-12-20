b3f143151fec4236ba3cc607b72af852d144cd9f
godcong <jumbycc@163.com>
2021-12-20 13:33:07 +0800

    feat(code): update new code

---
+++ code/0475/0475.heaters_test.go
@@ -0,0 +1,26 @@
+package _0475
+
+import (
+	"testing"
+)
+
+func Test_findRadius(t *testing.T) {
+	type args struct {
+		houses  []int
+		heaters []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRadius(tt.args.houses, tt.args.heaters); got != tt.want {
+				t.Errorf("findRadius() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
