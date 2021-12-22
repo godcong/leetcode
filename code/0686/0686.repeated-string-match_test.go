07506c7483f6428ddeb548ce97eaf16164ed9cea
godcong <jumbycc@163.com>
2021-12-22 14:11:19 +0800

    feat(code): update new code

---
+++ code/0686/0686.repeated-string-match_test.go
@@ -0,0 +1,26 @@
+package _0686
+
+import (
+	"testing"
+)
+
+func Test_repeatedStringMatch(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := repeatedStringMatch(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("repeatedStringMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
