c6bf69c1f83f1a345fdf5dd6b47c699848fa26f8
godcong <jumbycc@163.com>
2021-12-22 14:10:26 +0800

    feat(code): update new code

--- code/0686/0686.repeated-string-match.go
+++ code/0686/0686.repeated-string-match.go
@@ -1,9 +1,42 @@
 package _0686
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func repeatedStringMatch(a string, b string) int {
+	an, bn := len(a), len(b)
+	index := strStr(a, b)
+	if index == -1 {
+		return -1
+	}
+	if an-index >= bn {
+		return 1
+	}
+	return (bn+index-an-1)/an + 2
+}
 
+func strStr(haystack, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
+	}
+	pi := make([]int, m)
+	for i, j := 1, 0; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = pi[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		pi[i] = j
+	}
+	for i, j := 0, 0; i-j < n; i++ {
+		for j > 0 && haystack[i%n] != needle[j] {
+			j = pi[j-1]
+		}
+		if haystack[i%n] == needle[j] {
+			j++
+		}
+		if j == m {
+			return i - m + 1
+		}
+	}
+	return -1
 }
