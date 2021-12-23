464cf08837f6fe876552862a5bf0f0ba9847a077
godcong <jumbycc@163.com>
2021-12-23 14:17:42 +0800

    feat(code): update new code

--- code/1044/1044.longest-duplicate-substring.go
+++ code/1044/1044.longest-duplicate-substring.go
@@ -1,9 +1,36 @@
 package _1044
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"index/suffixarray"
+	"reflect"
+	"unsafe"
 )
 
 func longestDupSubstring(s string) string {
+	n := len(s)
+	sa := *(*[]int32)(unsafe.Pointer(reflect.ValueOf(suffixarray.New([]byte(s))).Elem().FieldByName("sa").Field(0).UnsafeAddr()))
+	rank := make([]int, n)
+	for i := range rank {
+		rank[sa[i]] = i
+	}
+	height := make([]int, n)
+	h := 0
+	for i, rk := range rank {
+		if h > 0 {
+			h--
+		}
+		if rk > 0 {
+			for j := int(sa[rk-1]); i+h < n && j+h < n && s[i+h] == s[j+h]; h++ {
+			}
+		}
+		height[rk] = h
+	}
 
+	idx, maxH := 0, 0
+	for i, h := range height {
+		if h > maxH {
+			idx, maxH = i, h
+		}
+	}
+	return s[sa[idx] : int(sa[idx])+maxH]
 }
