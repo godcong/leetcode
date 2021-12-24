7472ccf90031d91084df8760a7e78cae5b4bcab0
godcong <jumbycc@163.com>
2021-12-24 14:33:07 +0800

    feat(code): update new code

--- code/1705/1705.maximum-number-of-eaten-apples.go
+++ code/1705/1705.maximum-number-of-eaten-apples.go
@@ -4,40 +4,6 @@ import (
 	"container/heap"
 )
 
-func eatenApples(apples []int, days []int) int {
-	h := hp{}
-	i := 0
-	ans := 0
-	for ; i < len(apples); i++ {
-		for len(h) > 0 && h[0].end <= i {
-			heap.Pop(&h)
-		}
-		if apples[i] > 0 {
-			heap.Push(&h, pair{i + days[i], apples[i]})
-		}
-		if len(h) > 0 {
-			h[0].left--
-			if h[0].left == 0 {
-				heap.Pop(&h)
-			}
-			ans++
-		}
-	}
-	for len(h) > 0 {
-		for len(h) > 0 && h[0].end <= i {
-			heap.Pop(&h)
-		}
-		if len(h) == 0 {
-			break
-		}
-		p := heap.Pop(&h).(pair)
-		num := min(p.end-i, p.left)
-		ans += num
-		i += num
-	}
-	return ans
-}
-
 type pair struct{ end, left int }
 type hp []pair
 
@@ -46,10 +12,25 @@ func (h hp) Less(i, j int) bool  { return h[i].end < h[j].end }
 func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
 func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
 func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+func (h *hp) push(v pair)        { heap.Push(h, v) }
+func (h *hp) pop() pair          { return heap.Pop(h).(pair) }
 
-func min(a, b int) int {
-	if a > b {
-		return b
+func eatenApples(apples, days []int) int {
+	ans := 0
+	for i, n, h := 0, len(apples), (hp{}); i < n || len(h) > 0; i++ {
+		if i < n && apples[i] > 0 {
+			h.push(pair{i + days[i], apples[i]})
+		}
+		for len(h) > 0 && h[0].end <= i {
+			h.pop()
+		}
+		if len(h) > 0 {
+			h[0].left--
+			if h[0].left == 0 {
+				h.pop()
+			}
+			ans++
+		}
 	}
-	return a
+	return ans
 }
