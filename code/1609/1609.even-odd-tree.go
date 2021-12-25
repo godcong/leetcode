1d13706fcbaed2b14d2c2015ae9ddca497402181
godcong <jumbycc@163.com>
2021-12-25 14:23:23 +0800

    feat(code): update new code

--- code/1609/1609.even-odd-tree.go
+++ code/1609/1609.even-odd-tree.go
@@ -1,6 +1,9 @@
 package _1609
 
 import (
+	"math"
+	"runtime/debug"
+
 	. "github.com/godcong/leetcode/common"
 )
 
@@ -13,5 +16,31 @@ import (
  * }
  */
 func isEvenOddTree(root *TreeNode) bool {
-
+	queue := []*TreeNode{root}
+	level := 0
+	for queue != nil {
+		temp := queue
+		queue = nil
+		prev := 0
+		if level%2 == 1 {
+			prev = math.MaxInt32
+		}
+		for _, cur := range temp {
+			val := cur.Val
+			if val%2 == level%2 || level%2 == 0 && val <= prev || level%2 == 1 && val >= prev {
+				return false
+			}
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+			prev = val
+		}
+		level++
+	}
+	return true
 }
+
+func init() { debug.SetGCPercent(-1) }
