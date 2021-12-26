c93e15dc6d3a8a32a77fe4e7b17db6e600e57a5b
godcong <jumbycc@163.com>
2021-12-26 14:32:40 +0800

    feat(code): update new code

--- code/1078/1078.occurrences-after-bigram.go
+++ code/1078/1078.occurrences-after-bigram.go
@@ -1,9 +1,16 @@
 package _1078
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 func findOcurrences(text string, first string, second string) []string {
-
+	words := strings.Split(text, " ")
+	ans := make([]string, 0)
+	for i := 2; i < len(words); i++ {
+		if words[i-2] == first && words[i-1] == second {
+			ans = append(ans, words[i])
+		}
+	}
+	return ans
 }
