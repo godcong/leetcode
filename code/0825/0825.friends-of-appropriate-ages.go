8c7993e985890a965a0af954595fb7ce1ae6d818
godcong <jumbycc@163.com>
2021-12-27 14:37:29 +0800

    feat(code): update new code

--- code/0825/0825.friends-of-appropriate-ages.go
+++ code/0825/0825.friends-of-appropriate-ages.go
@@ -1,9 +1,21 @@
 package _0825
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+const mx = 121
 
 func numFriendRequests(ages []int) int {
+	var cnt, pre [mx]int
+	for _, age := range ages {
+		cnt[age]++
+	}
+	for i := 1; i < mx; i++ {
+		pre[i] = pre[i-1] + cnt[i]
+	}
 
+	var res int
+	for i := 15; i < mx; i++ {
+		if cnt[i] > 0 {
+			res += cnt[i] * (pre[i] - pre[i/2+7] - 1)
+		}
+	}
+	return res
 }
