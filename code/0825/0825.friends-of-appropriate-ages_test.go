ee969969c046f34c688d8afc7499bf1907d92a11
godcong <jumbycc@163.com>
2021-12-27 14:38:06 +0800

    feat(code): update new code

---
+++ code/0825/0825.friends-of-appropriate-ages_test.go
@@ -0,0 +1,25 @@
+package _0825
+
+import (
+	"testing"
+)
+
+func Test_numFriendRequests(t *testing.T) {
+	type args struct {
+		ages []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numFriendRequests(tt.args.ages); got != tt.want {
+				t.Errorf("numFriendRequests() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
