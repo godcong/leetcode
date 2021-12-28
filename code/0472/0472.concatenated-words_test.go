6e75fedf5ab5475914cec9b7fe474543d0e4c854
godcong <jumbycc@163.com>
2021-12-28 15:35:00 +0800

    feat(code): update new code

---
+++ code/0472/0472.concatenated-words_test.go
@@ -0,0 +1,26 @@
+package _0472
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findAllConcatenatedWordsInADict(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findAllConcatenatedWordsInADict(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findAllConcatenatedWordsInADict() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
