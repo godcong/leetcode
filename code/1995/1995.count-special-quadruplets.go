38da233b52f8968d4ff02bfde25cecc0bc45903c
godcong <jumbycc@163.com>
2021-12-29 15:42:52 +0800

    feat(code): update new code

--- code/1995/1995.count-special-quadruplets.go
+++ code/1995/1995.count-special-quadruplets.go
@@ -1,9 +1,16 @@
 package _1995
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countQuadruplets(nums []int) int {
+	cnt := map[int]int{}
+	ans := 0
+	for i := 1; i < len(nums)-2; i++ {
+		for j := 0; j < i; j++ {
+			cnt[nums[j]+nums[i]]++
+		}
+		for j := i + 2; j < len(nums); j++ {
+			ans += cnt[nums[j]-nums[i+1]]
+		}
+	}
 
+	return ans
 }
