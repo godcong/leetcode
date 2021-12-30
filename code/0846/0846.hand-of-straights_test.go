97e71a418bcc4a2313a3e2ef80d43c5b18285bc1
godcong <jumbycc@163.com>
2021-12-30 15:47:06 +0800

    feat(code): update new code

---
+++ code/0846/0846.hand-of-straights_test.go
@@ -0,0 +1,26 @@
+package _0846
+
+import (
+	"testing"
+)
+
+func Test_isNStraightHand(t *testing.T) {
+	type args struct {
+		hand      []int
+		groupSize int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isNStraightHand(tt.args.hand, tt.args.groupSize); got != tt.want {
+				t.Errorf("isNStraightHand() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
