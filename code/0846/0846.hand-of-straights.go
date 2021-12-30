c4bd3394a4db49ec6d37e48590641f0cd2cc0ef9
godcong <jumbycc@163.com>
2021-12-30 15:46:56 +0800

    feat(code): update new code

--- code/0846/0846.hand-of-straights.go
+++ code/0846/0846.hand-of-straights.go
@@ -1,9 +1,46 @@
 package _0846
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func isNStraightHand(hand []int, groupSize int) bool {
+	if len(hand)%groupSize != 0 {
+		return false
+	}
+	if groupSize == 1 {
+		return true
+	}
 
+	sort.Ints(hand)
+	for i := 0; i < len(hand); i++ {
+		if hand[i] == -1 {
+			continue
+		}
+
+		value, cnt, isFull := hand[i], 0, false
+		for j := i; j < len(hand); j++ {
+			if hand[j] == -1 {
+				continue
+			}
+
+			if hand[j] == value+cnt {
+				hand[j] = -1
+				cnt++
+				if cnt == groupSize {
+					isFull = true
+					break
+				}
+			}
+
+			if hand[j] >= value+groupSize {
+				return false
+			}
+		}
+
+		if !isFull {
+			return false
+		}
+	}
+	return true
 }
