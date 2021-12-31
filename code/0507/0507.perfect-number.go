8fefa668a73b5771d1d18c2ef77dabe9dca5c624
godcong <jumbycc@163.com>
2021-12-31 15:48:47 +0800

    feat(code): update new code

--- code/0507/0507.perfect-number.go
+++ code/0507/0507.perfect-number.go
@@ -1,9 +1,18 @@
 package _0507
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func checkPerfectNumber(num int) bool {
+	if num == 1 {
+		return false
+	}
 
+	sum := 1
+	for d := 2; d*d <= num; d++ {
+		if num%d == 0 {
+			sum += d
+			if d*d < num {
+				sum += num / d
+			}
+		}
+	}
+	return sum == num
 }
