6c858d503d59a6d01a220370864652aae8416133
godcong <jumbycc@163.com>
2021-12-31 15:48:55 +0800

    feat(code): update new code

---
+++ code/0507/0507.perfect-number_test.go
@@ -0,0 +1,25 @@
+package _0507
+
+import (
+	"testing"
+)
+
+func Test_checkPerfectNumber(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkPerfectNumber(tt.args.num); got != tt.want {
+				t.Errorf("checkPerfectNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
