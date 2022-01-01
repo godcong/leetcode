edb3334fdb011df9ab2efa982fd04415baa98f07
godcong <jumbycc@163.com>
2022-01-01 20:24:03 +0800

    feat(code): update new code

--- code/2022/2022.convert-1d-array-into-2d-array.go
+++ code/2022/2022.convert-1d-array-into-2d-array.go
@@ -1,9 +1,17 @@
 package _2022
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func construct2DArray(original []int, m int, n int) [][]int {
+	res := make([][]int, 0)
+	if len(original) != m*n {
+		return res
+	}
+	for i := 0; i < m; i++ {
+		temp := make([]int, n)
+		for j := 0; j < n; j++ {
+			temp[j] = original[i*n+j]
+		}
+		res = append(res, temp)
+	}
 
+	return res
 }
