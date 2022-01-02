36c941f7474e81735d78b0e6dcb582d109b57153
godcong <jumbycc@163.com>
2022-01-02 20:27:34 +0800

    feat(code): update new code

--- code/0390/0390.elimination-game.go
+++ code/0390/0390.elimination-game.go
@@ -1,9 +1,19 @@
 package _0390
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func lastRemaining(n int) int {
-
+	a1 := 1
+	k, cnt, step := 0, n, 1
+	for cnt > 1 {
+		if k%2 == 0 {
+			a1 += step
+		} else {
+			if cnt%2 == 1 {
+				a1 += step
+			}
+		}
+		k++
+		cnt >>= 1
+		step <<= 1
+	}
+	return a1
 }
