62bef73582b88614ad66222f403636ad3dabbb5e
godcong <jumbycc@163.com>
2022-01-03 20:31:23 +0800

    feat(code): update new code

---
+++ code/1185/1185.day-of-the-week_test.go
@@ -0,0 +1,27 @@
+package _1185
+
+import (
+	"testing"
+)
+
+func Test_dayOfTheWeek(t *testing.T) {
+	type args struct {
+		day   int
+		month int
+		year  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := dayOfTheWeek(tt.args.day, tt.args.month, tt.args.year); got != tt.want {
+				t.Errorf("dayOfTheWeek() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
