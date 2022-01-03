47f3f91fbfe54e95c67551e573b90a585f2adeec
godcong <jumbycc@163.com>
2022-01-03 20:31:09 +0800

    feat(code): update new code

--- code/1185/1185.day-of-the-week.go
+++ code/1185/1185.day-of-the-week.go
@@ -1,9 +1,20 @@
 package _1185
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+var week = []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+var monthDays = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30}
 
 func dayOfTheWeek(day int, month int, year int) string {
+	days := 0
 
+	days += 365*(year-1971) + (year-1969)/4
+
+	for _, d := range monthDays[:month-1] {
+		days += d
+	}
+	if month >= 3 && (year%400 == 0 || year%4 == 0 && year%100 != 0) {
+		days++
+	}
+
+	days += day
+	return week[(days+3)%7]
 }
