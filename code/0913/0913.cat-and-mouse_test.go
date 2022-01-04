9567c26c673a067c98ea0005256176b4d805b3b8
godcong <jumbycc@163.com>
2022-01-04 20:37:01 +0800

    feat(code): update new code

---
+++ code/0913/0913.cat-and-mouse_test.go
@@ -0,0 +1,25 @@
+package _0913
+
+import (
+	"testing"
+)
+
+func Test_catMouseGame(t *testing.T) {
+	type args struct {
+		graph [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := catMouseGame(tt.args.graph); got != tt.want {
+				t.Errorf("catMouseGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
