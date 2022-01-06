a6548c3ae9908701d03d22c859c78d7e9d50f018
godcong <jumbycc@163.com>
2022-01-06 20:45:21 +0800

    feat(code): update new code

---
+++ code/0071/0071.simplify-path_test.go
@@ -0,0 +1,25 @@
+package _0071
+
+import (
+	"testing"
+)
+
+func Test_simplifyPath(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := simplifyPath(tt.args.path); got != tt.want {
+				t.Errorf("simplifyPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
