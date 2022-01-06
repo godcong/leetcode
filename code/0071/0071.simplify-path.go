795df05920b8b9a6db8e66a7d69021789773ff46
godcong <jumbycc@163.com>
2022-01-06 20:45:05 +0800

    feat(code): update new code

--- code/0071/0071.simplify-path.go
+++ code/0071/0071.simplify-path.go
@@ -1,9 +1,19 @@
 package _0071
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 func simplifyPath(path string) string {
-
+	var stack []string
+	for _, name := range strings.Split(path, "/") {
+		if name == ".." {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		} else if name != "" && name != "." {
+			stack = append(stack, name)
+		}
+	}
+	return "/" + strings.Join(stack, "/")
 }
