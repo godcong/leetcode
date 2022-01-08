7355476dee5462c4f14b81d59d58368cb2a27047
godcong <jumbycc@163.com>
2022-01-08 20:17:10 +0800

    feat(code): update new code

--- code/0089/0089.gray-code.go
+++ code/0089/0089.gray-code.go
@@ -1,9 +1,9 @@
 package _0089
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func grayCode(n int) []int {
-
+	ans := make([]int, 1<<n)
+	for i := range ans {
+		ans[i] = i>>1 ^ i
+	}
+	return ans
 }
