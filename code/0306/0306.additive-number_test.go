9cb1b2d1177f713110e67ac12abb73378c0a962b
godcong <jumbycc@163.com>
2022-01-10 20:24:37 +0800

    feat(code): update new code

---
+++ code/0306/0306.additive-number_test.go
@@ -0,0 +1,25 @@
+package _0306
+
+import (
+	"testing"
+)
+
+func Test_isAdditiveNumber(t *testing.T) {
+	type args struct {
+		num string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAdditiveNumber(tt.args.num); got != tt.want {
+				t.Errorf("isAdditiveNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
