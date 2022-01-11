c9f446de4dbe76bd7195e768c2c64034ac95f586
godcong <jumbycc@163.com>
2022-01-11 20:25:49 +0800

    feat(code): update new code

---
+++ code/1036/1036.escape-a-large-maze_test.go
@@ -0,0 +1,27 @@
+package _1036
+
+import (
+	"testing"
+)
+
+func Test_isEscapePossible(t *testing.T) {
+	type args struct {
+		blocked [][]int
+		source  []int
+		target  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isEscapePossible(tt.args.blocked, tt.args.source, tt.args.target); got != tt.want {
+				t.Errorf("isEscapePossible() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
