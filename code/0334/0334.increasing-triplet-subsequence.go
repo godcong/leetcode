0e54abf122326764cab4f9cf81795348162781c3
godcong <jumbycc@163.com>
2022-01-12 20:42:07 +0800

    feat(code): update new code

--- code/0334/0334.increasing-triplet-subsequence.go
+++ code/0334/0334.increasing-triplet-subsequence.go
@@ -1,9 +1,24 @@
 package _0334
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
 )
 
 func increasingTriplet(nums []int) bool {
-
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	first, second := nums[0], math.MaxInt32
+	for i := 1; i < n; i++ {
+		num := nums[i]
+		if num > second {
+			return true
+		} else if num > first {
+			second = num
+		} else {
+			first = num
+		}
+	}
+	return false
 }
