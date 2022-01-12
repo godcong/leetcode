ec709093c3a1702869e9f4e1abbf8a0567c1bd00
godcong <jumbycc@163.com>
2022-01-12 20:39:53 +0800

    feat(code): update new code

---
+++ code/0334/0334.increasing-triplet-subsequence_test.go
@@ -0,0 +1,25 @@
+package _0334
+
+import (
+	"testing"
+)
+
+func Test_increasingTriplet(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := increasingTriplet(tt.args.nums); got != tt.want {
+				t.Errorf("increasingTriplet() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
