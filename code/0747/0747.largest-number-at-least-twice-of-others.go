65cba7b5b8eddcee35924d4a941a3b2ae77ad0bb
godcong <jumbycc@163.com>
2022-01-13 20:44:40 +0800

    feat(code): update new code

--- code/0747/0747.largest-number-at-least-twice-of-others.go
+++ code/0747/0747.largest-number-at-least-twice-of-others.go
@@ -1,9 +1,16 @@
 package _0747
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func dominantIndex(nums []int) int {
-
+	m1, m2, idx := -1, -1, 0
+	for i, num := range nums {
+		if num > m1 {
+			m1, m2, idx = num, m1, i
+		} else if num > m2 {
+			m2 = num
+		}
+	}
+	if m1 >= m2*2 {
+		return idx
+	}
+	return -1
 }
