79c11714dec92d487bd5f5a33071bf20e1150967
godcong <jumbycc@163.com>
2022-01-13 20:44:50 +0800

    feat(code): update new code

---
+++ code/0747/0747.largest-number-at-least-twice-of-others_test.go
@@ -0,0 +1,25 @@
+package _0747
+
+import (
+	"testing"
+)
+
+func Test_dominantIndex(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := dominantIndex(tt.args.nums); got != tt.want {
+				t.Errorf("dominantIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
