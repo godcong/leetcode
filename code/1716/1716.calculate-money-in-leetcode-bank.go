8095d8905809dfc69169549a3a5e651122b4faf0
godcong <jumbycc@163.com>
2022-01-15 13:37:54 +0800

    feat(code): update new code

--- code/1716/1716.calculate-money-in-leetcode-bank.go
+++ code/1716/1716.calculate-money-in-leetcode-bank.go
@@ -1,9 +1,15 @@
 package _1716
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func totalMoney(n int) int {
-
+	week, day := 1, 1
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans += week + day - 1
+		day++
+		if day == 8 {
+			day = 1
+			week++
+		}
+	}
+	return ans
 }
