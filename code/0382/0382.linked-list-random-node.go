941f168b6e9293115cc76a86213f2972f5ffb02c
godcong <jumbycc@163.com>
2022-01-16 13:41:04 +0800

    feat(code): update new code

--- code/0382/0382.linked-list-random-node.go
+++ code/0382/0382.linked-list-random-node.go
@@ -1,6 +1,8 @@
 package _0382
 
 import (
+	"math/rand"
+
 	. "github.com/godcong/leetcode/common"
 )
 
@@ -12,19 +14,20 @@ import (
  * }
  */
 type Solution struct {
-
+	s []int
 }
-
 
 func Constructor(head *ListNode) Solution {
-
+	s := Solution{}
+	for p := head; p != nil; p = p.Next {
+		s.s = append(s.s, p.Val)
+	}
+	return s
 }
-
 
 func (this *Solution) GetRandom() int {
-
+	return this.s[rand.Intn(len(this.s))]
 }
-
 
 /**
  * Your Solution object will be instantiated and called as such:
