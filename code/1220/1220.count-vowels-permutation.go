026ba1a9fc454dfbec0b85418c04ca0636e823cc
godcong <jumbycc@163.com>
2022-01-17 13:43:26 +0800

    feat(code): update new code

--- code/1220/1220.count-vowels-permutation.go
+++ code/1220/1220.count-vowels-permutation.go
@@ -1,9 +1,20 @@
 package _1220
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countVowelPermutation(n int) int {
-
+	const mod int = 1e9 + 7
+	dp := [5]int{1, 1, 1, 1, 1}
+	for i := 1; i < n; i++ {
+		dp = [5]int{
+			(dp[1] + dp[2] + dp[4]) % mod,
+			(dp[0] + dp[2]) % mod,
+			(dp[1] + dp[3]) % mod,
+			dp[2],
+			(dp[2] + dp[3]) % mod,
+		}
+	}
+	ans := 0
+	for _, v := range dp {
+		ans = (ans + v) % mod
+	}
+	return ans
 }
