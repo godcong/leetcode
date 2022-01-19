9a0a8a7651547ec302d2d514f12150a0aacfb7fa
godcong <jumbycc@163.com>
2022-01-19 13:49:40 +0800

    feat(code): update new code

--- code/0219/0219.contains-duplicate-ii.go
+++ code/0219/0219.contains-duplicate-ii.go
@@ -1,9 +1,17 @@
 package _0219
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func containsNearbyDuplicate(nums []int, k int) bool {
-
+	m := make(map[int]int, 0)
+	for idx, num := range nums {
+		if get, ok := m[num]; ok {
+			if idx-get <= k {
+				return true
+			} else {
+				m[num] = idx
+			}
+		} else {
+			m[num] = idx
+		}
+	}
+	return false
 }
