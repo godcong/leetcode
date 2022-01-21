02644dbb6df3d647c39a4e053371638c0d93cb11
godcong <jumbycc@163.com>
2022-01-21 13:57:01 +0800

    feat(code): update new code

---
+++ code/1345/1345.jump-game-iv_test.go
@@ -0,0 +1,25 @@
+package _1345
+
+import (
+	"testing"
+)
+
+func Test_minJumps(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minJumps(tt.args.arr); got != tt.want {
+				t.Errorf("minJumps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
