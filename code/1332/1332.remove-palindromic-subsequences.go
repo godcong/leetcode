a77561c97f15de1e2ff7b03936cda57c5415d0ff
godcong <jumbycc@163.com>
2022-01-22 13:59:25 +0800

    feat(code): update new code

--- code/1332/1332.remove-palindromic-subsequences.go
+++ code/1332/1332.remove-palindromic-subsequences.go
@@ -1,9 +1,10 @@
 package _1332
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func removePalindromeSub(s string) int {
-
+	for i, n := 0, len(s); i < n/2; i++ {
+		if s[i] != s[n-1-i] {
+			return 2
+		}
+	}
+	return 1
 }
