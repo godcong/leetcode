17aee91b4593d50420377f69af4b22b767499690
godcong <jumbycc@163.com>
2022-01-22 13:59:33 +0800

    feat(code): update new code

---
+++ code/1332/1332.remove-palindromic-subsequences_test.go
@@ -0,0 +1,25 @@
+package _1332
+
+import (
+	"testing"
+)
+
+func Test_removePalindromeSub(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removePalindromeSub(tt.args.s); got != tt.want {
+				t.Errorf("removePalindromeSub() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
