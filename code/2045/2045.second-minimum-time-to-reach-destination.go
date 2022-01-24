478aa7fb218f719c2698b1b6bec6576de81fc3ae
godcong <jumbycc@163.com>
2022-01-24 14:07:32 +0800

    feat(code): update new code

--- code/2045/2045.second-minimum-time-to-reach-destination.go
+++ code/2045/2045.second-minimum-time-to-reach-destination.go
@@ -1,9 +1,47 @@
 package _2045
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
+	"runtime/debug"
 )
 
 func secondMinimum(n int, edges [][]int, time int, change int) int {
+	graph := make([][]int, n+1)
+	for _, e := range edges {
+		x, y := e[0], e[1]
+		graph[x] = append(graph[x], y)
+		graph[y] = append(graph[y], x)
+	}
 
+	dist := make([][2]int, n+1)
+	dist[1][1] = math.MaxInt32
+	for i := 2; i <= n; i++ {
+		dist[i] = [2]int{math.MaxInt32, math.MaxInt32}
+	}
+	type pair struct{ x, d int }
+	q := []pair{{1, 0}}
+	for dist[n][1] == math.MaxInt32 {
+		p := q[0]
+		q = q[1:]
+		for _, y := range graph[p.x] {
+			d := p.d + 1
+			if d < dist[y][0] {
+				dist[y][0] = d
+				q = append(q, pair{y, d})
+			} else if dist[y][0] < d && d < dist[y][1] {
+				dist[y][1] = d
+				q = append(q, pair{y, d})
+			}
+		}
+	}
+	ans := 0
+	for i := 0; i < dist[n][1]; i++ {
+		if ans%(change*2) >= change {
+			ans += change*2 - ans%(change*2)
+		}
+		ans += time
+	}
+	return ans
 }
+
+func init() { debug.SetGCPercent(-1) }
