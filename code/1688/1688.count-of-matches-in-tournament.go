945fdecaaa305145f3686e677964e191012003b5
godcong <jumbycc@163.com>
2022-01-25 14:09:45 +0800

    feat(code): update new code

--- code/1688/1688.count-of-matches-in-tournament.go
+++ code/1688/1688.count-of-matches-in-tournament.go
@@ -1,9 +1,15 @@
 package _1688
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func numberOfMatches(n int) int {
-
+	ans := 0
+	for n > 1 {
+		if n%2 == 0 {
+			ans += n / 2
+			n /= 2
+		} else {
+			ans += (n - 1) / 2
+			n = (n-1)/2 + 1
+		}
+	}
+	return ans
 }
