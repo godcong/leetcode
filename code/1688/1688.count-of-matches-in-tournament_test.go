686b0254f6041f84750bf516e7484a039e55ba78
godcong <jumbycc@163.com>
2022-01-25 14:09:55 +0800

    feat(code): update new code

---
+++ code/1688/1688.count-of-matches-in-tournament_test.go
@@ -0,0 +1,25 @@
+package _1688
+
+import (
+	"testing"
+)
+
+func Test_numberOfMatches(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfMatches(tt.args.n); got != tt.want {
+				t.Errorf("numberOfMatches() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
