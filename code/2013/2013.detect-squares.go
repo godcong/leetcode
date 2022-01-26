c53be6641e35534f8b60b43250682c1be86b98a8
godcong <jumbycc@163.com>
2022-01-26 14:15:44 +0800

    feat(code): update new code

--- code/2013/2013.detect-squares.go
+++ code/2013/2013.detect-squares.go
@@ -1,28 +1,63 @@
 package _2013
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"runtime/debug"
+)
+
+const (
+	N = 1001
 )
 
 type DetectSquares struct {
-
+	m map[int]int
+	l [N][]int
 }
-
 
 func Constructor() DetectSquares {
-
+	return DetectSquares{
+		m: make(map[int]int, N),
+		l: [N][]int{},
+	}
 }
 
-
-func (this *DetectSquares) Add(point []int)  {
-
+func (this *DetectSquares) Add(point []int) {
+	f := ref(point[0], point[1])
+	this.m[f]++
+	if this.m[f] == 1 {
+		this.l[point[0]] = append(this.l[point[0]], point[1])
+	}
 }
-
 
 func (this *DetectSquares) Count(point []int) int {
-
+	ret := 0
+	for _, y := range this.l[point[0]] {
+		if y == point[1] {
+			continue
+		}
+		ll := abs(y - point[1])
+		bas := this.m[ref(point[0], y)]
+		if point[0]+ll >= 0 && point[0]+ll < N {
+			ret += bas * this.m[ref(point[0]+ll, point[1])] * this.m[ref(point[0]+ll, y)]
+		}
+		if point[0]-ll >= 0 && point[0]-ll < N {
+			ret += bas * this.m[ref(point[0]-ll, point[1])] * this.m[ref(point[0]-ll, y)]
+		}
+	}
+	return ret
 }
 
+func ref(x, y int) int {
+	return x*N + y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func init() { debug.SetGCPercent(-1) }
 
 /**
  * Your DetectSquares object will be instantiated and called as such:
