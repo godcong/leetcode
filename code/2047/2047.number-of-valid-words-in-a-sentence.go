2027c9b9f95e65b4f3bfe0a3ebd523a7334cc67a
godcong <jumbycc@163.com>
2022-01-27 14:18:26 +0800

    feat(code): update new code

--- code/2047/2047.number-of-valid-words-in-a-sentence.go
+++ code/2047/2047.number-of-valid-words-in-a-sentence.go
@@ -1,9 +1,32 @@
 package _2047
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
+	"unicode"
 )
 
-func countValidWords(sentence string) int {
+func valid(s string) bool {
+	hasHyphens := false
+	for i, ch := range s {
+		if unicode.IsDigit(ch) || strings.ContainsRune("!.,", ch) && i < len(s)-1 {
+			return false
+		}
+		if ch == '-' {
+			if hasHyphens || i == 0 || i == len(s)-1 || !unicode.IsLower(rune(s[i-1])) || !unicode.IsLower(rune(s[i+1])) {
+				return false
+			}
+			hasHyphens = true
+		}
+	}
+	return true
+}
 
+func countValidWords(sentence string) int {
+	ans := 0
+	for _, s := range strings.Fields(sentence) {
+		if valid(s) {
+			ans++
+		}
+	}
+	return ans
 }
