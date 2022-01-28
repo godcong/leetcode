5ec3500bf3b00e72c8e43531b50565554b1e8c81
godcong <jumbycc@163.com>
2022-01-28 15:00:21 +0800

    feat(code): update new code

---
+++ code/1996/1996.the-number-of-weak-characters-in-the-game_test.go
@@ -0,0 +1,25 @@
+package _1996
+
+import (
+	"testing"
+)
+
+func Test_numberOfWeakCharacters(t *testing.T) {
+	type args struct {
+		properties [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfWeakCharacters(tt.args.properties); got != tt.want {
+				t.Errorf("numberOfWeakCharacters() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
