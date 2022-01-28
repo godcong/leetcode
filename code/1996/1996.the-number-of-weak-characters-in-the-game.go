e1e020d9c3e5a0c6b6a9e516076f0e07eae4ea7a
godcong <jumbycc@163.com>
2022-01-28 15:00:11 +0800

    feat(code): update new code

--- code/1996/1996.the-number-of-weak-characters-in-the-game.go
+++ code/1996/1996.the-number-of-weak-characters-in-the-game.go
@@ -1,9 +1,33 @@
 package _1996
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func numberOfWeakCharacters(properties [][]int) int {
+	maxAttack := 0
+	for _, x := range properties {
+		if maxAttack < x[0] {
+			maxAttack = x[0]
+		}
+	}
 
+	attacks := make([]int, maxAttack+1)
+	for _, p := range properties {
+		if attacks[p[0]] < p[1] {
+			attacks[p[0]] = p[1]
+		}
+	}
+
+	maxDefence := 0
+	for i := maxAttack; i >= 0; i-- {
+		temp := maxDefence
+		if attacks[i] > maxDefence {
+			maxDefence = attacks[i]
+		}
+		attacks[i] = temp
+	}
+	ans := 0
+	for _, p := range properties {
+		if p[0] < maxAttack && p[1] < attacks[p[0]] {
+			ans++
+		}
+	}
+	return ans
 }
