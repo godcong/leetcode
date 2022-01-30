755c2503fc1b09522c7c5d2f281f68dd09b69e33
godcong <jumbycc@163.com>
2022-01-30 15:11:13 +0800

    feat(code): update new code

---
+++ code/0884/0884.uncommon-words-from-two-sentences_test.go
@@ -0,0 +1,27 @@
+package _0884
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_uncommonFromSentences(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uncommonFromSentences(tt.args.s1, tt.args.s2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("uncommonFromSentences() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
