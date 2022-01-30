8b99b65ef2a2069aa4f339c93dd2a34049846be0
godcong <jumbycc@163.com>
2022-01-30 15:11:06 +0800

    feat(code): update new code

--- code/0884/0884.uncommon-words-from-two-sentences.go
+++ code/0884/0884.uncommon-words-from-two-sentences.go
@@ -1,9 +1,27 @@
 package _0884
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 func uncommonFromSentences(s1 string, s2 string) []string {
+	freq := make(map[string]int)
 
+	insert := func(s string) {
+		words := strings.Split(s, " ")
+		for _, word := range words {
+			freq[word]++
+		}
+	}
+
+	insert(s1)
+	insert(s2)
+
+	ans := make([]string, 0)
+	for word, occ := range freq {
+		if occ == 1 {
+			ans = append(ans, word)
+		}
+	}
+	return ans
 }
