898f2b082a9a0cf8eb01bebb6a8c0646b9a0186f
godcong <jumbycc@163.com>
2022-01-31 15:12:41 +0800

    feat(code): update new code

--- code/1342/1342.number-of-steps-to-reduce-a-number-to-zero.go
+++ code/1342/1342.number-of-steps-to-reduce-a-number-to-zero.go
@@ -1,9 +1,12 @@
 package _1342
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func numberOfSteps(num int) int {
-
+	ans := 0
+	for ; num > 0; num >>= 1 {
+		ans += num & 1
+		if num > 1 {
+			ans++
+		}
+	}
+	return ans
 }
