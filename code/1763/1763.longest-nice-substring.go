388a280fe5794ae2e40970a6fb6f640d665986cf
godcong <jumbycc@163.com>
2022-02-01 15:16:06 +0800

    feat(code): update new code

--- code/1763/1763.longest-nice-substring.go
+++ code/1763/1763.longest-nice-substring.go
@@ -1,9 +1,23 @@
 package _1763
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"unicode"
 )
 
 func longestNiceSubstring(s string) string {
-
+	var ans string
+	for i := range s {
+		lower, upper := 0, 0
+		for j := i; j < len(s); j++ {
+			if unicode.IsLower(rune(s[j])) {
+				lower |= 1 << (s[j] - 'a')
+			} else {
+				upper |= 1 << (s[j] - 'A')
+			}
+			if lower == upper && j-i+1 > len(ans) {
+				ans = s[i : j+1]
+			}
+		}
+	}
+	return ans
 }
