c594d504a1a5b7612dea7b04de18ee026b9c50c0
godcong <jumbycc@163.com>
2022-02-02 15:19:45 +0800

    feat(code): update new code

--- code/2000/2000.reverse-prefix-of-word.go
+++ code/2000/2000.reverse-prefix-of-word.go
@@ -1,9 +1,18 @@
 package _2000
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strings"
 )
 
 func reversePrefix(word string, ch byte) string {
-
+	right := strings.IndexByte(word, ch)
+	if right < 0 {
+		return word
+	}
+	s := []byte(word)
+	for left := 0; left < right; left++ {
+		s[left], s[right] = s[right], s[left]
+		right--
+	}
+	return string(s)
 }
