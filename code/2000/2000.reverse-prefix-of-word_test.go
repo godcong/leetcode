de26bf82edb4e7307af8a2333c108203886e80b7
godcong <jumbycc@163.com>
2022-02-02 15:19:53 +0800

    feat(code): update new code

---
+++ code/2000/2000.reverse-prefix-of-word_test.go
@@ -0,0 +1,26 @@
+package _2000
+
+import (
+	"testing"
+)
+
+func Test_reversePrefix(t *testing.T) {
+	type args struct {
+		word string
+		ch   byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reversePrefix(tt.args.word, tt.args.ch); got != tt.want {
+				t.Errorf("reversePrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
