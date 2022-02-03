1d1bf9ecfabef66619488273398b69cb217242c4
godcong <jumbycc@163.com>
2022-02-03 15:21:21 +0800

    feat(code): update new code

---
+++ code/1414/1414.find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k_test.go
@@ -0,0 +1,25 @@
+package _1414
+
+import (
+	"testing"
+)
+
+func Test_findMinFibonacciNumbers(t *testing.T) {
+	type args struct {
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMinFibonacciNumbers(tt.args.k); got != tt.want {
+				t.Errorf("findMinFibonacciNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
