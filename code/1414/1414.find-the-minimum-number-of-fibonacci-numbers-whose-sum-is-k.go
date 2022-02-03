f2c59dbab9ff087b74b334047f26ca8e65d7d078
godcong <jumbycc@163.com>
2022-02-03 15:21:14 +0800

    feat(code): update new code

--- code/1414/1414.find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.go
+++ code/1414/1414.find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.go
@@ -1,9 +1,16 @@
 package _1414
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findMinFibonacciNumbers(k int) int {
-
+	f := []int{1, 1}
+	for f[len(f)-1] < k {
+		f = append(f, f[len(f)-1]+f[len(f)-2])
+	}
+	ans := 0
+	for i := len(f) - 1; k > 0; i-- {
+		if k >= f[i] {
+			k -= f[i]
+			ans++
+		}
+	}
+	return ans
 }
