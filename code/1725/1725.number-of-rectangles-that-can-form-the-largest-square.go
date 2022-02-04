ace377ec2c6e4cc14a7eba75721d9e74dbb74b8f
godcong <jumbycc@163.com>
2022-02-04 15:23:51 +0800

    feat(code): update new code

--- code/1725/1725.number-of-rectangles-that-can-form-the-largest-square.go
+++ code/1725/1725.number-of-rectangles-that-can-form-the-largest-square.go
@@ -1,9 +1,22 @@
 package _1725
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countGoodRectangles(rectangles [][]int) int {
+	maxLen := 0
+	ans := 0
+	for _, rect := range rectangles {
+		k := min(rect[0], rect[1])
+		if k == maxLen {
+			ans++
+		} else if k > maxLen {
+			maxLen, ans = k, 1
+		}
+	}
+	return ans
+}
 
+func min(i int, i2 int) int {
+	if i < i2 {
+		return i
+	}
+	return i2
 }
