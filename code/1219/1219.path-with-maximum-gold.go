55af5e741188b945cb21899b0b0cac8b07aefb8f
godcong <jumbycc@163.com>
2022-02-05 15:25:37 +0800

    feat(code): update new code

--- code/1219/1219.path-with-maximum-gold.go
+++ code/1219/1219.path-with-maximum-gold.go
@@ -1,9 +1,33 @@
 package _1219
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func getMaximumGold(grid [][]int) int {
+	ans := 0
+	var dfs func(x, y, gold int)
+	dfs = func(x, y, gold int) {
+		gold += grid[x][y]
+		if gold > ans {
+			ans = gold
+		}
 
+		rec := grid[x][y]
+		grid[x][y] = 0
+		for _, d := range dirs {
+			nx, ny := x+d.x, y+d.y
+			if 0 <= nx && nx < len(grid) && 0 <= ny && ny < len(grid[nx]) && grid[nx][ny] > 0 {
+				dfs(nx, ny, gold)
+			}
+		}
+		grid[x][y] = rec
+	}
+
+	for i, row := range grid {
+		for j, gold := range row {
+			if gold > 0 {
+				dfs(i, j, 0)
+			}
+		}
+	}
+	return ans
 }
+
+var dirs = []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
