86ba142de111bc4a8dc2ffc96ce189852217a7c2
godcong <jumbycc@163.com>
2022-02-07 15:28:32 +0800

    feat(code): update new code

--- code/1405/1405.longest-happy-string.go
+++ code/1405/1405.longest-happy-string.go
@@ -1,9 +1,33 @@
 package _1405
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"sort"
 )
 
 func longestDiverseString(a int, b int, c int) string {
-
+	ans := make([]byte, 0)
+	cnt := []struct {
+		c  int
+		ch byte
+	}{{a, 'a'}, {b, 'b'}, {c, 'c'}}
+	for {
+		sort.Slice(cnt, func(i, j int) bool { return cnt[i].c > cnt[j].c })
+		hasNext := false
+		for i, p := range cnt {
+			if p.c <= 0 {
+				break
+			}
+			m := len(ans)
+			if m >= 2 && ans[m-2] == p.ch && ans[m-1] == p.ch {
+				continue
+			}
+			hasNext = true
+			ans = append(ans, p.ch)
+			cnt[i].c--
+			break
+		}
+		if !hasNext {
+			return string(ans)
+		}
+	}
 }
