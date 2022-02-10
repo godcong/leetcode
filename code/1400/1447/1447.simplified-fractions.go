b69a56193dab8b4c12b620159fbc084ef8077a16
godcong <jumbycc@163.com>
2022-02-10 18:32:50 +0800

    feat(code): update new code

--- code/1400/1447/1447.simplified-fractions.go
+++ code/1400/1447/1447.simplified-fractions.go
@@ -1,9 +1,24 @@
 package _1447
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"strconv"
 )
 
 func simplifiedFractions(n int) []string {
+	ans := make([]string, 0)
+	for denominator := 2; denominator <= n; denominator++ {
+		for numerator := 1; numerator < denominator; numerator++ {
+			if gcd(numerator, denominator) == 1 {
+				ans = append(ans, strconv.Itoa(numerator)+"/"+strconv.Itoa(denominator))
+			}
+		}
+	}
+	return ans
+}
 
+func gcd(numerator int, denominator int) int {
+	for numerator != 0 {
+		numerator, denominator = denominator%numerator, numerator
+	}
+	return denominator
 }
