b7347c05af0a67f772bb75f8dc1a5bd7be8189bd
godcong <jumbycc@163.com>
2022-02-11 19:09:33 +0800

    feat(code): update new code

--- code/1900/1984/1984.minimum-difference-between-highest-and-lowest-of-k-scores.go
+++ code/1900/1984/1984.minimum-difference-between-highest-and-lowest-of-k-scores.go
@@ -1,9 +1,22 @@
 package _1984
 
 import (
-	. "github.com/godcong/leetcode/common"
+	"math"
+	"sort"
 )
 
 func minimumDifference(nums []int, k int) int {
+	sort.Ints(nums)
+	ans := math.MaxInt32
+	for i, num := range nums[:len(nums)-k+1] {
+		ans = min(ans, nums[i+k-1]-num)
+	}
+	return ans
+}
 
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
