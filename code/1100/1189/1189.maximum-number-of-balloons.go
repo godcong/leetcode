5838b5c383ae8de46cc709db2e41c174c2f2fdda
godcong <jumbycc@163.com>
2022-02-13 19:14:30 +0800

    feat(code): update new code

--- code/1100/1189/1189.maximum-number-of-balloons.go
+++ code/1100/1189/1189.maximum-number-of-balloons.go
@@ -1,9 +1,38 @@
 package _1189
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+var dirs = []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
-func maxNumberOfBalloons(text string) int {
-
+func numEnclaves(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	vis := make([][]bool, m)
+	for i := range vis {
+		vis[i] = make([]bool, n)
+	}
+	var dfs func(int, int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0 || vis[r][c] {
+			return
+		}
+		vis[r][c] = true
+		for _, d := range dirs {
+			dfs(r+d.x, c+d.y)
+		}
+	}
+	for i := range grid {
+		dfs(i, 0)
+		dfs(i, n-1)
+	}
+	for j := 1; j < n-1; j++ {
+		dfs(0, j)
+		dfs(m-1, j)
+	}
+	ans := 0
+	for i := 1; i < m-1; i++ {
+		for j := 1; j < n-1; j++ {
+			if grid[i][j] == 1 && !vis[i][j] {
+				ans++
+			}
+		}
+	}
+	return ans
 }
