76d64aef907f1f48b4c521b4327cfb2bf58eaf1f
godcong <jumbycc@163.com>
2022-02-13 19:13:22 +0800

    feat(code): add new code(1189) test for:maximum-number-of-balloons

---
+++ code/1100/1189/1189.maximum-number-of-balloons_test.go
@@ -0,0 +1,27 @@
+package _1189
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxNumberOfBalloons(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxNumberOfBalloons(tt.args.text); got != tt.want {
+				t.Errorf("maxNumberOfBalloons() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
