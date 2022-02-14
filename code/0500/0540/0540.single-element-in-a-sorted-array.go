15f71ebcbc34677bd8f3cd57d1ba9c10161d897a
godcong <jumbycc@163.com>
2022-02-14 19:15:43 +0800

    feat(code): update new code

--- code/0500/0540/0540.single-element-in-a-sorted-array.go
+++ code/0500/0540/0540.single-element-in-a-sorted-array.go
@@ -1,9 +1,14 @@
 package _0540
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func singleNonDuplicate(nums []int) int {
-
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid := low + (high-low)/2
+		if nums[mid] == nums[mid^1] {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return nums[low]
 }
