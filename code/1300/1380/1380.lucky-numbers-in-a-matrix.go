dc750ec868efe62b2548ffb9df1ad04d905a1ab7
godcong <jumbycc@163.com>
2022-02-15 19:21:24 +0800

    feat(code): update new code

--- code/1300/1380/1380.lucky-numbers-in-a-matrix.go
+++ code/1300/1380/1380.lucky-numbers-in-a-matrix.go
@@ -1,9 +1,22 @@
 package _1380
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
-func luckyNumbers (matrix [][]int) []int {
-
+func luckyNumbers(matrix [][]int) []int {
+	ans := make([]int, 0)
+	for _, row := range matrix {
+	next:
+		for j, x := range row {
+			for _, y := range row {
+				if y < x {
+					continue next
+				}
+			}
+			for _, r := range matrix {
+				if r[j] > x {
+					continue next
+				}
+			}
+			ans = append(ans, x)
+		}
+	}
+	return ans
 }
