f769e912decf01d9915e8812f41b05f87f172855
godcong <jumbycc@163.com>
2022-02-15 19:20:17 +0800

    feat(code): add new code(1380) test for:lucky-numbers-in-a-matrix

---
+++ code/1300/1380/1380.lucky-numbers-in-a-matrix_test.go
@@ -0,0 +1,28 @@
+package _1380
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_luckyNumbers(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := luckyNumbers(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("luckyNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
