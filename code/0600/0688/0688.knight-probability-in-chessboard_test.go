b26f9f9f12bf69471c701feae0f139ad44c6a9c2
godcong <jumbycc@163.com>
2022-02-17 19:28:33 +0800

    feat(code): add new code(688) test for:knight-probability-in-chessboard

---
+++ code/0600/0688/0688.knight-probability-in-chessboard_test.go
@@ -0,0 +1,30 @@
+package _0688
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_knightProbability(t *testing.T) {
+	type args struct {
+		n      int
+		k      int
+		row    int
+		column int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := knightProbability(tt.args.n, tt.args.k, tt.args.row, tt.args.column); got != tt.want {
+				t.Errorf("knightProbability() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
