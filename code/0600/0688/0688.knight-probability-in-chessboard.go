e6995cf3e614023db37fea1be9a973763382f8f8
godcong <jumbycc@163.com>
2022-02-17 19:36:16 +0800

    feat(code): update new code

--- code/0600/0688/0688.knight-probability-in-chessboard.go
+++ code/0600/0688/0688.knight-probability-in-chessboard.go
@@ -1,9 +1,26 @@
 package _0688
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
+var dirs = []struct{ i, j int }{{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}}
 
 func knightProbability(n int, k int, row int, column int) float64 {
 
+	dp := make([][][]float64, k+1)
+	for step := range dp {
+		dp[step] = make([][]float64, n)
+		for i := 0; i < n; i++ {
+			dp[step][i] = make([]float64, n)
+			for j := 0; j < n; j++ {
+				if step == 0 {
+					dp[step][i][j] = 1
+				} else {
+					for _, d := range dirs {
+						if x, y := i+d.i, j+d.j; 0 <= x && x < n && 0 <= y && y < n {
+							dp[step][i][j] += dp[step-1][x][y] / 8
+						}
+					}
+				}
+			}
+		}
+	}
+	return dp[k][row][column]
 }
