5e4c9fe53c966705e4b6f65d02d1180e1c4a2d30
godcong <jumbycc@163.com>
2022-02-19 00:48:16 +0800

    feat(code): add new code(969) test for:pancake-sorting

---
+++ code/0900/0969/0969.pancake-sorting_test.go
@@ -0,0 +1,28 @@
+package _0969
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_pancakeSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pancakeSort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pancakeSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
