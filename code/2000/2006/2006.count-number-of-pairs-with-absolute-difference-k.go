09eda7ba77c3c1e01fb9131b375156fe43b22876
godcong <jumbycc@163.com>
2022-02-18 17:02:16 +0800

    feat(code): update new code

--- code/2000/2006/2006.count-number-of-pairs-with-absolute-difference-k.go
+++ code/2000/2006/2006.count-number-of-pairs-with-absolute-difference-k.go
@@ -1,9 +1,12 @@
 package _2006
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func countKDifference(nums []int, k int) int {
+	cnt := map[int]int{}
+	c := 0
 
+	for _, v := range nums {
+		c += cnt[v+k] + cnt[v-k]
+		cnt[v]++
+	}
+	return c
 }
