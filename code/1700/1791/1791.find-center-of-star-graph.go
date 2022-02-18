399974a2acbe49d4b9d0d8efacd613d256d268f2
godcong <jumbycc@163.com>
2022-02-18 19:37:53 +0800

    feat(code): update new code

--- code/1700/1791/1791.find-center-of-star-graph.go
+++ code/1700/1791/1791.find-center-of-star-graph.go
@@ -1,9 +1,16 @@
 package _1791
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func findCenter(edges [][]int) int {
-
+	n := len(edges) + 1
+	degrees := make([]int, n+1)
+	for _, e := range edges {
+		degrees[e[0]]++
+		degrees[e[1]]++
+	}
+	for i, d := range degrees {
+		if d == n-1 {
+			return i
+		}
+	}
+	return -1
 }
