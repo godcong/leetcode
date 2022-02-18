d3d68e7e9ea29b230ed95b311bffc5be701a2e2c
godcong <jumbycc@163.com>
2022-02-19 00:49:43 +0800

    feat(code): update new code

--- code/0900/0969/0969.pancake-sorting.go
+++ code/0900/0969/0969.pancake-sorting.go
@@ -1,9 +1,24 @@
 package _0969
 
-import (
-	. "github.com/godcong/leetcode/common"
-)
-
 func pancakeSort(arr []int) []int {
-
+	ans := make([]int, 0)
+	for n := len(arr); n > 1; n-- {
+		index := 0
+		for i, v := range arr[:n] {
+			if v > arr[index] {
+				index = i
+			}
+		}
+		if index == n-1 {
+			continue
+		}
+		for i, m := 0, index; i < (m+1)/2; i++ {
+			arr[i], arr[m-i] = arr[m-i], arr[i]
+		}
+		for i := 0; i < n/2; i++ {
+			arr[i], arr[n-1-i] = arr[n-1-i], arr[i]
+		}
+		ans = append(ans, index+1, n)
+	}
+	return ans
 }
