4b884026e54f3bdc4bc48b9423f74ab2dba6f1fb
godcong <jumbycc@163.com>
2022-02-20 13:39:09 +0800

    feat(code): add new code(717) test for:1-bit-and-2-bit-characters

---
+++ code/0700/0717/0717.1-bit-and-2-bit-characters_test.go
@@ -0,0 +1,27 @@
+package _0717
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isOneBitCharacter(t *testing.T) {
+	type args struct {
+		bits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isOneBitCharacter(tt.args.bits); got != tt.want {
+				t.Errorf("isOneBitCharacter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
