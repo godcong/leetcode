1fa29b3b419774dd59f38097f1163341f75ac68e
godcong <jumbycc@163.com>
2022-03-10 12:33:35 +0800

    feat(code): add new code(589) n-ary-tree-preorder-traversal

---
+++ code/0500/0589/0589.n-ary-tree-preorder-traversal.go
@@ -0,0 +1,17 @@
+package _0589
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func preorder(root *Node) []int {
+    
+}
