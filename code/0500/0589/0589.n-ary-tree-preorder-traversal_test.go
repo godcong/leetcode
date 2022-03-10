5f990ea64e859444bc7900b128c9897c714ce080
godcong <jumbycc@163.com>
2022-03-10 12:33:35 +0800

    feat(code): add new code(589) test for:n-ary-tree-preorder-traversal

---
+++ code/0500/0589/0589.n-ary-tree-preorder-traversal_test.go
@@ -0,0 +1,28 @@
+package _0589
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_preorder(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := preorder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("preorder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
