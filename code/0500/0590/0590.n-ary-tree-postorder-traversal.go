68c8c0547b3b9c4ea6b3d2d0d9ca4bc5cb2a50dd
godcong <jumbycc@163.com>
2022-03-12 12:34:23 +0800

    feat(code): add new code(590) n-ary-tree-postorder-traversal

---
+++ code/0500/0590/0590.n-ary-tree-postorder-traversal.go
@@ -0,0 +1,17 @@
+package _0590
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+    
+}
