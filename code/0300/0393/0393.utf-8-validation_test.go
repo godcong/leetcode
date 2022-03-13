c6d6cc52a168303af9c89fead00248f9e5ed7fee
godcong <jumbycc@163.com>
2022-03-13 12:34:49 +0800

    feat(code): add new code(393) test for:utf-8-validation

---
+++ code/0300/0393/0393.utf-8-validation_test.go
@@ -0,0 +1,27 @@
+package _0393
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_validUtf8(t *testing.T) {
+	type args struct {
+		data []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validUtf8(tt.args.data); got != tt.want {
+				t.Errorf("validUtf8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
