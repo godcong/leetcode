ba39f1eda03cdbcf4c53a09e02c1a5610ececfa0
godcong <jumbycc@163.com>
2022-03-15 12:35:39 +0800

    feat(code): add new code(2044) test for:count-number-of-maximum-bitwise-or-subsets

---
+++ code/2000/2044/2044.count-number-of-maximum-bitwise-or-subsets_test.go
@@ -0,0 +1,27 @@
+package _2044
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countMaxOrSubsets(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countMaxOrSubsets(tt.args.nums); got != tt.want {
+				t.Errorf("countMaxOrSubsets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
