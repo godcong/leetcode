3c2a50f7d85b89ed22c5ade37ba2631a1456fd14
godcong <jumbycc@163.com>
2022-03-17 13:55:04 +0800

    feat(code): add new code(720) test for:longest-word-in-dictionary

---
+++ code/0700/0720/0720.longest-word-in-dictionary_test.go
@@ -0,0 +1,27 @@
+package _0720
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_longestWord(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestWord(tt.args.words); got != tt.want {
+				t.Errorf("longestWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
