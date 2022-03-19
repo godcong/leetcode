3609227db4d740f4ac00b7e7d9b5745bb18143b8
godcong <jumbycc@163.com>
2022-03-19 13:56:24 +0800

    feat(code): add new code(606) construct-string-from-binary-tree

---
+++ code/0600/0606/0606.construct-string-from-binary-tree.go
@@ -0,0 +1,17 @@
+package _0606
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func tree2str(root *TreeNode) string {
+
+}
