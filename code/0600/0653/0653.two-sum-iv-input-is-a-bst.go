b50336b46557a1e225ac2aa8f4d7d49ff2d54768
godcong <jumbycc@163.com>
2022-03-21 13:57:29 +0800

    feat(code): add new code(653) two-sum-iv-input-is-a-bst

---
+++ code/0600/0653/0653.two-sum-iv-input-is-a-bst.go
@@ -0,0 +1,17 @@
+package _0653
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTarget(root *TreeNode, k int) bool {
+
+}
