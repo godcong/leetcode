f997038e0614ee1fb2d6e475ba3484e186230c85
godcong <jumbycc@163.com>
2022-03-22 13:58:05 +0800

    feat(code): add new code(2038) test for:remove-colored-pieces-if-both-neighbors-are-the-same-color

---
+++ code/2000/2038/2038.remove-colored-pieces-if-both-neighbors-are-the-same-color_test.go
@@ -0,0 +1,27 @@
+package _2038
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_winnerOfGame(t *testing.T) {
+	type args struct {
+		colors string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := winnerOfGame(tt.args.colors); got != tt.want {
+				t.Errorf("winnerOfGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
