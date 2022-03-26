2792822b41934d4d81f466c7b914bb58ce148153
godcong <jumbycc@163.com>
2022-03-26 10:43:51 +0800

    feat(code): add new code(682) test for:baseball-game

---
+++ code/0600/0682/0682.baseball-game_test.go
@@ -0,0 +1,27 @@
+package _0682
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_calPoints(t *testing.T) {
+	type args struct {
+		ops []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calPoints(tt.args.ops); got != tt.want {
+				t.Errorf("calPoints() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
