26a248a5a9f241de04d78e00e7d9e18f0894689a
godcong <jumbycc@163.com>
2022-03-29 10:44:18 +0800

    feat(code): add new code(2024) test for:maximize-the-confusion-of-an-exam

---
+++ code/2000/2024/2024.maximize-the-confusion-of-an-exam_test.go
@@ -0,0 +1,28 @@
+package _2024
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxConsecutiveAnswers(t *testing.T) {
+	type args struct {
+		answerKey string
+		k         int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxConsecutiveAnswers(tt.args.answerKey, tt.args.k); got != tt.want {
+				t.Errorf("maxConsecutiveAnswers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
