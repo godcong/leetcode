914d620dceb2056446bd23c0f5f0e90bdde5a580
godcong <jumbycc@163.com>
2022-03-31 10:44:33 +0800

    feat(code): add new code(728) test for:self-dividing-numbers

---
+++ code/0700/0728/0728.self-dividing-numbers_test.go
@@ -0,0 +1,29 @@
+package _0728
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_selfDividingNumbers(t *testing.T) {
+	type args struct {
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := selfDividingNumbers(tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("selfDividingNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
