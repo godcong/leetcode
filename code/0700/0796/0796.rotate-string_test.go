995e137c556fb87925e6aa46e0e734fae0144d48
godcong <jumbycc@163.com>
2022-04-07 10:45:25 +0800

    feat(code): add new code(796) test for:rotate-string

---
+++ code/0700/0796/0796.rotate-string_test.go
@@ -0,0 +1,28 @@
+package _0796
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_rotateString(t *testing.T) {
+	type args struct {
+		s    string
+		goal string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rotateString(tt.args.s, tt.args.goal); got != tt.want {
+				t.Errorf("rotateString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
