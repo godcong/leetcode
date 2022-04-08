d84bfd44d230aa8ad45bd5e4f60c5e1e1d1a616b
godcong <jumbycc@163.com>
2022-04-08 10:45:31 +0800

    feat(code): add new code(429) n-ary-tree-level-order-traversal

---
+++ code/0400/0429/0429.n-ary-tree-level-order-traversal.go
@@ -0,0 +1,17 @@
+package _0429
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func levelOrder(root *Node) [][]int {
+    
+}
