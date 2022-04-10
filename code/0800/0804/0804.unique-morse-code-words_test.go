8c10a2d0ee8c6a00cc2798c6cc77b005b7a1a7f1
godcong <jumbycc@163.com>
2022-04-10 10:45:43 +0800

    feat(code): add new code(804) test for:unique-morse-code-words

---
+++ code/0800/0804/0804.unique-morse-code-words_test.go
@@ -0,0 +1,27 @@
+package _0804
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_uniqueMorseRepresentations(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniqueMorseRepresentations(tt.args.words); got != tt.want {
+				t.Errorf("uniqueMorseRepresentations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
