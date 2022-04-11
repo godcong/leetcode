cdb27761afa22c6fd4410d9ab61d78b56147a167
godcong <jumbycc@163.com>
2022-04-11 10:45:49 +0800

    feat(code): add new code(357) test for:count-numbers-with-unique-digits

---
+++ code/0300/0357/0357.count-numbers-with-unique-digits_test.go
@@ -0,0 +1,27 @@
+package _0357
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countNumbersWithUniqueDigits(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countNumbersWithUniqueDigits(tt.args.n); got != tt.want {
+				t.Errorf("countNumbersWithUniqueDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
