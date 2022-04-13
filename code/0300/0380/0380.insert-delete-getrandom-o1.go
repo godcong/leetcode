2eedee624ad21fae1dcd0e551ba57e4e34e3e178
godcong <jumbycc@163.com>
2022-04-13 10:45:59 +0800

    feat(code): add new code(380) insert-delete-getrandom-o1

---
+++ code/0300/0380/0380.insert-delete-getrandom-o1.go
@@ -0,0 +1,38 @@
+package _0380
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type RandomizedSet struct {
+
+}
+
+
+func Constructor() RandomizedSet {
+
+}
+
+
+func (this *RandomizedSet) Insert(val int) bool {
+
+}
+
+
+func (this *RandomizedSet) Remove(val int) bool {
+
+}
+
+
+func (this *RandomizedSet) GetRandom() int {
+
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
