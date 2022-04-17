c924c0c6924a181d3cbdfc6942025c00075ba064
godcong <jumbycc@163.com>
2022-04-17 10:46:22 +0800

    feat(code): add new code(819) test for:most-common-word

---
+++ code/0800/0819/0819.most-common-word_test.go
@@ -0,0 +1,28 @@
+package _0819
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_mostCommonWord(t *testing.T) {
+	type args struct {
+		paragraph string
+		banned    []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mostCommonWord(tt.args.paragraph, tt.args.banned); got != tt.want {
+				t.Errorf("mostCommonWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
