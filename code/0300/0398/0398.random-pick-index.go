6148ca1e71602de44c6dcf7aa1ad39a534ae6496
godcong <jumbycc@163.com>
2022-04-25 10:53:14 +0800

    feat(code): add new code(398) random-pick-index

---
+++ code/0300/0398/0398.random-pick-index.go
@@ -0,0 +1,26 @@
+package _0398
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type Solution struct {
+
+}
+
+
+func Constructor(nums []int) Solution {
+
+}
+
+
+func (this *Solution) Pick(target int) int {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Pick(target);
+ */
