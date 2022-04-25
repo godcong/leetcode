f1b70abcdf2cd7926b8952ab145be81bfb8a3c9a
godcong <jumbycc@163.com>
2022-04-25 10:53:15 +0800

    feat(code): add new code(398) test for:random-pick-index

---
+++ code/0300/0398/0398.random-pick-index_test.go
@@ -0,0 +1,49 @@
+package _0398
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func TestConstructor(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Solution
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution_Pick(t *testing.T) {
+	type args struct {
+		target int
+	}
+	tests := []struct {
+		name string
+		this *Solution
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.this.Pick(tt.args.target); got != tt.want {
+				t.Errorf("Solution.Pick() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
