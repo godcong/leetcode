414191f4c5eb6084434d7d2b64f5c9c11f1ca603
godcong <jumbycc@163.com>
2022-04-28 10:53:30 +0800

    feat(code): add new code(905) test for:sort-array-by-parity

---
+++ code/0900/0905/0905.sort-array-by-parity_test.go
@@ -0,0 +1,28 @@
+package _0905
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_sortArrayByParity(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortArrayByParity(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortArrayByParity() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
