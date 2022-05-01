99a0e09d6856625345874d03714b43b6e868b47d
godcong <jumbycc@163.com>
2022-05-01 10:53:58 +0800

    feat(code): add new code(1305) all-elements-in-two-binary-search-trees

---
+++ code/1300/1305/1305.all-elements-in-two-binary-search-trees.go
@@ -0,0 +1,17 @@
+package _1305
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getAllElements(root1 *TreeNode, root2 *TreeNode) []int {
+
+}
