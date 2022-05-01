e688c22ffc3c9c4a10ee7ffa2fe4a70ab0297fa6
godcong <jumbycc@163.com>
2022-05-01 10:53:58 +0800

    feat(code): add new code(1305) test for:all-elements-in-two-binary-search-trees

---
+++ code/1300/1305/1305.all-elements-in-two-binary-search-trees_test.go
@@ -0,0 +1,29 @@
+package _1305
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_getAllElements(t *testing.T) {
+	type args struct {
+		root1 *TreeNode
+		root2 *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getAllElements(tt.args.root1, tt.args.root2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getAllElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
