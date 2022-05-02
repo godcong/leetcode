a499df754894ae8f2c2dee3ceb9700184fc0e554
godcong <jumbycc@163.com>
2022-05-02 10:54:04 +0800

    feat(code): add new code(591) test for:tag-validator

---
+++ code/0500/0591/0591.tag-validator_test.go
@@ -0,0 +1,27 @@
+package _0591
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		code string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.code); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
