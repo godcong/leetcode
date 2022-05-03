2a657c1d1c9c1286d1c106a255c07b3ffbfed5b1
godcong <jumbycc@163.com>
2022-05-03 10:54:08 +0800

    feat(code): add new code(937) test for:reorder-data-in-log-files

---
+++ code/0900/0937/0937.reorder-data-in-log-files_test.go
@@ -0,0 +1,28 @@
+package _0937
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_reorderLogFiles(t *testing.T) {
+	type args struct {
+		logs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reorderLogFiles(tt.args.logs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reorderLogFiles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
