c3bacf6d4b8e152140b7cb03f6ec2b6036680421
godcong <jumbycc@163.com>
2022-05-07 10:54:58 +0800

    feat(code): add new code(433) test for:minimum-genetic-mutation

---
+++ code/0400/0433/0433.minimum-genetic-mutation_test.go
@@ -0,0 +1,29 @@
+package _0433
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minMutation(t *testing.T) {
+	type args struct {
+		start string
+		end   string
+		bank  []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minMutation(tt.args.start, tt.args.end, tt.args.bank); got != tt.want {
+				t.Errorf("minMutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
