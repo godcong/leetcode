c8bf98498ca7a5bb32eeebd0bdc7943d4ad637ec
godcong <jumbycc@163.com>
2022-05-09 10:55:08 +0800

    feat(code): add new code(942) test for:di-string-match

---
+++ code/0900/0942/0942.di-string-match_test.go
@@ -0,0 +1,28 @@
+package _0942
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_diStringMatch(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := diStringMatch(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("diStringMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
