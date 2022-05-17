c7713591c93263ef972f7b16f2af0119295aa331
godcong <jumbycc@163.com>
2022-05-17 10:55:47 +0800

    feat(code): add new code(953) test for:verifying-an-alien-dictionary

---
+++ code/0900/0953/0953.verifying-an-alien-dictionary_test.go
@@ -0,0 +1,28 @@
+package _0953
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isAlienSorted(t *testing.T) {
+	type args struct {
+		words []string
+		order string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAlienSorted(tt.args.words, tt.args.order); got != tt.want {
+				t.Errorf("isAlienSorted() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
