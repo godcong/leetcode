418ba376254ad167f3a09da81ba9317833a76595
godcong <jumbycc@163.com>
2022-05-22 10:56:14 +0800

    feat(code): add new code(464) test for:can-i-win

---
+++ code/0400/0464/0464.can-i-win_test.go
@@ -0,0 +1,28 @@
+package _0464
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_canIWin(t *testing.T) {
+	type args struct {
+		maxChoosableInteger int
+		desiredTotal        int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canIWin(tt.args.maxChoosableInteger, tt.args.desiredTotal); got != tt.want {
+				t.Errorf("canIWin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
