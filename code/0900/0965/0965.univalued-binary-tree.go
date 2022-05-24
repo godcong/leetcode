2df2ca56830646b203d8187718d9d02f3baafce5
godcong <jumbycc@163.com>
2022-05-24 10:56:23 +0800

    feat(code): add new code(965) univalued-binary-tree

---
+++ code/0900/0965/0965.univalued-binary-tree.go
@@ -0,0 +1,17 @@
+package _0965
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isUnivalTree(root *TreeNode) bool {
+
+}
