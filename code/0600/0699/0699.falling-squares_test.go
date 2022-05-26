2dd6010f692409758d0d17020425656847adfa2e
godcong <jumbycc@163.com>
2022-05-26 10:56:34 +0800

    feat(code): add new code(699) test for:falling-squares

---
+++ code/0600/0699/0699.falling-squares_test.go
@@ -0,0 +1,28 @@
+package _0699
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_fallingSquares(t *testing.T) {
+	type args struct {
+		positions [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fallingSquares(tt.args.positions); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fallingSquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
