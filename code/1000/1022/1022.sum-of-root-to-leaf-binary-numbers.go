a98cd421395fd1cd8e8d75eb79b568d169fd33c3
godcong <jumbycc@163.com>
2022-05-30 10:56:59 +0800

    feat(code): add new code(1022) sum-of-root-to-leaf-binary-numbers

---
+++ code/1000/1022/1022.sum-of-root-to-leaf-binary-numbers.go
@@ -0,0 +1,17 @@
+package _1022
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumRootToLeaf(root *TreeNode) int {
+
+}
