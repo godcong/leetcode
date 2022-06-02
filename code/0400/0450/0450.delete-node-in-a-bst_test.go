6200caf09ba8167e8184c6564940d52a95e02ca4
godcong <jumbycc@163.com>
2022-06-02 10:57:24 +0800

    feat(code): add new code(450) test for:delete-node-in-a-bst

---
+++ code/0400/0450/0450.delete-node-in-a-bst_test.go
@@ -0,0 +1,29 @@
+package _0450
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_deleteNode(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		key  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteNode(tt.args.root, tt.args.key); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
