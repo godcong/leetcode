37992c29eb38e790d8d9dcb4a31c848ec88f63ce
godcong <jumbycc@163.com>
2022-06-02 10:57:23 +0800

    feat(code): add new code(450) delete-node-in-a-bst

---
+++ code/0400/0450/0450.delete-node-in-a-bst.go
@@ -0,0 +1,17 @@
+package _0450
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+
+}
