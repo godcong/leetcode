a45b02c9bc295bc0eb350c3ef9b158f82e62da08
godcong <jumbycc@163.com>
2022-06-07 14:41:29 +0800

    feat(code): add new code(875) test for:koko-eating-bananas

---
+++ code/0800/0875/0875.koko-eating-bananas_test.go
@@ -0,0 +1,28 @@
+package _0875
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minEatingSpeed(t *testing.T) {
+	type args struct {
+		piles []int
+		h     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minEatingSpeed(tt.args.piles, tt.args.h); got != tt.want {
+				t.Errorf("minEatingSpeed() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
