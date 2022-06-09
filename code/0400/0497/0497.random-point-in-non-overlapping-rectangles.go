35b0c17e63d4abce79b78bc3e6fc04473a1e2bc1
godcong <jumbycc@163.com>
2022-06-09 14:41:41 +0800

    feat(code): add new code(497) random-point-in-non-overlapping-rectangles

---
+++ code/0400/0497/0497.random-point-in-non-overlapping-rectangles.go
@@ -0,0 +1,26 @@
+package _0497
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type Solution struct {
+
+}
+
+
+func Constructor(rects [][]int) Solution {
+
+}
+
+
+func (this *Solution) Pick() []int {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(rects);
+ * param_1 := obj.Pick();
+ */
