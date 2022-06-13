3b6e1b4c88f6faf9f6019c08e13af6895e250c9d
godcong <jumbycc@163.com>
2022-06-13 14:42:12 +0800

    feat(code): add new code(1051) test for:height-checker

---
+++ code/1000/1051/1051.height-checker_test.go
@@ -0,0 +1,27 @@
+package _1051
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_heightChecker(t *testing.T) {
+	type args struct {
+		heights []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := heightChecker(tt.args.heights); got != tt.want {
+				t.Errorf("heightChecker() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
