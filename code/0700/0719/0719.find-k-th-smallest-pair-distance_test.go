16c321e90d47f132de52af110502d9077731c2cd
godcong <jumbycc@163.com>
2022-06-15 14:42:26 +0800

    feat(code): add new code(719) test for:find-k-th-smallest-pair-distance

---
+++ code/0700/0719/0719.find-k-th-smallest-pair-distance_test.go
@@ -0,0 +1,28 @@
+package _0719
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_smallestDistancePair(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := smallestDistancePair(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("smallestDistancePair() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
