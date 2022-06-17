3845ef8f2d364b81653de780ad81748611ef6184
godcong <jumbycc@163.com>
2022-06-17 14:42:42 +0800

    feat(code): add new code(1089) test for:duplicate-zeros

---
+++ code/1000/1089/1089.duplicate-zeros_test.go
@@ -0,0 +1,24 @@
+package _1089
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_duplicateZeros(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			duplicateZeros(tt.args.arr)
+		})
+	}
+}
