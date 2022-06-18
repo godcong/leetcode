62c6ea882567141a349b7baabd7c7318c587a3d6
godcong <jumbycc@163.com>
2022-06-18 14:42:48 +0800

    feat(code): add new code(剑指 Offer II 029) 4ueAj6

---
+++ offer/剑指 Offer II 000/SwordRefers_Offer_II_029/SwordRefers_Offer_II_029.4ueAj6.go
@@ -0,0 +1,17 @@
+package _SwordRefers_Offer_II_029
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ * }
+ */
+
+func insert(aNode *Node, x int) *Node {
+    
+}
