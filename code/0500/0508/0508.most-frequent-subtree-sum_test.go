c7cde4ba6b70c647bce50968a816e954bd9e9ece
godcong <jumbycc@163.com>
2022-06-19 14:42:56 +0800

    feat(code): add new code(508) test for:most-frequent-subtree-sum

---
+++ code/0500/0508/0508.most-frequent-subtree-sum_test.go
@@ -0,0 +1,28 @@
+package _0508
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findFrequentTreeSum(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findFrequentTreeSum(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findFrequentTreeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
