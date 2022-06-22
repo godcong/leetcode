7c3638efa9c3def8e44e00bffd1225d11dc8e022
godcong <jumbycc@163.com>
2022-06-22 14:43:33 +0800

    feat(code): add new code(513) test for:find-bottom-left-tree-value

---
+++ code/0500/0513/0513.find-bottom-left-tree-value_test.go
@@ -0,0 +1,27 @@
+package _0513
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findBottomLeftValue(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findBottomLeftValue(tt.args.root); got != tt.want {
+				t.Errorf("findBottomLeftValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
