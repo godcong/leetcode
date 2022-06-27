e1c725133addc785419a98b8b8f19e72f0e2d7c6
godcong <jumbycc@163.com>
2022-06-27 20:25:46 +0800

    feat(code): add new code(522) test for:longest-uncommon-subsequence-ii

---
+++ code/0500/0522/0522.longest-uncommon-subsequence-ii_test.go
@@ -0,0 +1,27 @@
+package _0522
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findLUSlength(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLUSlength(tt.args.strs); got != tt.want {
+				t.Errorf("findLUSlength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
