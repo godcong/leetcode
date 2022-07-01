b718dd4cf865fde1827e26d3fc78d7c717274cbc
godcong <jumbycc@163.com>
2022-07-01 18:01:03 +0800

    feat(code): add new code(241) test for:different-ways-to-add-parentheses

---
+++ code/0200/0241/0241.different-ways-to-add-parentheses_test.go
@@ -0,0 +1,28 @@
+package _0241
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_diffWaysToCompute(t *testing.T) {
+	type args struct {
+		expression string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := diffWaysToCompute(tt.args.expression); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("diffWaysToCompute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
