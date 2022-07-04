1d395e19fef2a3e245f67d4eef744fede5103db7
godcong <jumbycc@163.com>
2022-07-04 18:01:15 +0800

    feat(code): add new code(1200) test for:minimum-absolute-difference

---
+++ code/1200/1200/1200.minimum-absolute-difference_test.go
@@ -0,0 +1,28 @@
+package _1200
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumAbsDifference(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumAbsDifference(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("minimumAbsDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
