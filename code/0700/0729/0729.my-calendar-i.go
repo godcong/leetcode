f098e3b255af6f5ff657b2f4919eadf3f29e0278
godcong <jumbycc@163.com>
2022-07-05 18:01:19 +0800

    feat(code): add new code(729) my-calendar-i

---
+++ code/0700/0729/0729.my-calendar-i.go
@@ -0,0 +1,26 @@
+package _0729
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MyCalendar struct {
+
+}
+
+
+func Constructor() MyCalendar {
+
+}
+
+
+func (this *MyCalendar) Book(start int, end int) bool {
+
+}
+
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Book(start,end);
+ */
