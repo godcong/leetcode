71f079ad4c6ce75e81fc95d402b071b0fa8d444f
godcong <jumbycc@163.com>
2022-07-09 18:01:35 +0800

    feat(code): add new code(873) test for:length-of-longest-fibonacci-subsequence

---
+++ code/0800/0873/0873.length-of-longest-fibonacci-subsequence_test.go
@@ -0,0 +1,27 @@
+package _0873
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_lenLongestFibSubseq(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lenLongestFibSubseq(tt.args.arr); got != tt.want {
+				t.Errorf("lenLongestFibSubseq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
