10e143bf867720e2ce80911f2d273a2172b7dffc
godcong <jumbycc@163.com>
2022-07-11 18:01:43 +0800

    feat(code): add new code(676) implement-magic-dictionary

---
+++ code/0600/0676/0676.implement-magic-dictionary.go
@@ -0,0 +1,32 @@
+package _0676
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MagicDictionary struct {
+
+}
+
+
+func Constructor() MagicDictionary {
+
+}
+
+
+func (this *MagicDictionary) BuildDict(dictionary []string)  {
+
+}
+
+
+func (this *MagicDictionary) Search(searchWord string) bool {
+
+}
+
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.BuildDict(dictionary);
+ * param_2 := obj.Search(searchWord);
+ */
