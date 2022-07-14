50c1b2cd6c26c8d4e165e49cd987eeea5c6f0701
godcong <jumbycc@163.com>
2022-07-14 18:01:55 +0800

    feat(code): add new code(745) prefix-and-suffix-search

---
+++ code/0700/0745/0745.prefix-and-suffix-search.go
@@ -0,0 +1,26 @@
+package _0745
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type WordFilter struct {
+
+}
+
+
+func Constructor(words []string) WordFilter {
+
+}
+
+
+func (this *WordFilter) F(pref string, suff string) int {
+
+}
+
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * obj := Constructor(words);
+ * param_1 := obj.F(pref,suff);
+ */
