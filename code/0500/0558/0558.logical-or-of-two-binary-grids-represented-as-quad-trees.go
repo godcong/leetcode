eb782c8092675ab0cf8e861177e3dae7ad346d60
godcong <jumbycc@163.com>
2022-07-15 18:01:59 +0800

    feat(code): add new code(558) logical-or-of-two-binary-grids-represented-as-quad-trees

---
+++ code/0500/0558/0558.logical-or-of-two-binary-grids-represented-as-quad-trees.go
@@ -0,0 +1,21 @@
+package _0558
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a QuadTree node.
+ * type Node struct {
+ *     Val bool
+ *     IsLeaf bool
+ *     TopLeft *Node
+ *     TopRight *Node
+ *     BottomLeft *Node
+ *     BottomRight *Node
+ * }
+ */
+
+func intersect(quadTree1 *Node, quadTree2 *Node) *Node {
+    
+}
