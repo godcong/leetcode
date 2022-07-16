b1731f4f7cd55d6a482ba1bf20149b333a069402
godcong <jumbycc@163.com>
2022-07-16 18:02:03 +0800

    feat(code): add new code(剑指 Offer II 041) qIsx9U

---
+++ offer/剑指 Offer II 000/SwordRefers_Offer_II_041/SwordRefers_Offer_II_041.qIsx9U.go
@@ -0,0 +1,27 @@
+package _SwordRefers_Offer_II_041
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MovingAverage struct {
+
+}
+
+
+/** Initialize your data structure here. */
+func Constructor(size int) MovingAverage {
+
+}
+
+
+func (this *MovingAverage) Next(val int) float64 {
+
+}
+
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * obj := Constructor(size);
+ * param_1 := obj.Next(val);
+ */
