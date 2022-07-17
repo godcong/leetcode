08a494fbd192237ff583efd30ce910aab90db87b
godcong <jumbycc@163.com>
2022-07-17 18:02:08 +0800

    feat(code): add new code(565) test for:array-nesting

---
+++ code/0500/0565/0565.array-nesting_test.go
@@ -0,0 +1,27 @@
+package _0565
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_arrayNesting(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := arrayNesting(tt.args.nums); got != tt.want {
+				t.Errorf("arrayNesting() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
