dd7f27da6da4df720328269de4d5b621c8c20584
godcong <jumbycc@163.com>
2022-07-21 18:02:23 +0800

    feat(code): add new code(814) binary-tree-pruning

---
+++ code/0800/0814/0814.binary-tree-pruning.go
@@ -0,0 +1,17 @@
+package _0814
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+
+}
