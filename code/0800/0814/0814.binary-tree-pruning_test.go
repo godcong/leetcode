257773b9c1c518b1661b9e63212075aef66bc6e3
godcong <jumbycc@163.com>
2022-07-21 18:02:23 +0800

    feat(code): add new code(814) test for:binary-tree-pruning

---
+++ code/0800/0814/0814.binary-tree-pruning_test.go
@@ -0,0 +1,28 @@
+package _0814
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_pruneTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pruneTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pruneTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
