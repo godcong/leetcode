b567a69d9d55a64dce3d0705eb8f8a5d60094b2d
godcong <jumbycc@163.com>
2022-07-22 18:02:27 +0800

    feat(code): add new code(757) test for:set-intersection-size-at-least-two

---
+++ code/0700/0757/0757.set-intersection-size-at-least-two_test.go
@@ -0,0 +1,27 @@
+package _0757
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_intersectionSizeTwo(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersectionSizeTwo(tt.args.intervals); got != tt.want {
+				t.Errorf("intersectionSizeTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
