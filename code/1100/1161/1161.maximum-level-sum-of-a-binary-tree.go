ffafc69ddde53b5f9fb553c744e6983ddfc90b61
godcong <jumbycc@163.com>
2022-07-31 18:28:12 +0800

    feat(code): add new code(1161) maximum-level-sum-of-a-binary-tree

---
+++ code/1100/1161/1161.maximum-level-sum-of-a-binary-tree.go
@@ -0,0 +1,17 @@
+package _1161
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxLevelSum(root *TreeNode) int {
+
+}
