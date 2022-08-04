a6276c6c5319da8567a0350862d528f4e84dc868
godcong <jumbycc@163.com>
2022-08-04 18:29:07 +0800

    feat(code): add new code(1403) test for:minimum-subsequence-in-non-increasing-order

---
+++ code/1400/1403/1403.minimum-subsequence-in-non-increasing-order_test.go
@@ -0,0 +1,28 @@
+package _1403
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minSubsequence(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSubsequence(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("minSubsequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
