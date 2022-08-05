9f9ac29582537d603fdf68d444229f0aba44c4ce
godcong <jumbycc@163.com>
2022-08-05 18:29:12 +0800

    feat(code): add new code(623) test for:add-one-row-to-tree

---
+++ code/0600/0623/0623.add-one-row-to-tree_test.go
@@ -0,0 +1,30 @@
+package _0623
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_addOneRow(t *testing.T) {
+	type args struct {
+		root  *TreeNode
+		val   int
+		depth int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addOneRow(tt.args.root, tt.args.val, tt.args.depth); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addOneRow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
