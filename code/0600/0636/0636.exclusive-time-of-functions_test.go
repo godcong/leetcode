9ba38ef38ffbd661c1bcd04e2fb1ee8d58c1e65b
godcong <jumbycc@163.com>
2022-08-07 18:29:22 +0800

    feat(code): add new code(636) test for:exclusive-time-of-functions

---
+++ code/0600/0636/0636.exclusive-time-of-functions_test.go
@@ -0,0 +1,29 @@
+package _0636
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_exclusiveTime(t *testing.T) {
+	type args struct {
+		n    int
+		logs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := exclusiveTime(tt.args.n, tt.args.logs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("exclusiveTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
