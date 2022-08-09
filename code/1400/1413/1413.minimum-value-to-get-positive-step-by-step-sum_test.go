ebb03deb69e6105749904df31a17fd0b7819ae33
godcong <jumbycc@163.com>
2022-08-09 18:29:31 +0800

    feat(code): add new code(1413) test for:minimum-value-to-get-positive-step-by-step-sum

---
+++ code/1400/1413/1413.minimum-value-to-get-positive-step-by-step-sum_test.go
@@ -0,0 +1,27 @@
+package _1413
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minStartValue(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minStartValue(tt.args.nums); got != tt.want {
+				t.Errorf("minStartValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
