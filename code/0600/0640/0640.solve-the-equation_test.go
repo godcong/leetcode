ad671fc7aaa1abb21b5c5d117b00d4461424618d
godcong <jumbycc@163.com>
2022-08-10 18:29:35 +0800

    feat(code): add new code(640) test for:solve-the-equation

---
+++ code/0600/0640/0640.solve-the-equation_test.go
@@ -0,0 +1,27 @@
+package _0640
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_solveEquation(t *testing.T) {
+	type args struct {
+		equation string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveEquation(tt.args.equation); got != tt.want {
+				t.Errorf("solveEquation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
