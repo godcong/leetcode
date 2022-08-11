ade43b2fa1abd668a5c31e2e388df4b47c1c8d2a
godcong <jumbycc@163.com>
2022-08-11 18:29:39 +0800

    feat(code): add new code(1417) test for:reformat-the-string

---
+++ code/1400/1417/1417.reformat-the-string_test.go
@@ -0,0 +1,27 @@
+package _1417
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_reformat(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reformat(tt.args.s); got != tt.want {
+				t.Errorf("reformat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
