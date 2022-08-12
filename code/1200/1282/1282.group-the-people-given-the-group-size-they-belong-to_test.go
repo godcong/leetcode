42681459b934767e417080d11ee39a4b1eaf9655
godcong <jumbycc@163.com>
2022-08-12 18:29:44 +0800

    feat(code): add new code(1282) test for:group-the-people-given-the-group-size-they-belong-to

---
+++ code/1200/1282/1282.group-the-people-given-the-group-size-they-belong-to_test.go
@@ -0,0 +1,28 @@
+package _1282
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_groupThePeople(t *testing.T) {
+	type args struct {
+		groupSizes []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := groupThePeople(tt.args.groupSizes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupThePeople() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
