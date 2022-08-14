72494d7c69b2889a6410ed18a561316f1a5ebb85
godcong <jumbycc@163.com>
2022-08-14 18:29:54 +0800

    feat(code): add new code(1422) test for:maximum-score-after-splitting-a-string

---
+++ code/1400/1422/1422.maximum-score-after-splitting-a-string_test.go
@@ -0,0 +1,27 @@
+package _1422
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxScore(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScore(tt.args.s); got != tt.want {
+				t.Errorf("maxScore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
