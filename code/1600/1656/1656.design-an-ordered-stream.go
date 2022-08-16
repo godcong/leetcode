39d7118477199c6ceb47d9085fe42c7d7083b8fd
godcong <jumbycc@163.com>
2022-08-16 18:30:03 +0800

    feat(code): add new code(1656) design-an-ordered-stream

---
+++ code/1600/1656/1656.design-an-ordered-stream.go
@@ -0,0 +1,26 @@
+package _1656
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type OrderedStream struct {
+
+}
+
+
+func Constructor(n int) OrderedStream {
+
+}
+
+
+func (this *OrderedStream) Insert(idKey int, value string) []string {
+
+}
+
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Insert(idKey,value);
+ */
