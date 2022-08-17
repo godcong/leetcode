d14f4fc1a5f03b9ebac951363f62011509fbae67
godcong <jumbycc@163.com>
2022-08-17 18:30:07 +0800

    feat(code): add new code(1302) deepest-leaves-sum

---
+++ code/1300/1302/1302.deepest-leaves-sum.go
@@ -0,0 +1,17 @@
+package _1302
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deepestLeavesSum(root *TreeNode) int {
+
+}
