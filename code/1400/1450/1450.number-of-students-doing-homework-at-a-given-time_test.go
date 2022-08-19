76175d35147e566edb69a5936dd051fb86166176
godcong <jumbycc@163.com>
2022-08-19 18:30:16 +0800

    feat(code): add new code(1450) test for:number-of-students-doing-homework-at-a-given-time

---
+++ code/1400/1450/1450.number-of-students-doing-homework-at-a-given-time_test.go
@@ -0,0 +1,29 @@
+package _1450
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_busyStudent(t *testing.T) {
+	type args struct {
+		startTime []int
+		endTime   []int
+		queryTime int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := busyStudent(tt.args.startTime, tt.args.endTime, tt.args.queryTime); got != tt.want {
+				t.Errorf("busyStudent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
