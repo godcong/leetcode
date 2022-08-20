37847980ce4ac8eb4a4e27161fd0a9cafbc8120b
godcong <jumbycc@163.com>
2022-08-20 18:30:21 +0800

    feat(code): add new code(654) test for:maximum-binary-tree

---
+++ code/0600/0654/0654.maximum-binary-tree_test.go
@@ -0,0 +1,28 @@
+package _0654
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_constructMaximumBinaryTree(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := constructMaximumBinaryTree(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructMaximumBinaryTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
