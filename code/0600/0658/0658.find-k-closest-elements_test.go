3b40dbb0e0e5aac05db44c41dbbcbdf253fe06f1
godcong <jumbycc@163.com>
2022-08-25 14:18:39 +0800

    feat(code): add new code(658) test for:find-k-closest-elements

---
+++ code/0600/0658/0658.find-k-closest-elements_test.go
@@ -0,0 +1,30 @@
+package _0658
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findClosestElements(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+		x   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findClosestElements(tt.args.arr, tt.args.k, tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findClosestElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
