03fb55d06132085f997a0c166213284c4483fa8b
godcong <jumbycc@163.com>
2022-08-27 14:18:49 +0800

    feat(code): add new code(662) maximum-width-of-binary-tree

---
+++ code/0600/0662/0662.maximum-width-of-binary-tree.go
@@ -0,0 +1,17 @@
+package _0662
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func widthOfBinaryTree(root *TreeNode) int {
+
+}
