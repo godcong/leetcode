f8137bab54e3c5503e6c3184285c69841433d602
godcong <jumbycc@163.com>
2022-08-29 14:24:09 +0800

    feat(code): add new code(1470) test for:shuffle-the-array

---
+++ code/1400/1470/1470.shuffle-the-array_test.go
@@ -0,0 +1,29 @@
+package _1470
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_shuffle(t *testing.T) {
+	type args struct {
+		nums []int
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shuffle(tt.args.nums, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("shuffle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
