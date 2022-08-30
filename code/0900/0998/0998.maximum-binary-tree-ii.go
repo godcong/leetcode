60e02b26727f340205c87c8dc55ac1a0cd7db67b
godcong <jumbycc@163.com>
2022-08-30 14:24:13 +0800

    feat(code): add new code(998) maximum-binary-tree-ii

---
+++ code/0900/0998/0998.maximum-binary-tree-ii.go
@@ -0,0 +1,17 @@
+package _0998
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoMaxTree(root *TreeNode, val int) *TreeNode {
+
+}
