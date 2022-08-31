2e879a6057c81432bfb9601b34537724e5bbd9c9
godcong <jumbycc@163.com>
2022-08-31 14:25:24 +0800

    feat(code): add new code(946) test for:validate-stack-sequences

---
+++ code/0900/0946/0946.validate-stack-sequences_test.go
@@ -0,0 +1,28 @@
+package _0946
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_validateStackSequences(t *testing.T) {
+	type args struct {
+		pushed []int
+		popped []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateStackSequences(tt.args.pushed, tt.args.popped); got != tt.want {
+				t.Errorf("validateStackSequences() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
