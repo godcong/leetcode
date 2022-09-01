2863a1368a2ea663427ae4f90c9e40f50f479ea6
godcong <jumbycc@163.com>
2022-09-01 14:26:23 +0800

    feat(code): add new code(1475) test for:final-prices-with-a-special-discount-in-a-shop

---
+++ code/1400/1475/1475.final-prices-with-a-special-discount-in-a-shop_test.go
@@ -0,0 +1,28 @@
+package _1475
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_finalPrices(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := finalPrices(tt.args.prices); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("finalPrices() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
