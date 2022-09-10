cd6a4773576273f88016d514ac480f5801777a0a
godcong <jumbycc@163.com>
2022-09-10 14:27:07 +0800

    feat(code): add new code(669) test for:trim-a-binary-search-tree

---
+++ code/0600/0669/0669.trim-a-binary-search-tree_test.go
@@ -0,0 +1,30 @@
+package _0669
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_trimBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		low  int
+		high int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trimBST(tt.args.root, tt.args.low, tt.args.high); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("trimBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
