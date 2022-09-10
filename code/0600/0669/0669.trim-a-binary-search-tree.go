77e8e0b1095499560272c438bb808ea81d985cbc
godcong <jumbycc@163.com>
2022-09-10 14:27:07 +0800

    feat(code): add new code(669) trim-a-binary-search-tree

---
+++ code/0600/0669/0669.trim-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package _0669
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+
+}
