91a7d4ff8bb058b89c77ab7e02b535906faf9e93
godcong <jumbycc@163.com>
2022-09-16 18:40:30 +0800

    feat(code): add new code(850) test for:rectangle-area-ii

---
+++ code/0800/0850/0850.rectangle-area-ii_test.go
@@ -0,0 +1,27 @@
+package _0850
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_rectangleArea(t *testing.T) {
+	type args struct {
+		rectangles [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rectangleArea(tt.args.rectangles); got != tt.want {
+				t.Errorf("rectangleArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
