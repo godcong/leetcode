4fa8e39e9aa62eebefd7278d110192aa5c7e09ba
godcong <jumbycc@163.com>
2022-09-25 18:42:23 +0800

    feat(code): add new code(788) test for:rotated-digits

---
+++ code/0700/0788/0788.rotated-digits_test.go
@@ -0,0 +1,27 @@
+package _0788
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_rotatedDigits(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rotatedDigits(tt.args.n); got != tt.want {
+				t.Errorf("rotatedDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
