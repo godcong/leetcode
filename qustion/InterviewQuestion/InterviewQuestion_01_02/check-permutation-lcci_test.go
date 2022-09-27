0e285997f8688d36fe81b3482298837299044d79
godcong <jumbycc@163.com>
2022-09-27 19:39:09 +0800

    feat(code): add new code(InterviewQuestion_01_02) test for:check-permutation-lcci

---
+++ qustion/InterviewQuestion/InterviewQuestion_01_02/check-permutation-lcci_test.go
@@ -0,0 +1,28 @@
+package _InterviewQuestion_01_02
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func TestCheckPermutation(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CheckPermutation(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("CheckPermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
