79da6245f99253150948b12dad2b49fedf84c59c
godcong <jumbycc@163.com>
2022-09-30 19:39:27 +0800

    feat(code): add new code(InterviewQuestion_01_08) test for:zero-matrix-lcci

---
+++ qustion/InterviewQuestion/InterviewQuestion_01_08/zero-matrix-lcci_test.go
@@ -0,0 +1,24 @@
+package _InterviewQuestion_01_08
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_setZeroes(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setZeroes(tt.args.matrix)
+		})
+	}
+}
