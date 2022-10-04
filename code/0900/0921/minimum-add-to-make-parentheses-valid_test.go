3db616ccec9ce7431357950ebbdc770ccf626b40
godcong <jumbycc@163.com>
2022-10-04 15:53:18 +0800

    feat(code): add new code(0921) test for:minimum-add-to-make-parentheses-valid

---
+++ code/0900/0921/minimum-add-to-make-parentheses-valid_test.go
@@ -0,0 +1,27 @@
+package _0921
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minAddToMakeValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minAddToMakeValid(tt.args.s); got != tt.want {
+				t.Errorf("minAddToMakeValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
