f1422dbf2c9b628f1ea76e13fbcd74a4bcfc60d9
godcong <jumbycc@163.com>
2022-10-05 15:53:24 +0800

    feat(code): add new code(0811) test for:subdomain-visit-count

---
+++ code/0800/0811/subdomain-visit-count_test.go
@@ -0,0 +1,28 @@
+package _0811
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_subdomainVisits(t *testing.T) {
+	type args struct {
+		cpdomains []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subdomainVisits(tt.args.cpdomains); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subdomainVisits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
