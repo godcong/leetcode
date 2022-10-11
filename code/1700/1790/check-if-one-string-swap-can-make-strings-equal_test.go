ae57f026d0cbeaa66a85029990d26c3ca1389ee9
godcong <jumbycc@163.com>
2022-10-11 15:54:09 +0800

    feat(code): add new code(1790) test for:check-if-one-string-swap-can-make-strings-equal

---
+++ code/1700/1790/check-if-one-string-swap-can-make-strings-equal_test.go
@@ -0,0 +1,28 @@
+package _1790
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_areAlmostEqual(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := areAlmostEqual(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("areAlmostEqual() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
