e5cd0f46253d80d3220647000266d031dbb93e60
godcong <jumbycc@163.com>
2022-10-17 15:54:48 +0800

    feat(code): add new code(0904) test for:fruit-into-baskets

---
+++ code/0900/0904/fruit-into-baskets_test.go
@@ -0,0 +1,27 @@
+package _0904
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_totalFruit(t *testing.T) {
+	type args struct {
+		fruits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := totalFruit(tt.args.fruits); got != tt.want {
+				t.Errorf("totalFruit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
