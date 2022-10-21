941da596d1f1b1aa68793ce9080eac52f74b1488
godcong <jumbycc@163.com>
2022-10-21 15:55:13 +0800

    feat(code): add new code(0901) online-stock-span

---
+++ code/0900/0901/online-stock-span.go
@@ -0,0 +1,26 @@
+package _0901
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type StockSpanner struct {
+
+}
+
+
+func Constructor() StockSpanner {
+
+}
+
+
+func (this *StockSpanner) Next(price int) int {
+
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Next(price);
+ */
