82d0097303ee318b140c1c07b8d0a71ed14b9e99
godcong <jumbycc@163.com>
2022-10-22 15:55:20 +0800

    feat(code): add new code(1235) test for:maximum-profit-in-job-scheduling

---
+++ code/1200/1235/maximum-profit-in-job-scheduling_test.go
@@ -0,0 +1,29 @@
+package _1235
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_jobScheduling(t *testing.T) {
+	type args struct {
+		startTime []int
+		endTime   []int
+		profit    []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jobScheduling(tt.args.startTime, tt.args.endTime, tt.args.profit); got != tt.want {
+				t.Errorf("jobScheduling() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
