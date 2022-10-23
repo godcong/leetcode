512a037595fc49ddb453907b9cd16e8cd721a6db
godcong <jumbycc@163.com>
2022-10-23 15:55:26 +0800

    feat(code): add new code(1768) test for:merge-strings-alternately

---
+++ code/1700/1768/merge-strings-alternately_test.go
@@ -0,0 +1,28 @@
+package _1768
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_mergeAlternately(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeAlternately(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("mergeAlternately() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
