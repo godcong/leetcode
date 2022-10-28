da5e019ce1fb0b341e45b4cf964cb3b92fce8d05
godcong <jumbycc@163.com>
2022-10-28 15:55:58 +0800

    feat(code): add new code(0907) test for:sum-of-subarray-minimums

---
+++ code/0900/0907/sum-of-subarray-minimums_test.go
@@ -0,0 +1,27 @@
+package _0907
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_sumSubarrayMins(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumSubarrayMins(tt.args.arr); got != tt.want {
+				t.Errorf("sumSubarrayMins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
