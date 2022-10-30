cc001c3e6b852bf71d7c9d402c7682b1f322ef11
godcong <jumbycc@163.com>
2022-10-30 21:24:28 +0800

    feat(code): add new code(0784) test for:letter-case-permutation

---
+++ code/0700/0784/letter-case-permutation_test.go
@@ -0,0 +1,28 @@
+package _0784
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_letterCasePermutation(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := letterCasePermutation(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("letterCasePermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
