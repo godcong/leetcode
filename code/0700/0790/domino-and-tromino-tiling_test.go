cf9ebc70034ef9c4717e9d45bb2ca3629f8371fd
godcong <jumbycc@163.com>
2022-11-12 21:26:21 +0800

    feat(code): add new code(0790) test for:domino-and-tromino-tiling

---
+++ code/0700/0790/domino-and-tromino-tiling_test.go
@@ -0,0 +1,27 @@
+package _0790
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numTilings(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numTilings(tt.args.n); got != tt.want {
+				t.Errorf("numTilings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
