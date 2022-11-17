4a17d4dc1609c8c5ce4597eab75c65349e98fe5e
godcong <jumbycc@163.com>
2022-11-17 21:26:40 +0800

    feat(code): add new code(0792) test for:number-of-matching-subsequences

---
+++ code/0700/0792/number-of-matching-subsequences_test.go
@@ -0,0 +1,28 @@
+package _0792
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numMatchingSubseq(t *testing.T) {
+	type args struct {
+		s     string
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numMatchingSubseq(tt.args.s, tt.args.words); got != tt.want {
+				t.Errorf("numMatchingSubseq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
