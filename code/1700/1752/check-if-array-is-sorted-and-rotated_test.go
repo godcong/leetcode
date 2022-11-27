0b5a0ff3dee0b88362032619385a5eba69be3fb6
godcong <jumbycc@163.com>
2022-11-27 21:27:15 +0800

    feat(code): add new code(1752) test for:check-if-array-is-sorted-and-rotated

---
+++ code/1700/1752/check-if-array-is-sorted-and-rotated_test.go
@@ -0,0 +1,27 @@
+package _1752
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_check(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := check(tt.args.nums); got != tt.want {
+				t.Errorf("check() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
