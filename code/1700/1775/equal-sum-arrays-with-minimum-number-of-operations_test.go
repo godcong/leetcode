f9a81002c15218b9e60e4c982d3a7bcfeac57035
godcong <jumbycc@163.com>
2022-12-07 16:32:46 +0800

    feat(code): add new code(1775) test for:equal-sum-arrays-with-minimum-number-of-operations

---
+++ code/1700/1775/equal-sum-arrays-with-minimum-number-of-operations_test.go
@@ -0,0 +1,28 @@
+package _1775
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minOperations(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minOperations(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("minOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
