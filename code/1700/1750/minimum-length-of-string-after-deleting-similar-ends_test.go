97c7300384abe405f183d5093069ff8e74298f96
godcong <jumbycc@163.com>
2022-12-28 16:34:05 +0800

    feat(code): add new code(1750) test for:minimum-length-of-string-after-deleting-similar-ends

---
+++ code/1700/1750/minimum-length-of-string-after-deleting-similar-ends_test.go
@@ -0,0 +1,27 @@
+package _1750
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumLength(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumLength(tt.args.s); got != tt.want {
+				t.Errorf("minimumLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
