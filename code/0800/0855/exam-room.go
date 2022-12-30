dec5fc0bd6daf3e3fa87d2340ddd59165d63895f
godcong <jumbycc@163.com>
2022-12-30 16:34:12 +0800

    feat(code): add new code(0855) exam-room

---
+++ code/0800/0855/exam-room.go
@@ -0,0 +1,32 @@
+package _0855
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type ExamRoom struct {
+
+}
+
+
+func Constructor(n int) ExamRoom {
+
+}
+
+
+func (this *ExamRoom) Seat() int {
+
+}
+
+
+func (this *ExamRoom) Leave(p int)  {
+
+}
+
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Seat();
+ * obj.Leave(p);
+ */
