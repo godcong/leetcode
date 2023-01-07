8eb23c2b42603a378ece77246e8bc5403c488a53
godcong <jumbycc@163.com>
2023-01-07 16:51:57 +0800

    feat(code): add new code(1658) test for:minimum-operations-to-reduce-x-to-zero

---
+++ code/1600/1658/minimum-operations-to-reduce-x-to-zero_test.go
@@ -0,0 +1,28 @@
+package _1658
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minOperations(t *testing.T) {
+	type args struct {
+		nums []int
+		x    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minOperations(tt.args.nums, tt.args.x); got != tt.want {
+				t.Errorf("minOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
