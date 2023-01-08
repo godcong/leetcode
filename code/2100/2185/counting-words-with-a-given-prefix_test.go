5db4426cd0360783f12b0fb01cc4b464c60664ca
godcong <jumbycc@163.com>
2023-01-08 16:52:02 +0800

    feat(code): add new code(2185) test for:counting-words-with-a-given-prefix

---
+++ code/2100/2185/counting-words-with-a-given-prefix_test.go
@@ -0,0 +1,28 @@
+package _2185
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_prefixCount(t *testing.T) {
+	type args struct {
+		words []string
+		pref  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := prefixCount(tt.args.words, tt.args.pref); got != tt.want {
+				t.Errorf("prefixCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
