154708864fbd7592f24bac36d75249796a69c64e
godcong <jumbycc@163.com>
2023-01-10 16:52:14 +0800

    feat(code): add new code(0753) test for:cracking-the-safe

---
+++ code/0700/0753/cracking-the-safe_test.go
@@ -0,0 +1,28 @@
+package _0753
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_crackSafe(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := crackSafe(tt.args.n, tt.args.k); got != tt.want {
+				t.Errorf("crackSafe() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
