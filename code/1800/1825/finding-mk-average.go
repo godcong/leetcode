ebe28f1a48e57f70fd8a7e92966f2b5ab3c7a283
godcong <jumbycc@163.com>
2023-01-18 16:52:51 +0800

    feat(code): add new code(1825) finding-mk-average

---
+++ code/1800/1825/finding-mk-average.go
@@ -0,0 +1,32 @@
+package _1825
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MKAverage struct {
+
+}
+
+
+func Constructor(m int, k int) MKAverage {
+
+}
+
+
+func (this *MKAverage) AddElement(num int)  {
+
+}
+
+
+func (this *MKAverage) CalculateMKAverage() int {
+
+}
+
+
+/**
+ * Your MKAverage object will be instantiated and called as such:
+ * obj := Constructor(m, k);
+ * obj.AddElement(num);
+ * param_2 := obj.CalculateMKAverage();
+ */
