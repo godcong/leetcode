53b26e5a257f2d7781d28080e25a15c0e0c1d1c0
godcong <jumbycc@163.com>
2023-01-30 12:53:23 +0800

    feat(code): add new code(1669) merge-in-between-linked-lists

---
+++ code/1600/1669/merge-in-between-linked-lists.go
@@ -0,0 +1,16 @@
+package _1669
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
+    
+}
