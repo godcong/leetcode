af046fcd376ab7826d884d114e7ecdb1a75ca0bd
godcong <jumbycc@163.com>
2023-02-03 12:54:02 +0800

    feat(code): add new code(1145) binary-tree-coloring-game

---
+++ code/1100/1145/binary-tree-coloring-game.go
@@ -0,0 +1,17 @@
+package _1145
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func btreeGameWinningMove(root *TreeNode, n int, x int) bool {
+
+}
