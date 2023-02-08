fbee90e6347e414a11c2fad1f33071943ccdd211
godcong <jumbycc@163.com>
2023-02-08 18:21:22 +0800

    feat(code): add new code(1233) test for:remove-sub-folders-from-the-filesystem

---
+++ code/1200/1233/remove-sub-folders-from-the-filesystem_test.go
@@ -0,0 +1,28 @@
+package _1233
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_removeSubfolders(t *testing.T) {
+	type args struct {
+		folder []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeSubfolders(tt.args.folder); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeSubfolders() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
