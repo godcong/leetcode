4a9efbb70540b76c7455d429846dc44435dacf8f
godcong <jumbycc@163.com>
2023-02-10 18:21:30 +0800

    feat(code): add new code(1223) test for:dice-roll-simulation

---
+++ code/1200/1223/dice-roll-simulation_test.go
@@ -0,0 +1,28 @@
+package _1223
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_dieSimulator(t *testing.T) {
+	type args struct {
+		n       int
+		rollMax []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := dieSimulator(tt.args.n, tt.args.rollMax); got != tt.want {
+				t.Errorf("dieSimulator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
