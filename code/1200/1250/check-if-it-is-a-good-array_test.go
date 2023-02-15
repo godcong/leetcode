d5c3ae0a3dec6d705c9618b325fd1648f919e99a
godcong <jumbycc@163.com>
2023-02-15 18:21:50 +0800

    feat(code): add new code(1250) test for:check-if-it-is-a-good-array

---
+++ code/1200/1250/check-if-it-is-a-good-array_test.go
@@ -0,0 +1,27 @@
+package _1250
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isGoodArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isGoodArray(tt.args.nums); got != tt.want {
+				t.Errorf("isGoodArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
