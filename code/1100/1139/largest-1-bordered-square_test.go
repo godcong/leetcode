be2d546b1ea979a5c49e6ef607a76802e41a64f8
godcong <jumbycc@163.com>
2023-02-17 18:22:00 +0800

    feat(code): add new code(1139) test for:largest-1-bordered-square

---
+++ code/1100/1139/largest-1-bordered-square_test.go
@@ -0,0 +1,27 @@
+package _1139
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_largest1BorderedSquare(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largest1BorderedSquare(tt.args.grid); got != tt.want {
+				t.Errorf("largest1BorderedSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
