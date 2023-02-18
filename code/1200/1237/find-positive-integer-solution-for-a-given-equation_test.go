cc3ebd8924cb5d5c1e8a525d523f37fc8b2566b5
godcong <jumbycc@163.com>
2023-02-18 18:22:04 +0800

    feat(code): add new code(1237) test for:find-positive-integer-solution-for-a-given-equation

---
+++ code/1200/1237/find-positive-integer-solution-for-a-given-equation_test.go
@@ -0,0 +1,29 @@
+package _1237
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findSolution(t *testing.T) {
+	type args struct {
+		customFunction func(int, int) int
+		z              int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findSolution(tt.args.customFunction, tt.args.z); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findSolution() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
