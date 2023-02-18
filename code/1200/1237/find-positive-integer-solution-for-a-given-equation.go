6a58d317158336980cf1fb23743f9e239cbba782
godcong <jumbycc@163.com>
2023-02-18 18:22:04 +0800

    feat(code): add new code(1237) find-positive-integer-solution-for-a-given-equation

---
+++ code/1200/1237/find-positive-integer-solution-for-a-given-equation.go
@@ -0,0 +1,18 @@
+package _1237
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/** 
+ * This is the declaration of customFunction API.
+ * @param  x    int
+ * @param  x    int
+ * @return 	    Returns f(x, y) for any given positive integers x and y.
+ *			    Note that f(x, y) is increasing with respect to both x and y.
+ *              i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ */
+
+func findSolution(customFunction func(int, int) int, z int) [][]int {
+    
+}
