01b3f26c2703fd629f86a2a613f609cebe8df41c
godcong <jumbycc@163.com>
2023-02-21 18:22:20 +0800

    feat(code): add new code(1326) test for:minimum-number-of-taps-to-open-to-water-a-garden

---
+++ code/1300/1326/minimum-number-of-taps-to-open-to-water-a-garden_test.go
@@ -0,0 +1,28 @@
+package _1326
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minTaps(t *testing.T) {
+	type args struct {
+		n      int
+		ranges []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minTaps(tt.args.n, tt.args.ranges); got != tt.want {
+				t.Errorf("minTaps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
