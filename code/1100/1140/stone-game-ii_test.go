4f5a9978d95597d9979f84adf3d6db6de24bbd8f
godcong <jumbycc@163.com>
2023-02-22 18:22:25 +0800

    feat(code): add new code(1140) test for:stone-game-ii

---
+++ code/1100/1140/stone-game-ii_test.go
@@ -0,0 +1,27 @@
+package _1140
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_stoneGameII(t *testing.T) {
+	type args struct {
+		piles []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := stoneGameII(tt.args.piles); got != tt.want {
+				t.Errorf("stoneGameII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
