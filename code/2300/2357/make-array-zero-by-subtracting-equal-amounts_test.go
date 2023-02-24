9d3249322412146697c2c3630415e7ae09bea01e
godcong <jumbycc@163.com>
2023-02-24 18:22:36 +0800

    feat(code): add new code(2357) test for:make-array-zero-by-subtracting-equal-amounts

---
+++ code/2300/2357/make-array-zero-by-subtracting-equal-amounts_test.go
@@ -0,0 +1,27 @@
+package _2357
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumOperations(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumOperations(tt.args.nums); got != tt.want {
+				t.Errorf("minimumOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
