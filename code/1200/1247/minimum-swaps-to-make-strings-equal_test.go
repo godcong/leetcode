dbb3ecaa395bd387bf19b6d375a121ab581cd6f3
godcong <jumbycc@163.com>
2023-02-25 17:50:04 +0800

    feat(code): add new code(1247) test for:minimum-swaps-to-make-strings-equal

---
+++ code/1200/1247/minimum-swaps-to-make-strings-equal_test.go
@@ -0,0 +1,28 @@
+package _1247
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumSwap(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumSwap(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("minimumSwap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
