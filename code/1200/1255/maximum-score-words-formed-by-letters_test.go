33fec67a413f378e648f6f97f096cf0144784740
godcong <jumbycc@163.com>
2023-02-26 17:50:07 +0800

    feat(code): add new code(1255) test for:maximum-score-words-formed-by-letters

---
+++ code/1200/1255/maximum-score-words-formed-by-letters_test.go
@@ -0,0 +1,29 @@
+package _1255
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxScoreWords(t *testing.T) {
+	type args struct {
+		words   []string
+		letters []byte
+		score   []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScoreWords(tt.args.words, tt.args.letters, tt.args.score); got != tt.want {
+				t.Errorf("maxScoreWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
