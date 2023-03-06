8fc3de35d0a4fc9a6f4c16fb60796eb2e39bed28
godcong <jumbycc@163.com>
2023-03-06 17:50:31 +0800

    feat(code): add new code(1653) test for:minimum-deletions-to-make-string-balanced

---
+++ code/1600/1653/minimum-deletions-to-make-string-balanced_test.go
@@ -0,0 +1,27 @@
+package _1653
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumDeletions(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumDeletions(tt.args.s); got != tt.want {
+				t.Errorf("minimumDeletions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
