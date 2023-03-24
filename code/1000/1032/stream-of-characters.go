5e5aff1f8a887f05b3a8f58b2df929ae6728e455
godcong <jumbycc@163.com>
2023-03-24 17:51:23 +0800

    feat(code): add new code(1032) stream-of-characters

---
+++ code/1000/1032/stream-of-characters.go
@@ -0,0 +1,26 @@
+package _1032
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type StreamChecker struct {
+
+}
+
+
+func Constructor(words []string) StreamChecker {
+
+}
+
+
+func (this *StreamChecker) Query(letter byte) bool {
+
+}
+
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * obj := Constructor(words);
+ * param_1 := obj.Query(letter);
+ */
