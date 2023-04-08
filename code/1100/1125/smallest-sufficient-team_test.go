9081300a171cfb6fe281cf5f7cf08796143b90db
godcong <jumbycc@163.com>
2023-04-08 17:25:13 +0800

    feat(code): add new code(1125) test for:smallest-sufficient-team

---
+++ code/1100/1125/smallest-sufficient-team_test.go
@@ -0,0 +1,29 @@
+package _1125
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_smallestSufficientTeam(t *testing.T) {
+	type args struct {
+		req_skills []string
+		people     [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := smallestSufficientTeam(tt.args.req_skills, tt.args.people); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("smallestSufficientTeam() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
