adff4c8a55efafdf1e71321901a2842dc7e36419
godcong <jumbycc@163.com>
2023-04-10 17:25:23 +0800

    feat(code): add new code(1019) next-greater-node-in-linked-list

---
+++ code/1000/1019/next-greater-node-in-linked-list.go
@@ -0,0 +1,16 @@
+package _1019
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func nextLargerNodes(head *ListNode) []int {
+
+}
