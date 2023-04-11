d5fb92211e85c73af673306eb63f6cdec343d167
godcong <jumbycc@163.com>
2023-04-11 17:25:29 +0800

    feat(code): add new code(1041) test for:robot-bounded-in-circle

---
+++ code/1000/1041/robot-bounded-in-circle_test.go
@@ -0,0 +1,27 @@
+package _1041
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isRobotBounded(t *testing.T) {
+	type args struct {
+		instructions string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRobotBounded(tt.args.instructions); got != tt.want {
+				t.Errorf("isRobotBounded() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
