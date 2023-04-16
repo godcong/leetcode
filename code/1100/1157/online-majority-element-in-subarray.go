86bb22ccd44714e8fb299333002e39e573c68037
godcong <jumbycc@163.com>
2023-04-16 17:25:58 +0800

    feat(code): add new code(1157) online-majority-element-in-subarray

---
+++ code/1100/1157/online-majority-element-in-subarray.go
@@ -0,0 +1,26 @@
+package _1157
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MajorityChecker struct {
+
+}
+
+
+func Constructor(arr []int) MajorityChecker {
+
+}
+
+
+func (this *MajorityChecker) Query(left int, right int, threshold int) int {
+
+}
+
+
+/**
+ * Your MajorityChecker object will be instantiated and called as such:
+ * obj := Constructor(arr);
+ * param_1 := obj.Query(left,right,threshold);
+ */
