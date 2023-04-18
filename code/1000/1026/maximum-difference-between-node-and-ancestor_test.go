82a9d106d6f2524a877f9e4673e66157f6f26ed3
godcong <jumbycc@163.com>
2023-04-18 17:26:11 +0800

    feat(code): add new code(1026) test for:maximum-difference-between-node-and-ancestor

---
+++ code/1000/1026/maximum-difference-between-node-and-ancestor_test.go
@@ -0,0 +1,27 @@
+package _1026
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxAncestorDiff(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxAncestorDiff(tt.args.root); got != tt.want {
+				t.Errorf("maxAncestorDiff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
