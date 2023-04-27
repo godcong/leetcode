cc7160a7ea33bba0faaf6011a982a81fd1e8d625
godcong <jumbycc@163.com>
2023-04-27 17:27:05 +0800

    feat(code): add new code(1048) test for:longest-string-chain

---
+++ code/1000/1048/longest-string-chain_test.go
@@ -0,0 +1,27 @@
+package _1048
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_longestStrChain(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestStrChain(tt.args.words); got != tt.want {
+				t.Errorf("longestStrChain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
