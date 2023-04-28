1f057fc9d5d45316c0dc1c99ba1f36aa5ff2e293
godcong <jumbycc@163.com>
2023-04-28 17:27:11 +0800

    feat(code): add new code(1172) dinner-plate-stacks

---
+++ code/1100/1172/dinner-plate-stacks.go
@@ -0,0 +1,38 @@
+package _1172
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type DinnerPlates struct {
+
+}
+
+
+func Constructor(capacity int) DinnerPlates {
+
+}
+
+
+func (this *DinnerPlates) Push(val int)  {
+
+}
+
+
+func (this *DinnerPlates) Pop() int {
+
+}
+
+
+func (this *DinnerPlates) PopAtStack(index int) int {
+
+}
+
+
+/**
+ * Your DinnerPlates object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * obj.Push(val);
+ * param_2 := obj.Pop();
+ * param_3 := obj.PopAtStack(index);
+ */
