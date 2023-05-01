a6896de58dceff3557151aa0551f8ffec6cde395
godcong <jumbycc@163.com>
2023-05-01 17:27:30 +0800

    feat(code): add new code(1376) test for:time-needed-to-inform-all-employees

---
+++ code/1300/1376/time-needed-to-inform-all-employees_test.go
@@ -0,0 +1,30 @@
+package _1376
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numOfMinutes(t *testing.T) {
+	type args struct {
+		n          int
+		headID     int
+		manager    []int
+		informTime []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numOfMinutes(tt.args.n, tt.args.headID, tt.args.manager, tt.args.informTime); got != tt.want {
+				t.Errorf("numOfMinutes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
