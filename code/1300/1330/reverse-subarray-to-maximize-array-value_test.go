e77c11cc11988349cd2ca7800cd43a42b6ce87fb
godcong <jumbycc@163.com>
2023-05-12 17:28:34 +0800

    feat(code): add new code(1330) test for:reverse-subarray-to-maximize-array-value

---
+++ code/1300/1330/reverse-subarray-to-maximize-array-value_test.go
@@ -0,0 +1,27 @@
+package _1330
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxValueAfterReverse(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxValueAfterReverse(tt.args.nums); got != tt.want {
+				t.Errorf("maxValueAfterReverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
