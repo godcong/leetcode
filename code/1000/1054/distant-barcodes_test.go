7ff7bad9ad2d049d999a900cad0079766750a627
godcong <jumbycc@163.com>
2023-05-14 17:45:46 +0800

    feat(code): add new code(1054) test for:distant-barcodes

---
+++ code/1000/1054/distant-barcodes_test.go
@@ -0,0 +1,28 @@
+package _1054
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_rearrangeBarcodes(t *testing.T) {
+	type args struct {
+		barcodes []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rearrangeBarcodes(tt.args.barcodes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rearrangeBarcodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
