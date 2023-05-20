8004fd86b96f3c784e8d5f966ab6239880ae8030
godcong <jumbycc@163.com>
2023-05-20 17:46:09 +0800

    feat(code): add new code(1373) maximum-sum-bst-in-binary-tree

---
+++ code/1300/1373/maximum-sum-bst-in-binary-tree.go
@@ -0,0 +1,17 @@
+package _1373
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxSumBST(root *TreeNode) int {
+
+}
