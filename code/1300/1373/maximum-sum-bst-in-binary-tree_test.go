1df94889116f565b2fd13f16849358500745b594
godcong <jumbycc@163.com>
2023-05-20 17:46:10 +0800

    feat(code): add new code(1373) test for:maximum-sum-bst-in-binary-tree

---
+++ code/1300/1373/maximum-sum-bst-in-binary-tree_test.go
@@ -0,0 +1,27 @@
+package _1373
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxSumBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSumBST(tt.args.root); got != tt.want {
+				t.Errorf("maxSumBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
