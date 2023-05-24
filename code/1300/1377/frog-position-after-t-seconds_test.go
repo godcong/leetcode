ae9df324e2db71690209262952fecae681e50eb6
godcong <jumbycc@163.com>
2023-05-24 17:46:26 +0800

    feat(code): add new code(1377) test for:frog-position-after-t-seconds

---
+++ code/1300/1377/frog-position-after-t-seconds_test.go
@@ -0,0 +1,30 @@
+package _1377
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_frogPosition(t *testing.T) {
+	type args struct {
+		n      int
+		edges  [][]int
+		t      int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := frogPosition(tt.args.n, tt.args.edges, tt.args.t, tt.args.target); got != tt.want {
+				t.Errorf("frogPosition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
