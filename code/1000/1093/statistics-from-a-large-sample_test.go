d246dee2b20b13d9b00a2296072da496bbf7da63
godcong <jumbycc@163.com>
2023-05-27 17:46:38 +0800

    feat(code): add new code(1093) test for:statistics-from-a-large-sample

---
+++ code/1000/1093/statistics-from-a-large-sample_test.go
@@ -0,0 +1,28 @@
+package _1093
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_sampleStats(t *testing.T) {
+	type args struct {
+		count []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sampleStats(tt.args.count); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sampleStats() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
