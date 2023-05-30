86752a25bda9a3d032fba57a74c489a55eaf0546
godcong <jumbycc@163.com>
2023-05-30 17:46:51 +0800

    feat(code): add new code(1110) test for:delete-nodes-and-return-forest

---
+++ code/1100/1110/delete-nodes-and-return-forest_test.go
@@ -0,0 +1,29 @@
+package _1110
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_delNodes(t *testing.T) {
+	type args struct {
+		root      *TreeNode
+		to_delete []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := delNodes(tt.args.root, tt.args.to_delete); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("delNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
