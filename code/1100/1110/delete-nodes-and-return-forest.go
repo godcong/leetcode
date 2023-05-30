cd74e51eb37653ddaf24499e7dd6568fb9adcd4b
godcong <jumbycc@163.com>
2023-05-30 17:46:50 +0800

    feat(code): add new code(1110) delete-nodes-and-return-forest

---
+++ code/1100/1110/delete-nodes-and-return-forest.go
@@ -0,0 +1,17 @@
+package _1110
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func delNodes(root *TreeNode, to_delete []int) []*TreeNode {
+    
+}
