cc7700fc00d40dc80d3ba3b6a5da258e2afce8e8
godcong <jumbycc@163.com>
2023-05-31 17:46:55 +0800

    feat(code): add new code(1130) test for:minimum-cost-tree-from-leaf-values

---
+++ code/1100/1130/minimum-cost-tree-from-leaf-values_test.go
@@ -0,0 +1,27 @@
+package _1130
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_mctFromLeafValues(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mctFromLeafValues(tt.args.arr); got != tt.want {
+				t.Errorf("mctFromLeafValues() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
