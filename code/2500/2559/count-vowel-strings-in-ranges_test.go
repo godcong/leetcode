78b1f5c2dbb1fab8c0d0d48b1b50b094e0b32dcb
godcong <jumbycc@163.com>
2023-06-02 19:18:35 +0800

    feat(code): add new code(2559) test for:count-vowel-strings-in-ranges

---
+++ code/2500/2559/count-vowel-strings-in-ranges_test.go
@@ -0,0 +1,29 @@
+package _2559
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_vowelStrings(t *testing.T) {
+	type args struct {
+		words   []string
+		queries [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := vowelStrings(tt.args.words, tt.args.queries); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("vowelStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
