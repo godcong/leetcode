68f534ab3efbb6f183f525a92f6d6175a2823c37
godcong <jumbycc@163.com>
2023-06-03 19:18:38 +0800

    feat(code): add new code(1156) test for:swap-for-longest-repeated-character-substring

---
+++ code/1100/1156/swap-for-longest-repeated-character-substring_test.go
@@ -0,0 +1,27 @@
+package _1156
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxRepOpt1(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxRepOpt1(tt.args.text); got != tt.want {
+				t.Errorf("maxRepOpt1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
