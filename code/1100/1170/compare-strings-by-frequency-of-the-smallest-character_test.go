cefdacbdf4c573267999d9a79df6ae2d53e2f5f3
godcong <jumbycc@163.com>
2023-06-10 19:19:02 +0800

    feat(code): add new code(1170) test for:compare-strings-by-frequency-of-the-smallest-character

---
+++ code/1100/1170/compare-strings-by-frequency-of-the-smallest-character_test.go
@@ -0,0 +1,29 @@
+package _1170
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numSmallerByFrequency(t *testing.T) {
+	type args struct {
+		queries []string
+		words   []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSmallerByFrequency(tt.args.queries, tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("numSmallerByFrequency() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
