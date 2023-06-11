e9158db8ff1d9547c211bfdc2837d27eaa36340a
godcong <jumbycc@163.com>
2023-06-11 19:19:05 +0800

    feat(code): add new code(1171) remove-zero-sum-consecutive-nodes-from-linked-list

---
+++ code/1100/1171/remove-zero-sum-consecutive-nodes-from-linked-list.go
@@ -0,0 +1,16 @@
+package _1171
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeZeroSumSublists(head *ListNode) *ListNode {
+
+}
