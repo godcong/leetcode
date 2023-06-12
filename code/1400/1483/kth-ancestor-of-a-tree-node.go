143fd6efaaac1b1a69d6c8f1e9e8ef47db4056a7
godcong <jumbycc@163.com>
2023-06-12 19:19:08 +0800

    feat(code): add new code(1483) kth-ancestor-of-a-tree-node

---
+++ code/1400/1483/kth-ancestor-of-a-tree-node.go
@@ -0,0 +1,26 @@
+package _1483
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type TreeAncestor struct {
+
+}
+
+
+func Constructor(n int, parent []int) TreeAncestor {
+
+}
+
+
+func (this *TreeAncestor) GetKthAncestor(node int, k int) int {
+
+}
+
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * obj := Constructor(n, parent);
+ * param_1 := obj.GetKthAncestor(node,k);
+ */
