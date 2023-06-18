42822f1c33f1cff6a6ddf65bb24a710aa49f5bec
godcong <jumbycc@163.com>
2023-06-18 19:19:29 +0800

    feat(code): add new code(1254) test for:number-of-closed-islands

---
+++ code/1200/1254/number-of-closed-islands_test.go
@@ -0,0 +1,27 @@
+package _1254
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_closedIsland(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := closedIsland(tt.args.grid); got != tt.want {
+				t.Errorf("closedIsland() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
