766bc9feff8d9027c8ca59b26863923e2970a488
godcong <jumbycc@163.com>
2023-07-03 19:44:13 +0800

    feat(code): add new code(0445) test for:add-two-numbers-ii

---
+++ code/0400/0445/add-two-numbers-ii_test.go
@@ -0,0 +1,29 @@
+package _0445
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
