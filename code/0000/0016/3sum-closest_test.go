ea226cc6ca9a0dddd868f822701eb89ab1077907
godcong <jumbycc@163.com>
2023-07-10 19:44:38 +0800

    feat(code): add new code(0016) test for:3sum-closest

---
+++ code/0000/0016/3sum-closest_test.go
@@ -0,0 +1,28 @@
+package _0016
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_threeSumClosest(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("threeSumClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
