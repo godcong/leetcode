00180294958c4fc2f9a56875d86066541fad850c
godcong <jumbycc@163.com>
2023-07-14 19:44:53 +0800

    feat(code): add new code(0979) distribute-coins-in-binary-tree

---
+++ code/0900/0979/distribute-coins-in-binary-tree.go
@@ -0,0 +1,17 @@
+package _0979
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func distributeCoins(root *TreeNode) int {
+
+}
