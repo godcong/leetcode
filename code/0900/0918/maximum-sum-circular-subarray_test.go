c4e753bc00524736a880741798647b786c9da510
godcong <jumbycc@163.com>
2023-07-20 19:45:19 +0800

    feat(code): add new code(0918) test for:maximum-sum-circular-subarray

---
+++ code/0900/0918/maximum-sum-circular-subarray_test.go
@@ -0,0 +1,27 @@
+package _0918
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxSubarraySumCircular(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubarraySumCircular(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubarraySumCircular() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
