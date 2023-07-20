4961060b319c605f74767bae1bf59b6e4ed8fc58
godcong <jumbycc@163.com>
2023-07-20 19:51:09 +0800

    feat(code): fix code(0001) two-sum

--- code/0000/0001/two-sum_test.go
+++ code/0000/0001/two-sum_test.go
@@ -1,10 +1,9 @@
 package _0001
 
 import (
+	"github.com/godcong/leetcode/common"
 	"reflect"
 	"testing"
-
-	. "github.com/godcong/leetcode/common"
 )
 
 func Test_twoSum(t *testing.T) {
@@ -18,12 +17,38 @@ func Test_twoSum(t *testing.T) {
 		want []int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				nums:   common.StrToIntArray("[2,7,11,15]"),
+				target: 9,
+			},
+			want: common.StrToIntArray("[0,1]"),
+		},
+		{
+			name: "",
+			args: args{
+				nums:   common.StrToIntArray("[3,2,4]"),
+				target: 6,
+			},
+			want: common.StrToIntArray("[1,2]"),
+		},
+		{
+			name: "",
+			args: args{
+				nums:   common.StrToIntArray("[3,3]"),
+				target: 6,
+			},
+			want: common.StrToIntArray("[0,1]"),
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("twoSum() = %v, want %v", got, tt.want)
-			}
-		})
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("twoSum() = %v, want %v", got, tt.want)
+				}
+			},
+		)
 	}
 }
