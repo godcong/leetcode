473adff661b72725368a58a569d15ce9627f02d8
godcong <jumbycc@163.com>
2023-07-21 20:25:49 +0800

    feat(code): add new code(1499) test for:max-value-of-equation

---
+++ code/1400/1499/max-value-of-equation_test.go
@@ -0,0 +1,28 @@
+package _1499
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findMaxValueOfEquation(t *testing.T) {
+	type args struct {
+		points [][]int
+		k      int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaxValueOfEquation(tt.args.points, tt.args.k); got != tt.want {
+				t.Errorf("findMaxValueOfEquation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
