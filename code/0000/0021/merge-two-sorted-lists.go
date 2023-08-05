3fdf8ed65b840bfbdafbd771e86b3083202a198f
godcong <jumbycc@163.com>
2023-08-05 18:45:25 +0800

    feat(code): add new code(0021) merge-two-sorted-lists

---
+++ code/0000/0021/merge-two-sorted-lists.go
@@ -0,0 +1,16 @@
+package _0021
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+}
