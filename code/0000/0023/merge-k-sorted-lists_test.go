f2c96c771cc23b3b9ce8f6f00de0f146291fd9ee
godcong <jumbycc@163.com>
2023-08-12 18:46:00 +0800

    feat(code): add new code(0023) test for:merge-k-sorted-lists

---
+++ code/0000/0023/merge-k-sorted-lists_test.go
@@ -0,0 +1,28 @@
+package _0023
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_mergeKLists(t *testing.T) {
+	type args struct {
+		lists []*ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeKLists(tt.args.lists); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeKLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
