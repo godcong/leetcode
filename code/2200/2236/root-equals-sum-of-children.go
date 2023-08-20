9b71a3421e29c122ba3ed701c50422a7d74a7a85
godcong <jumbycc@163.com>
2023-08-20 19:49:04 +0800

    feat(code): add new code(2236) root-equals-sum-of-children

---
+++ code/2200/2236/root-equals-sum-of-children.go
@@ -0,0 +1,17 @@
+package _2236
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+
+}
