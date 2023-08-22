e50695c6c8765f546baa0b1c138b400144320d41
godcong <jumbycc@163.com>
2023-08-22 19:49:19 +0800

    feat(code): add new code(0849) test for:maximize-distance-to-closest-person

---
+++ code/0800/0849/maximize-distance-to-closest-person_test.go
@@ -0,0 +1,27 @@
+package _0849
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxDistToClosest(t *testing.T) {
+	type args struct {
+		seats []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDistToClosest(tt.args.seats); got != tt.want {
+				t.Errorf("maxDistToClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
