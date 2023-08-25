8e71957cc26f5160d172b90729ca16e88a0a3a5a
godcong <jumbycc@163.com>
2023-08-25 16:20:11 +0800

    feat(code): add new code(1448) test for:count-good-nodes-in-binary-tree

---
+++ code/1400/1448/count-good-nodes-in-binary-tree_test.go
@@ -0,0 +1,27 @@
+package _1448
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_goodNodes(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := goodNodes(tt.args.root); got != tt.want {
+				t.Errorf("goodNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
