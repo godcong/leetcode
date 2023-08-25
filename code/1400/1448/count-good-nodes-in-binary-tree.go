eb1fdbecd1e482ebb18c11b9246ee8eb9bae8f9e
godcong <jumbycc@163.com>
2023-08-25 16:20:08 +0800

    feat(code): add new code(1448) count-good-nodes-in-binary-tree

---
+++ code/1400/1448/count-good-nodes-in-binary-tree.go
@@ -0,0 +1,17 @@
+package _1448
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func goodNodes(root *TreeNode) int {
+
+}
