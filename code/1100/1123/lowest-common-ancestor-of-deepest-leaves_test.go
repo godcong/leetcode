8688dfec33e7e6040a4620c6233558232a93d715
godcong <jumbycc@163.com>
2023-09-06 16:24:15 +0800

    feat(code): add new code(1123) test for:lowest-common-ancestor-of-deepest-leaves

---
+++ code/1100/1123/lowest-common-ancestor-of-deepest-leaves_test.go
@@ -0,0 +1,28 @@
+package _1123
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_lcaDeepestLeaves(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lcaDeepestLeaves(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lcaDeepestLeaves() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
