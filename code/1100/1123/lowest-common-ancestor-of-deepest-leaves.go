3f6b6d370834ac07f911cc7a4287050c621be156
godcong <jumbycc@163.com>
2023-09-06 16:24:14 +0800

    feat(code): add new code(1123) lowest-common-ancestor-of-deepest-leaves

---
+++ code/1100/1123/lowest-common-ancestor-of-deepest-leaves.go
@@ -0,0 +1,17 @@
+package _1123
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lcaDeepestLeaves(root *TreeNode) *TreeNode {
+
+}
