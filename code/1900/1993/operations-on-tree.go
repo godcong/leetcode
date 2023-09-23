b8d191277ba47c3e54e5b37c4803d7e2c59db4c4
godcong <jumbycc@163.com>
2023-09-23 16:25:33 +0800

    feat(code): add new code(1993) operations-on-tree

---
+++ code/1900/1993/operations-on-tree.go
@@ -0,0 +1,38 @@
+package _1993
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type LockingTree struct {
+
+}
+
+
+func Constructor(parent []int) LockingTree {
+
+}
+
+
+func (this *LockingTree) Lock(num int, user int) bool {
+
+}
+
+
+func (this *LockingTree) Unlock(num int, user int) bool {
+
+}
+
+
+func (this *LockingTree) Upgrade(num int, user int) bool {
+
+}
+
+
+/**
+ * Your LockingTree object will be instantiated and called as such:
+ * obj := Constructor(parent);
+ * param_1 := obj.Lock(num,user);
+ * param_2 := obj.Unlock(num,user);
+ * param_3 := obj.Upgrade(num,user);
+ */
