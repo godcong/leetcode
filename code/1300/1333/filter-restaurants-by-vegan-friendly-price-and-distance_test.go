f1993d4d909c9a07f97ed4657d91872302f8df08
godcong <jumbycc@163.com>
2023-09-27 16:25:52 +0800

    feat(code): add new code(1333) test for:filter-restaurants-by-vegan-friendly-price-and-distance

---
+++ code/1300/1333/filter-restaurants-by-vegan-friendly-price-and-distance_test.go
@@ -0,0 +1,31 @@
+package _1333
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_filterRestaurants(t *testing.T) {
+	type args struct {
+		restaurants   [][]int
+		veganFriendly int
+		maxPrice      int
+		maxDistance   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := filterRestaurants(tt.args.restaurants, tt.args.veganFriendly, tt.args.maxPrice, tt.args.maxDistance); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("filterRestaurants() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
