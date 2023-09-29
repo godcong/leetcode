6e87ae3b5a8dbabb87d4fb76935ff1c28b7ca1dc
godcong <jumbycc@163.com>
2023-09-29 16:26:01 +0800

    feat(code): add new code(0605) test for:can-place-flowers

---
+++ code/0600/0605/can-place-flowers_test.go
@@ -0,0 +1,28 @@
+package _0605
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_canPlaceFlowers(t *testing.T) {
+	type args struct {
+		flowerbed []int
+		n         int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canPlaceFlowers(tt.args.flowerbed, tt.args.n); got != tt.want {
+				t.Errorf("canPlaceFlowers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
