c42ab123e93ac91324eb5833d42fecff816e1327
godcong <jumbycc@163.com>
2023-10-03 16:26:18 +0800

    feat(code): add new code(0123) test for:best-time-to-buy-and-sell-stock-iii

---
+++ code/0100/0123/best-time-to-buy-and-sell-stock-iii_test.go
@@ -0,0 +1,27 @@
+package _0123
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
