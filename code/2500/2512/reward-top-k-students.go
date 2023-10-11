6c8286d3ed209e486f8191fc0656004cac203d70
godcong <jumbycc@163.com>
2023-10-11 16:26:52 +0800

    feat(code): add new code(2512) reward-top-k-students

---
+++ code/2500/2512/reward-top-k-students.go
@@ -0,0 +1,9 @@
+package _2512
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+func topStudents(positive_feedback []string, negative_feedback []string, report []string, student_id []int, k int) []int {
+
+}
