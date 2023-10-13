37f2029e9ac927175df80a3274adf9c1c9f963ac
godcong <jumbycc@163.com>
2023-10-13 16:27:01 +0800

    feat(code): add new code(1488) test for:avoid-flood-in-the-city

---
+++ code/1400/1488/avoid-flood-in-the-city_test.go
@@ -0,0 +1,28 @@
+package _1488
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_avoidFlood(t *testing.T) {
+	type args struct {
+		rains []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := avoidFlood(tt.args.rains); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("avoidFlood() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
