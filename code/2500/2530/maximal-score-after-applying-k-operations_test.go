6c9ad75b6243984579e292c1b65a7f595757ac01
godcong <jumbycc@163.com>
2023-10-18 17:25:57 +0800

    feat(code): add new code(2530) test for:maximal-score-after-applying-k-operations

---
+++ code/2500/2530/maximal-score-after-applying-k-operations_test.go
@@ -0,0 +1,28 @@
+package _2530
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxKelements(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxKelements(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("maxKelements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
