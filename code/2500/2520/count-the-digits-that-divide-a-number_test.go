042fc9526ce67eaea265ef0f99234994410b9302
godcong <jumbycc@163.com>
2023-10-26 17:26:44 +0800

    feat(code): add new code(2520) test for:count-the-digits-that-divide-a-number

---
+++ code/2500/2520/count-the-digits-that-divide-a-number_test.go
@@ -0,0 +1,27 @@
+package _2520
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countDigits(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countDigits(tt.args.num); got != tt.want {
+				t.Errorf("countDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
