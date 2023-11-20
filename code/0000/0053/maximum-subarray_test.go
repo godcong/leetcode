9f9bb031d3bb0dc93f93c044870000ee81671f3f
godcong <jumbycc@163.com>
2023-11-20 17:29:14 +0800

    feat(code): add new code(0053) test for:maximum-subarray

---
+++ code/0000/0053/maximum-subarray_test.go
@@ -0,0 +1,27 @@
+package _0053
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
