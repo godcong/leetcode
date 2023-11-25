02fe6ff71865c1d01ee630bdc897427f8a991314
godcong <jumbycc@163.com>
2023-11-25 17:29:49 +0800

    feat(code): add new code(1457) test for:pseudo-palindromic-paths-in-a-binary-tree

---
+++ code/1400/1457/pseudo-palindromic-paths-in-a-binary-tree_test.go
@@ -0,0 +1,27 @@
+package _1457
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_pseudoPalindromicPaths(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pseudoPalindromicPaths(tt.args.root); got != tt.want {
+				t.Errorf("pseudoPalindromicPaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
