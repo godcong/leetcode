942aad953e652051045843166ace45d82a5122d1
godcong <jumbycc@163.com>
2023-11-25 17:29:48 +0800

    feat(code): add new code(1457) pseudo-palindromic-paths-in-a-binary-tree

---
+++ code/1400/1457/pseudo-palindromic-paths-in-a-binary-tree.go
@@ -0,0 +1,17 @@
+package _1457
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pseudoPalindromicPaths (root *TreeNode) int {
+
+}
