e955ed2767e89e25fdec661310fbe3db5f491353
godcong <jumbycc@163.com>
2023-11-30 17:30:22 +0800

    feat(code): add new code(1657) test for:determine-if-two-strings-are-close

---
+++ code/1600/1657/determine-if-two-strings-are-close_test.go
@@ -0,0 +1,28 @@
+package _1657
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_closeStrings(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := closeStrings(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("closeStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
