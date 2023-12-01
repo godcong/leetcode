d1172341e037c358c7566acd2a0fab92f4c61456
godcong <jumbycc@163.com>
2023-12-01 17:30:29 +0800

    feat(code): add new code(2661) test for:first-completely-painted-row-or-column

---
+++ code/2600/2661/first-completely-painted-row-or-column_test.go
@@ -0,0 +1,28 @@
+package _2661
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_firstCompleteIndex(t *testing.T) {
+	type args struct {
+		arr []int
+		mat [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstCompleteIndex(tt.args.arr, tt.args.mat); got != tt.want {
+				t.Errorf("firstCompleteIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
