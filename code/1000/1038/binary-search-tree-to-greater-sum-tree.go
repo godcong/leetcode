42954bfb362711a24acb8e2b2bcde69267f35da5
godcong <jumbycc@163.com>
2023-12-04 17:30:45 +0800

    feat(code): add new code(1038) binary-search-tree-to-greater-sum-tree

---
+++ code/1000/1038/binary-search-tree-to-greater-sum-tree.go
@@ -0,0 +1,17 @@
+package _1038
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstToGst(root *TreeNode) *TreeNode {
+
+}
