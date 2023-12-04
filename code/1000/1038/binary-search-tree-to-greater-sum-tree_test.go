b1a764a8e893f89303a28c668905f9d1182dae25
godcong <jumbycc@163.com>
2023-12-04 17:30:46 +0800

    feat(code): add new code(1038) test for:binary-search-tree-to-greater-sum-tree

---
+++ code/1000/1038/binary-search-tree-to-greater-sum-tree_test.go
@@ -0,0 +1,28 @@
+package _1038
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_bstToGst(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := bstToGst(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("bstToGst() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
