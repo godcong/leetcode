9bf28f95ced6f25dd73a64604fec6dbcae174df3
godcong <jumbycc@163.com>
2023-12-15 17:09:52 +0800

    feat(code): add new code(2415) reverse-odd-levels-of-binary-tree

---
+++ code/2400/2415/reverse-odd-levels-of-binary-tree.go
@@ -0,0 +1,17 @@
+package _2415
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func reverseOddLevels(root *TreeNode) *TreeNode {
+
+}
