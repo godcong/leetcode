448c77dade38103abde8520fde512e2c169c5da3
godcong <jumbycc@163.com>
2023-12-16 17:09:57 +0800

    feat(code): add new code(2276) count-integers-in-intervals

---
+++ code/2200/2276/count-integers-in-intervals.go
@@ -0,0 +1,32 @@
+package _2276
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type CountIntervals struct {
+
+}
+
+
+func Constructor() CountIntervals {
+
+}
+
+
+func (this *CountIntervals) Add(left int, right int)  {
+
+}
+
+
+func (this *CountIntervals) Count() int {
+
+}
+
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(left,right);
+ * param_2 := obj.Count();
+ */
