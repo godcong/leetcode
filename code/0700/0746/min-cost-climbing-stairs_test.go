c17a0fca4ab32953161ff7f396f017139c39ee86
godcong <jumbycc@163.com>
2023-12-17 17:10:03 +0800

    feat(code): add new code(0746) test for:min-cost-climbing-stairs

---
+++ code/0700/0746/min-cost-climbing-stairs_test.go
@@ -0,0 +1,27 @@
+package _0746
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minCostClimbingStairs(t *testing.T) {
+	type args struct {
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCostClimbingStairs(tt.args.cost); got != tt.want {
+				t.Errorf("minCostClimbingStairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
