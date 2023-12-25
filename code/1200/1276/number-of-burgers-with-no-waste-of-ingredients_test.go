d16da16a4e808eb7e665e897d8634c1bf120aacd
godcong <jumbycc@163.com>
2023-12-25 17:10:42 +0800

    feat(code): add new code(1276) test for:number-of-burgers-with-no-waste-of-ingredients

---
+++ code/1200/1276/number-of-burgers-with-no-waste-of-ingredients_test.go
@@ -0,0 +1,29 @@
+package _1276
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numOfBurgers(t *testing.T) {
+	type args struct {
+		tomatoSlices int
+		cheeseSlices int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numOfBurgers(tt.args.tomatoSlices, tt.args.cheeseSlices); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("numOfBurgers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
