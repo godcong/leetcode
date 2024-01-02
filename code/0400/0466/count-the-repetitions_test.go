748fc354f556c48ad88efe7ea687f44c74521314
godcong <jumbycc@163.com>
2024-01-02 17:11:21 +0800

    feat(code): add new code(0466) test for:count-the-repetitions

---
+++ code/0400/0466/count-the-repetitions_test.go
@@ -0,0 +1,30 @@
+package _0466
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_getMaxRepetitions(t *testing.T) {
+	type args struct {
+		s1 string
+		n1 int
+		s2 string
+		n2 int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMaxRepetitions(tt.args.s1, tt.args.n1, tt.args.s2, tt.args.n2); got != tt.want {
+				t.Errorf("getMaxRepetitions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
