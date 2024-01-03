04fbf13211234499e8cbaa16270d8afc1e401cae
godcong <jumbycc@163.com>
2024-01-03 17:11:26 +0800

    feat(code): add new code(2487) remove-nodes-from-linked-list

---
+++ code/2400/2487/remove-nodes-from-linked-list.go
@@ -0,0 +1,16 @@
+package _2487
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNodes(head *ListNode) *ListNode {
+
+}
