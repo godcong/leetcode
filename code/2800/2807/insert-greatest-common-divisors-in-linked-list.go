22a0e955b12316e3e53ebd2baddb6ea7a1248936
godcong <jumbycc@163.com>
2024-01-06 17:11:43 +0800

    feat(code): add new code(2807) insert-greatest-common-divisors-in-linked-list

---
+++ code/2800/2807/insert-greatest-common-divisors-in-linked-list.go
@@ -0,0 +1,16 @@
+package _2807
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func insertGreatestCommonDivisors(head *ListNode) *ListNode {
+
+}
