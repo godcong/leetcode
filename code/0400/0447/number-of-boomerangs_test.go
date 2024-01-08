f498c137a6347d41c110e5695e76a5a794090704
godcong <jumbycc@163.com>
2024-01-08 17:11:53 +0800

    feat(code): add new code(0447) test for:number-of-boomerangs

---
+++ code/0400/0447/number-of-boomerangs_test.go
@@ -0,0 +1,27 @@
+package _0447
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numberOfBoomerangs(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfBoomerangs(tt.args.points); got != tt.want {
+				t.Errorf("numberOfBoomerangs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
