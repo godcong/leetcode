a2d886a57112403638625d4244805001efc89d43
godcong <jumbycc@163.com>
2024-01-14 17:12:25 +0800

    feat(code): add new code(0083) test for:remove-duplicates-from-sorted-list

---
+++ code/0000/0083/remove-duplicates-from-sorted-list_test.go
@@ -0,0 +1,28 @@
+package _0083
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_deleteDuplicates(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
