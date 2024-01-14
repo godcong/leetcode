cfdf4a1aa514be8f370239ef036f75cd17f72c3e
godcong <jumbycc@163.com>
2024-01-14 17:12:25 +0800

    feat(code): add new code(0083) remove-duplicates-from-sorted-list

---
+++ code/0000/0083/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,16 @@
+package _0083
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+
+}
