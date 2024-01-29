6bc4fe1df1b0761f65c5ad6a320abf2cd6dc00ae
godcong <jumbycc@163.com>
2024-01-29 19:32:01 +0800

    feat(code): add new code(0514) test for:freedom-trail

---
+++ code/0500/0514/freedom-trail_test.go
@@ -0,0 +1,28 @@
+package _0514
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findRotateSteps(t *testing.T) {
+	type args struct {
+		ring string
+		key  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRotateSteps(tt.args.ring, tt.args.key); got != tt.want {
+				t.Errorf("findRotateSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
