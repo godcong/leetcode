b1f0fd12e5f2de8d9e35ce06d774b45159242193
godcong <jumbycc@163.com>
2024-02-08 19:32:57 +0800

    feat(code): add new code(0993) cousins-in-binary-tree

---
+++ code/0900/0993/cousins-in-binary-tree.go
@@ -0,0 +1,17 @@
+package _0993
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isCousins(root *TreeNode, x int, y int) bool {
+
+}
