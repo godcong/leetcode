e71d54483e39cd42685021ba2c9585054d76616a
godcong <jumbycc@163.com>
2024-02-08 19:32:58 +0800

    feat(code): add new code(0993) test for:cousins-in-binary-tree

---
+++ code/0900/0993/cousins-in-binary-tree_test.go
@@ -0,0 +1,29 @@
+package _0993
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isCousins(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		x    int
+		y    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isCousins(tt.args.root, tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("isCousins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
