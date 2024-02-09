d5dcb1f4a8716bc3749ae1b836e219107848dc1a
godcong <jumbycc@163.com>
2024-02-09 19:33:03 +0800

    feat(code): add new code(0236) lowest-common-ancestor-of-a-binary-tree

---
+++ code/0200/0236/lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,17 @@
+package _0236
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+  
+}
