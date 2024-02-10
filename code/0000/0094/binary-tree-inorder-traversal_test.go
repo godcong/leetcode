2b27f3ecf5830ebc617d5ab746f4c5af4495f7a4
godcong <jumbycc@163.com>
2024-02-10 19:33:09 +0800

    feat(code): add new code(0094) test for:binary-tree-inorder-traversal

---
+++ code/0000/0094/binary-tree-inorder-traversal_test.go
@@ -0,0 +1,28 @@
+package _0094
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_inorderTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
