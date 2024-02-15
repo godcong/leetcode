77d0f6072526c418101b821e3621fdecb340aedb
godcong <jumbycc@163.com>
2024-02-15 19:33:38 +0800

    feat(code): add new code(0107) test for:binary-tree-level-order-traversal-ii

---
+++ code/0100/0107/binary-tree-level-order-traversal-ii_test.go
@@ -0,0 +1,28 @@
+package _0107
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_levelOrderBottom(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := levelOrderBottom(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("levelOrderBottom() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
