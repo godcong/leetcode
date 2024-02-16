ed42e35240b01b4db07e97c0174febc56f0ed4d2
godcong <jumbycc@163.com>
2024-02-16 19:33:44 +0800

    feat(code): add new code(0103) test for:binary-tree-zigzag-level-order-traversal

---
+++ code/0100/0103/binary-tree-zigzag-level-order-traversal_test.go
@@ -0,0 +1,28 @@
+package _0103
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_zigzagLevelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := zigzagLevelOrder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("zigzagLevelOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
