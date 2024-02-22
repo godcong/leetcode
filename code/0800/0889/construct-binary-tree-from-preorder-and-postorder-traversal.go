021033305b00879a0673b50ad4dec42539587abe
godcong <jumbycc@163.com>
2024-02-22 15:30:49 +0800

    feat(code): add new code(0889) construct-binary-tree-from-preorder-and-postorder-traversal

---
+++ code/0800/0889/construct-binary-tree-from-preorder-and-postorder-traversal.go
@@ -0,0 +1,17 @@
+package _0889
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
+
+}
