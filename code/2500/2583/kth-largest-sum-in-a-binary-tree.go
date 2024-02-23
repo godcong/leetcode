6c522698a54eb4854d434da6271e3c8cfe15fff2
godcong <jumbycc@163.com>
2024-02-23 15:30:55 +0800

    feat(code): add new code(2583) kth-largest-sum-in-a-binary-tree

---
+++ code/2500/2583/kth-largest-sum-in-a-binary-tree.go
@@ -0,0 +1,17 @@
+package _2583
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthLargestLevelSum(root *TreeNode, k int) int64 {
+
+}
