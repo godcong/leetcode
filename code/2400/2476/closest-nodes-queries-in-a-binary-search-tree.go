2c726a7e071b91ee5b156612cc5be4e3416abe4b
godcong <jumbycc@163.com>
2024-02-24 15:31:02 +0800

    feat(code): add new code(2476) closest-nodes-queries-in-a-binary-search-tree

---
+++ code/2400/2476/closest-nodes-queries-in-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package _2476
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func closestNodes(root *TreeNode, queries []int) [][]int {
+
+}
