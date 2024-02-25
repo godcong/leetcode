8e8dc4d88c82c944c3f36d4b5c58386d238b5d4f
godcong <jumbycc@163.com>
2024-02-25 15:31:08 +0800

    feat(code): add new code(0235) test for:lowest-common-ancestor-of-a-binary-search-tree

---
+++ code/0200/0235/lowest-common-ancestor-of-a-binary-search-tree_test.go
@@ -0,0 +1,30 @@
+package _0235
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_lowestCommonAncestor(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		p    *TreeNode
+		q    *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lowestCommonAncestor(tt.args.root, tt.args.p, tt.args.q); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lowestCommonAncestor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
