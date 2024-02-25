b2c6261466fee67734fd832654fd3a7488da9fdf
godcong <jumbycc@163.com>
2024-02-25 15:31:08 +0800

    feat(code): add new code(0235) lowest-common-ancestor-of-a-binary-search-tree

---
+++ code/0200/0235/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,18 @@
+package _0235
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	
+}
