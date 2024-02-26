9a3a6b74160e193aea2ee7bff489874d2971e361
godcong <jumbycc@163.com>
2024-02-26 15:31:14 +0800

    feat(code): add new code(0938) range-sum-of-bst

---
+++ code/0900/0938/range-sum-of-bst.go
@@ -0,0 +1,17 @@
+package _0938
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+
+}
