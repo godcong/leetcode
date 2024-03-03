8e85a3980fe0c4f4624924c4810906bac2e1f134
godcong <jumbycc@163.com>
2024-03-03 15:31:54 +0800

    feat(code): add new code(0225) implement-stack-using-queues

---
+++ code/0200/0225/implement-stack-using-queues.go
@@ -0,0 +1,44 @@
+package _0225
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type MyStack struct {
+
+}
+
+
+func Constructor() MyStack {
+
+}
+
+
+func (this *MyStack) Push(x int)  {
+
+}
+
+
+func (this *MyStack) Pop() int {
+
+}
+
+
+func (this *MyStack) Top() int {
+
+}
+
+
+func (this *MyStack) Empty() bool {
+
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
