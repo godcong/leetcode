bef57844a5264e01eb21beff7032f05efe9bdce0
godcong <jumbycc@163.com>
2024-03-12 15:32:50 +0800

    feat(code): add new code(1261) find-elements-in-a-contaminated-binary-tree

---
+++ code/1200/1261/find-elements-in-a-contaminated-binary-tree.go
@@ -0,0 +1,34 @@
+package _1261
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type FindElements struct {
+
+}
+
+
+func Constructor(root *TreeNode) FindElements {
+
+}
+
+
+func (this *FindElements) Find(target int) bool {
+
+}
+
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Find(target);
+ */
