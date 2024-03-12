5655ecdcb8f342dad30d9e49c34a1a63cc2d5678
godcong <jumbycc@163.com>
2024-03-12 15:32:51 +0800

    feat(code): add new code(1261) test for:find-elements-in-a-contaminated-binary-tree

---
+++ code/1200/1261/find-elements-in-a-contaminated-binary-tree_test.go
@@ -0,0 +1,49 @@
+package _1261
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func TestConstructor(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want FindElements
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFindElements_Find(t *testing.T) {
+	type args struct {
+		target int
+	}
+	tests := []struct {
+		name string
+		this *FindElements
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.this.Find(tt.args.target); got != tt.want {
+				t.Errorf("FindElements.Find() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
