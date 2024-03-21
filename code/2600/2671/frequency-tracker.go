83c115a486e9e944ced1f03d22370f3bc7b770a4
godcong <jumbycc@163.com>
2024-03-21 15:33:42 +0800

    feat(code): add new code(2671) frequency-tracker

---
+++ code/2600/2671/frequency-tracker.go
@@ -0,0 +1,38 @@
+package _2671
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type FrequencyTracker struct {
+
+}
+
+
+func Constructor() FrequencyTracker {
+
+}
+
+
+func (this *FrequencyTracker) Add(number int)  {
+
+}
+
+
+func (this *FrequencyTracker) DeleteOne(number int)  {
+
+}
+
+
+func (this *FrequencyTracker) HasFrequency(frequency int) bool {
+
+}
+
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(number);
+ * obj.DeleteOne(number);
+ * param_3 := obj.HasFrequency(frequency);
+ */
