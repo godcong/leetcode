f0c32c16fec3799e0406bead47814fb2c9a34371
godcong <jumbycc@163.com>
2024-03-24 15:34:10 +0800

    feat(code): add new code(0322) test for:coin-change

---
+++ code/0300/0322/coin-change_test.go
@@ -0,0 +1,28 @@
+package _0322
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
