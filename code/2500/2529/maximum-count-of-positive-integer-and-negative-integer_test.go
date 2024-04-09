f3deb7aa640f69d753ea85e562cd40a521f9a094
godcong <jumbycc@163.com>
2024-04-09 15:49:11 +0800

    feat(code): add new code(2529) test for:maximum-count-of-positive-integer-and-negative-integer

---
+++ code/2500/2529/maximum-count-of-positive-integer-and-negative-integer_test.go
@@ -0,0 +1,27 @@
+package _2529
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maximumCount(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumCount(tt.args.nums); got != tt.want {
+				t.Errorf("maximumCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
