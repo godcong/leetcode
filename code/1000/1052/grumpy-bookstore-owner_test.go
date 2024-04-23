d9cd2a5ae2d798236f8079f1374e1d21198a3a05
godcong <jumbycc@163.com>
2024-04-23 15:50:33 +0800

    feat(code): add new code(1052) test for:grumpy-bookstore-owner

---
+++ code/1000/1052/grumpy-bookstore-owner_test.go
@@ -0,0 +1,29 @@
+package _1052
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxSatisfied(t *testing.T) {
+	type args struct {
+		customers []int
+		grumpy    []int
+		minutes   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSatisfied(tt.args.customers, tt.args.grumpy, tt.args.minutes); got != tt.want {
+				t.Errorf("maxSatisfied() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
