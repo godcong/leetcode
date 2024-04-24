8cc7d77944466bbce3048e1c7dd6d9020c6212fc
godcong <jumbycc@163.com>
2024-04-24 15:50:38 +0800

    feat(code): add new code(2385) amount-of-time-for-binary-tree-to-be-infected

---
+++ code/2300/2385/amount-of-time-for-binary-tree-to-be-infected.go
@@ -0,0 +1,17 @@
+package _2385
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func amountOfTime(root *TreeNode, start int) int {
+
+}
