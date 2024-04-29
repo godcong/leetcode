8416a30320dd5a9ffe9e5ffcd407415551b555f3
godcong <jumbycc@163.com>
2024-04-29 15:51:05 +0800

    feat(code): add new code(1329) test for:sort-the-matrix-diagonally

---
+++ code/1300/1329/sort-the-matrix-diagonally_test.go
@@ -0,0 +1,28 @@
+package _1329
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_diagonalSort(t *testing.T) {
+	type args struct {
+		mat [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := diagonalSort(tt.args.mat); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("diagonalSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
