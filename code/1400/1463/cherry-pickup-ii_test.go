62114279edbc653695bab5600635d23c47a0cac9
godcong <jumbycc@163.com>
2024-05-07 15:51:45 +0800

    feat(code): add new code(1463) test for:cherry-pickup-ii

---
+++ code/1400/1463/cherry-pickup-ii_test.go
@@ -0,0 +1,27 @@
+package _1463
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_cherryPickup(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cherryPickup(tt.args.grid); got != tt.want {
+				t.Errorf("cherryPickup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
