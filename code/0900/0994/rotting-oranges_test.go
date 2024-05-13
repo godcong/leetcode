c806f5ca60125ffdaec095b84f09a0f6ae35ea12
godcong <jumbycc@163.com>
2024-05-13 15:52:21 +0800

    feat(code): add new code(0994) test for:rotting-oranges

---
+++ code/0900/0994/rotting-oranges_test.go
@@ -0,0 +1,27 @@
+package _0994
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_orangesRotting(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := orangesRotting(tt.args.grid); got != tt.want {
+				t.Errorf("orangesRotting() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
