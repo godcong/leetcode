833278e4331a3b7e458bce9c1883b862df264b3b
godcong <jumbycc@163.com>
2024-05-17 15:52:44 +0800

    feat(code): add new code(0826) test for:most-profit-assigning-work

---
+++ code/0800/0826/most-profit-assigning-work_test.go
@@ -0,0 +1,29 @@
+package _0826
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxProfitAssignment(t *testing.T) {
+	type args struct {
+		difficulty []int
+		profit     []int
+		worker     []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfitAssignment(tt.args.difficulty, tt.args.profit, tt.args.worker); got != tt.want {
+				t.Errorf("maxProfitAssignment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
