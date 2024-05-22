24fe795b2cab0d70103e1f5d8f1418f19bdde13e
godcong <jumbycc@163.com>
2024-05-22 15:53:14 +0800

    feat(code): add new code(2225) test for:find-players-with-zero-or-one-losses

---
+++ code/2200/2225/find-players-with-zero-or-one-losses_test.go
@@ -0,0 +1,28 @@
+package _2225
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findWinners(t *testing.T) {
+	type args struct {
+		matches [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findWinners(tt.args.matches); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findWinners() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
