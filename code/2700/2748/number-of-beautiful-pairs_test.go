e6c2595a22ab0db85708a1ae2cad548c5f1bcdde
godcong <jumbycc@163.com>
2024-06-20 16:57:54 +0800

    feat(code): add new code(2748) test for:number-of-beautiful-pairs

---
+++ code/2700/2748/number-of-beautiful-pairs_test.go
@@ -0,0 +1,27 @@
+package _2748
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countBeautifulPairs(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countBeautifulPairs(tt.args.nums); got != tt.want {
+				t.Errorf("countBeautifulPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
