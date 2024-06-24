4af763a9920ec724714499e66f8360bd8e2e0d18
godcong <jumbycc@163.com>
2024-06-24 17:00:08 +0800

    feat(code): add new code(0503) test for:next-greater-element-ii

---
+++ code/0500/0503/next-greater-element-ii_test.go
@@ -0,0 +1,28 @@
+package _0503
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_nextGreaterElements(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextGreaterElements(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("nextGreaterElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
