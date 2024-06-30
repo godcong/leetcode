d729d0109c130f80f64d9b0a33704debbc6b0233
godcong <jumbycc@163.com>
2024-06-30 17:01:03 +0800

    feat(code): add new code(0494) test for:target-sum

---
+++ code/0400/0494/target-sum_test.go
@@ -0,0 +1,28 @@
+package _0494
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findTargetSumWays(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTargetSumWays(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("findTargetSumWays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
