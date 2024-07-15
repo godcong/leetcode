f07bdfe2e1f134f284acd08c8ad63f482dd9bde9
godcong <jumbycc@163.com>
2024-07-15 17:03:28 +0800

    feat(code): add new code(0721) test for:accounts-merge

---
+++ code/0700/0721/accounts-merge_test.go
@@ -0,0 +1,28 @@
+package _0721
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_accountsMerge(t *testing.T) {
+	type args struct {
+		accounts [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := accountsMerge(tt.args.accounts); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("accountsMerge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
