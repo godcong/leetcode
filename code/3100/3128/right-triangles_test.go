b057dc7b8fa71c0e207196d4962ef4902737bccc
godcong <jumbycc@163.com>
2024-08-02 15:13:55 +0800

    feat(code): add new code(3128) test for:right-triangles

---
+++ code/3100/3128/right-triangles_test.go
@@ -0,0 +1,27 @@
+package _3128
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numberOfRightTriangles(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfRightTriangles(tt.args.grid); got != tt.want {
+				t.Errorf("numberOfRightTriangles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
