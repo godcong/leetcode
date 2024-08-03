d2505767e72e8080052f1b1163e75b143256afb2
godcong <jumbycc@163.com>
2024-08-03 15:14:02 +0800

    feat(code): add new code(3143) test for:maximum-points-inside-the-square

---
+++ code/3100/3143/maximum-points-inside-the-square_test.go
@@ -0,0 +1,28 @@
+package _3143
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxPointsInsideSquare(t *testing.T) {
+	type args struct {
+		points [][]int
+		s      string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxPointsInsideSquare(tt.args.points, tt.args.s); got != tt.want {
+				t.Errorf("maxPointsInsideSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
