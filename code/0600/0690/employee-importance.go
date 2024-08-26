080401680d36170d4d612628ffbcbdaa252bd7f5
godcong <jumbycc@163.com>
2024-08-26 15:16:51 +0800

    feat(code): add new code(0690) employee-importance

---
+++ code/0600/0690/employee-importance.go
@@ -0,0 +1,18 @@
+package _0690
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for Employee.
+ * type Employee struct {
+ *     Id int
+ *     Importance int
+ *     Subordinates []int
+ * }
+ */
+
+func getImportance(employees []*Employee, id int) int {
+    
+}
