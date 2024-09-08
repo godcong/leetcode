241144bf4758949449ac01864a79fbd3f986b568
godcong <jumbycc@163.com>
2024-09-08 15:18:29 +0800

    feat(code): add new code(0977) test for:squares-of-a-sorted-array

---
+++ code/0900/0977/squares-of-a-sorted-array_test.go
@@ -0,0 +1,28 @@
+package _0977
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_sortedSquares(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedSquares(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedSquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
