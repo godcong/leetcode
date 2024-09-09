1dbe049767cde09d504117acc81bdc367803d919
godcong <jumbycc@163.com>
2024-09-09 15:18:36 +0800

    feat(code): add new code(2181) merge-nodes-in-between-zeros

---
+++ code/2100/2181/merge-nodes-in-between-zeros.go
@@ -0,0 +1,16 @@
+package _2181
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeNodes(head *ListNode) *ListNode {
+
+}
