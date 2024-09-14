b0ef917d12514a70c6e9a3c6d050f66d71ffa846
godcong <jumbycc@163.com>
2024-09-14 15:19:24 +0800

    feat(code): add new code(2390) test for:removing-stars-from-a-string

---
+++ code/2300/2390/removing-stars-from-a-string_test.go
@@ -0,0 +1,27 @@
+package _2390
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_removeStars(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeStars(tt.args.s); got != tt.want {
+				t.Errorf("removeStars() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
