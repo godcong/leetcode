0e80559709fd7db85bb4b6b65af99bff66706334
godcong <jumbycc@163.com>
2024-09-18 15:19:57 +0800

    feat(code): add new code(2332) test for:the-latest-time-to-catch-a-bus

---
+++ code/2300/2332/the-latest-time-to-catch-a-bus_test.go
@@ -0,0 +1,29 @@
+package _2332
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_latestTimeCatchTheBus(t *testing.T) {
+	type args struct {
+		buses      []int
+		passengers []int
+		capacity   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := latestTimeCatchTheBus(tt.args.buses, tt.args.passengers, tt.args.capacity); got != tt.want {
+				t.Errorf("latestTimeCatchTheBus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
