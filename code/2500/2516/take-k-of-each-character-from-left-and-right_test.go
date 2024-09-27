a6e17d2f63afd2f29c7ad42ca1ab7a126e13995f
godcong <jumbycc@163.com>
2024-09-27 16:30:46 +0800

    feat(code): add new code(2516) test for:take-k-of-each-character-from-left-and-right

---
+++ code/2500/2516/take-k-of-each-character-from-left-and-right_test.go
@@ -0,0 +1,28 @@
+package _2516
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_takeCharacters(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := takeCharacters(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("takeCharacters() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
