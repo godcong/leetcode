c7e0a5e5685849430d546598334afd652e7843ce
godcong <jumbycc@163.com>
2024-10-02 16:31:20 +0800

    feat(code): add new code(1870) test for:minimum-speed-to-arrive-on-time

---
+++ code/1800/1870/minimum-speed-to-arrive-on-time_test.go
@@ -0,0 +1,28 @@
+package _1870
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minSpeedOnTime(t *testing.T) {
+	type args struct {
+		dist []int
+		hour float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSpeedOnTime(tt.args.dist, tt.args.hour); got != tt.want {
+				t.Errorf("minSpeedOnTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
