18af27ff7a12838a0f7ff8c76198f70ce6e90b5c
godcong <jumbycc@163.com>
2024-10-04 16:31:33 +0800

    feat(code): add new code(1227) test for:airplane-seat-assignment-probability

---
+++ code/1200/1227/airplane-seat-assignment-probability_test.go
@@ -0,0 +1,27 @@
+package _1227
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_nthPersonGetsNthSeat(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nthPersonGetsNthSeat(tt.args.n); got != tt.want {
+				t.Errorf("nthPersonGetsNthSeat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
