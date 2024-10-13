f2ec910a9286d918a0eec77599fd83abff69bd03
godcong <jumbycc@163.com>
2024-10-13 16:32:38 +0800

    feat(code): add new code(1884) test for:egg-drop-with-2-eggs-and-n-floors

---
+++ code/1800/1884/egg-drop-with-2-eggs-and-n-floors_test.go
@@ -0,0 +1,27 @@
+package _1884
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_twoEggDrop(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoEggDrop(tt.args.n); got != tt.want {
+				t.Errorf("twoEggDrop() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
