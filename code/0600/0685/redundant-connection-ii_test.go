100f992a76e7c1d381405cbe7dbc9bcaf743c23c
godcong <jumbycc@163.com>
2024-10-28 16:34:33 +0800

    feat(code): add new code(0685) test for:redundant-connection-ii

---
+++ code/0600/0685/redundant-connection-ii_test.go
@@ -0,0 +1,28 @@
+package _0685
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findRedundantDirectedConnection(t *testing.T) {
+	type args struct {
+		edges [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRedundantDirectedConnection(tt.args.edges); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findRedundantDirectedConnection() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
