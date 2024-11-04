78cdc82916e0a5c670199ff2ce16a2dd146b2bf7
godcong <jumbycc@163.com>
2024-11-04 16:35:28 +0800

    feat(code): add new code(0633) test for:sum-of-square-numbers

---
+++ code/0600/0633/sum-of-square-numbers_test.go
@@ -0,0 +1,27 @@
+package _0633
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_judgeSquareSum(t *testing.T) {
+	type args struct {
+		c int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := judgeSquareSum(tt.args.c); got != tt.want {
+				t.Errorf("judgeSquareSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
