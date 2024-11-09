b9b4062d2efe1ee5c22685cced0bfa168daaf3b1
godcong <jumbycc@163.com>
2024-11-09 16:36:08 +0800

    feat(code): add new code(3242) design-neighbor-sum-service

---
+++ code/3200/3242/design-neighbor-sum-service.go
@@ -0,0 +1,32 @@
+package _3242
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type NeighborSum struct {
+    
+}
+
+
+func Constructor(grid [][]int) NeighborSum {
+    
+}
+
+
+func (this *NeighborSum) AdjacentSum(value int) int {
+    
+}
+
+
+func (this *NeighborSum) DiagonalSum(value int) int {
+    
+}
+
+
+/**
+ * Your NeighborSum object will be instantiated and called as such:
+ * obj := Constructor(grid);
+ * param_1 := obj.AdjacentSum(value);
+ * param_2 := obj.DiagonalSum(value);
+ */
