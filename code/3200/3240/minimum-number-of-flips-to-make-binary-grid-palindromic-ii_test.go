e877ba8ede4f515c5cc756c1147948d30ab41da7
godcong <jumbycc@163.com>
2024-11-16 18:18:52 +0800

    feat(code): add new code(3240) test for:minimum-number-of-flips-to-make-binary-grid-palindromic-ii

---
+++ code/3200/3240/minimum-number-of-flips-to-make-binary-grid-palindromic-ii_test.go
@@ -0,0 +1,27 @@
+package _3240
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minFlips(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minFlips(tt.args.grid); got != tt.want {
+				t.Errorf("minFlips() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
