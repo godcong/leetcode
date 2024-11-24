260a13ff4b0ef432a3f27da84a9fc429486eb1b9
godcong <jumbycc@163.com>
2024-11-24 18:19:40 +0800

    feat(code): add new code(0632) test for:smallest-range-covering-elements-from-k-lists

---
+++ code/0600/0632/smallest-range-covering-elements-from-k-lists_test.go
@@ -0,0 +1,28 @@
+package _0632
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_smallestRange(t *testing.T) {
+	type args struct {
+		nums [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := smallestRange(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("smallestRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
