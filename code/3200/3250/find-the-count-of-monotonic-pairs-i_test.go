25ffa1a227f85907d7453688a85013dd683a33c6
godcong <jumbycc@163.com>
2024-11-28 18:20:06 +0800

    feat(code): add new code(3250) test for:find-the-count-of-monotonic-pairs-i

---
+++ code/3200/3250/find-the-count-of-monotonic-pairs-i_test.go
@@ -0,0 +1,27 @@
+package _3250
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countOfPairs(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countOfPairs(tt.args.nums); got != tt.want {
+				t.Errorf("countOfPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
