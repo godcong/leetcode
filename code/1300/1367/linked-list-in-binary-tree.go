2b852e16f507454303911e02ef7db5408f579e57
godcong <jumbycc@163.com>
2024-12-30 18:23:37 +0800

    feat(code): add new code(1367) linked-list-in-binary-tree

---
+++ code/1300/1367/linked-list-in-binary-tree.go
@@ -0,0 +1,24 @@
+package _1367
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubPath(head *ListNode, root *TreeNode) bool {
+    
+}
