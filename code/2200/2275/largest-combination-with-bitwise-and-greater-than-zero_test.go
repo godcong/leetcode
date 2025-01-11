a86ef2ccd95d6cb8849764e8c79fe704f4ac5209
godcong <jumbycc@163.com>
2025-01-12 01:04:32 +0800

    feat(code): add new code(2275) test for:largest-combination-with-bitwise-and-greater-than-zero

---
+++ code/2200/2275/largest-combination-with-bitwise-and-greater-than-zero_test.go
@@ -0,0 +1,27 @@
+package _2275
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_largestCombination(t *testing.T) {
+	type args struct {
+		candidates []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestCombination(tt.args.candidates); got != tt.want {
+				t.Errorf("largestCombination() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
