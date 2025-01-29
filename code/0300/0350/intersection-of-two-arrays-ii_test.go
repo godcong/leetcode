7d69058a8e40b9587bcbfb6c0a5699b24ffa830a
godcong <jumbycc@163.com>
2025-01-30 01:05:48 +0800

    feat(code): add new code(0350) test for:intersection-of-two-arrays-ii

---
+++ code/0300/0350/intersection-of-two-arrays-ii_test.go
@@ -0,0 +1,29 @@
+package _0350
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_intersect(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersect(tt.args.nums1, tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
