05cd16d8a7e858b4e5b7f6fd60cab9b172f6ca71
godcong <jumbycc@163.com>
2025-02-09 17:23:56 +0800

    feat(code): add new code(0080) test for:remove-duplicates-from-sorted-array-ii

---
+++ code/0000/0080/remove-duplicates-from-sorted-array-ii_test.go
@@ -0,0 +1,27 @@
+package _0080
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
