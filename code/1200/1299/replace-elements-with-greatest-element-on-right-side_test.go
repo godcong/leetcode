0b53768daaa49152cf94c01e81b3d389fd0c9bb3
godcong <jumbycc@163.com>
2025-02-16 17:24:32 +0800

    feat(code): add new code(1299) test for:replace-elements-with-greatest-element-on-right-side

---
+++ code/1200/1299/replace-elements-with-greatest-element-on-right-side_test.go
@@ -0,0 +1,28 @@
+package _1299
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_replaceElements(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := replaceElements(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("replaceElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
