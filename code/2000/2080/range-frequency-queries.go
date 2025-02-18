a507d1c4436d09e98c27741b8d9ce885a4b72ce5
godcong <jumbycc@163.com>
2025-02-18 17:24:43 +0800

    feat(code): add new code(2080) range-frequency-queries

---
+++ code/2000/2080/range-frequency-queries.go
@@ -0,0 +1,26 @@
+package _2080
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type RangeFreqQuery struct {
+    
+}
+
+
+func Constructor(arr []int) RangeFreqQuery {
+    
+}
+
+
+func (this *RangeFreqQuery) Query(left int, right int, value int) int {
+    
+}
+
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * obj := Constructor(arr);
+ * param_1 := obj.Query(left,right,value);
+ */
