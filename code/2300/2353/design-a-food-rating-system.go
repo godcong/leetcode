d2a8c6a400700f9253218de2c91ceaa385688d33
godcong <jumbycc@163.com>
2025-02-28 17:25:39 +0800

    feat(code): add new code(2353) design-a-food-rating-system

---
+++ code/2300/2353/design-a-food-rating-system.go
@@ -0,0 +1,32 @@
+package _2353
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type FoodRatings struct {
+    
+}
+
+
+func Constructor(foods []string, cuisines []string, ratings []int) FoodRatings {
+    
+}
+
+
+func (this *FoodRatings) ChangeRating(food string, newRating int)  {
+    
+}
+
+
+func (this *FoodRatings) HighestRated(cuisine string) string {
+    
+}
+
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * obj := Constructor(foods, cuisines, ratings);
+ * obj.ChangeRating(food,newRating);
+ * param_2 := obj.HighestRated(cuisine);
+ */
