6aef41d0ee91ac37434b61a15c93093fd7ccc6cc
godcong <jumbycc@163.com>
2025-03-03 17:25:58 +0800

    feat(code): add new code(1278) test for:palindrome-partitioning-iii

---
+++ code/1200/1278/palindrome-partitioning-iii_test.go
@@ -0,0 +1,28 @@
+package _1278
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_palindromePartition(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := palindromePartition(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("palindromePartition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
