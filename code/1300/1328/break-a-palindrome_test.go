aa858e829eeeb09321cfda1d69cba03b374ffc9f
godcong <jumbycc@163.com>
2025-03-05 17:26:09 +0800

    feat(code): add new code(1328) test for:break-a-palindrome

---
+++ code/1300/1328/break-a-palindrome_test.go
@@ -0,0 +1,27 @@
+package _1328
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_breakPalindrome(t *testing.T) {
+	type args struct {
+		palindrome string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := breakPalindrome(tt.args.palindrome); got != tt.want {
+				t.Errorf("breakPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
