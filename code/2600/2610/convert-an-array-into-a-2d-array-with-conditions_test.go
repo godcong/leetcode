d10ce0cb67856c1e56eb63ca0f75af9206181eaa
godcong <jumbycc@163.com>
2025-03-19 16:46:41 +0800

    feat(code): add new code(2610) test for:convert-an-array-into-a-2d-array-with-conditions

---
+++ code/2600/2610/convert-an-array-into-a-2d-array-with-conditions_test.go
@@ -0,0 +1,28 @@
+package _2610
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findMatrix(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMatrix(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
