15a65686ba14c8be272945bb66c4d56c7a7e3bb9
godcong <jumbycc@163.com>
2025-03-25 16:49:52 +0800

    feat(code): add new code(2711) test for:difference-of-number-of-distinct-values-on-diagonals

---
+++ code/2700/2711/difference-of-number-of-distinct-values-on-diagonals_test.go
@@ -0,0 +1,28 @@
+package _2711
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_differenceOfDistinctValues(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := differenceOfDistinctValues(tt.args.grid); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("differenceOfDistinctValues() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
