948382d0d585d5db93ea319f3ff46ed2d91617fe
godcong <jumbycc@163.com>
2025-04-07 16:55:48 +0800

    feat(code): add new code(0416) test for:partition-equal-subset-sum

---
+++ code/0400/0416/partition-equal-subset-sum_test.go
@@ -0,0 +1,27 @@
+package _0416
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_canPartition(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canPartition(tt.args.nums); got != tt.want {
+				t.Errorf("canPartition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
