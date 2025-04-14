711f62286939796383ebb8238ded077556a94d64
godcong <jumbycc@163.com>
2025-04-14 16:59:41 +0800

    feat(code): add new code(1534) test for:count-good-triplets

---
+++ code/1500/1534/count-good-triplets_test.go
@@ -0,0 +1,30 @@
+package _1534
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countGoodTriplets(t *testing.T) {
+	type args struct {
+		arr []int
+		a   int
+		b   int
+		c   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countGoodTriplets(tt.args.arr, tt.args.a, tt.args.b, tt.args.c); got != tt.want {
+				t.Errorf("countGoodTriplets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
