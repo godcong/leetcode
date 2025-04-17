4943bc629a637e258685493024dbe4b308978ca2
godcong <jumbycc@163.com>
2025-04-17 17:01:09 +0800

    feat(code): add new code(2176) test for:count-equal-and-divisible-pairs-in-an-array

---
+++ code/2100/2176/count-equal-and-divisible-pairs-in-an-array_test.go
@@ -0,0 +1,28 @@
+package _2176
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countPairs(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPairs(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("countPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
