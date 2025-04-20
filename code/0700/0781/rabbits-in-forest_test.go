3950e6113012fbd2767a69709fae1483697b3f31
godcong <jumbycc@163.com>
2025-04-20 17:02:25 +0800

    feat(code): add new code(0781) test for:rabbits-in-forest

---
+++ code/0700/0781/rabbits-in-forest_test.go
@@ -0,0 +1,27 @@
+package _0781
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numRabbits(t *testing.T) {
+	type args struct {
+		answers []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numRabbits(tt.args.answers); got != tt.want {
+				t.Errorf("numRabbits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
