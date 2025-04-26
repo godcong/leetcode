5a8050687e60efa492a3dcc061cc746d406902c6
godcong <jumbycc@163.com>
2025-04-26 17:05:02 +0800

    feat(code): add new code(2444) test for:count-subarrays-with-fixed-bounds

---
+++ code/2400/2444/count-subarrays-with-fixed-bounds_test.go
@@ -0,0 +1,29 @@
+package _2444
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_countSubarrays(t *testing.T) {
+	type args struct {
+		nums []int
+		minK int
+		maxK int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSubarrays(tt.args.nums, tt.args.minK, tt.args.maxK); got != tt.want {
+				t.Errorf("countSubarrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
