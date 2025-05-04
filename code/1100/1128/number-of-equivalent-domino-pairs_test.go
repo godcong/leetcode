981765b1a5dce0216650686ab3cdaba260eec146
godcong <jumbycc@163.com>
2025-05-04 17:08:18 +0800

    feat(code): add new code(1128) test for:number-of-equivalent-domino-pairs

---
+++ code/1100/1128/number-of-equivalent-domino-pairs_test.go
@@ -0,0 +1,27 @@
+package _1128
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numEquivDominoPairs(t *testing.T) {
+	type args struct {
+		dominoes [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numEquivDominoPairs(tt.args.dominoes); got != tt.want {
+				t.Errorf("numEquivDominoPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
