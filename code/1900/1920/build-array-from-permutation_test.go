264ead3f670af57d62bff7f2bf463389c5c53790
godcong <jumbycc@163.com>
2025-05-06 17:09:00 +0800

    feat(code): add new code(1920) test for:build-array-from-permutation

---
+++ code/1900/1920/build-array-from-permutation_test.go
@@ -0,0 +1,28 @@
+package _1920
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_buildArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildArray(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
