ceadffe4dcbfe427ba5b83c9336c4b920c4b0ffa
godcong <jumbycc@163.com>
2025-05-25 17:18:17 +0800

    feat(code): add new code(2131) test for:longest-palindrome-by-concatenating-two-letter-words

---
+++ code/2100/2131/longest-palindrome-by-concatenating-two-letter-words_test.go
@@ -0,0 +1,27 @@
+package _2131
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.words); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
