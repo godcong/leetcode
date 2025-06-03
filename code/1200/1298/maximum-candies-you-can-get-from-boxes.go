455c90ef0c66d68609ecab69e819793f603a19bf
godcong <jumbycc@163.com>
2025-06-03 17:24:06 +0800

    feat(code): add new code(1298) maximum-candies-you-can-get-from-boxes

---
+++ code/1200/1298/maximum-candies-you-can-get-from-boxes.go
@@ -0,0 +1,9 @@
+package _1298
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+func maxCandies(status []int, candies []int, keys [][]int, containedBoxes [][]int, initialBoxes []int) int {
+    
+}
