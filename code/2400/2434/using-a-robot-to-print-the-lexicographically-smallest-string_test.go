7edbf6955321d9d1934bfb0947ccc8611af6ca06
godcong <jumbycc@163.com>
2025-06-06 17:26:39 +0800

    feat(code): add new code(2434) test for:using-a-robot-to-print-the-lexicographically-smallest-string

---
+++ code/2400/2434/using-a-robot-to-print-the-lexicographically-smallest-string_test.go
@@ -0,0 +1,27 @@
+package _2434
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_robotWithString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := robotWithString(tt.args.s); got != tt.want {
+				t.Errorf("robotWithString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
