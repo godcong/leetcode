ee9397c87633d3fdb94a2873db4edd3c440588b7
godcong <jumbycc@163.com>
2025-06-15 19:06:01 +0800

    feat(code): add new code(1432) test for:max-difference-you-can-get-from-changing-an-integer

---
+++ code/1400/1432/max-difference-you-can-get-from-changing-an-integer_test.go
@@ -0,0 +1,27 @@
+package _1432
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxDiff(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDiff(tt.args.num); got != tt.want {
+				t.Errorf("maxDiff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
