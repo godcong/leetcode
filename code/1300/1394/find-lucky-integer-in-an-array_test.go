049064cae3b850ffc1137a39c64b692eff36737d
godcong <jumbycc@163.com>
2025-07-05 19:12:29 +0800

    feat(code): add new code(1394) test for:find-lucky-integer-in-an-array

---
+++ code/1300/1394/find-lucky-integer-in-an-array_test.go
@@ -0,0 +1,27 @@
+package _1394
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findLucky(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLucky(tt.args.arr); got != tt.want {
+				t.Errorf("findLucky() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
