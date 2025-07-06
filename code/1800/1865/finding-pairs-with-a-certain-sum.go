f00bc79ff12c27395d2b2ed0073104f83019ace0
godcong <jumbycc@163.com>
2025-07-06 19:12:54 +0800

    feat(code): add new code(1865) finding-pairs-with-a-certain-sum

---
+++ code/1800/1865/finding-pairs-with-a-certain-sum.go
@@ -0,0 +1,32 @@
+package _1865
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type FindSumPairs struct {
+    
+}
+
+
+func Constructor(nums1 []int, nums2 []int) FindSumPairs {
+    
+}
+
+
+func (this *FindSumPairs) Add(index int, val int)  {
+    
+}
+
+
+func (this *FindSumPairs) Count(tot int) int {
+    
+}
+
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * obj := Constructor(nums1, nums2);
+ * obj.Add(index,val);
+ * param_2 := obj.Count(tot);
+ */
