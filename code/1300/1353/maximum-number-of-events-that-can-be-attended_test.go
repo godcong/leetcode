2084d229299fccaeab792481009b0c0307f14368
godcong <jumbycc@163.com>
2025-07-07 19:13:28 +0800

    feat(code): add new code(1353) test for:maximum-number-of-events-that-can-be-attended

---
+++ code/1300/1353/maximum-number-of-events-that-can-be-attended_test.go
@@ -0,0 +1,27 @@
+package _1353
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxEvents(t *testing.T) {
+	type args struct {
+		events [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxEvents(tt.args.events); got != tt.want {
+				t.Errorf("maxEvents() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
