c2fbf9bb8b635a3b4110db17f6eee21d489352f5
godcong <jumbycc@163.com>
2025-07-14 19:16:47 +0800

    feat(code): add new code(1290) convert-binary-number-in-a-linked-list-to-integer

---
+++ code/1200/1290/convert-binary-number-in-a-linked-list-to-integer.go
@@ -0,0 +1,16 @@
+package _1290
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) int {
+    
+}
