309565f756ca57c61d7ade7496217105573811d2
godcong <jumbycc@163.com>
2025-07-15 19:17:17 +0800

    feat(code): add new code(3136) test for:valid-word

---
+++ code/3100/3136/valid-word_test.go
@@ -0,0 +1,27 @@
+package _3136
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		word string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.word); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
