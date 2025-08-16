ec7bbba5dadf003da72b123cbcbb4accd4cb46aa
godcong <jumbycc@163.com>
2025-08-16 19:36:28 +0800

    feat(code): add new code(1323) test for:maximum-69-number

---
+++ code/1300/1323/maximum-69-number_test.go
@@ -0,0 +1,27 @@
+package _1323
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maximum69Number(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximum69Number(tt.args.num); got != tt.want {
+				t.Errorf("maximum69Number() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
