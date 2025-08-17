0a48c1e835c9bd5fa21fabe8d33b435ce57e63a9
godcong <jumbycc@163.com>
2025-08-17 19:37:27 +0800

    feat(code): add new code(0837) test for:new-21-game

---
+++ code/0800/0837/new-21-game_test.go
@@ -0,0 +1,29 @@
+package _0837
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_new21Game(t *testing.T) {
+	type args struct {
+		n      int
+		k      int
+		maxPts int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := new21Game(tt.args.n, tt.args.k, tt.args.maxPts); got != tt.want {
+				t.Errorf("new21Game() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
