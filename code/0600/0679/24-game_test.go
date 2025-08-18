b5607ef96c070d0e96c9e6588b629ff5dc7a573d
godcong <jumbycc@163.com>
2025-08-18 19:38:15 +0800

    feat(code): add new code(0679) test for:24-game

---
+++ code/0600/0679/24-game_test.go
@@ -0,0 +1,27 @@
+package _0679
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_judgePoint24(t *testing.T) {
+	type args struct {
+		cards []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := judgePoint24(tt.args.cards); got != tt.want {
+				t.Errorf("judgePoint24() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
