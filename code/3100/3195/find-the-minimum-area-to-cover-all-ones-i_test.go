faf40250d1a48814793345d6cb8b78979cf00ec5
godcong <jumbycc@163.com>
2025-08-22 19:41:24 +0800

    feat(code): add new code(3195) test for:find-the-minimum-area-to-cover-all-ones-i

---
+++ code/3100/3195/find-the-minimum-area-to-cover-all-ones-i_test.go
@@ -0,0 +1,27 @@
+package _3195
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minimumArea(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumArea(tt.args.grid); got != tt.want {
+				t.Errorf("minimumArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
