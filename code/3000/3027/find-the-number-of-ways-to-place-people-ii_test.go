b497c0b10e175e4bb05e031215fe0347bc558734
godcong <jumbycc@163.com>
2025-09-03 19:50:39 +0800

    feat(code): add new code(3027) test for:find-the-number-of-ways-to-place-people-ii

---
+++ code/3000/3027/find-the-number-of-ways-to-place-people-ii_test.go
@@ -0,0 +1,27 @@
+package _3027
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numberOfPairs(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfPairs(tt.args.points); got != tt.want {
+				t.Errorf("numberOfPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
