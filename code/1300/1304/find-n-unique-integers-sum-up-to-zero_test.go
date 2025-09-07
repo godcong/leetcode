e0648526048cb6c891ed8963a85351757f8dc9a0
godcong <jumbycc@163.com>
2025-09-07 19:53:52 +0800

    feat(code): add new code(1304) test for:find-n-unique-integers-sum-up-to-zero

---
+++ code/1300/1304/find-n-unique-integers-sum-up-to-zero_test.go
@@ -0,0 +1,28 @@
+package _1304
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_sumZero(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumZero(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sumZero() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
