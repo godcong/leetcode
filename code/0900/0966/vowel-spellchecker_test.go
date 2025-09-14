6b4efb15faeafccf613dd6d2389d1bbc92339aba
godcong <jumbycc@163.com>
2025-09-14 20:00:01 +0800

    feat(code): add new code(0966) test for:vowel-spellchecker

---
+++ code/0900/0966/vowel-spellchecker_test.go
@@ -0,0 +1,29 @@
+package _0966
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_spellchecker(t *testing.T) {
+	type args struct {
+		wordlist []string
+		queries  []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spellchecker(tt.args.wordlist, tt.args.queries); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spellchecker() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
