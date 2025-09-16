f433df2ba7b0b5c3e150c025bdf6986800a5529d
godcong <jumbycc@163.com>
2025-09-16 20:01:47 +0800

    feat(code): add new code(2197) test for:replace-non-coprime-numbers-in-array

---
+++ code/2100/2197/replace-non-coprime-numbers-in-array_test.go
@@ -0,0 +1,28 @@
+package _2197
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_replaceNonCoprimes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := replaceNonCoprimes(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("replaceNonCoprimes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
