14f8a500c93c42f42eaee7f156b25860ab4bd81a
godcong <jumbycc@163.com>
2025-09-17 20:02:36 +0800

    feat(code): add new code(2349) design-a-number-container-system

---
+++ code/2300/2349/design-a-number-container-system.go
@@ -0,0 +1,32 @@
+package _2349
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+type NumberContainers struct {
+    
+}
+
+
+func Constructor() NumberContainers {
+    
+}
+
+
+func (this *NumberContainers) Change(index int, number int)  {
+    
+}
+
+
+func (this *NumberContainers) Find(number int) int {
+    
+}
+
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Change(index,number);
+ * param_2 := obj.Find(number);
+ */
