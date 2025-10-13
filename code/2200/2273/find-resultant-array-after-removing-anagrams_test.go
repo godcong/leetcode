15a61ee96a6c543850333ea1ab7c6721330b3944
godcong <jumbycc@163.com>
2025-10-13 20:20:42 +0800

    feat(code): add new code(2273) test for:find-resultant-array-after-removing-anagrams

---
+++ code/2200/2273/find-resultant-array-after-removing-anagrams_test.go
@@ -0,0 +1,28 @@
+package _2273
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_removeAnagrams(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeAnagrams(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
