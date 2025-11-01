a24b4efbabf8dd8d45e1e4fbd8b10e00b3259027
godcong <jumbycc@163.com>
2025-11-01 20:35:09 +0800

    feat(code): add new code(3217) delete-nodes-from-linked-list-present-in-array

---
+++ code/3200/3217/delete-nodes-from-linked-list-present-in-array.go
@@ -0,0 +1,16 @@
+package _3217
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func modifiedList(nums []int, head *ListNode) *ListNode {
+    
+}
