75cdbff4e7b66dc32f7fa68192efb84b06e1f63b
godcong <jumbycc@163.com>
2025-11-03 20:36:48 +0800

    feat(code): add new code(1578) test for:minimum-time-to-make-rope-colorful

---
+++ code/1500/1578/minimum-time-to-make-rope-colorful_test.go
@@ -0,0 +1,28 @@
+package _1578
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_minCost(t *testing.T) {
+	type args struct {
+		colors     string
+		neededTime []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCost(tt.args.colors, tt.args.neededTime); got != tt.want {
+				t.Errorf("minCost() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
