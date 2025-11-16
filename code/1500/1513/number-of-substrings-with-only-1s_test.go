420a337d68634c4c6324aa4acce0a058b23b6f50
godcong <jumbycc@163.com>
2025-11-16 20:47:15 +0800

    feat(code): add new code(1513) test for:number-of-substrings-with-only-1s

---
+++ code/1500/1513/number-of-substrings-with-only-1s_test.go
@@ -0,0 +1,27 @@
+package _1513
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numSub(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSub(tt.args.s); got != tt.want {
+				t.Errorf("numSub() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
