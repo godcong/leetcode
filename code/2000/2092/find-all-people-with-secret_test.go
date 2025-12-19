435927dad3180a0f350d7e924a10dcf97efaa8ff
godcong <jumbycc@163.com>
2025-12-19 21:10:55 +0800

    feat(code): add new code(2092) test for:find-all-people-with-secret

---
+++ code/2000/2092/find-all-people-with-secret_test.go
@@ -0,0 +1,30 @@
+package _2092
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_findAllPeople(t *testing.T) {
+	type args struct {
+		n           int
+		meetings    [][]int
+		firstPerson int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findAllPeople(tt.args.n, tt.args.meetings, tt.args.firstPerson); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findAllPeople() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
