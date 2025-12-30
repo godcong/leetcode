650bbf70db0de1dd551a8bf146b6c97527a0d78a
godcong <jumbycc@163.com>
2025-12-30 21:18:11 +0800

    feat(code): add new code(0840) test for:magic-squares-in-grid

---
+++ code/0800/0840/magic-squares-in-grid_test.go
@@ -0,0 +1,27 @@
+package _0840
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_numMagicSquaresInside(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numMagicSquaresInside(tt.args.grid); got != tt.want {
+				t.Errorf("numMagicSquaresInside() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
