e2be08e3fe5aa530879fe724609b47f5b8927da6
godcong <jumbycc@163.com>
2026-01-11 21:25:36 +0800

    feat(code): add new code(0085) test for:maximal-rectangle

---
+++ code/0000/0085/maximal-rectangle_test.go
@@ -0,0 +1,27 @@
+package _0085
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maximalRectangle(t *testing.T) {
+	type args struct {
+		matrix [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximalRectangle(tt.args.matrix); got != tt.want {
+				t.Errorf("maximalRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
