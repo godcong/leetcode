efdf9bea26a93b87f3c0525e1db6aa41068e2662
godcong <jumbycc@163.com>
2026-01-19 21:31:03 +0800

    feat(code): add new code(1292) test for:maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold

---
+++ code/1200/1292/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold_test.go
@@ -0,0 +1,28 @@
+package _1292
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_maxSideLength(t *testing.T) {
+	type args struct {
+		mat       [][]int
+		threshold int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSideLength(tt.args.mat, tt.args.threshold); got != tt.want {
+				t.Errorf("maxSideLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
