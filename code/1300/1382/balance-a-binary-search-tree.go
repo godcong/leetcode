ba929bc173ccec528a9315519190b88dc098169b
godcong <jumbycc@163.com>
2026-02-09 21:44:22 +0800

    feat(code): add new code(1382) balance-a-binary-search-tree

---
+++ code/1300/1382/balance-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package _1382
+
+import (
+	. "github.com/godcong/leetcode/common"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func balanceBST(root *TreeNode) *TreeNode {
+    
+}
