16598cf6fd4415ce9d47dd0bc5b00c5a1fcfecef
godcong <jumbycc@163.com>
2026-02-12 21:46:07 +0800

    feat(code): add new code(3713) test for:longest-balanced-substring-i

---
+++ code/3700/3713/longest-balanced-substring-i_test.go
@@ -0,0 +1,27 @@
+package _3713
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_longestBalanced(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestBalanced(tt.args.s); got != tt.want {
+				t.Errorf("longestBalanced() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
