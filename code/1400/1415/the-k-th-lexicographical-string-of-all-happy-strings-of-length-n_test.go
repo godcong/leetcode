f861a274144b1f50062795bbe06c8199fb74f0fa
godcong <jumbycc@163.com>
2026-03-14 22:04:47 +0800

    feat(code): add new code(1415) test for:the-k-th-lexicographical-string-of-all-happy-strings-of-length-n

---
+++ code/1400/1415/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n_test.go
@@ -0,0 +1,28 @@
+package _1415
+
+import (
+	"testing"
+
+	. "github.com/godcong/leetcode/common"
+)
+
+func Test_getHappyString(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getHappyString(tt.args.n, tt.args.k); got != tt.want {
+				t.Errorf("getHappyString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
